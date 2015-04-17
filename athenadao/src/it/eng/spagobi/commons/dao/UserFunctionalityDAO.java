f3f0ffb448399c65ed9a8f8188c5562b568588c4
Marco Cortella <marco.cortella@eng.it>
2015-04-17 16:41:45 +0000

    added check for tenants' product type when retrieving user functionality for a role

--- athenadao/src/it/eng/spagobi/commons/dao/UserFunctionalityDAO.java
+++ athenadao/src/it/eng/spagobi/commons/dao/UserFunctionalityDAO.java
@@ -1,17 +1,20 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.commons.dao;
 
 import it.eng.spago.error.EMFErrorSeverity;
 import it.eng.spago.error.EMFUserError;
 import it.eng.spagobi.commons.metadata.SbiExtRoles;
+import it.eng.spagobi.commons.metadata.SbiOrganizationProductType;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
@@ -19,18 +22,16 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class UserFunctionalityDAO extends AbstractHibernateDAO implements
-		IUserFunctionalityDAO {
+public class UserFunctionalityDAO extends AbstractHibernateDAO implements IUserFunctionalityDAO {
 
 	static private Logger logger = Logger.getLogger(UserFunctionalityDAO.class);
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * it.eng.spagobi.commons.dao.IUserFunctionalityDAO#readUserFunctionality
-	 * (java.lang.String[])
+	 * @see it.eng.spagobi.commons.dao.IUserFunctionalityDAO#readUserFunctionality (java.lang.String[])
 	 */
+	@Override
 	public String[] readUserFunctionality(String[] roles) throws Exception {
 		logger.debug("IN");
 		if (roles == null || roles.length == 0) {
@@ -45,6 +46,7 @@ public class UserFunctionalityDAO extends AbstractHibernateDAO implements
 			tx = aSession.beginTransaction();
 
 			List roleTypes = new ArrayList();
+			List<String> roleTypesTenant = new ArrayList();
 
 			for (int i = 0; i < roles.length; i++) {
 				String hql = "from SbiExtRoles ser where ser.name=?";
@@ -54,16 +56,40 @@ public class UserFunctionalityDAO extends AbstractHibernateDAO implements
 				SbiExtRoles spaobiRole = (SbiExtRoles) query.uniqueResult();
 				if (spaobiRole != null) {
 					String roleTypeCode = spaobiRole.getRoleType().getValueCd();
-					if (!roleTypes.contains(roleTypeCode))
+					String roleTypeTenant = spaobiRole.getCommonInfo().getOrganization();
+					if (!roleTypes.contains(roleTypeCode)) {
 						roleTypes.add(roleTypeCode);
+					}
+					if (!roleTypesTenant.contains(roleTypeTenant)) {
+						roleTypesTenant.add(roleTypeTenant);
+					}
 				} else {
-					logger.warn("The role " + roles[i]
-							+ "doesn't exist in SBI_EXT_ROLES");
+					logger.warn("The role " + roles[i] + "doesn't exist in SBI_EXT_ROLES");
 				}
 			}
 			logger.debug("Role type=" + roleTypes);
 			if (roleTypes.size() == 0)
 				logger.warn("No role types found for the user...!!!!!");
+
+			// ADDED
+			// Get corresponding Product Type Id for roles'tenants
+			Set<Integer> productTypesId = new HashSet<Integer>();
+			for (String tenant : roleTypesTenant) {
+				String hql = "from SbiOrganizationProductType opt where opt.commonInfo.organization=?";
+				Query query = aSession.createQuery(hql);
+				query.setParameter(0, tenant);
+				List productTypes = query.list();
+				Iterator iter = productTypes.iterator();
+				while (iter.hasNext()) {
+					SbiOrganizationProductType sbiOrganizationProductType = (SbiOrganizationProductType) iter.next();
+					productTypesId.add(sbiOrganizationProductType.getSbiProductType().getProductTypeId());
+				}
+			}
+
+			if (productTypesId.isEmpty()) {
+				logger.error("No Product Types found for the user!!!");
+				throw new EMFUserError(EMFErrorSeverity.ERROR, 100);
+			}
 
 			// String hql =
 			// "from SbiRolesUserFunctionality suf where suf.userFunctionality.domainCd = 'USER_FUNCTIONALITY'"
@@ -71,11 +97,11 @@ public class UserFunctionalityDAO extends AbstractHibernateDAO implements
 			// " and suf.roleType.valueCd in ("+strRoles+")";
 			// String hql =
 			// "Select distinct suf.name from SbiUserFunctionality suf where suf.roleType.valueCd in ("+strRoles+") and suf.roleType.domainCd='ROLE_TYPE'";
-			String hql = "Select distinct suf.name from SbiUserFunctionality suf left join suf.roleType rt "
-					+ "where rt.valueCd IN (:ROLE_TYPES) "
-					+ "and rt.domainCd='ROLE_TYPE'";
+			String hql = "Select distinct suf.name from SbiUserFunctionality suf left join suf.roleType rt " + "where rt.valueCd IN (:ROLE_TYPES) "
+					+ " and rt.domainCd='ROLE_TYPE'" + " and suf.productType.productTypeId IN (:PRODUCT_TYPES) ";
 			Query query = aSession.createQuery(hql);
 			query.setParameterList("ROLE_TYPES", roleTypes);
+			query.setParameterList("PRODUCT_TYPES", productTypesId);
 			List userFuncList = query.list();
 			Iterator iter = userFuncList.iterator();
 			while (iter.hasNext()) {
