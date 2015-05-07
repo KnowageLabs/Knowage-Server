abafd1c8fa3a82ec52a456aa1ff24e1930958940
Benedetto Milazzo <benedetto.milazzo@eng.it>
2015-05-07 07:35:11 +0000

    Updated Serie/Categorie model class

--- athenachartengine/WebContent/js/src/ext5/sbi/chart/designer/AxisesContainerModel.js
+++ athenachartengine/WebContent/js/src/ext5/sbi/chart/designer/AxisesContainerModel.js
@@ -1,4 +1,18 @@
 Ext.define('Sbi.chart.designer.AxisesContainerModel',{
     extend: 'Ext.data.Model',
-    fields: ['axisName', 'axisType', 'groupingFunction']
+    fields: [
+		// {name: 'id', type: 'int'}, 
+		'axisName', 'axisType', 
+		'categoryColumn', 'categoryGroupby', 'categoryStacked', 
+		'categoryStackedType', 'categoryOrderColumn', 'categoryOrderType', 
+		
+		'serieGroupingFunction', 'serieType', 'serieOrderType', 
+		'serieColumn', 'serieColor', 'serieShowValue', 'seriePrecision',
+		'seriePrefixChar', 'seriePostfixChar',
+		
+		//Da vedere se scorporare e mettere nel model 'Sbi.chart.designer.AxisesTooltipModel'
+		'serieTooltipTemplateHtml', 'serieTooltipBackgroundColor', 'serieTooltipAlign', 
+		'serieTooltipColor', 'serieTooltipFont', 'serieTooltipFontWeight', 'serieTooltipFontSize'
+		
+	],
 });
