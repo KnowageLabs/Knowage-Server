d229469e2cff211f5d1147f5e45b2fec6277447d
dalaneve <dalaneve@eng.it>
2015-05-07 13:50:04 +0000

    changes in chart palette

--- athenachartengine/WebContent/js/src/ext5/sbi/chart/designer/ChartConfigurationPalette.js
+++ athenachartengine/WebContent/js/src/ext5/sbi/chart/designer/ChartConfigurationPalette.js
@@ -1,66 +1,75 @@
-var paletteStore = Ext.create('Ext.data.ArrayStore', {
-    fields: ['value']
-});
-
-var paletteGrid = Ext.create('Ext.grid.Panel', {
-    store: paletteStore,
-    width: 100,
-    margin:'0 10 0 0',
-    multiSelect: true,
-    columns: [{
-        text     : 'Colore',
-        flex     : 1,
-        sortable : false,
-        dataIndex: 'value',
-        renderer : function(value, meta) {
-        	meta.style = "background-color:#"+value+";";
-            return value;
-        }
-    }]
-    }
-);
-
 Ext.define('Sbi.chart.designer.ChartConfigurationPalette', {
 	extend : 'Ext.panel.Panel',
 	columnWidth: 0.3,
 	title : 'Palette Colori',
 	bodyPadding : 10,
-    items : [{
-        xtype : 'fieldcontainer',
-        layout : 'hbox',
-        items: [
-           	paletteGrid,	
-        {
-            xtype : 'fieldcontainer',
-        	layout : 'vbox',
-            defaults: {
-                arrowCls: '',
-                width: 22
-            },
-            items: [{
-                xtype : 'button',
-                text: '+',
-                menu : Ext.create('Ext.menu.ColorPicker',{
-                    listeners : {
-                        select : function(picker, selColor) {
-                            paletteStore.add({value:selColor});
-                        }
-                    }
-                }),                 
-            },{
-                xtype : 'button',
-                text: '-',
-                handler: function(){
-                    var selectedRows = paletteGrid.getSelectionModel().getSelection();
-                    if (selectedRows.length) {
-                    	paletteStore.remove(selectedRows);
-                    } else {
-                        Ext.Msg.alert('Status', 'Please select at least one record to delete!');
-                    }
-                }
-            }]
-        }]
-    }
-    ],
+	items : [],
+	
+	config : {
+		paletteGrid : Ext.create('Ext.grid.Panel', {
+		    store: Ext.create('Ext.data.ArrayStore', { fields: ['value']} ),
+		    width: 180,
+		    margin:'0 10 0 0',
+		    multiSelect: true,
+		    columns: [{
+		        text     : 'Colore',
+		        flex     : 1,
+		        sortable : false,
+		        dataIndex: 'value',
+		        renderer : function(value, meta) {
+		        	meta.style = "background-color:#"+value+";";
+		            return value;
+		        }
+		    }]
+		    }
+		)
+	},
+	
+	constructor: function(config) {
+        this.callParent(config);
+        var grid = this.paletteGrid;
+        
+		var item = [{
+	        xtype : 'fieldcontainer',
+	        layout : 'hbox',
+	        items: [
+	           	grid,	
+	        {
+	            xtype : 'fieldcontainer',
+	        	layout : 'vbox',
+	            defaults: {
+	                arrowCls: '',
+	                width: 22
+	            },
+	            items: [{
+	                xtype : 'button',
+	                text: '+',
+	                menu : Ext.create('Ext.menu.ColorPicker',{
+	                    listeners : {
+	                        select : function(picker, selColor) {
+	                            grid.store.add({value:selColor});
+	                        }
+	                    }
+	                }),                 
+	            },{
+	                xtype : 'button',
+	                text: '-',
+	                handler: function(){
+	                    var selectedRows = grid.getSelectionModel().getSelection();
+	                    if (selectedRows.length) {
+	                    	grid.store.remove(selectedRows);
+	                    } else {
+	                        Ext.Msg.alert('Messaggio', 'Selezionare almeno un colore da eliminare');
+	                    }
+	                }
+	            }]
+	        }]
+	    }
+	    ];
+		
+		this.add(item);
+	},
+	
+    
     
 });
