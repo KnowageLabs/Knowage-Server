823a70c7fa6720e6e663ffe01632463463b64df0
Daniele Davì <daniele.davi@eng.it>
2015-05-25 11:11:24 +0000

    removed

--- athenachartengine/WebContent/js/src/ext5/sbi/chart/viewer/initializers/GenericInitializer.js
+++
@@ -1,8 +0,0 @@
-Ext.define("Sbi.chart.viewer.initializers.GenericInitializer", {
-	initChartLibrary: function(panelId) {
-		Ext.log({level: 'error'}, 'You must define your library specific initChartLibrary method!');
-	},	
-	renderChart: function(chartConf) {
-		Ext.log({level: 'error'}, 'You must define your library specific initChartLibrary method!');
-	}
-});
