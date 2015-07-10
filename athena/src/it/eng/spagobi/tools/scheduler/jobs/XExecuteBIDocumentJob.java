3b4eb988530bacaf2b7e9eeef80073bb95ecd5a2
Giulio Gavardi <giulio.gavardi@eng.it>
2015-07-10 09:27:22 +0000

    only style changes

--- athena/src/it/eng/spagobi/tools/scheduler/jobs/XExecuteBIDocumentJob.java
+++ athena/src/it/eng/spagobi/tools/scheduler/jobs/XExecuteBIDocumentJob.java
@@ -1,4 +1,4 @@
-/* SpagoBI, the Open Source Business Intelligence suite
+/* SpagoBI,  the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
