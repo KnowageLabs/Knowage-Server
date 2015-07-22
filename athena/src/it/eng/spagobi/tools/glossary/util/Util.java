91bfaa2df24298a726e500e63083a5e1860899a1
Salvatore Lupo <salvatore.lupo@eng.it>
2015-07-22 14:35:35 +0000

    Glossary Util.java - added 'name' and 'description' to json object returned by 'fromDocumentLight'

--- athena/src/it/eng/spagobi/tools/glossary/util/Util.java
+++ athena/src/it/eng/spagobi/tools/glossary/util/Util.java
@@ -14,32 +14,35 @@ public class Util {
 	}
 
 	public static Integer getNumberOrNull(Object str) {
-		if(str instanceof Integer)return (Integer)str;
-		return isNumber(""+str) ? Integer.valueOf(""+str) : null;
+		if (str instanceof Integer)
+			return (Integer) str;
+		return isNumber("" + str) ? Integer.valueOf("" + str) : null;
 	}
 
-	public static JSONObject fromContentsLight(SbiGlContents sbiGlContents,boolean wordChild,boolean ContentsChild) throws JSONException {
+	public static JSONObject fromContentsLight(SbiGlContents sbiGlContents, boolean wordChild, boolean ContentsChild) throws JSONException {
 		JSONObject ret = new JSONObject();
 		ret.put("CONTENT_ID", sbiGlContents.getContentId());
 		ret.put("CONTENT_NM", sbiGlContents.getContentNm());
 		ret.put("HAVE_WORD_CHILD", wordChild);
 		ret.put("HAVE_CONTENTS_CHILD", ContentsChild);
-		
+
 		return ret;
 	}
 
-	public static JSONObject fromDocumentLight(BIObject sbiob)throws JSONException {
+	public static JSONObject fromDocumentLight(BIObject sbiob) throws JSONException {
 		JSONObject ret = new JSONObject();
 		ret.put("DOCUMENT_ID", sbiob.getId());
 		ret.put("DOCUMENT_NM", sbiob.getLabel());
-		
+		ret.put("DOCUMENT_NAME", sbiob.getName());
+		ret.put("DOCUMENT_DESCR", sbiob.getDescription());
 		return ret;
 	}
-	public static JSONObject fromDocumentLight(SbiObjects sbiob)throws JSONException {
+
+	public static JSONObject fromDocumentLight(SbiObjects sbiob) throws JSONException {
 		JSONObject ret = new JSONObject();
 		ret.put("DOCUMENT_ID", sbiob.getBiobjId());
 		ret.put("DOCUMENT_NM", sbiob.getLabel());
-		
+
 		return ret;
 	}
 }
