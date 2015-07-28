9b50bf316be7f4e7d267308917b15a8331bec315
koslazar <koslazar@eng.it>
2015-07-28 14:53:10 +0000

    Added multilevel feature to treemap chart

--- athenachartengine/WebContent/js/src/treemap/treemap.js
+++ athenachartengine/WebContent/js/src/treemap/treemap.js
@@ -1,55 +1,50 @@
 function renderTreemap(chartConf) {
+
+	var points = [];
+
+	var counter=0;
 	
-	var points = [],
-	level0_p,
-	level0_val,
-	level0_i,
-	level1_p,
-	level1_i,
-	level2_p,
-	level2_i,
-	level2_name = [];
+	for (var dataset in chartConf.data[0]){
 
-	level0_i = 0;
-
-	for (var level0 in chartConf.data[0]){
-
-		level0_val = 0;
-		level0_p = {
-				id: "id_" + level0_i,
-				name: level0,
-				color: chartConf.colors[level0_i]
-		};
-
-		level1_i = 0;
-		for (var level1 in chartConf.data[0][level0]) {
-
-			level1_p = {
-					id: level0_p.id + "_" + level1_i,
-					name: level1,
-					parent: level0_p.id
-			};
-			points.push(level1_p);
-			level2_i = 0;
-
-			for (var level2 in chartConf.data[0][level0][level1]) {
-
-				level2_p = {
-						id: level1_p.id + "_" + level2_i,
-						//name: level2_name[level2],
-						name: level1,
-						parent: level1_p.id,
-						value: Math.round(+chartConf.data[0][level0][level1][level2])
-				};
-				level0_val += level2_p.value;
-				points.push(level2_p);
-				level2_i++;
-			}
-			level1_i++;
+		level = {
+				id: "id_" + counter,
+				name: dataset,
+				color: chartConf.colors[counter]
+				
 		}
-		//level0_p.value = Math.round(level0_val / level1_i);
-		points.push(level0_p);
-		level0_i++;
+		counter++;
+		points.push(level);
+		func(chartConf.data[0][dataset],dataset, level);
+
+	}
+
+	function func(resultData, nameds, dataValue){
+		var counter=0;
+		for (var resultRecord in resultData){
+			
+			level = {
+
+					id: dataValue.id + "_" + counter,
+					name: resultRecord,
+					parent: dataValue.id
+			
+			}
+			
+			if (resultData[resultRecord].value){
+				
+				level.value = Math.round(Number(resultData[resultRecord].value));
+				points.push(level);
+			}
+			else{
+				
+				points.push(level);
+				func(resultData[resultRecord], resultRecord, level);
+				
+			}
+
+			counter++;
+		}
+
 
 	}
 
@@ -59,10 +54,10 @@ function renderTreemap(chartConf) {
 			height: chartConf.chart.height,
 			width: chartConf.chart.width,
 			style: {
-	            fontFamily: chartConf.chart.style.fontFamily,
-	            fontSize: chartConf.chart.style.fontSize,
-	            fontWeight: chartConf.chart.style.fontWeight
-	        }
+				fontFamily: chartConf.chart.style.fontFamily,
+				fontSize: chartConf.chart.style.fontSize,
+				fontWeight: chartConf.chart.style.fontWeight
+			}
 		},
 		series: [{
 			type: "treemap",
@@ -85,37 +80,36 @@ function renderTreemap(chartConf) {
 			text: chartConf.subtitle.text,
 			align: chartConf.subtitle.style.textAlign,
 			style: {
-                color: chartConf.subtitle.style.fontColor,
-                fontWeight: chartConf.subtitle.style.fontWeight,
-                fontSize: chartConf.subtitle.style.fontSize,
-                fontFamily: chartConf.subtitle.style.fontFamily
-            }
+				color: chartConf.subtitle.style.fontColor,
+				fontWeight: chartConf.subtitle.style.fontWeight,
+				fontSize: chartConf.subtitle.style.fontSize,
+				fontFamily: chartConf.subtitle.style.fontFamily
+			}
 		},
 		title: {
 			text: chartConf.title.text,
 			align: chartConf.title.style.textAlign,
 			style: {
-                color: chartConf.title.style.fontColor,
-                fontWeight: chartConf.title.style.fontWeight,
-                fontSize: chartConf.title.style.fontSize,
-                fontFamily: chartConf.title.style.fontFamily
-            }
+				color: chartConf.title.style.fontColor,
+				fontWeight: chartConf.title.style.fontWeight,
+				fontSize: chartConf.title.style.fontSize,
+				fontFamily: chartConf.title.style.fontFamily
+			}
 		},
 		noData: {
 			text: chartConf.emptymessage.text,
 			align: chartConf.emptymessage.style.textAlign,
 			style: {
-                color: chartConf.emptymessage.style.fontColor,
-                fontWeight: chartConf.emptymessage.style.fontWeight,
-                fontSize: chartConf.emptymessage.style.fontSize,
-                fontFamily: chartConf.emptymessage.style.fontFamily
-            }
+				color: chartConf.emptymessage.style.fontColor,
+				fontWeight: chartConf.emptymessage.style.fontWeight,
+				fontSize: chartConf.emptymessage.style.fontSize,
+				fontFamily: chartConf.emptymessage.style.fontFamily
+			}
 		}
-	
+
 	});
 
 }
-
 
 function renderHeatmap(chartConf){
 	
@@ -154,8 +148,6 @@ function renderHeatmap(chartConf){
     	var stop=[i*(1/(colors.length-1)),colors[i]];
     	colorStops.push(stop);
     }
-     
-    console.log();
     
     var chart = new Highcharts.Chart({
        
