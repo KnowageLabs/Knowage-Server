7ad2af40d1c026cb0a4a84aa0fd69622d9c727f5
Salvatore Lupo <salvatore.lupo@eng.it>
2015-08-03 10:38:07 +0000

    Cockpit - DocumentWidget: every chart included in a Cockpit will be rendered by a new instance of ChartEngineInstance

--- athenachartengine/src/it/eng/spagobi/engine/chart/api/JsonChartTemplateService.java
+++ athenachartengine/src/it/eng/spagobi/engine/chart/api/JsonChartTemplateService.java
@@ -2,6 +2,8 @@ package it.eng.spagobi.engine.chart.api;
 
 import static it.eng.spagobi.engine.util.ChartEngineUtil.ve;
 import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.engine.chart.ChartEngine;
+import it.eng.spagobi.engine.chart.ChartEngineInstance;
 import it.eng.spagobi.engine.util.ChartEngineDataUtil;
 import it.eng.spagobi.engine.util.ChartEngineUtil;
 import it.eng.spagobi.tools.dataset.bo.IDataSet;
@@ -23,6 +25,9 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Path("/1.0/jsonChartTemplate")
 public class JsonChartTemplateService extends AbstractChartEngineResource {
@@ -32,12 +37,17 @@ public class JsonChartTemplateService extends AbstractChartEngineResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@SuppressWarnings("rawtypes")
-	public String getJSONChartTemplate(@FormParam("jsonTemplate") String jsonTemplate, @Context HttpServletResponse servletResponse) {
+	public String getJSONChartTemplate(@FormParam("jsonTemplate") String jsonTemplate, @FormParam("driverParams") String driverParams,
+			@Context HttpServletResponse servletResponse) {
 		try {
-			IDataSet dataSet = getEngineInstance().getDataSet();
-			Map analyticalDrivers = getEngineInstance().getAnalyticalDrivers();
+			ChartEngineInstance engineInstance = getEngineInstance();
+			if (driverParams != null && !driverParams.isEmpty()) {
+				refreshDriverParams(getEngineInstance().getEnv(), driverParams);
+				engineInstance = ChartEngine.createInstance(jsonTemplate, getEngineInstance().getEnv());
+			}
+			IDataSet dataSet = engineInstance.getDataSet();
+			Map analyticalDrivers = engineInstance.getAnalyticalDrivers();
 			Map profileAttributes = UserProfileUtils.getProfileAttributes((UserProfile) this.getEnv().get(EngineConstants.ENV_USER_PROFILE));
-
 			String jsonData = ChartEngineDataUtil.loadJsonData(jsonTemplate, dataSet, analyticalDrivers, profileAttributes, getLocale());
 
 			VelocityContext velocityContext = ChartEngineUtil.loadVelocityContext(jsonTemplate, jsonData);
@@ -48,6 +58,21 @@ public class JsonChartTemplateService extends AbstractChartEngineResource {
 		} catch (Throwable t) {
 			throw new SpagoBIServiceException(this.request.getPathInfo(),
 					"An unexpected error occured while executing service: JsonChartTemplateService.getJSONChartTemplate", t);
+		}
+	}
+
+	private void refreshDriverParams(Map analyticalDrivers, String driverParams) throws JSONException {
+		if (driverParams != null && !driverParams.trim().isEmpty() && analyticalDrivers != null && !analyticalDrivers.isEmpty()) {
+			JSONObject params = new JSONObject(driverParams);
+			for (Object key : analyticalDrivers.keySet()) {
+				Object newValue = params.opt((String) key);
+				if (newValue instanceof JSONArray) {
+					newValue = ((JSONArray) newValue).get(0);
+				}
+				if (newValue != null) {
+					analyticalDrivers.put(key, newValue);
+				}
+			}
 		}
 	}
 
