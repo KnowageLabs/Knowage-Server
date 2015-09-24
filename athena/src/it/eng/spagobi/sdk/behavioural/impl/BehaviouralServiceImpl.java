255e09536f23578d6413dca0d210a07bf9925c14
Marco Cortella <marco.cortella@eng.it>
2015-09-24 13:16:53 +0000

    added implementations for new Role methods

--- athena/src/it/eng/spagobi/sdk/behavioural/impl/BehaviouralServiceImpl.java
+++ athena/src/it/eng/spagobi/sdk/behavioural/impl/BehaviouralServiceImpl.java
@@ -1,13 +1,15 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.sdk.behavioural.impl;
 
 import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.commons.bo.Role;
 import it.eng.spagobi.commons.dao.DAOFactory;
 import it.eng.spagobi.profiling.bean.SbiAttribute;
+import it.eng.spagobi.profiling.bean.SbiUser;
 import it.eng.spagobi.sdk.AbstractSDKService;
 import it.eng.spagobi.sdk.behavioural.BehaviouralService;
 import it.eng.spagobi.sdk.behavioural.bo.SDKAttribute;
@@ -63,12 +65,85 @@ public class BehaviouralServiceImpl extends AbstractSDKService implements Behavi
 	}
 
 	public SDKRole[] getRoles() throws NotAllowedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.debug("IN");
+		SDKRole[] toReturn = null;
+
+		this.setTenant();
+
+		try {
+			IEngUserProfile profile = getUserProfile();
+
+			List roles = DAOFactory.getRoleDAO().loadAllRoles();
+
+			List sdkRoles = new ArrayList();
+
+			if (roles != null) {
+				Iterator it = roles.iterator();
+				while (it.hasNext()) {
+					Role role = (Role) it.next();
+					SDKRole sdkRole = new SDKRole();
+					sdkRole.setCode(role.getCode());
+					sdkRole.setDescr(role.getDescription());
+					sdkRole.setExtRoleId(role.getId());
+					sdkRole.setName(role.getName());
+					sdkRole.setOrganization(role.getOrganization());
+					sdkRole.setRoleTypeCd(role.getRoleTypeCD());
+					sdkRole.setRoleTypeId(role.getRoleTypeID());
+					sdkRoles.add(sdkRole);
+				}
+			}
+			toReturn = new SDKRole[sdkRoles.size()];
+			toReturn = (SDKRole[]) sdkRoles.toArray(toReturn);
+		} catch (NotAllowedOperationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e);
+		} finally {
+			this.unsetTenant();
+			logger.debug("OUT");
+		}
+		return toReturn;
 	}
 
 	public SDKRole[] getRolesByUserId(String userId) throws NotAllowedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.debug("IN");
+		SDKRole[] toReturn = null;
+
+		this.setTenant();
+
+		try {
+			IEngUserProfile profile = getUserProfile();
+
+			SbiUser user = DAOFactory.getSbiUserDAO().loadSbiUserByUserId(userId);
+			List roles = DAOFactory.getSbiUserDAO().loadSbiUserRolesById(user.getId());
+
+			List sdkRoles = new ArrayList();
+
+			if (roles != null) {
+				Iterator it = roles.iterator();
+				while (it.hasNext()) {
+					Role role = (Role) it.next();
+					SDKRole sdkRole = new SDKRole();
+					sdkRole.setCode(role.getCode());
+					sdkRole.setDescr(role.getDescription());
+					sdkRole.setExtRoleId(role.getId());
+					sdkRole.setName(role.getName());
+					sdkRole.setOrganization(role.getOrganization());
+					sdkRole.setRoleTypeCd(role.getRoleTypeCD());
+					sdkRole.setRoleTypeId(role.getRoleTypeID());
+					sdkRoles.add(sdkRole);
+				}
+			}
+			toReturn = new SDKRole[sdkRoles.size()];
+			toReturn = (SDKRole[]) sdkRoles.toArray(toReturn);
+		} catch (NotAllowedOperationException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e);
+		} finally {
+			this.unsetTenant();
+			logger.debug("OUT");
+		}
+		return toReturn;
 	}
 }
