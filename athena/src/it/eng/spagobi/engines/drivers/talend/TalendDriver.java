9c2e4c7e34b79cdbfa21715ae78027212ff8127e
Giulio Gavardi <giulio.gavardi@eng.it>
2015-10-05 14:36:22 +0000

    only style changes

--- athena/src/it/eng/spagobi/engines/drivers/talend/TalendDriver.java
+++ athena/src/it/eng/spagobi/engines/drivers/talend/TalendDriver.java
@@ -6,19 +6,10 @@
 
 package it.eng.spagobi.engines.drivers.talend;
 
-import it.eng.spago.base.SourceBean;
-import it.eng.spago.configuration.ConfigSingleton;
-import it.eng.spago.error.EMFInternalError;
-import it.eng.spago.error.EMFUserError;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
-import it.eng.spagobi.analiticalmodel.document.bo.ObjTemplate;
-import it.eng.spagobi.analiticalmodel.document.dao.IObjTemplateDAO;
 import it.eng.spagobi.behaviouralmodel.analyticaldriver.bo.BIObjectParameter;
-import it.eng.spagobi.commons.dao.DAOFactory;
 import it.eng.spagobi.commons.utilities.ParameterValuesEncoder;
-import it.eng.spagobi.commons.utilities.PortletUtilities;
-import it.eng.spagobi.commons.utilities.messages.MessageBuilder;
 import it.eng.spagobi.engines.drivers.AbstractDriver;
 import it.eng.spagobi.engines.drivers.EngineURL;
 import it.eng.spagobi.engines.drivers.IEngineDriver;
@@ -26,43 +17,40 @@ import it.eng.spagobi.engines.drivers.exceptions.InvalidOperationRequest;
 
 import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.Locale;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-
-
 /**
- * Driver Implementation (IEngineDriver Interface) for Talend External Engine. 
+ * Driver Implementation (IEngineDriver Interface) for Talend External Engine.
  */
 public class TalendDriver extends AbstractDriver implements IEngineDriver {
 
 	static private Logger logger = Logger.getLogger(TalendDriver.class);
 
-
-
 	/**
-	 * Returns a map of parameters which will be send in the request to the
-	 * engine application.
+	 * Returns a map of parameters which will be send in the request to the engine application.
 	 * 
-	 * @param profile Profile of the user
-	 * @param roleName the name of the execution role
-	 * @param biobject the biobject
+	 * @param profile
+	 *            Profile of the user
+	 * @param roleName
+	 *            the name of the execution role
+	 * @param biobject
+	 *            the biobject
 	 * 
 	 * @return Map The map of the execution call parameters
 	 */
 	public Map getParameterMap(Object biobject, IEngUserProfile profile, String roleName) {
 		logger.debug("IN");
 		Map map = new Hashtable();
-		try{
-			BIObject biobj = (BIObject)biobject;
+		try {
+			BIObject biobj = (BIObject) biobject;
 			map = getMap(biobj);
-			//map.put("user", profile.getUserUniqueIdentifier());
+			// map.put("user", profile.getUserUniqueIdentifier());
 		} catch (ClassCastException cce) {
 			logger.error("The parameter is not a BIObject type", cce);
-		} 
-		map = applySecurity(map,profile);
+		}
+		map = applySecurity(map, profile);
 		logger.debug("OUT");
 		return map;
 	}
@@ -70,10 +58,14 @@ public class TalendDriver extends AbstractDriver implements IEngineDriver {
 	/**
 	 * SpagoBITalendEngine does not manage subobejcts, so this method is equivalent to <code>getParameterMap(object, profile, roleName)</code>.
 	 * 
-	 * @param subObject SubObject to execute
-	 * @param profile Profile of the user
-	 * @param roleName the name of the execution role
-	 * @param object the object
+	 * @param subObject
+	 *            SubObject to execute
+	 * @param profile
+	 *            Profile of the user
+	 * @param roleName
+	 *            the name of the execution role
+	 * @param object
+	 *            the object
 	 * 
 	 * @return Map The map of the execution call parameters
 	 */
@@ -81,51 +73,52 @@ public class TalendDriver extends AbstractDriver implements IEngineDriver {
 		return getParameterMap(object, profile, roleName);
 	}
 
-
-
-
 	/**
-	 * Starting from a BIObject extracts from it the map of the paramaeters for the
-	 * execution call
-	 * @param biobj BIObject to execute
+	 * Starting from a BIObject extracts from it the map of the paramaeters for the execution call
+	 * 
+	 * @param biobj
+	 *            BIObject to execute
 	 * @return Map The map of the execution call parameters
-	 */    
+	 */
 	private Map getMap(BIObject biobj) {
 		Map pars = new Hashtable();
 
-		String documentId=biobj.getId().toString();
+		String documentId = biobj.getId().toString();
 		pars.put("document", documentId);
-		logger.debug("Add document parameter:"+documentId);
+		logger.debug("Add document parameter:" + documentId);
 		pars = addBIParameters(biobj, pars);
 		pars = addBIParameterDescriptions(biobj, pars);
 		logger.debug("OUT");
 		return pars;
-	} 
+	}
 
 	/**
 	 * Add into the parameters map the BIObject's BIParameter names and values
-	 * @param biobj BIOBject to execute
-	 * @param pars Map of the parameters for the execution call  
+	 * 
+	 * @param biobj
+	 *            BIOBject to execute
+	 * @param pars
+	 *            Map of the parameters for the execution call
 	 * @return Map The map of the execution call parameters
 	 */
 	private Map addBIParameters(BIObject biobj, Map pars) {
 		logger.debug("IN");
 		if (biobj == null) {
-			logger.warn("BIObject parameter null");	    
+			logger.warn("BIObject parameter null");
 			return pars;
 		}
 
 		ParameterValuesEncoder parValuesEncoder = new ParameterValuesEncoder();
-		if(biobj.getBiObjectParameters() != null){
+		if (biobj.getBiObjectParameters() != null) {
 			BIObjectParameter biobjPar = null;
-			for(Iterator it = biobj.getBiObjectParameters().iterator(); it.hasNext();){
+			for (Iterator it = biobj.getBiObjectParameters().iterator(); it.hasNext();) {
 				try {
-					biobjPar = (BIObjectParameter)it.next();
+					biobjPar = (BIObjectParameter) it.next();
 					String value = parValuesEncoder.encode(biobjPar);
 					pars.put(biobjPar.getParameterUrlName(), value);
-					logger.debug("Add parameter:"+biobjPar.getParameterUrlName()+"/"+value);
+					logger.debug("Add parameter:" + biobjPar.getParameterUrlName() + "/" + value);
 				} catch (Exception e) {
-					logger.error("Error while processing a BIParameter",e);
+					logger.error("Error while processing a BIParameter", e);
 				}
 			}
 		}
@@ -133,20 +126,20 @@ public class TalendDriver extends AbstractDriver implements IEngineDriver {
 		return pars;
 	}
 
-
-
 	/**
 	 * Function not implemented. Thid method should not be called
 	 * 
-	 * @param biobject The BIOBject to edit
-	 * @param profile the profile
+	 * @param biobject
+	 *            The BIOBject to edit
+	 * @param profile
+	 *            the profile
 	 * 
 	 * @return the edits the document template build url
 	 * 
-	 * @throws InvalidOperationRequest the invalid operation request
+	 * @throws InvalidOperationRequest
+	 *             the invalid operation request
 	 */
-	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject,
-			IEngUserProfile profile) throws InvalidOperationRequest {
+	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
 		logger.warn("Function not implemented");
 		throw new InvalidOperationRequest();
 	}
@@ -154,18 +147,19 @@ public class TalendDriver extends AbstractDriver implements IEngineDriver {
 	/**
 	 * Function not implemented. Thid method should not be called
 	 * 
-	 * @param biobject  The BIOBject to edit
-	 * @param profile the profile
+	 * @param biobject
+	 *            The BIOBject to edit
+	 * @param profile
+	 *            the profile
 	 * 
 	 * @return the new document template build url
 	 * 
-	 * @throws InvalidOperationRequest the invalid operation request
+	 * @throws InvalidOperationRequest
+	 *             the invalid operation request
 	 */
-	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
-	throws InvalidOperationRequest {
+	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
 		logger.warn("Function not implemented");
 		throw new InvalidOperationRequest();
 	}
 
 }
-
