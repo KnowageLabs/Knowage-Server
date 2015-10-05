92323215df20a75f769562211ae464353c697b7d
Giulio Gavardi <giulio.gavardi@eng.it>
2015-10-05 14:34:11 +0000

    only style changes

--- athena/src/it/eng/spagobi/engines/drivers/qbe/QbeDriver.java
+++ athena/src/it/eng/spagobi/engines/drivers/qbe/QbeDriver.java
@@ -2,7 +2,7 @@
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * If a copy of the MPL  was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engines.drivers.qbe;
 
 import it.eng.spago.error.EMFUserError;
@@ -30,23 +30,22 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-
-
 /**
- * Driver Implementation (IEngineDriver Interface) for Qbe External Engine. 
+ * Driver Implementation (IEngineDriver Interface) for Qbe External Engine.
  */
 public class QbeDriver extends AbstractDriver implements IEngineDriver {
 
 	static private Logger logger = Logger.getLogger(QbeDriver.class);
 
-
 	/**
-	 * Returns a map of parameters which will be send in the request to the
-	 * engine application.
+	 * Returns a map of parameters which will be send in the request to the engine application.
 	 * 
-	 * @param profile Profile of the user
-	 * @param roleName the name of the execution role
-	 * @param analyticalDocument the biobject
+	 * @param profile
+	 *            Profile of the user
+	 * @param roleName
+	 *            the name of the execution role
+	 * @param analyticalDocument
+	 *            the biobject
 	 * 
 	 * @return Map The map of the execution call parameters
 	 */
@@ -58,14 +57,15 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 
 		try {
 			Assert.assertNotNull(analyticalDocument, "Input parameter [analyticalDocument] cannot be null");
-			Assert.assertTrue((analyticalDocument instanceof BIObject), "Input parameter [analyticalDocument] cannot be an instance of [" + analyticalDocument.getClass().getName()+ "]");
+			Assert.assertTrue((analyticalDocument instanceof BIObject), "Input parameter [analyticalDocument] cannot be an instance of ["
+					+ analyticalDocument.getClass().getName() + "]");
 
-			biObject = (BIObject)analyticalDocument;
+			biObject = (BIObject) analyticalDocument;
 
 			parameters = new Hashtable();
 			parameters = getRequestParameters(biObject);
 			parameters = applySecurity(parameters, profile);
-			//parameters = addDocumentParametersInfo(parameters, biObject);
+			// parameters = addDocumentParametersInfo(parameters, biObject);
 			parameters = applyService(parameters, biObject);
 			parameters = applyDatasourceForWriting(parameters, biObject);
 		} finally {
@@ -76,13 +76,16 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 	}
 
 	/**
-	 * Returns a map of parameters which will be send in the request to the
-	 * engine application.
+	 * Returns a map of parameters which will be send in the request to the engine application.
 	 * 
-	 * @param analyticalDocumentSubObject SubObject to execute
-	 * @param profile Profile of the user
-	 * @param roleName the name of the execution role
-	 * @param analyticalDocument the object
+	 * @param analyticalDocumentSubObject
+	 *            SubObject to execute
+	 * @param profile
+	 *            Profile of the user
+	 * @param roleName
+	 *            the name of the execution role
+	 * @param analyticalDocument
+	 *            the object
 	 * 
 	 * @return Map The map of the execution call parameters
 	 */
@@ -94,27 +97,29 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 
 		logger.debug("IN");
 
-		try{
+		try {
 			Assert.assertNotNull(analyticalDocument, "Input parameter [analyticalDocument] cannot be null");
-			Assert.assertTrue((analyticalDocument instanceof BIObject), "Input parameter [analyticalDocument] cannot be an instance of [" + analyticalDocument.getClass().getName()+ "]");
-			biObject = (BIObject)analyticalDocument;
+			Assert.assertTrue((analyticalDocument instanceof BIObject), "Input parameter [analyticalDocument] cannot be an instance of ["
+					+ analyticalDocument.getClass().getName() + "]");
+			biObject = (BIObject) analyticalDocument;
 
-			if(analyticalDocumentSubObject == null) {
+			if (analyticalDocumentSubObject == null) {
 				logger.warn("Input parameter [subObject] is null");
 				return getParameterMap(analyticalDocument, profile, roleName);
-			}				
-			Assert.assertTrue((analyticalDocumentSubObject instanceof SubObject), "Input parameter [subObjectDetail] cannot be an instance of [" + analyticalDocumentSubObject.getClass().getName()+ "]");
+			}
+			Assert.assertTrue((analyticalDocumentSubObject instanceof SubObject), "Input parameter [subObjectDetail] cannot be an instance of ["
+					+ analyticalDocumentSubObject.getClass().getName() + "]");
 			subObject = (SubObject) analyticalDocumentSubObject;
 
 			parameters = getRequestParameters(biObject);
 
-			parameters.put("nameSubObject",  subObject.getName() != null? subObject.getName(): "" );
-			parameters.put("descriptionSubObject", subObject.getDescription() != null? subObject.getDescription(): "");
-			parameters.put("visibilitySubObject", subObject.getIsPublic().booleanValue()?"Public":"Private" );
+			parameters.put("nameSubObject", subObject.getName() != null ? subObject.getName() : "");
+			parameters.put("descriptionSubObject", subObject.getDescription() != null ? subObject.getDescription() : "");
+			parameters.put("visibilitySubObject", subObject.getIsPublic().booleanValue() ? "Public" : "Private");
 			parameters.put("subobjectId", subObject.getId());
 
 			parameters = applySecurity(parameters, profile);
-			//parameters = addDocumentParametersInfo(parameters, biObject);
+			// parameters = addDocumentParametersInfo(parameters, biObject);
 			parameters = applyService(parameters, biObject);
 			parameters = applyDatasourceForWriting(parameters, biObject);
 			parameters.put("isFromCross", "false");
@@ -126,38 +131,39 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 
 	}
 
-	//    private Map addDocumentParametersInfo(Map map, BIObject biobject) {
-	//    	logger.debug("IN");
-	//    	JSONArray parametersJSON = new JSONArray();
-	//    	try {
-	//	    	Locale locale = getLocale();
-	//			List parameters = biobject.getBiObjectParameters();
-	//			if (parameters != null && parameters.size() > 0) {
-	//				Iterator iter = parameters.iterator();
-	//				while (iter.hasNext()) {
-	//					BIObjectParameter biparam = (BIObjectParameter) iter.next();
-	//					JSONObject jsonParam = new JSONObject();
-	//					jsonParam.put("id", biparam.getParameterUrlName());
-	//					IMessageBuilder msgBuilder = MessageBuilderFactory.getMessageBuilder();
-	//					jsonParam.put("label", msgBuilder.getUserMessage(biparam.getLabel(), SpagoBIConstants.DEFAULT_USER_BUNDLE, locale));
-	//					jsonParam.put("type", biparam.getParameter().getType());
-	//					parametersJSON.put(jsonParam);
-	//				}
-	//			}
-	//    	} catch (Exception e) {
-	//    		logger.error("Error while adding document parameters info", e);
-	//    	}
-	//    	map.put("SBI_DOCUMENT_PARAMETERS", parametersJSON.toString());
-	//    	logger.debug("OUT");
-	//		return map;
-	//	}
+	// private Map addDocumentParametersInfo(Map map, BIObject biobject) {
+	// logger.debug("IN");
+	// JSONArray parametersJSON = new JSONArray();
+	// try {
+	// Locale locale = getLocale();
+	// List parameters = biobject.getBiObjectParameters();
+	// if (parameters != null && parameters.size() > 0) {
+	// Iterator iter = parameters.iterator();
+	// while (iter.hasNext()) {
+	// BIObjectParameter biparam = (BIObjectParameter) iter.next();
+	// JSONObject jsonParam = new JSONObject();
+	// jsonParam.put("id", biparam.getParameterUrlName());
+	// IMessageBuilder msgBuilder = MessageBuilderFactory.getMessageBuilder();
+	// jsonParam.put("label", msgBuilder.getUserMessage(biparam.getLabel(), SpagoBIConstants.DEFAULT_USER_BUNDLE, locale));
+	// jsonParam.put("type", biparam.getParameter().getType());
+	// parametersJSON.put(jsonParam);
+	// }
+	// }
+	// } catch (Exception e) {
+	// logger.error("Error while adding document parameters info", e);
+	// }
+	// map.put("SBI_DOCUMENT_PARAMETERS", parametersJSON.toString());
+	// logger.debug("OUT");
+	// return map;
+	// }
 
 	/**
-	 * Starting from a BIObject extracts from it the map of the paramaeters for the
-	 * execution call
-	 * @param biObject BIObject to execute
+	 * Starting from a BIObject extracts from it the map of the paramaeters for the execution call
+	 * 
+	 * @param biObject
+	 *            BIObject to execute
 	 * @return Map The map of the execution call parameters
-	 */    
+	 */
 	private Map getRequestParameters(BIObject biObject) {
 		logger.debug("IN");
 
@@ -170,7 +176,7 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 
 		parameters = null;
 
-		try {		
+		try {
 			parameters = new Hashtable();
 			template = this.getTemplate(biObject);
 
@@ -178,10 +184,10 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 				contentDAO = DAOFactory.getBinContentDAO();
 				Assert.assertNotNull(contentDAO, "Impossible to instantiate contentDAO");
 
-				content = contentDAO.getBinContent(template.getBinId());		    
+				content = contentDAO.getBinContent(template.getBinId());
 				Assert.assertNotNull(content, "Template content cannot be null");
-			} catch (Throwable t){
-				throw new RuntimeException("Impossible to load template content for document [" + biObject.getLabel()+ "]", t);
+			} catch (Throwable t) {
+				throw new RuntimeException("Impossible to load template content for document [" + biObject.getLabel() + "]", t);
 			}
 
 			appendRequestParameter(parameters, "document", biObject.getId().toString());
@@ -192,36 +198,37 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 		}
 
 		return parameters;
-	} 
-
-
+	}
 
 	/**
 	 * Add into the parameters map the BIObject's BIParameter names and values
-	 * @param biobj BIOBject to execute
-	 * @param pars Map of the parameters for the execution call  
+	 * 
+	 * @param biobj
+	 *            BIOBject to execute
+	 * @param pars
+	 *            Map of the parameters for the execution call
 	 * @return Map The map of the execution call parameters
 	 */
 	private Map appendAnalyticalDriversToRequestParameters(BIObject biobj, Map pars) {
 		logger.debug("IN");
 
-		if(biobj==null) {
-			logger.warn("BIObject parameter null");	    
+		if (biobj == null) {
+			logger.warn("BIObject parameter null");
 			return pars;
 		}
 
 		ParameterValuesEncoder parValuesEncoder = new ParameterValuesEncoder();
-		if(biobj.getBiObjectParameters() != null){
+		if (biobj.getBiObjectParameters() != null) {
 			BIObjectParameter biobjPar = null;
-			for(Iterator it = biobj.getBiObjectParameters().iterator(); it.hasNext();){
+			for (Iterator it = biobj.getBiObjectParameters().iterator(); it.hasNext();) {
 				try {
-					biobjPar = (BIObjectParameter)it.next();									
+					biobjPar = (BIObjectParameter) it.next();
 					String value = parValuesEncoder.encode(biobjPar);
-					if(value != null)
+					if (value != null)
 						pars.put(biobjPar.getParameterUrlName(), value);
-					logger.debug("Add parameter:"+biobjPar.getParameterUrlName()+"/"+value);
+					logger.debug("Add parameter:" + biobjPar.getParameterUrlName() + "/" + value);
 				} catch (Exception e) {
-					logger.error("Error while processing a BIParameter",e);
+					logger.error("Error while processing a BIParameter", e);
 				}
 			}
 		}
@@ -233,15 +240,17 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 	/**
 	 * Function not implemented. Thid method should not be called
 	 * 
-	 * @param biobject The BIOBject to edit
-	 * @param profile the profile
+	 * @param biobject
+	 *            The BIOBject to edit
+	 * @param profile
+	 *            the profile
 	 * 
 	 * @return the edits the document template build url
 	 * 
-	 * @throws InvalidOperationRequest the invalid operation request
+	 * @throws InvalidOperationRequest
+	 *             the invalid operation request
 	 */
-	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
-			throws InvalidOperationRequest {
+	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
 		logger.warn("Function not implemented");
 		throw new InvalidOperationRequest();
 	}
@@ -249,20 +258,20 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 	/**
 	 * Function not implemented. Thid method should not be called
 	 * 
-	 * @param biobject  The BIOBject to edit
-	 * @param profile the profile
+	 * @param biobject
+	 *            The BIOBject to edit
+	 * @param profile
+	 *            the profile
 	 * 
 	 * @return the new document template build url
 	 * 
-	 * @throws InvalidOperationRequest the invalid operation request
+	 * @throws InvalidOperationRequest
+	 *             the invalid operation request
 	 */
-	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
-			throws InvalidOperationRequest {
+	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
 		logger.warn("Function not implemented");
 		throw new InvalidOperationRequest();
 	}
-
-
 
 	public final static String PARAM_SERVICE_NAME = "ACTION_NAME";
 	public final static String PARAM_NEW_SESSION = "NEW_SESSION";
@@ -276,16 +285,16 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 			Assert.assertNotNull(parameters, "Input [parameters] cannot be null");
 
 			template = getTemplate(biObject);
-			if(template.getName().trim().toLowerCase().endsWith(".xml")) {
+			if (template.getName().trim().toLowerCase().endsWith(".xml")) {
 				parameters.put(PARAM_SERVICE_NAME, "QBE_ENGINE_START_ACTION");
-			} else if(template.getName().trim().toLowerCase().endsWith(".json")) {
+			} else if (template.getName().trim().toLowerCase().endsWith(".json")) {
 				parameters.put(PARAM_SERVICE_NAME, "FORM_ENGINE_START_ACTION");
 			} else {
 				Assert.assertUnreachable("Active template [" + template.getName() + "] extension is not valid (valid extensions are: .xml ; .json)");
 			}
 
 			parameters.put(PARAM_NEW_SESSION, "TRUE");
-		} catch(Throwable t) {
+		} catch (Throwable t) {
 			throw new RuntimeException("Impossible to guess from template extension the engine startup service to call");
 		} finally {
 			logger.debug("OUT");
@@ -306,12 +315,12 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 			templateDAO = DAOFactory.getObjTemplateDAO();
 			Assert.assertNotNull(templateDAO, "Impossible to instantiate templateDAO");
 
-			template = templateDAO.getBIObjectActiveTemplate( biObject.getId() );
-			Assert.assertNotNull(template, "Loaded template cannot be null");	
+			template = templateDAO.getBIObjectActiveTemplate(biObject.getId());
+			Assert.assertNotNull(template, "Loaded template cannot be null");
 
 			logger.debug("Active template [" + template.getName() + "] of document [" + biObject.getLabel() + "] loaded succesfully");
-		} catch(Throwable t) {
-			throw new RuntimeException("Impossible to load template for document [" + biObject.getLabel()+ "]", t);
+		} catch (Throwable t) {
+			throw new RuntimeException("Impossible to load template for document [" + biObject.getLabel() + "]", t);
 		} finally {
 			logger.debug("OUT");
 		}
@@ -319,19 +328,17 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 		return template;
 	}
 
-
 	private void appendRequestParameter(Map parameters, String pname, String pvalue) {
 		parameters.put(pname, pvalue);
 		logger.debug("Added parameter [" + pname + "] with value [" + pvalue + "] to request parameters list");
 	}
-	
+
 	private Map applyDatasourceForWriting(Map parameters, BIObject biObject) {
 		IDataSource datasource;
 		try {
 			datasource = DAOFactory.getDataSourceDAO().loadDataSourceWriteDefault();
 		} catch (EMFUserError e) {
-			throw new SpagoBIRuntimeException(
-					"Error while loading default datasource for writing", e);
+			throw new SpagoBIRuntimeException("Error while loading default datasource for writing", e);
 		}
 		if (datasource != null) {
 			parameters.put(EngineConstants.DEFAULT_DATASOURCE_FOR_WRITING_LABEL, datasource.getLabel());
@@ -341,4 +348,3 @@ public class QbeDriver extends AbstractDriver implements IEngineDriver {
 		return parameters;
 	}
 }
-
