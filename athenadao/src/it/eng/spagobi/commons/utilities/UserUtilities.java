8acb52f6d2c0f1a6786990f4849b76aad13ff57e
Giulio Gavardi <giulio.gavardi@eng.it>
2015-10-05 16:04:53 +0000

    only style changes

--- athenadao/src/it/eng/spagobi/commons/utilities/UserUtilities.java
+++ athenadao/src/it/eng/spagobi/commons/utilities/UserUtilities.java
@@ -2,7 +2,7 @@
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
- * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * If a copy of the MPL was not  distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.commons.utilities;
 
 import it.eng.spago.base.RequestContainer;
@@ -82,9 +82,9 @@ public class UserUtilities {
 
 	/**
 	 * Gets the user profile.
-	 *
+	 * 
 	 * @return the user profile
-	 *
+	 * 
 	 * @throws Exception
 	 *             the exception
 	 */
@@ -276,12 +276,12 @@ public class UserUtilities {
 
 	/**
 	 * User functionality root exists.
-	 *
+	 * 
 	 * @param username
 	 *            the username
-	 *
+	 * 
 	 * @return true, if successful
-	 *
+	 * 
 	 * @throws Exception
 	 *             the exception
 	 */
@@ -300,12 +300,12 @@ public class UserUtilities {
 
 	/**
 	 * User functionality root exists.
-	 *
+	 * 
 	 * @param userProfile
 	 *            the user profile
-	 *
+	 * 
 	 * @return true, if successful
-	 *
+	 * 
 	 * @throws Exception
 	 *             the exception
 	 */
@@ -324,12 +324,12 @@ public class UserUtilities {
 	/**
 	 * Load the user personal folder as a LowFunctionality object. If the personal folder exists, it is returned; if it does not exist and create is false, null
 	 * is returned, otherwise the personal folder is created and then returned.
-	 *
+	 * 
 	 * @param userProfile
 	 *            UserProfile the user profile object
 	 * @param createIfNotExisting
 	 *            Boolean that specifies if the personal folder must be created if it doesn't exist
-	 *
+	 * 
 	 * @return the personal folder as a LowFunctionality object, or null in case the personal folder does not exist and create is false
 	 */
 	public static LowFunctionality loadUserFunctionalityRoot(UserProfile userProfile, boolean createIfNotExisting) {
@@ -385,10 +385,10 @@ public class UserUtilities {
 
 	/**
 	 * Creates the user functionality root.
-	 *
+	 * 
 	 * @param userProfile
 	 *            the user profile
-	 *
+	 * 
 	 * @throws Exception
 	 *             the exception
 	 */
@@ -805,7 +805,7 @@ public class UserUtilities {
 	/**
 	 * Clones the input profile object. We don't implement the SpagoBIUserProfile.clone method because SpagoBIUserProfile is created by Axis tools, and
 	 * therefore, when generating the class we may lost that method.
-	 *
+	 * 
 	 * @param profile
 	 *            The input SpagoBIUserProfile object
 	 * @return a clone of the input SpagoBIUserProfile object
