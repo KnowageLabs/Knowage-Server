080739ff244ab96ea03b024729fb15aab61e5bb7
Giulio Gavardi <giulio.gavardi@eng.it>
2015-10-05 14:23:51 +0000

    only style changes

--- athena/src/it/eng/spagobi/engines/drivers/console/ConsoleDriver.java
+++ athena/src/it/eng/spagobi/engines/drivers/console/ConsoleDriver.java
@@ -2,7 +2,7 @@
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * If a copy of  the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engines.drivers.console;
 
 import it.eng.spago.base.RequestContainer;
@@ -24,141 +24,149 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-
-
 /**
- * Driver Implementation (IEngineDriver Interface) for Chart External Engine. 
+ * Driver Implementation (IEngineDriver Interface) for Chart External Engine.
  */
-public class ConsoleDriver extends GenericDriver  {
-	
+public class ConsoleDriver extends GenericDriver {
+
 	static private Logger logger = Logger.getLogger(ConsoleDriver.class);
-	 
-		
+
 	/**
-	 * Returns a map of parameters which will be send in the request to the
-	 * engine application.
+	 * Returns a map of parameters which will be send in the request to the engine application.
 	 * 
-	 * @param profile Profile of the user
-	 * @param roleName the name of the execution role
-	 * @param analyticalDocument the biobject
+	 * @param profile
+	 *            Profile of the user
+	 * @param roleName
+	 *            the name of the execution role
+	 * @param analyticalDocument
+	 *            the biobject
 	 * 
 	 * @return Map The map of the execution call parameters
 	 */
+	@Override
 	public Map getParameterMap(Object analyticalDocument, IEngUserProfile profile, String roleName) {
 		Map parameters;
 		BIObject biObject;
-		
+
 		logger.debug("IN");
-		
+
 		try {
 			parameters = super.getParameterMap(analyticalDocument, profile, roleName);
 			parameters = applyService(parameters, null);
 		} finally {
 			logger.debug("OUT");
 		}
-		
+
 		return parameters;
 	}
-	
-	
+
 	/**
-	 * Returns a map of parameters which will be send in the request to the
-	 * engine application.
+	 * Returns a map of parameters which will be send in the request to the engine application.
 	 * 
-	 * @param analyticalDocumentSubObject SubObject to execute
-	 * @param profile Profile of the user
-	 * @param roleName the name of the execution role
-	 * @param analyticalDocument the object
+	 * @param analyticalDocumentSubObject
+	 *            SubObject to execute
+	 * @param profile
+	 *            Profile of the user
+	 * @param roleName
+	 *            the name of the execution role
+	 * @param analyticalDocument
+	 *            the object
 	 * 
 	 * @return Map The map of the execution call parameters
 	 */
+	@Override
 	public Map getParameterMap(Object analyticalDocument, Object analyticalDocumentSubObject, IEngUserProfile profile, String roleName) {
-		return super.getParameterMap(analyticalDocument, analyticalDocumentSubObject, profile, roleName);		
+		return super.getParameterMap(analyticalDocument, analyticalDocumentSubObject, profile, roleName);
 	}
 
-	
-	 /**
- 	 * Function not implemented. Thid method should not be called
- 	 * 
- 	 * @param biobject The BIOBject to edit
- 	 * @param profile the profile
- 	 * 
- 	 * @return the edits the document template build url
- 	 * 
- 	 * @throws InvalidOperationRequest the invalid operation request
- 	 */
-    public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
-	throws InvalidOperationRequest {
-    	logger.warn("Function not implemented");
-    	throw new InvalidOperationRequest();
-    }
+	/**
+	 * Function not implemented. Thid method should not be called
+	 * 
+	 * @param biobject
+	 *            The BIOBject to edit
+	 * @param profile
+	 *            the profile
+	 * 
+	 * @return the edits the document template build url
+	 * 
+	 * @throws InvalidOperationRequest
+	 *             the invalid operation request
+	 */
+	@Override
+	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
+		logger.warn("Function not implemented");
+		throw new InvalidOperationRequest();
+	}
 
-    /**
-     * Function not implemented. Thid method should not be called
-     * 
-     * @param biobject  The BIOBject to edit
-     * @param profile the profile
-     * 
-     * @return the new document template build url
-     * 
-     * @throws InvalidOperationRequest the invalid operation request
-     */
-    public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
-	throws InvalidOperationRequest {
-    	logger.warn("Function not implemented");
-    	throw new InvalidOperationRequest();
-    }
+	/**
+	 * Function not implemented. Thid method should not be called
+	 * 
+	 * @param biobject
+	 *            The BIOBject to edit
+	 * @param profile
+	 *            the profile
+	 * 
+	 * @return the new document template build url
+	 * 
+	 * @throws InvalidOperationRequest
+	 *             the invalid operation request
+	 */
+	@Override
+	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
+		logger.warn("Function not implemented");
+		throw new InvalidOperationRequest();
+	}
 
-    private final static String PARAM_SERVICE_NAME = "ACTION_NAME";
-    private final static String PARAM_NEW_SESSION = "NEW_SESSION";
-    private final static String PARAM_MODALITY = "MODALITY";
-    
+	private final static String PARAM_SERVICE_NAME = "ACTION_NAME";
+	private final static String PARAM_NEW_SESSION = "NEW_SESSION";
+	private final static String PARAM_MODALITY = "MODALITY";
+
 	private Map applyService(Map parameters, BIObject biObject) {
 		ObjTemplate template;
-		
+
 		logger.debug("IN");
-		
+
 		try {
 			Assert.assertNotNull(parameters, "Input [parameters] cannot be null");
 			parameters.put(PARAM_SERVICE_NAME, "CONSOLE_ENGINE_START_ACTION");
-			parameters.put(PARAM_MODALITY, "VIEW");			
+			parameters.put(PARAM_MODALITY, "VIEW");
 			parameters.put(PARAM_NEW_SESSION, "TRUE");
-		} catch(Throwable t) {
+		} catch (Throwable t) {
 			throw new RuntimeException("Impossible to guess from template extension the engine startup service to call");
 		} finally {
 			logger.debug("OUT");
 		}
-		
+
 		return parameters;
 	}
-	
+
 	private ObjTemplate getTemplate(BIObject biObject) {
 		ObjTemplate template;
 		IObjTemplateDAO templateDAO;
-		
+
 		logger.debug("IN");
-		
+
 		try {
 			Assert.assertNotNull(biObject, "Input [biObject] cannot be null");
-			
+
 			templateDAO = DAOFactory.getObjTemplateDAO();
 			Assert.assertNotNull(templateDAO, "Impossible to instantiate templateDAO");
-		
-			template = templateDAO.getBIObjectActiveTemplate( biObject.getId() );
-			Assert.assertNotNull(template, "Loaded template cannot be null");	
-			
+
+			template = templateDAO.getBIObjectActiveTemplate(biObject.getId());
+			Assert.assertNotNull(template, "Loaded template cannot be null");
+
 			logger.debug("Active template [" + template.getName() + "] of document [" + biObject.getLabel() + "] loaded succesfully");
-		} catch(Throwable t) {
-			throw new RuntimeException("Impossible to load template for document [" + biObject.getLabel()+ "]", t);
+		} catch (Throwable t) {
+			throw new RuntimeException("Impossible to load template for document [" + biObject.getLabel() + "]", t);
 		} finally {
 			logger.debug("OUT");
 		}
-		
+
 		return template;
 	}
-	
-    private Locale getLocale() {
-    	logger.debug("IN");
+
+	private Locale getLocale() {
+		logger.debug("IN");
 		try {
 			Locale locale = null;
 			RequestContainer requestContainer = RequestContainer.getRequestContainer();
@@ -169,12 +177,11 @@ public class ConsoleDriver extends GenericDriver  {
 			locale = new Locale(language, country);
 			return locale;
 		} catch (Exception e) {
-		    logger.error("Error while getting locale; using default one", e);
-		    return GeneralUtilities.getDefaultLocale();
-		} finally  {
+			logger.error("Error while getting locale; using default one", e);
+			return GeneralUtilities.getDefaultLocale();
+		} finally {
 			logger.debug("OUT");
-		}	
+		}
 	}
-   
-}
 
+}
