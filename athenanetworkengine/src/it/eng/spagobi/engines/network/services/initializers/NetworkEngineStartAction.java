cffa976e072d0c4cb1d21efe3deda40ca26759b3
Antonella Giachino <antonella.giachino@eng.it>
2015-10-19 09:58:51 +0000

    Fixed issue ATHENA-200: "Network Engine doesn't work"

--- athenanetworkengine/src/it/eng/spagobi/engines/network/services/initializers/NetworkEngineStartAction.java
+++ athenanetworkengine/src/it/eng/spagobi/engines/network/services/initializers/NetworkEngineStartAction.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package it.eng.spagobi.engines.network.services.initializers;
@@ -20,114 +20,110 @@ import java.util.Locale;
 
 import org.apache.log4j.Logger;
 
-
 /**
  * Entry point action.
  */
 public class NetworkEngineStartAction extends AbstractEngineStartAction {
-	
 
 	// INPUT PARAMETERS
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -3424138720053064514L;
 	// OUTPUT PARAMETERS
 	public static final String LANGUAGE = "LANGUAGE";
 	public static final String COUNTRY = "COUNTRY";
-	
-	// SESSION PARAMETRES	
-	public static final String ENGINE_INSTANCE = EngineConstants.ENGINE_INSTANCE;
-	
-	/** Logger component. */
-    public static transient Logger logger = Logger.getLogger(NetworkEngineStartAction.class);
-    
-    private static final String ENGINE_NAME = "SpagoBINetworkEngine";
-  //  private static final String REQUEST_DISPATCHER_URL = "/WEB-INF/jsp/network.jsp";
-	
 
-	public void service(SourceBean serviceRequest, SourceBean serviceResponse)  {
-		logger.debug("IN");		
+	// SESSION PARAMETRES
+	public static final String ENGINE_INSTANCE = EngineConstants.ENGINE_INSTANCE;
+
+	/** Logger component. */
+	public static transient Logger logger = Logger.getLogger(NetworkEngineStartAction.class);
+
+	private static final String ENGINE_NAME = "athenanetworkengine";
+
+	// private static final String REQUEST_DISPATCHER_URL = "/WEB-INF/jsp/network.jsp";
+
+	@Override
+	public void service(SourceBean serviceRequest, SourceBean serviceResponse) {
+		logger.debug("IN");
 		Locale locale;
 		NetworkEngineInstance networkEngineInstance = null;
-		
+
 		try {
 			setEngineName(ENGINE_NAME);
 			super.service(serviceRequest, serviceResponse);
-			
+
 			logger.debug("User Id: " + getUserId());
 			logger.debug("Audit Id: " + getAuditId());
 			logger.debug("Document Id: " + getDocumentId());
 			logger.debug("Template: " + getTemplateAsString());
-						
-			if(getAuditServiceProxy() != null) {
+
+			if (getAuditServiceProxy() != null) {
 				logger.debug("Audit enabled: [TRUE]");
 				getAuditServiceProxy().notifyServiceStartEvent();
 			} else {
 				logger.debug("Audit enabled: [FALSE]");
 			}
-			
+
 			logger.debug("Creating engine instance ...");
-			
+
 			try {
-				networkEngineInstance = NetworkEngine.createInstance( getTemplateAsString(), getEnv() );
-			} catch(Throwable t) {
+				networkEngineInstance = NetworkEngine.createInstance(getTemplateAsString(), getEnv());
+			} catch (Throwable t) {
 				SpagoBIEngineStartupException serviceException;
 				String msg = "Impossible to create engine instance for document [" + getDocumentId() + "].";
 				Throwable rootException = t;
-				while(rootException.getCause() != null) {
+				while (rootException.getCause() != null) {
 					rootException = rootException.getCause();
 				}
-				String str = rootException.getMessage()!=null? rootException.getMessage(): rootException.getClass().getName();
+				String str = rootException.getMessage() != null ? rootException.getMessage() : rootException.getClass().getName();
 				msg += "\nThe root cause of the error is: " + str;
 				serviceException = new SpagoBIEngineStartupException(ENGINE_NAME, msg, t);
-				
-				
+
 				throw serviceException;
 			}
-			
-			
+
 			IDataSet dataset = getDataSet();
-			INetwork net = NetworkBuilder.buildNetwork(dataset,networkEngineInstance.getTemplate());
+			INetwork net = NetworkBuilder.buildNetwork(dataset, networkEngineInstance.getTemplate());
 			networkEngineInstance.setNet(net);
-			
+
 			logger.debug("Engine instance succesfully created");
-			
-			locale = (Locale)networkEngineInstance.getEnv().get(EngineConstants.ENV_LOCALE);
-			
-			setAttributeInSession( ENGINE_INSTANCE, networkEngineInstance);		
+
+			locale = (Locale) networkEngineInstance.getEnv().get(EngineConstants.ENV_LOCALE);
+
+			setAttributeInSession(ENGINE_INSTANCE, networkEngineInstance);
 			setAttribute(ENGINE_INSTANCE, networkEngineInstance);
-			
+
 			setAttribute(LANGUAGE, locale.getLanguage());
 			setAttribute(COUNTRY, locale.getCountry());
-			
-			
+
 		} catch (Exception e) {
 			SpagoBIEngineStartupException serviceException = null;
-						
-			if(e instanceof SpagoBIEngineStartupException) {
-				serviceException = (SpagoBIEngineStartupException)e;
+
+			if (e instanceof SpagoBIEngineStartupException) {
+				serviceException = (SpagoBIEngineStartupException) e;
 			} else {
 				Throwable rootException = e;
-				while(rootException.getCause() != null) {
+				while (rootException.getCause() != null) {
 					rootException = rootException.getCause();
 				}
-				String str = rootException.getMessage()!=null? rootException.getMessage(): rootException.getClass().getName();
-				String message = "An unpredicted error occurred while executing " + getEngineName() + " service."
-								 + "\nThe root cause of the error is: " + str;
-				
+				String str = rootException.getMessage() != null ? rootException.getMessage() : rootException.getClass().getName();
+				String message = "An unpredicted error occurred while executing " + getEngineName() + " service." + "\nThe root cause of the error is: " + str;
+
 				serviceException = new SpagoBIEngineStartupException(getEngineName(), message, e);
 			}
-			
+
 			throw serviceException;
 		} finally {
 			logger.debug("OUT");
 		}
 	}
-	
+
+	@Override
 	public IDataSet getDataSet() {
-		return getDataSetServiceProxy().getDataSet( getDocumentId() );
+		return getDataSetServiceProxy().getDataSet(getDocumentId());
 	}
-	
+
 }
