7ee6d1fd70bb91b0872e465ee77ae9870e54492e
Alessandro Portosa <alessandro.portosa@eng.it>
2015-10-28 10:04:57 +0000

    bug fix on encoding

--- athena/src/it/eng/spagobi/mapcatalogue/service/LayerServices.java
+++ athena/src/it/eng/spagobi/mapcatalogue/service/LayerServices.java
@@ -1,50 +1,47 @@
 package it.eng.spagobi.mapcatalogue.service;
 
+import it.eng.spagobi.commons.SingletonConfig;
+import it.eng.spagobi.commons.utilities.SpagoBIUtilities;
+
 import java.util.Arrays;
 
 import org.apache.commons.codec.binary.Base64;
-
-import it.eng.spagobi.commons.SingletonConfig;
-import it.eng.spagobi.commons.utilities.SpagoBIUtilities;
 
 public class LayerServices {
 	private SingletonConfig configSingleton;
 	private String path;
 	private String resourcePath;
 
-
-	LayerServices(){
+	LayerServices() {
 		setPath();
 
 	}
 
-	public void setPath(){
+	public void setPath() {
 		configSingleton = SingletonConfig.getInstance();
-		path  = configSingleton.getConfigValue("SPAGOBI.RESOURCE_PATH_JNDI_NAME");
-		resourcePath= SpagoBIUtilities.readJndiResource(path);
+		path = configSingleton.getConfigValue("SPAGOBI.RESOURCE_PATH_JNDI_NAME");
+		resourcePath = SpagoBIUtilities.readJndiResource(path);
 	}
 
-	public String getResourcePath(byte[] data){
+	public String getResourcePath(byte[] data) {
 		System.out.println(resourcePath);
 		return resourcePath;
 	}
 
-	public  byte[] getFile(byte[] data){
-		//15 perchè 15 sono i bye prima dell'inizi del doc... nel caso di txt
-		byte[] result = Arrays.copyOfRange(data, 15, data.length -1);
+	public byte[] getFile(byte[] data) {
+		// 15 perchÃ¨ 15 sono i bye prima dell'inizi del doc... nel caso di txt
+		byte[] result = Arrays.copyOfRange(data, 15, data.length - 1);
 		System.out.println(result);
-		System.out.println("a confronto"+result.length+" "+data.length);
+		System.out.println("a confronto" + result.length + " " + data.length);
 		return result;
 
 	}
 
-
-	public byte[] decode64(byte[] data){
+	public byte[] decode64(byte[] data) {
 		byte[] result = new byte[0];
-		try{
+		try {
 			result = Base64.decodeBase64(data);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 
 		}
 		return result;
