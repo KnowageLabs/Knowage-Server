cfe6c3ed9386988096791376acc8b9dd90e927af
Ana Tomic <ana.tomic@eng.it>
2015-11-03 16:56:34 +0000

    changed navigateTo function to handle new types of parameters

--- athenachartengine/WebContent/js/src/ext5/sbi/chart/viewer/HighchartsCrossNavigationHelper.js
+++ athenachartengine/WebContent/js/src/ext5/sbi/chart/viewer/HighchartsCrossNavigationHelper.js
@@ -5,17 +5,37 @@ Ext.define('Sbi.chart.viewer.HighchartsCrossNavigationHelper', {
 	statics: {
 		breadcrumb: [],
 		
-		navigateTo: function(documentName, documentParameters, categoryName, serieName){ 
+		navigateTo: function(documentName, documentParameters, categoryName, categoryValue, serieName, serieValue, groupingCategoryName, groupingCategoryValue){ 
 			var parametersAsString = '';
 			
 			for(var i = 0; i < documentParameters.length; i++) {
 				var param = documentParameters[i];
 				
-				if(param.type == 'CATEGORY_NAME') {
+				if(param.type === 'CATEGORY_NAME') {
+					if(categoryName != null){
 					parametersAsString += param.urlName + '=' + categoryName + '&';
-				} else if(param.type == 'SERIE_NAME') {
+					}
+				}else if(param.type==='CATEGORY_VALUE'){
+					if(categoryValue != null){
+						parametersAsString += param.urlName + '=' + categoryValue + '&';
+						}
+				}else if(param.type === 'SERIE_NAME') {
+					if(serieName != null){
 					parametersAsString += param.urlName +  '=' + serieName + '&';
-				} else if(param.type == 'ABSOLUTE' || param.type == 'RELATIVE') {
+					}
+				}else if(param.type==='SERIE_VALUE'){
+					if(serieValue != null){
+						parametersAsString += param.urlName +  '=' + serieValue + '&';
+						}
+				}else if(param.type==='GROUPING_NAME'){
+					if(groupingCategoryName != null){
+						parametersAsString += param.urlName +  '=' + groupingCategoryName + '&';
+						}
+				}else if(param.type==='GROUPING_VALUE'){
+					if(groupingCategoryValue != null){
+						parametersAsString += param.urlName +  '=' + groupingCategoryValue + '&';
+						}
+				}else if(param.type == 'ABSOLUTE' || param.type == 'RELATIVE') {
 					parametersAsString += param.urlName +  '=' + param.value + '&';
 				}
 			}
