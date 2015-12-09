32c49bc0536d1a9f28c6982e0292ab2cc649dab3
Giulio Gavardi <giulio.gavardi@eng.it>
2015-12-09 15:00:28 +0000

    style changes only

--- knowage/src/it/eng/spagobi/tools/importexport/IExportManager.java
+++ knowage/src/it/eng/spagobi/tools/importexport/IExportManager.java
@@ -14,39 +14,44 @@ public interface IExportManager {
 	/**
 	 * Prepare the environment for export.
 	 * 
-	 * @param pathExpFold Path of the export folder
-	 * @param nameExpFile the name to give to the exported file
-	 * @param expSubObj Flag which tells if it's necessary to export subobjects
-	 * @param expSnaps the exp snaps
-	 * 
-	 * @throws EMFUserError the EMF user error
+	 * @param pathExpFold
+	 *            Path of the export folder
+	 * @param nameExpFile
+	 *            the name to give to the exported file
+	 * @param expSubObj
+	 *            Flag which tells if it's necessary to export subobjects
+	 * @param expSnaps
+	 *            the exp snaps
+	 * @throws EMFUserError
+	 *             the EMF user error
 	 */
-	public void prepareExport(String pathExpFold, String nameExpFile, 
-			boolean expSubObj, boolean expSnaps) throws EMFUserError;
-	
+	public void prepareExport(String pathExpFold, String nameExpFile, boolean expSubObj, boolean expSnaps) throws EMFUserError;
+
 	/**
 	 * Exports objects
 	 * 
-	 * @param objPaths List of path of the objects to export
-	 * 
-	 * @throws EMFUserError the EMF user error
+	 * @param objPaths
+	 *            List of path of the objects to export
+	 * @throws EMFUserError
+	 *             the EMF user error
 	 */
 	public void exportObjects(List objPaths) throws EMFUserError;
-	
+
 	/**
 	 * Exports resources (OLAP schemas, ETL jobs, datamarts)
 	 * 
-	 * @throws EMFUserError the EMF user error
+	 * @throws EMFUserError
+	 *             the EMF user error
 	 */
-//	public void exportResources() throws EMFUserError;
-	
+	// public void exportResources() throws EMFUserError;
+
 	/**
 	 * Creates the archive export file
 	 * 
 	 * @throws EMFUserError
 	 */
 	public void createExportArchive() throws EMFUserError;
-	
+
 	/**
 	 * Clean the export environment (close sessions and delete temporary files).
 	 */
