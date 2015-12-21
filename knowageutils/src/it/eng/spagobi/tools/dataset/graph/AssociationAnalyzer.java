39a273be980aa2b60e5457bd36b2bbeda140ab4d
Francesco Lucchi <francesco.lucchi@eng.it>
2015-12-21 15:38:12 +0000

    added EdgeGroupTest and fixes

--- knowageutils/src/it/eng/spagobi/tools/dataset/graph/AssociationAnalyzer.java
+++ knowageutils/src/it/eng/spagobi/tools/dataset/graph/AssociationAnalyzer.java
@@ -2,58 +2,60 @@ package it.eng.spagobi.tools.dataset.graph;
 
 import it.eng.spagobi.tools.dataset.common.association.Association;
 import it.eng.spagobi.tools.dataset.common.association.Association.Field;
+import it.eng.spagobi.tools.dataset.common.association.AssociationGroup;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.jgrapht.graph.ClassBasedEdgeFactory;
 import org.jgrapht.graph.Pseudograph;
 
 public class AssociationAnalyzer {
+	private Collection<Association> associations;
 	private Map<String, Map<String, String>> datasetAssociationColumnMap;
 	private Pseudograph<String, LabeledEdge<String>> graph;
 
 	public AssociationAnalyzer(Collection<Association> associations) {
+		this.associations = associations;
 		this.datasetAssociationColumnMap = new HashMap<String, Map<String, String>>();
-		this.graph = new Pseudograph<String, LabeledEdge<String>>(new ClassBasedEdgeFactory<String, LabeledEdge<String>>((Class<LabeledEdge<String>>)(Object)LabeledEdge.class));
-		calc(associations, datasetAssociationColumnMap, graph);
+		this.graph = new Pseudograph<String, LabeledEdge<String>>(new ClassBasedEdgeFactory<String, LabeledEdge<String>>(
+				(Class<LabeledEdge<String>>) (Object) LabeledEdge.class));
 	}
 
-	private void calc(Collection<Association> associations,
-			Map<String, Map<String, String>> datasetToAssociationColumns,
-			Pseudograph<String, LabeledEdge<String>> graph) {
-		
+	public void process() {
+
 		for (Association association : associations) {
 			String associationId = association.getId();
 			List<Field> associationFields = association.getFields();
-			
+
 			for (Field field : associationFields) {
 				String datasetLabel = field.getDataSetLabel();
-				
+
 				// add vertex to the graph
 				if (!graph.containsVertex(datasetLabel)) {
 					graph.addVertex(datasetLabel);
 				}
-				
+
 				// add column of association to the map
 				Map<String, String> associationToColumns = null;
-				if(datasetToAssociationColumns.containsKey(datasetLabel)){
-					associationToColumns = datasetToAssociationColumns.get(datasetLabel);
+				if (datasetAssociationColumnMap.containsKey(datasetLabel)) {
+					associationToColumns = datasetAssociationColumnMap.get(datasetLabel);
 				} else {
 					associationToColumns = new HashMap<String, String>();
-					datasetToAssociationColumns.put(datasetLabel, associationToColumns);
+					datasetAssociationColumnMap.put(datasetLabel, associationToColumns);
 				}
 				associationToColumns.put(associationId, field.getFieldName());
 			}
-			
+
 			// add edges to the graph
 			for (int i = 0; i < associationFields.size() - 1; i++) {
 				String source = associationFields.get(i).getDataSetLabel();
 				for (int j = i + 1; j < associationFields.size(); j++) {
 					String target = associationFields.get(j).getDataSetLabel();
-					LabeledEdge<String> labeledEdge= new LabeledEdge<String>(source, target, associationId);
+					LabeledEdge<String> labeledEdge = new LabeledEdge<String>(source, target, associationId);
 					graph.addEdge(source, target, labeledEdge);
 				}
 			}
@@ -66,5 +68,47 @@ public class AssociationAnalyzer {
 
 	public Pseudograph<String, LabeledEdge<String>> getGraph() {
 		return graph;
+	}
+
+	public static Map<String, Map<String, Set<String>>> getSelections(AssociationGroup associationGroup, Map<EdgeGroup, Set<String>> egdeGroupValuesMap) {
+		Map<String, Map<String, Set<String>>> selections = new HashMap<String, Map<String, Set<String>>>();
+
+		for (EdgeGroup edgeGroup : egdeGroupValuesMap.keySet()) {
+			Set<String> valuesSet = egdeGroupValuesMap.get(edgeGroup);
+			String[] associationIds = edgeGroup.getColumnNames().split(",");
+
+			Map<String, String> dataSetColumnsMap = new HashMap<String, String>();
+			for (String associationId : associationIds) {
+				Association association = associationGroup.getAssociation(associationId);
+
+				for (Field field : association.getFields()) {
+					String dataSet = field.getDataSetLabel();
+					String column = field.getFieldName();
+
+					if (dataSetColumnsMap.containsKey(dataSet)) {
+						String columns = dataSetColumnsMap.get(dataSet);
+						dataSetColumnsMap.put(dataSet, columns + "," + column);
+					} else {
+						dataSetColumnsMap.put(dataSet, column);
+					}
+				}
+			}
+
+			for (String dataSet : dataSetColumnsMap.keySet()) {
+				String columns = dataSetColumnsMap.get(dataSet);
+
+				Map<String, Set<String>> columnsValuesMap = null;
+				if (selections.containsKey(dataSet)) {
+					columnsValuesMap = selections.get(dataSet);
+				} else {
+					columnsValuesMap = new HashMap<String, Set<String>>();
+					selections.put(dataSet, columnsValuesMap);
+				}
+
+				columnsValuesMap.put(columns, valuesSet);
+			}
+		}
+
+		return selections;
 	}
 }
--- knowageutils/test/it/eng/spagobi/tools/dataset/graph/AssociationAnalyzerTest.java
+++ knowageutils/test/it/eng/spagobi/tools/dataset/graph/AssociationAnalyzerTest.java
@@ -4,86 +4,97 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import it.eng.spagobi.tools.dataset.common.association.Association;
 import it.eng.spagobi.tools.dataset.common.association.Association.Field;
+import it.eng.spagobi.tools.dataset.common.association.AssociationGroup;
 
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.jgrapht.UndirectedGraph;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AssociationAnalyzerTest {
-	public static final String K = "k";
-	public static final String W = "w";
-	public static final String X = "x";
-	public static final String Y = "y";
-	public static final String Z = "z";
-	
-	public static final String A1 = "A1";
-	public static final String A3 = "A3";
-	public static final String A4 = "A4";
-	public static final String A5 = "A5";
-	public static final String A6 = "A6";
-	
+	private static final String K = "k";
+	private static final String W = "w";
+	private static final String X = "x";
+	private static final String Y = "y";
+	private static final String Z = "z";
+
+	private static final String A1 = "A1";
+	private static final String A3 = "A3";
+	private static final String A4 = "A4";
+	private static final String A5 = "A5";
+	private static final String A6 = "A6";
+
+	private Map<String, Association> oneAssociationMap;
+	private Map<String, Association> fiveAssociationsMap;
+
+	@Before
+	public void setUp() {
+		oneAssociationMap = new HashMap<String, Association>();
+		oneAssociationMap.put(A1, buildAssociation(A1, new String[] { X, Y }));
+
+		fiveAssociationsMap = new HashMap<String, Association>();
+		fiveAssociationsMap.put(A1, buildAssociation(A1, new String[] { X, Y, Z }));
+		fiveAssociationsMap.put(A3, buildAssociation(A3, new String[] { K, Z }));
+		fiveAssociationsMap.put(A4, buildAssociation(A4, new String[] { W, Y }));
+		fiveAssociationsMap.put(A5, buildAssociation(A5, new String[] { X, Y, Z }));
+		fiveAssociationsMap.put(A6, buildAssociation(A6, new String[] { X, Z }));
+	}
+
 	@Test
-	public void testOneAssociation() {
-		Collection<Association> associations = new HashSet<Association>();
-		associations.add(buildAssociation(A1, new String[] {X, Y}));
-		
-		AssociationAnalyzer analyzer = new AssociationAnalyzer(associations);
+	public void testProcessOneAssociation() {
+		AssociationAnalyzer analyzer = new AssociationAnalyzer(oneAssociationMap.values());
+		analyzer.process();
 		Map<String, Map<String, String>> datasetAssociationColumnMap = analyzer.getDatasetAssociationColumnMap();
 		UndirectedGraph<String, LabeledEdge<String>> graph = analyzer.getGraph();
-		
+
 		assertTrue(datasetAssociationColumnMap.containsKey(X));
 		assertTrue(datasetAssociationColumnMap.containsKey(Y));
-		
+
 		Map<String, String> map = null;
-		
+
 		map = datasetAssociationColumnMap.get(X);
 		assertTrue(map.containsKey(A1));
 		assertEquals(buildColumnLabel(X, A1), map.get(A1));
-		
+
 		map = datasetAssociationColumnMap.get(Y);
 		assertTrue(map.containsKey(A1));
 		assertEquals(buildColumnLabel(Y, A1), map.get(A1));
-		
+
 		assertEquals(2, graph.vertexSet().size());
 		assertTrue(graph.containsVertex(X));
 		assertTrue(graph.containsVertex(Y));
-		
+
 		assertEquals(1, graph.edgeSet().size());
 		assertTrue(graph.containsEdge(new LabeledEdge<String>(Y, X, A1)));
 	}
-	
+
 	@Test
-	public void testFiveAssociations() {
-		Collection<Association> associations = new HashSet<Association>();
-		associations.add(buildAssociation(A1, new String[] {X, Y, Z}));
-		associations.add(buildAssociation(A3, new String[] {K, Z}));
-		associations.add(buildAssociation(A4, new String[] {W, Y}));
-		associations.add(buildAssociation(A5, new String[] {X, Y, Z}));
-		associations.add(buildAssociation(A6, new String[] {X, Z}));
-		
-		AssociationAnalyzer analyzer = new AssociationAnalyzer(associations);
+	public void testProcessFiveAssociations() {
+		AssociationAnalyzer analyzer = new AssociationAnalyzer(fiveAssociationsMap.values());
+		analyzer.process();
 		Map<String, Map<String, String>> datasetAssociationColumnMap = analyzer.getDatasetAssociationColumnMap();
 		UndirectedGraph<String, LabeledEdge<String>> graph = analyzer.getGraph();
-		
+
 		assertTrue(datasetAssociationColumnMap.containsKey(K));
 		assertTrue(datasetAssociationColumnMap.containsKey(W));
 		assertTrue(datasetAssociationColumnMap.containsKey(X));
 		assertTrue(datasetAssociationColumnMap.containsKey(Y));
 		assertTrue(datasetAssociationColumnMap.containsKey(Z));
-		
+
 		Map<String, String> map = null;
-		
+
 		map = datasetAssociationColumnMap.get(K);
 		assertTrue(map.containsKey(A3));
 		assertEquals(buildColumnLabel(K, A3), map.get(A3));
-		
+
 		map = datasetAssociationColumnMap.get(W);
 		assertTrue(map.containsKey(A4));
 		assertEquals(buildColumnLabel(W, A4), map.get(A4));
-		
+
 		map = datasetAssociationColumnMap.get(X);
 		assertTrue(map.containsKey(A1));
 		assertEquals(buildColumnLabel(X, A1), map.get(A1));
@@ -91,7 +102,7 @@ public class AssociationAnalyzerTest {
 		assertEquals(buildColumnLabel(X, A5), map.get(A5));
 		assertTrue(map.containsKey(A6));
 		assertEquals(buildColumnLabel(X, A6), map.get(A6));
-		
+
 		map = datasetAssociationColumnMap.get(Y);
 		assertTrue(map.containsKey(A1));
 		assertEquals(buildColumnLabel(Y, A1), map.get(A1));
@@ -99,7 +110,7 @@ public class AssociationAnalyzerTest {
 		assertEquals(buildColumnLabel(Y, A4), map.get(A4));
 		assertTrue(map.containsKey(A5));
 		assertEquals(buildColumnLabel(Y, A5), map.get(A5));
-		
+
 		map = datasetAssociationColumnMap.get(Z);
 		assertTrue(map.containsKey(A1));
 		assertEquals(buildColumnLabel(Z, A1), map.get(A1));
@@ -107,14 +118,14 @@ public class AssociationAnalyzerTest {
 		assertEquals(buildColumnLabel(Z, A5), map.get(A5));
 		assertTrue(map.containsKey(A6));
 		assertEquals(buildColumnLabel(Z, A6), map.get(A6));
-		
+
 		assertEquals(5, graph.vertexSet().size());
 		assertTrue(graph.containsVertex(K));
 		assertTrue(graph.containsVertex(W));
 		assertTrue(graph.containsVertex(X));
 		assertTrue(graph.containsVertex(Y));
 		assertTrue(graph.containsVertex(Z));
-		
+
 		assertEquals(9, graph.edgeSet().size());
 		assertTrue(graph.containsEdge(new LabeledEdge<String>(X, Y, A1)));
 		assertTrue(graph.containsEdge(new LabeledEdge<String>(Y, Z, A1)));
@@ -126,16 +137,48 @@ public class AssociationAnalyzerTest {
 		assertTrue(graph.containsEdge(new LabeledEdge<String>(X, Z, A5)));
 		assertTrue(graph.containsEdge(new LabeledEdge<String>(Z, X, A6)));
 	}
-	
+
+	@Test
+	public void testGetOneSelection() {
+		Set<LabeledEdge<String>> edgeSet = new HashSet<LabeledEdge<String>>();
+		edgeSet.add(new LabeledEdge<String>(X, Y, A1));
+		edgeSet.add(new LabeledEdge<String>(X, Y, A5));
+		EdgeGroup resultEdgeGroup = new EdgeGroup(edgeSet);
+		Set<String> resultValues = new HashSet<String>();
+		resultValues.add("('744','2275')");
+		resultValues.add("('744','10226')");
+		Map<EdgeGroup, Set<String>> egdeGroupValuesMap = new HashMap<EdgeGroup, Set<String>>();
+		egdeGroupValuesMap.put(resultEdgeGroup, resultValues);
+
+		AssociationGroup associationGroup = new AssociationGroup();
+		associationGroup.addAssociations(fiveAssociationsMap.values());
+		Map<String, Map<String, Set<String>>> selections = AssociationAnalyzer.getSelections(associationGroup, egdeGroupValuesMap);
+
+		Map<String, Set<String>> map = null;
+		String columns = null;
+
+		assertTrue(selections.containsKey(X));
+		map = selections.get(X);
+		columns = buildColumnLabel(X, A1) + "," + buildColumnLabel(X, A5);
+		assertTrue(map.containsKey(columns));
+		assertTrue(map.get(columns).equals(resultValues));
+
+		assertTrue(selections.containsKey(Y));
+		map = selections.get(Y);
+		columns = buildColumnLabel(Y, A1) + "," + buildColumnLabel(Y, A5);
+		assertTrue(map.containsKey(columns));
+		assertTrue(map.get(columns).equals(resultValues));
+	}
+
 	private Association buildAssociation(String associationId, String[] datasets) {
 		Association association = new Association(associationId, "");
-		for(String dataset : datasets) {
+		for (String dataset : datasets) {
 			association.addField(new Field(dataset, buildColumnLabel(dataset, associationId)));
 		}
 		return association;
 	}
-	
-	private String buildColumnLabel(String dataset, String associationId){
+
+	private String buildColumnLabel(String dataset, String associationId) {
 		return dataset + "_" + associationId;
 	}
 }
