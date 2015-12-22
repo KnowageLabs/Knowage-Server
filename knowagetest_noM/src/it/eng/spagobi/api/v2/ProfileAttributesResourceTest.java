8926a8e980ea0ac9e27785888942bc7b4cdc2751
Ana Tomic <ana.tomic@eng.it>
2015-12-22 11:33:23 +0000

    junit test for ProfileAttributeResource get, post put and delete methods

--- knowagetest_noM/src/it/eng/spagobi/api/v2/ProfileAttributesResourceTest.java
+++ knowagetest_noM/src/it/eng/spagobi/api/v2/ProfileAttributesResourceTest.java
@@ -10,12 +10,15 @@ import it.eng.spagobi.profiling.bo.ProfileAttribute;
 
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProfileAttributesResourceTest extends AbstractV2BasicAuthTestCase {
@@ -32,20 +35,44 @@ public class ProfileAttributesResourceTest extends AbstractV2BasicAuthTestCase {
 	@Test
 	public void getAll() {
 
-		expect().contentType(ContentType.JSON).statusCode(200).when().log().all().get("/attributes");
+		expect().contentType(ContentType.JSON).statusCode(200).when().get("/attributes");
 		when().get("/attributes").then().body("attributeName", hasItems("name", "email", "address"));
 	}
 
 	@Test
 	public void postTest() {
 		ProfileAttribute test = buildAttribute();
-		given().contentType(ContentType.JSON).body(test).when().post("/attributes").then().statusCode(200)
-				.body("attributeName", equalTo(test.getAttributeName()));
+		Response response = given().contentType(ContentType.JSON).body(test).when().post("/attributes").then().statusCode(200)
+				.body("attributeName", equalTo(test.getAttributeName())).extract().response();
+		try {
+			JSONObject attr = new JSONObject(response.asString());
+			id = attr.getInt("attributeId");
+
+		} catch (JSONException e) {
+
+		}
+	}
+
+	@Test
+	public void putTest() {
+		ProfileAttribute attr = buildAttribute();
+		attr.setAttributeId(id);
+		attr.setAttributeName("junitTestEdited");
+		attr.setAttributeDescription("junitTestEdited");
+
+		given().contentType("application/json").and().body(attr).when().put("/attributes/" + id).then().statusCode(200);
+
+	}
+
+	@Test
+	public void removeTest() {
+
+		expect().statusCode(200).when().delete("/attributes/" + id).thenReturn();
 	}
 
 	public ProfileAttribute buildAttribute() {
 		ProfileAttribute attr = new ProfileAttribute();
-		attr.setAttributeName("junitTest1");
+		attr.setAttributeName("junitTest");
 		attr.setAttributeDescription("junitTest");
 		return attr;
 	}
