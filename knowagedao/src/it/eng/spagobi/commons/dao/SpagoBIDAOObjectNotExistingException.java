77513c5fae2efdd85ff09e4e92fe2924686ac3cd
Dragan Pirkovic <dragan.pirkovic@eng.it>
2015-12-28 07:51:25 +0000

    exception for non existing object

---
+++ knowagedao/src/it/eng/spagobi/commons/dao/SpagoBIDAOObjectNotExistingException.java
@@ -0,0 +1,17 @@
+package it.eng.spagobi.commons.dao;
+
+/**
+ * @author Dragan Pirkovic (Dragan.Pirkovic@mht.net)
+ *
+ */
+
+public class SpagoBIDAOObjectNotExistingException extends SpagoBIDOAException {
+
+	private static final long serialVersionUID = 1L;
+
+	public SpagoBIDAOObjectNotExistingException(String message) {
+		super(message);
+
+	}
+
+}
