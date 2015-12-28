e383bfa066c38f163b5846303c9f5142b333d19a
Alessio Conese <alessio.conese@eng.it>
2015-12-28 16:49:40 +0000

    [hierarchies] create right side of technical tab

--- knowage/web-content/js/src/angular_1.4/tools/hierarchies/hierTechnical/hierTechnicalController.js
+++ knowage/web-content/js/src/angular_1.4/tools/hierarchies/hierTechnical/hierTechnicalController.js
@@ -10,50 +10,78 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 	$scope.filterBySrc = '';
 	$scope.orderByFields = ['name','id'];
 	
+	/*Initialization Source variable*/
 	$scope.hierarchiesTypeSrc = ['Master', $scope.translate.load('sbi.hierarchies.type.technical')];
 	$scope.dateFilterSrc = new Date();
 	$scope.hierarchiesSrcMap = {};
-	$scope.hierTreeTarget = [];
 	$scope.dimensionSrc = [];
 	$scope.hierarchiesSrc=[];
 	$scope.hierTreeSrc = [];
 	$scope.metadataMap = {};
-	$scope.seeFilter=false;
+	$scope.seeFilterSrc=false;
+
+	/*Initialization Target variable*/
+	$scope.dateFilterTarget = new Date();
+	$scope.hierarchiesTargetMap = {};
+	$scope.hierTreeTarget = [];
+	$scope.dimensionTarget = [];
+	$scope.hierarchiesTarget = [];
+	$scope.metadataMap = {};
+	$scope.seeFilterTarget=false;
 
 	$scope.keys = {'subfolders' : 'children'};
 	
 	$scope.hierTreeSrc.push(angular.copy(dataJson));
 	$scope.hierTreeTarget.push(angular.copy(dataJson));
 	
-	$scope.toogleSeeFilter= function(){
-		$scope.seeFilter = !$scope.seeFilter;
-	}
+	$scope.toogleSeeFilter= function(choose){
+		if (choose == 'src'){
+			$scope.seeFilterSrc = !$scope.seeFilterSrc;
+		}else{
+			$scope.seeFilterTarget = !$scope.seeFilterTarget;
+		}
+	}	
+	
+	$scope.indexOf = function(myArray, myElement, key) {
+		if (myArray ===undefined || myElement === undefined) return -1;
+		for (var i = 0; i < myArray.length; i++) {
+			if (myArray[i][key] !== undefined && myArray[i][key] !== null && myArray[i][key] == myElement[key]) {
+				return i;
+			}
+		}
+		return -1;
+	};
 	
 	$scope.restService.get("dimensions","getDimensions")
 		.success(
 			function(data, status, headers, config) {
-				$scope.dimensionSrc = data;
+				$scope.dimensionSrc = angular.copy(data);
+				$scope.dimensionTarget = angular.copy(data);
 			})	
 		.error(function(data, status){
 			$scope.log.log('GET dimensions error of ' + data + ' with status :' + status);
 		});
 	
-	$scope.getHierarchies = function (dim){
-		if ($scope.hierTypeSrc !== undefined && $scope.dimSrc !== undefined){
-			$scope.hierarchiesSrc = [];
-			var type = $scope.hierTypeSrc;
-			var dimName = $scope.dimSrc.DIMENSION_NM;
+	$scope.getHierarchies = function (choose){
+		var type = choose == 'src' ? $scope.hierTypeSrc : 'Technical' ;
+		var dim = choose == 'src' ?  $scope.dimSrc : $scope.dimTarget;
+		var map = choose == 'src' ? $scope.hierarchiesSrcMap : $scope.hierarchiesTargetMap; 
+		var hierarchies = choose == 'src' ? $scope.hierarchiesSrc : $scope.hierarchiesTarget;
+		if (type !== undefined && dim !== undefined){
+			hierarchies = [];
+			var dimName = dim.DIMENSION_NM;
 			var keyMap = type+'_'+dimName; 
 			var serviceName = (type.toUpperCase() == 'AUTO' || type.toUpperCase() == 'MASTER' )? 'getHierarchiesMaster' : 'getHierarchiesTechnical';
 			
 			//if the hierarchies[dim][type] is not defined, get the hierarchies and save in the map. Else, get them from the map 
-			if ($scope.hierarchiesSrcMap[keyMap] === undefined){
+			if (map[keyMap] === undefined){
 				$scope.restService.get("hierarchies",serviceName,"dimension="+dimName)
 					.success(
 						function(data, status, headers, config) {
 							if (data.errors === undefined){
-								$scope.hierarchiesSrc = angular.copy(data);
-								$scope.hierarchiesSrcMap[keyMap] = data;
+								hierarchies = angular.copy(data);
+								map[keyMap] = data;
+								choose == 'src' ?  $scope.hierarchiesSrc = hierarchies : $scope.hierarchiesTarget = hierarchies;
 							}else{
 								$scope.log.log('GET hierarchies error of ' + data + ' with message : "' + data.errors[0].message + '"');
 							}
@@ -62,11 +90,11 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 						$scope.log.log('GET hierarchies error of ' + type +'-'+ dimName + ' with status :' + status);
 					});
 			}else{
-				$scope.hierarchiesSrc = angular.copy($scope.hierarchiesSrcMap[keyMap]);
+				choose == 'src' ? $scope.hierarchiesSrc = map[keyMap] : $scope.hierarchiesTarget = map[keyMap];
 			}
 		}
 		//get the metadata for the dimension
-		var dimName = $scope.dimSrc.DIMENSION_NM;
+		var dimName = dim.DIMENSION_NM;
 		if ($scope.metadataMap !== undefined && $scope.metadataMap[dimName] == undefined){
 			$scope.restService.get("hierarchies","nodeMetadata","dimension="+dimName+"&excludeLeaf=false")
 			.success(
@@ -115,9 +143,10 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 	
 	 $scope.editNode = function(item,parent){
 		 var parentEl = angular.element(document.body);
-		 var metTmp =  $scope.metadataMap[$scope.dimSrc.DIMENSION_NM];
+		 var dimName = $scope.dimSrc !== undefined ? $scope.dimSrc.DIMENSION_NM : 'ACCOUNT'; //TODO remove hard coded for test
+		 var metTmp =  $scope.metadataMap[dimName];
 		 var metadata = parent == undefined || parent == null ? metTmp.GENERAL_FIELDS : item.leaf == true ? metTmp.LEAF_FIELDS : metTmp.NODE_FIELDS;
-		 metadata == undefined ? metadata =  metTmp.GENERAL_FIELDS : metadata = metadata;
+		 metadata == undefined ? metadata =  metTmp.GENERAL_FIELDS : metadata = metadata; //TODO remove hard coded for test
 		 $mdDialog.show({
 			templateUrl: sbiModule_config.contextName +'/js/src/angular_1.4/tools/hierarchies/templates/hierSrcDialog.html',
 			parent: angular.element(document.body),
@@ -161,6 +190,7 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 		if (newItem !== null && newItem !== undefined){
 			item = newItem;
 		}
+		
 	}
 	
 	$scope.duplicateLeaf =  function(item,parent,event){
@@ -169,10 +199,24 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 			//must modify the dates of validity
 			newItem = $scope.editNode(newItem,parent);
 		}
+		var idx = $scope.indexOf(parent.children,item,'id');
+		if (idx>=0 && newItem !== undefined && newItem !== null){
+			parent.children.splice(idx,0,newItem);
+		}
 	}
 	
 	$scope.deleteHier =  function(item,parent,event){
-		
+		//rest service for deleting
+		var response=$scope.showConfirm(item);
+		response.then(
+			function() {
+				if (parent !== undefined && parent !== null){
+					var idx = $scope.indexOf(parent.children,item,'id');
+					parent.children.splice(idx,1);
+				}else{
+					item = {};
+				}
+			}, function() {});
 	}
 	 
 	$scope.menuOptionSrc = [{
@@ -198,18 +242,41 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 		}
 	];
 	 
-	$scope.applyFilter = function(){
+	$scope.applyFilter = function(choose){
 		//use to apply the filter only when is clicked the icon
-		$scope.filterBySrcTrigger = angular.copy($scope.filterBySrc);
-		$scope.orderBySrcTrigger = angular.copy($scope.orderBySrc);
+		if (choose = 'src'){
+			$scope.filterBySrcTrigger = angular.copy($scope.filterBySrc);
+			$scope.orderBySrcTrigger = angular.copy($scope.orderBySrc);
+		} else if (choose = 'target'){
+			$scope.filterByTargetTrigger = angular.copy($scope.filterByTarget);
+			$scope.orderByTargetTrigger = angular.copy($scope.orderByTarget);
+		}
 	}
 	
-	$scope.removeFilter = function(){
-		$scope.filterBySrcTrigger = "";
-		$scope.filterBySrc = "";
-		$scope.orderBySrcTrigger = "";
-		$scope.orderBySrc = "";
+	$scope.removeFilter = function(choose){
+		if (choose = 'src'){
+			$scope.filterBySrcTrigger = "";
+			$scope.filterBySrc = "";
+			$scope.orderBySrcTrigger = "";
+			$scope.orderBySrc = "";
+		}else if (choose = 'target'){
+			$scope.filterByTargetTrigger = "";
+			$scope.filterByTarget = "";
+			$scope.orderByTargetTrigger = "";
+			$scope.orderByTarget = "";
+		}
 	}
+	
+	$scope.showConfirm = function(hier) {
+	    var confirm = $mdDialog
+			.confirm()
+			.title('Delete ' + hier.name.toUpperCase())
+			.content('Would you like to delete the item?')
+			.ariaLabel('Lucky day')
+			.ok('Yes')
+			.cancel('No');
+	    return  $mdDialog.show(confirm);
+  	};
 	
 	$scope.debug = function (){
 		var none='none';
