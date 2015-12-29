21b87c06fb4aa66086a6f46e36056774dc3b9519
Alessio Conese <alessio.conese@eng.it>
2015-12-29 10:27:44 +0000

    [hierarchies] -add error message -implement add node function

--- knowage/web-content/js/src/angular_1.4/tools/hierarchies/hierTechnical/hierTechnicalController.js
+++ knowage/web-content/js/src/angular_1.4/tools/hierarchies/hierTechnical/hierTechnicalController.js
@@ -59,7 +59,9 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 				$scope.dimensionTarget = angular.copy(data);
 			})	
 		.error(function(data, status){
-			$scope.log.log('GET dimensions error of ' + data + ' with status :' + status);
+			var message = 'GET dimensions error of ' + data + ' with status :' + status;
+			$scope.showAlert('ERROR',message);
+			
 		});
 	
 	$scope.getHierarchies = function (choose){
@@ -83,11 +85,13 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 								map[keyMap] = data;
 								choose == 'src' ?  $scope.hierarchiesSrc = hierarchies : $scope.hierarchiesTarget = hierarchies;
 							}else{
-								$scope.log.log('GET hierarchies error of ' + data + ' with message : "' + data.errors[0].message + '"');
+								$scope.showAlert('ERROR',data.errors[0].message);
 							}
 						})
 					.error(function(data, status){
-						$scope.log.log('GET hierarchies error of ' + type +'-'+ dimName + ' with status :' + status);
+						var message='GET hierarchies error of ' + type +'-'+ dimName + ' with status :' + status;
+						$scope.showAlert('ERROR',message);
+						
 					});
 			}else{
 				choose == 'src' ? $scope.hierarchiesSrc = map[keyMap] : $scope.hierarchiesTarget = map[keyMap];
@@ -102,11 +106,12 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 					if (data.errors === undefined){
 						$scope.metadataMap[dimName] = data;
 					}else{
-						$scope.log.log('GET hierarchies error of ' + data + ' with message : "' + data.errors[0].message + '"');
+						$scope.showAlert('ERROR',data.errors[0].message);
 					}
 			})
 			.error(function(data, status){
-				$scope.log.log('GET hierarchies error of ' + type +'-'+ dimName + ' with status :' + status);
+				var message = 'GET hierarchies error of ' + type +'-'+ dimName + ' with status :' + status;
+				$scope.showAlert('ERROR',message);
 			});
 		}
 	}
@@ -135,40 +140,39 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 							choose =='src' ? $scope.hierTreeSrc = data : $scope.hierTreeTarget = data;
 						}else{
 							var params = 'date = ' + date + ' dimension = ' + dim.DIMENSION_NM + ' type = ' +  type + ' hierachies = ' + hier.HIER_NM;
-							$scope.log.log('GET tree source error with parameters' + params + ' with message : "' + data.errors[0].message + '"');
+							$scope.showAlert('ERROR',data.errors[0].message);
 						}
 					})
 				.error(function(data, status){
 					var params = 'date = ' + date + ' dimension = ' + dim.DIMENSION_NM + ' type = ' +  type + ' hierachies = ' + hier.HIER_NM;
-					$scope.log.log('GET tree source error with parameters' + params + ' with status: "' + status+ '"');
+					var message='GET tree source error with parameters' + params + ' with status: "' + status+ '"';
+					$scope.showAlert('ERROR',message);
 				});
 		}	
 	}
 	
 	 $scope.editNode = function(item,parent){
 		 var parentEl = angular.element(document.body);
-		 var dimName = $scope.dimSrc !== undefined ? $scope.dimSrc.DIMENSION_NM : 'ACCOUNT'; //TODO remove hard coded for test
+		 var dimName = $scope.dimTarget !== undefined ? $scope.dimTarget.DIMENSION_NM : 'ACCOUNT'; //TODO remove hard coded for test
 		 var metTmp =  $scope.metadataMap[dimName];
+		 if (metTmp === undefined){
+			 $scope.showAlert('Error','No metadata found for dimension '+ dimName );
+		 }
 		 var metadata = parent == undefined || parent == null ? metTmp.GENERAL_FIELDS : item.leaf == true ? metTmp.LEAF_FIELDS : metTmp.NODE_FIELDS;
 		 metadata == undefined ? metadata =  metTmp.GENERAL_FIELDS : metadata = metadata; //TODO remove hard coded for test
-		 $mdDialog.show({
-			templateUrl: sbiModule_config.contextName +'/js/src/angular_1.4/tools/hierarchies/templates/hierSrcDialog.html',
-			parent: angular.element(document.body),
-			locals: {
-				   translate: $scope.translate,
-		           hier:  item,
-		           metadata : metadata
-		         },
-			preserveScope : true,
-			clickOutsideToClose:false,
-			controller: DialogController 
-		})
-		.then(function(item) {
-			return item;
-		}, function() {
-			return null; 
-		});
-	 
+		 return $mdDialog.show({
+					templateUrl: sbiModule_config.contextName +'/js/src/angular_1.4/tools/hierarchies/templates/hierSrcDialog.html',
+					parent: angular.element(document.body),
+					locals: {
+						   translate: $scope.translate,
+				           hier:  item,
+				           metadata : metadata
+				         },
+					preserveScope : true,
+					clickOutsideToClose:false,
+					controller: DialogController 
+				});
+	
 	 	function DialogController($scope, $mdDialog, translate, hier, metadata) {
 	 		$scope.translate = translate;
 			$scope.hier = hier;
@@ -183,10 +187,17 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 	 }
 	
 	$scope.addHier =  function(item,parent,event){
-		var newItem = $scope.editNode({},parent);
-		if (newItem !== null && newItem !== undefined){
-			item.children.push(newItem);
-		}
+		var promise = $scope.editNode({},parent);
+		promise
+			.then(function(newItem){
+					var tmpItem =angular.copy(item);
+					for ( key in newItem){ tmpItem[key] = newItem[key];}
+					tmpItem.children = [];
+					tmpItem.expanded = false;
+					item.children.splice(0,0,tmpItem);
+				},function(){
+				//nothing to do, request cancelled.
+			});
 	}
 	
 	$scope.modifyHier =  function(item,parent,event){
@@ -199,13 +210,15 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 	
 	$scope.duplicateLeaf =  function(item,parent,event){
 		var newItem = angular.copy(item);
-		if ( $scope.metadataMap[$scope.dimSrc.DIMENSION_NM].ALLOW_DUPLICATE == false){
-			//must modify the dates of validity
-			newItem = $scope.editNode(newItem,parent);
-		}
-		var idx = $scope.indexOf(parent.children,item,'id');
-		if (idx>=0 && newItem !== undefined && newItem !== null){
-			parent.children.splice(idx,0,newItem);
+		if ($scope.dimTarget && $scope.dimTarget.DIMENSION_NM && $scope.dimTarget.DIMENSION_NM.length > 0){ 
+			if ($scope.metadataMap[$scope.dimTarget.DIMENSION_NM].ALLOW_DUPLICATE == false){
+				//must modify the dates of validity
+				newItem = $scope.editNode(newItem,parent);
+			}
+			var idx = $scope.indexOf(parent.children,item,'id');
+			if (idx>=0 && newItem !== undefined && newItem !== null){
+				parent.children.splice(idx,0,newItem);
+			}
 		}
 	}
 	
@@ -285,6 +298,31 @@ function hierarchyTechFunction(sbiModule_config,sbiModule_translate,sbiModule_re
 			.cancel('No');
 	    return  $mdDialog.show(confirm);
   	};
+  	
+  	//Create an alert dialog with a message
+	$scope.showAlert = function (title, message){
+		$scope.log.log(message);
+		//if angular material version < 1.0.0_rc5 not has textContent function
+		if (typeof $mdDialog.alert().textContent == 'function'){
+			$mdDialog.show( 
+				$mdDialog.alert()
+			        .parent(angular.element(document.body))
+			        .clickOutsideToClose(false)
+			        .title(title)
+			        .textContent(message) //FROM angular material 1.0 
+			        .ok('Ok')
+				);
+		}else {
+			$mdDialog.show( 
+				$mdDialog.alert()
+			        .parent(angular.element(document.body))
+			        .clickOutsideToClose(false)
+			        .title(title)
+			        .content(message)
+			        .ok('Ok')
+		        );
+		}
+	};
 	
 	$scope.debug = function (){
 		var none='none';
