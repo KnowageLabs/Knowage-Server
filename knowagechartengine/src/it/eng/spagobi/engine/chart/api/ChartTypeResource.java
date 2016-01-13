2fe6ed3ab35626da4e16981ed1ecc643fb3b6a1d
Ana Tomic <ana.tomic@eng.it>
2016-01-13 16:18:46 +0000

    added service for fetching available chart types from config xml file

---
+++ knowagechartengine/src/it/eng/spagobi/engine/chart/api/ChartTypeResource.java
@@ -0,0 +1,37 @@
+package it.eng.spagobi.engine.chart.api;
+
+import it.eng.spagobi.engine.chart.ChartEngineConfig;
+import it.eng.spagobi.engine.chart.model.conf.ChartConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@Path("/types")
+public class ChartTypeResource {
+	static private Logger logger = Logger.getLogger(ChartTypeResource.class);
+
+	@GET
+	public JSONObject getChartTypes() {
+		Map<String, ChartConfig> chartLibConf = ChartEngineConfig.getChartLibConf();
+		Object chartTypes = chartLibConf.keySet().toArray();
+		Map<String, Object> types = new HashMap<>();
+		types.put("types", chartTypes);
+		JSONObject jo = null;
+		try {
+			jo = new JSONObject(types);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			logger.error("Failed fatching chart types");
+		}
+
+		return jo;
+	}
+
+}
