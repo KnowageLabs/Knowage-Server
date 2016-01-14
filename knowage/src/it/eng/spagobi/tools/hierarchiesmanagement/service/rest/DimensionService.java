0c471217a74863fa6c08ff497b778d3e5e8f31fb
Giorgio Federici <giorgio.federici@eng.it>
2016-01-14 10:51:09 +0000

    Work in progress with Hierarchies

--- knowage/src/it/eng/spagobi/tools/hierarchiesmanagement/service/rest/DimensionService.java
+++ knowage/src/it/eng/spagobi/tools/hierarchiesmanagement/service/rest/DimensionService.java
@@ -1,7 +1,6 @@
 package it.eng.spagobi.tools.hierarchiesmanagement.service.rest;
 
 import it.eng.spago.base.SourceBean;
-import it.eng.spagobi.tools.dataset.bo.AbstractJDBCDataset;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
 import it.eng.spagobi.tools.datasource.bo.IDataSource;
 import it.eng.spagobi.tools.hierarchiesmanagement.Hierarchies;
@@ -119,30 +118,21 @@ public class DimensionService {
 			Dimension dimension = hierarchies.getDimension(dimensionLabel);
 			Assert.assertNotNull(dimension, "Impossible to find a valid dimension with label [" + dimensionLabel + "]");
 
+			String hierTableName = hierarchies.getHierarchyTableName(dimensionLabel);
+			String prefix = hierarchies.getPrefix(dimensionLabel);
+
 			List<Field> metadataFields = new ArrayList<Field>(dimension.getMetadataFields());
 
-			String dimensionName = dimension.getName();
-
-			// 1 - get datasource label name
 			IDataSource dataSource = HierarchyUtils.getDataSource(dimensionLabel);
 
 			if (dataSource == null) {
 				throw new SpagoBIServiceException("An unexpected error occured while retriving hierarchies names", "No datasource found for Hierarchies");
 			}
 
-			// 2 - execute query to get dimension data
-			String queryText = "";
+			IDataStore dataStore = HierarchyUtils.getDimensionDataStore(dataSource, dimension, dimensionLabel, metadataFields, validityDate, filterDate,
+					filterHierarchy, filterHierType, hierTableName, prefix);
 
-			String hierTableName = hierarchies.getHierarchyTableName(dimensionLabel);
-			String dimFilterField = hierarchies.getPrefix(dimensionLabel) + HierarchyConstants.DIM_FILTER_FIELD;
-			String hierFilterField = hierarchies.getPrefix(dimensionLabel) + HierarchyConstants.SELECT_HIER_FILTER_FIELD;
-
-			queryText = this.createDimensionDataQuery(dataSource, metadataFields, dimensionName, validityDate, filterDate, filterHierarchy, filterHierType,
-					hierTableName, dimFilterField, hierFilterField);
-
-			IDataStore dataStore = dataSource.executeStatement(queryText, 0, 0);
-
-			// 3 - Create JSON for Dimension data from datastore
+			// Create JSON for Dimension data from datastore
 			JSONArray rootArray = HierarchyUtils.createRootData(dataStore);
 			JSONArray columnsArray = HierarchyUtils.createJSONArrayFromFieldsList(metadataFields, false);
 			JSONArray columnsSearchArray = HierarchyUtils.createColumnsSearch(metadataFields);
@@ -170,63 +160,4 @@ public class DimensionService {
 		return result.toString();
 
 	}
-
-	private String createDimensionDataQuery(IDataSource dataSource, List<Field> metadataFields, String dimensionName, String validityDate, String filterDate,
-			String filterHierarchy, String filterHierType, String hierTableName, String dimFilterField, String hierFilterField) {
-
-		logger.debug("START");
-
-		// select
-		StringBuffer selectClauseBuffer = new StringBuffer(" ");
-		String sep = ",";
-
-		int fieldsSize = metadataFields.size();
-
-		for (int i = 0; i < fieldsSize; i++) {
-			Field tmpField = metadataFields.get(i);
-			String column = AbstractJDBCDataset.encapsulateColumnName(tmpField.getId(), dataSource);
-
-			if (i == fieldsSize - 1) {
-				sep = " ";
-			}
-
-			selectClauseBuffer.append(column + sep);
-		}
-
-		String selectClause = selectClauseBuffer.toString();
-
-		// where
-
-		String beginDtColumn = AbstractJDBCDataset.encapsulateColumnName(HierarchyConstants.BEGIN_DT, dataSource);
-		String endDtColumn = AbstractJDBCDataset.encapsulateColumnName(HierarchyConstants.END_DT, dataSource);
-
-		String vDateConverted = HierarchyUtils.getConvertedDate(validityDate, dataSource);
-
-		String vDateWhereClause = vDateConverted + " >= " + beginDtColumn + " AND " + vDateConverted + " <= " + endDtColumn;
-
-		StringBuffer query = new StringBuffer("SELECT " + selectClause + " FROM " + dimensionName + " WHERE " + vDateWhereClause);
-
-		if (filterDate != null) {
-			logger.debug("Filter date is [" + filterDate + "]");
-
-			query.append(HierarchyUtils.createDateAfterCondition(dataSource, filterDate, beginDtColumn));
-		}
-
-		if (filterHierarchy != null) {
-			logger.debug("Filter Hierarchy is [" + filterHierarchy + "]");
-
-			String hierNameCol = AbstractJDBCDataset.encapsulateColumnName(HierarchyConstants.HIER_NM, dataSource);
-			String hierTypeCol = AbstractJDBCDataset.encapsulateColumnName(HierarchyConstants.HIER_TP, dataSource);
-			String dimFilterFieldCol = AbstractJDBCDataset.encapsulateColumnName(dimFilterField, dataSource);
-			String selectFilterField = AbstractJDBCDataset.encapsulateColumnName(hierFilterField, dataSource);
-
-			query.append(HierarchyUtils.createNotInHierarchyCondition(dataSource, hierTableName, hierNameCol, filterHierarchy, hierTypeCol, filterHierType,
-					dimFilterFieldCol, selectFilterField, vDateWhereClause));
-		}
-
-		logger.debug("Query for dimension data is: " + query);
-		logger.debug("END");
-		return query.toString();
-	}
-
 }
