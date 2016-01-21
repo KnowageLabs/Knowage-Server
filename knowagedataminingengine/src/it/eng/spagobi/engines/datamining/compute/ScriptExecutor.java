4b8d234e4dddc2bf766c87dd02b54f5c8c110fd9
Alessio Conese <alessio.conese@eng.it>
2016-01-21 09:21:52 +0000

    [datamining] -in ScriptExecutore add creation directories if they don't exist.

--- knowagedataminingengine/src/it/eng/spagobi/engines/datamining/compute/ScriptExecutor.java
+++ knowagedataminingengine/src/it/eng/spagobi/engines/datamining/compute/ScriptExecutor.java
@@ -1,7 +1,7 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engines.datamining.compute;
 
@@ -53,7 +53,7 @@ public class ScriptExecutor {
 		// checks whether executed before
 		if (rerun || command.getExecuted() == null || !command.getExecuted()) {
 			logger.debug("rerun or first execution");
-			//load libraries from local dir (if needed)
+			// load libraries from local dir (if needed)
 			DataMiningScript script = getScript(command);
 			loadLibrariesFromRLocal(script.getLibraries());
 			logger.debug("loaded libraries from local dir (if needed)");
@@ -84,74 +84,78 @@ public class ScriptExecutor {
 		}
 		logger.debug("OUT");
 	}
+
 	protected void evalExternalScript(String fileName, Map params) throws Exception {
 		logger.debug("IN");
 		String path = DataMiningUtils.UPLOADED_FILE_PATH + DataMiningConstants.DATA_MINING_EXTERNAL_CODE_PATH;
-		String codeResource = path+fileName;
+		String codeResource = path + fileName;
 		logger.debug(codeResource);
-		if(params!= null && !params.isEmpty()){
-			//get libraries param  to load libraries before execution
+		if (params != null && !params.isEmpty()) {
+			// get libraries param to load libraries before execution
 			logger.debug("get libraries param  to load libraries before execution");
-			if(params.keySet().contains(DataMiningConstants.PARAM_LIBRARIES)){
-				String libs = (String)params.get(DataMiningConstants.PARAM_LIBRARIES);
-				if(libs != null && !libs.equals("")){
+			if (params.keySet().contains(DataMiningConstants.PARAM_LIBRARIES)) {
+				String libs = (String) params.get(DataMiningConstants.PARAM_LIBRARIES);
+				if (libs != null && !libs.equals("")) {
 					loadLibrariesFromRLocal(libs);
-					logger.debug("Loaded libraries "+libs);
+					logger.debug("Loaded libraries " + libs);
 				}
 			}
-			
-			String codeResourceTemp = path+ "temp_"+ fileName;		
-			logger.debug("Needs params for temp script "+codeResourceTemp);
+
+			String codeResourceTemp = path + "temp_" + fileName;
+			logger.debug("Needs params for temp script " + codeResourceTemp);
 			File codeResourceFile = new File(codeResource);
-			if(codeResourceFile.exists()){
+			if (codeResourceFile.exists()) {
 				BufferedReader br = null;
-				String code=null;
-				FileWriter fw =null;
-				BufferedWriter bw =null;
+				String code = null;
+				FileWriter fw = null;
+				BufferedWriter bw = null;
 				try {
-		 
+
 					String sCurrentLine;
-					StringBuffer content =new StringBuffer();
+					StringBuffer content = new StringBuffer();
 					br = new BufferedReader(new FileReader(codeResourceFile));
-		 
+
 					while ((sCurrentLine = br.readLine()) != null) {
-						content.append(sCurrentLine+"\n");
+						content.append(sCurrentLine + "\n");
 					}
 					code = content.toString();
 					logger.debug("code read from input file");
-					if(code != null && !code.equals("")){
+					if (code != null && !code.equals("")) {
 						code = StringUtilities.substituteParametersInString(code, params, null, false);
 						logger.debug("parameters replaced");
 					}
-					
+
 				} catch (IOException e) {
-					logger.error("Unable to read file "+codeResource);				
+					logger.error("Unable to read file " + codeResource);
 					throw e;
 				} finally {
 					try {
-						if (br != null) br.close();
+						if (br != null)
+							br.close();
 					} catch (IOException ex) {
-						logger.error("Unable to close file "+codeResource);
+						logger.error("Unable to close file " + codeResource);
 						throw ex;
 					}
 				}
-				try{
+				try {
 					File codeResourceFileTemp = new File(codeResourceTemp);
-					fw= new FileWriter(codeResourceFileTemp);
+					fw = new FileWriter(codeResourceFileTemp);
 					bw = new BufferedWriter(fw);
-					
+
 					bw.write(code);
 					bw.close();
 					fw.close();
-					
+
 				} finally {
 					logger.debug("temp file created");
 					try {
-						if (bw != null)bw.close();
-						if (fw != null)fw.close();
-						
+						if (bw != null)
+							bw.close();
+						if (fw != null)
+							fw.close();
+
 					} catch (IOException ex) {
-						logger.error("Unable to close file writer "+codeResource);
+						logger.error("Unable to close file writer " + codeResource);
 						throw ex;
 					}
 				}
@@ -160,16 +164,16 @@ public class ScriptExecutor {
 				logger.debug("External script executed with params");
 				deleteTemporarySourceScript(codeResourceTemp);
 				logger.debug("Deleted temp source file");
-			}else{
+			} else {
 				logger.debug("Ready to execute external script without params");
 				re.eval("source(\"" + codeResource + "\")");
 				logger.debug("External script executed without params");
 			}
 
-		}	
+		}
 		logger.debug("OUT");
 	}
-	
+
 	protected void deleteTemporarySourceScript(String path) {
 		logger.debug("IN");
 		boolean success = (new File(path)).delete();
@@ -181,22 +185,23 @@ public class ScriptExecutor {
 		String code = "";
 
 		DataMiningScript script = getScript(command);
-		if (script != null) {			
+		if (script != null) {
 			code = DataMiningUtils.replaceVariables(command.getVariables(), script.getCode());
 		}
 		logger.debug("OUT");
 		return code;
 	}
-	private void loadLibrariesFromRLocal(String libraryNames){
+
+	private void loadLibrariesFromRLocal(String libraryNames) {
 		logger.debug("IN");
 
 		REXP rHome = re.eval("libdir<-paste(R.home(),\"library\", sep=\"/\")");
-		if(rHome != null){
-			if(libraryNames != null){
+		if (rHome != null) {
+			if (libraryNames != null) {
 				String[] libs = libraryNames.split(",");
 				for (int i = 0; i < libs.length; i++) {
 					String lib = libs[i].trim();
-					re.eval("library("+lib+",lib.loc=libdir)");
+					re.eval("library(" + lib + ",lib.loc=libdir)");
 				}
 			}
 		}
@@ -204,14 +209,13 @@ public class ScriptExecutor {
 		logger.debug("OUT");
 	}
 
-	
-	private DataMiningScript getScript(DataMiningCommand command){
+	private DataMiningScript getScript(DataMiningCommand command) {
 		logger.debug("IN");
 		String scriptName = command.getScriptName();
 		if (dataminingInstance.getScripts() != null && !dataminingInstance.getScripts().isEmpty()) {
 			for (Iterator it = dataminingInstance.getScripts().iterator(); it.hasNext();) {
 				DataMiningScript script = (DataMiningScript) it.next();
-				if (script.getName().equals(scriptName)) {					
+				if (script.getName().equals(scriptName)) {
 					return script;
 				}
 			}
@@ -219,12 +223,14 @@ public class ScriptExecutor {
 		logger.debug("OUT");
 		return null;
 	}
-	
 
 	private String createTemporarySourceScript(String code) throws IOException {
 		logger.debug("IN");
 		String name = RandomStringUtils.randomAlphabetic(10);
 		File temporarySource = new File(DataMiningUtils.getUserResourcesPath(profile) + DataMiningConstants.DATA_MINING_TEMP_PATH_SUFFIX + name + ".R");
+		if (!temporarySource.getParentFile().exists()) {
+			temporarySource.getParentFile().mkdirs();
+		}
 		FileWriter fw = null;
 		String ret = "";
 		try {
@@ -246,6 +252,5 @@ public class ScriptExecutor {
 		return ret;
 
 	}
-
 
 }
