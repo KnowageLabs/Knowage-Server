18c463f8a2024d67a4bdd876668165750ba6e2f7
Alessio Conese <alessio.conese@eng.it>
2016-01-26 15:50:26 +0000

    [hierarchies] -check if the date is completely correct

--- knowage/web-content/js/src/angular_1.4/tools/hierarchies/hierarchiesController.js
+++ knowage/web-content/js/src/angular_1.4/tools/hierarchies/hierarchiesController.js
@@ -4,19 +4,30 @@ app.config(function($mdDateLocaleProvider) {
 	 $mdDateLocaleProvider.parseDate = function(date) {
 		 if (typeof date == "string"){
 			 var re = /(\d{1,2})\/(\d{1,2})\/(\d{4})/; //RegExp for a date
-			 var result= date.match(re);
+			 var result = date.match(re);
+			 var dateCorrect = false;
 			 if (result && result.length == 4){
-				 return new Date(result[3],result[2]-1,result[1]); //Date has month starting from 0, so result[2]-1 is the correct month
-			 }else{
-				 return null;
+				 var day = result[1];
+				 var mm = result[2];
+				 var yy = result[3]
+				 if (mm>=1 && mm<=12 && day>=1 && day <= 31){
+					 if ( (mm == 11 || mm == 4 || mm == 6 || mm == 9) && day<=30){
+						 dateCorrect = true;	
+					 }else if ( mm == 2 && ( ((yy % 4) == 0 && day<=29) || ((yy % 4) != 0 && day<=28))){
+						 dateCorrect = true;
+					 }else if (mm == 1 || mm == 3 || mm == 5  || mm == 7 || mm == 8 || mm == 10 || mm == 12){
+						 dateCorrect = true;
+					 }
+				 }
 			 }
+			 return dateCorrect == true ? new Date(result[3],result[2]-1,result[1]) : null; //Date has month starting from 0, so result[2]-1 is the correct month
 		 }else{
 		    return date;
 		 }
 	};
 });
 
-app.controller('hierCtrl', ['sbiModule_translate',"$scope",funzione ]);
+app.controller('hierCtrl', ['sbiModule_translate',"$scope",funzione]);
 
 function funzione(sbiModule_translate, $scope){ 	
 	sbiModule_translate.addMessageFile("messages");
