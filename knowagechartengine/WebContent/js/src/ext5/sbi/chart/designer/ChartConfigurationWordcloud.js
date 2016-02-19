3a34cac5f5f8c776785b47bfffb5bafc10c43558
danristo <danristo@eng.it>
2016-02-19 10:03:00 +0000

    IMPROVED: - The Wordcloud configuration panel is refined and improved (padding of the word layout field and handling behavior of panel when the two items that are appearing when the value for the layout is 'custom' are showing/hiding).

--- knowagechartengine/WebContent/js/src/ext5/sbi/chart/designer/ChartConfigurationWordcloud.js
+++ knowagechartengine/WebContent/js/src/ext5/sbi/chart/designer/ChartConfigurationWordcloud.js
@@ -11,18 +11,8 @@ Ext.define
 		
 		id : "wordcloudConfiguration",
 		
-		/**
-		 * NOTE: 
-		 * This is a temporal solution (for bugs ATHENA-154 and ATHENA-157):
-		 * Instead of using dynamic width for this panel that relies
-		 * on the width of the width of the window of the browser, fix this
-		 * value so it can be entirely visible to the end user. Also the
-		 * height will be defined as the fixed value.
-		 * 
-		 * @author: danristo (danilo.ristovski@mht.net)
-		 */
 		columnWidth: 1,
-		height: 400,
+		height: 295,
 		
 		title : LN("sbi.chartengine.configuration.wordcloud.configPanelTitle"),
 		bodyPadding : 10,
@@ -40,9 +30,12 @@ Ext.define
 //		},
 
 		constructor : function(config) {
+			
 			this.callParent(config);
 			this.viewModel = config.viewModel;
 
+			var globalScope = this;
+			
 			var item = [
 
 					{
@@ -111,7 +104,7 @@ Ext.define
 								allowBlank: true,
 								fieldLabel : LN("sbi.chartengine.configuration.wordcloud.maxWords") + Sbi.settings.chart.configurationStep.htmlForMandatoryFields,
 								width: Sbi.settings.chart.configurationStep.widthOfFields,
-			        			padding:Sbi.settings.chart.configurationStep.paddingOfInnerFields,
+			        			padding: Sbi.settings.chart.configurationStep.paddingOfInnerFields,
 								maxValue : '300',
 								minValue : '10',
 								emptyText: LN("sbi.chartengine.configuration.wordcloudMaxNumWords.emptyText"),
@@ -135,89 +128,175 @@ Ext.define
                 	 },
                 	 
                 	 {
- 						xtype : 'combo',
- 						queryMode : 'local',
- 						forceSelection : true,
- 						editable : false,
- 						width: Sbi.settings.chart.configurationStep.widthOfFields,
- 	        			padding:Sbi.settings.chart.configurationStep.paddingOfInnerFields,
- 						fieldLabel : LN("sbi.chartengine.configuration.wordcloud.wordLayout") + Sbi.settings.chart.configurationStep.htmlForMandatoryFields,
- 						bind : '{configModel.wordLayout}',
- 						displayField : 'name',
- 						valueField : 'value',					
- 						emptyText: LN("sbi.chartengine.configuration.wordcloudWordLayout.emptyText"),
- 						
- 						store : 
- 						{
- 							fields : [ 'name', 'value' ],
- 							
- 							data : 
- 							[
- 								{
- 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.horizontal'),
- 									value : 'horizontal'
- 								},
- 								{
- 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.vertical'),
- 									value : 'vertical'
- 								},
- 								{
- 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.horizontalAndVerticaal'),
- 									value : 'horizontalAndVertical'
- 								},
- 								{
- 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.randomAngle'),
- 									value : 'custom'
- 								}
- 							]
- 						},
- 						
- 						listeners:
- 						{
- 							change: function(a,currentValue)
- 							{
- 								if (currentValue)
- 								{
- 									this.labelEl.update(LN("sbi.chartengine.configuration.wordcloud.wordLayout") + ":");
- 									switch(currentValue){
- 									case 'horizontal':
- 										Ext.getCmp("wordcloudMaxAngle").setValue(0);
- 										Ext.getCmp("wordcloudMinAngle").setValue(0);
- 										Ext.getCmp("wordcloudMaxAngle").hide();
- 										Ext.getCmp("wordcloudMinAngle").hide();
- 									   break;
- 									case 'vartical':
- 										Ext.getCmp("wordcloudMaxAngle").setValue(90);
- 										Ext.getCmp("wordcloudMinAngle").setValue(90);
- 										Ext.getCmp("wordcloudMaxAngle").hide();
- 										Ext.getCmp("wordcloudMinAngle").hide();
- 									   break;   
- 									case 'horizontalAndVertical':
- 										Ext.getCmp("wordcloudMaxAngle").setValue(90);
- 										Ext.getCmp("wordcloudMinAngle").setValue(0);
- 										Ext.getCmp("wordcloudMaxAngle").hide();
- 										Ext.getCmp("wordcloudMinAngle").hide();
- 									   break;
- 									case 'custom':
- 										Ext.getCmp("wordcloudMaxAngle").setValue('');
- 										Ext.getCmp("wordcloudMinAngle").setValue('');
- 										Ext.getCmp("wordcloudMaxAngle").show();
- 										Ext.getCmp("wordcloudMinAngle").show();
- 									   break;
- 										
- 									   
- 									   
- 									}
- 									
- 									
- 								}
- 								else
- 								{
- 									this.labelEl.update(LN("sbi.chartengine.configuration.wordcloud.wordLayout") + Sbi.settings.chart.configurationStep.htmlForMandatoryFields + ":");
- 								}
- 							}
- 						}
- 					},
+                		 xtype : 'fieldcontainer',
+                	 
+	                	 layout : 'hbox',
+	                	 
+	                	 defaults : 
+	                	 {
+	//                		 labelWidth : '100%',
+	                		 //margin:'0 30 0 0'
+	                	 },
+		                    	 
+	                	 items: 
+	            		 [	
+		                	 {                		 
+		 						xtype : 'combo',
+		 						queryMode : 'local',
+		 						forceSelection : true,
+		 						editable : false,
+		 						width: Sbi.settings.chart.configurationStep.widthOfFields,
+			        			padding: Sbi.settings.chart.configurationStep.paddingOfInnerFields,
+		 						fieldLabel : LN("sbi.chartengine.configuration.wordcloud.wordLayout") + Sbi.settings.chart.configurationStep.htmlForMandatoryFields,
+		 						bind : '{configModel.wordLayout}',
+		 						displayField : 'name',
+		 						valueField : 'value',					
+		 						emptyText: LN("sbi.chartengine.configuration.wordcloudWordLayout.emptyText"),
+		 						
+		 						store : 
+		 						{
+		 							fields : [ 'name', 'value' ],
+		 							
+		 							data : 
+		 							[
+		 								{
+		 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.horizontal'),
+		 									value : 'horizontal'
+		 								},
+		 								{
+		 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.vertical'),
+		 									value : 'vertical'
+		 								},
+		 								{
+		 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.horizontalAndVerticaal'),
+		 									value : 'horizontalAndVertical'
+		 								},
+		 								{
+		 									name : LN('sbi.chartengine.configuration.wordcloud.wordLayout.randomAngle'),
+		 									value : 'custom'
+		 								}
+		 							]
+		 						},
+		 						
+		 						listeners:
+		 						{
+		 							change: function(a,currentValue,previousValue,c)
+		 							{		 								
+		 								if (currentValue)
+		 								{
+		 									this.labelEl.update(LN("sbi.chartengine.configuration.wordcloud.wordLayout") + ":");		 									
+		 									
+		 									switch(currentValue){			 									
+		 										case 'horizontal':
+			 										Ext.getCmp("wordcloudMaxAngle").setValue(0);
+			 										Ext.getCmp("wordcloudMinAngle").setValue(0);
+			 										
+			 										/**
+			 										 * Hide field containers that contain the two fields that 
+			 										 * we need to hide.
+			 										 * @commentBy Danilo Ristovski (danristo, danilo.ristovski@mht.net)
+			 										 */
+			 										Ext.getCmp("wordcloudMaxAngle").ownerCt.hide();
+			 										Ext.getCmp("wordcloudMinAngle").ownerCt.hide();
+			 										
+			 										/**
+			 										 * Decrease the height of the panel since we are hiding
+			 										 * two fields which height is 20px each and also take away 
+			 										 * some additional space that is taken by the padding between 
+			 										 * them and the one that was set for making the distance 
+			 										 * after the last field - 15px for each field. This will be 
+			 										 * performed only if the previous value was 'custom' (the one
+			 										 * that introduced these two fields that we need to hide). 
+			 										 * 
+			 										 * NOTE: The same goes for next two cases ('vertical',
+			 										 * 'horizontalAndVertical').
+			 										 * 
+			 										 * @author Danilo Ristovski (danristo, danilo.ristovski@mht.net)
+			 										 */
+			 										if (previousValue == "custom")
+		 											{
+			 											globalScope.height = globalScope.height-2*20-2*15;
+				 										globalScope.update();
+				 										globalScope.ownerCt.update();
+		 											}
+			 									   break;
+			 									case 'vartical':
+			 										Ext.getCmp("wordcloudMaxAngle").setValue(90);
+			 										Ext.getCmp("wordcloudMinAngle").setValue(90);
+			 										
+			 										/**
+			 										 * Hide field containers that contain the two fields that 
+			 										 * we need to hide.
+			 										 * @commentBy Danilo Ristovski (danristo, danilo.ristovski@mht.net)
+			 										 */
+			 										Ext.getCmp("wordcloudMaxAngle").ownerCt.hide();
+			 										Ext.getCmp("wordcloudMinAngle").ownerCt.hide();
+			 										
+			 										if (previousValue == "custom")
+		 											{
+			 											globalScope.height = globalScope.height-2*20-2*15;
+				 										globalScope.update();
+				 										globalScope.ownerCt.update();
+		 											}
+			 									   break;   
+			 									case 'horizontalAndVertical':
+			 										Ext.getCmp("wordcloudMaxAngle").setValue(90);
+			 										Ext.getCmp("wordcloudMinAngle").setValue(0);
+			 										
+			 										/**
+			 										 * Hide field containers that contain the two fields that 
+			 										 * we need to hide.
+			 										 * @commentBy Danilo Ristovski (danristo, danilo.ristovski@mht.net)
+			 										 */
+			 										Ext.getCmp("wordcloudMaxAngle").ownerCt.hide();
+			 										Ext.getCmp("wordcloudMinAngle").ownerCt.hide();
+			 										
+			 										if (previousValue == "custom")
+		 											{
+			 											globalScope.height = globalScope.height-2*20-2*15;
+				 										globalScope.update();
+				 										globalScope.ownerCt.update();
+		 											}
+			 									   break;
+			 									case 'custom':
+			 										Ext.getCmp("wordcloudMaxAngle").setValue('');
+			 										Ext.getCmp("wordcloudMinAngle").setValue('');
+			 										
+			 										/**
+			 										 * Show field containers that contain the two fields
+			 										 * that we need to show to the user. Before that, show
+			 										 * these two fields, since they were hidden previously.
+			 										 * @commentBy Danilo Ristovski (danristo, danilo.ristovski@mht.net)
+			 										 */
+			 										Ext.getCmp("wordcloudMaxAngle").show();
+			 										Ext.getCmp("wordcloudMinAngle").show();
+			 										Ext.getCmp("wordcloudMaxAngle").ownerCt.show();
+			 										Ext.getCmp("wordcloudMinAngle").ownerCt.show();
+			 										
+			 										/**
+			 										 * Increase the height of the panel since we are introducing
+			 										 * (showing) two new fields which height is 20px each and
+			 										 * provide some additional space that is taken by the padding 
+			 										 * between them and the one that is expected after the last
+			 										 * GUI item (distance from the bottom edge) - 15px for each.
+			 										 * 
+			 										 * @author Danilo Ristovski (danristo, danilo.ristovski@mht.net)
+			 										 */
+			 										globalScope.height = globalScope.height+2*20+2*15;
+			 										globalScope.update();
+			 										globalScope.ownerCt.update();
+			 									   break;		 									   
+			 									}
+		 								}
+		 								else
+		 								{
+		 									this.labelEl.update(LN("sbi.chartengine.configuration.wordcloud.wordLayout") + Sbi.settings.chart.configurationStep.htmlForMandatoryFields + ":");
+		 								}
+		 							}
+		 						}
+		                	 }
+	                	 ]
+                	 },
                 	 
 	                 {            
 	                	 xtype : 'fieldcontainer',
