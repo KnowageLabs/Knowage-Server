ac015e4e62bdd0245b654638f3c2a4fa20b32a34
Alessandro Portosa <alessandro.portosa@eng.it>
2016-02-25 10:58:46 +0000

    Implementing Serializable to avoid "SEVERE: IOException while loading persisted sessions: java.io.WriteAbortedException: writing aborted; java.io.NotSerializableException: it.eng.spagobi.engines.drivers.E ngineURL" during startup

--- knowageutils/src/it/eng/spagobi/engines/drivers/EngineURL.java
+++ knowageutils/src/it/eng/spagobi/engines/drivers/EngineURL.java
@@ -1,33 +1,36 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package it.eng.spagobi.engines.drivers;
 
+import java.io.Serializable;
 import java.util.Map;
 
-public class EngineURL {
-	
+public class EngineURL implements Serializable {
+
 	private String mainURL = null;
-	
+
 	private Map parameters = null;
 
 	/**
 	 * Instantiates a new engine url.
-	 * 
-	 * @param mainURL the main url
-	 * @param parameters the parameters
+	 *
+	 * @param mainURL
+	 *            the main url
+	 * @param parameters
+	 *            the parameters
 	 */
 	public EngineURL(String mainURL, Map parameters) {
 		this.mainURL = mainURL;
 		this.parameters = parameters;
 	}
-	
+
 	/**
 	 * Gets the main url.
-	 * 
+	 *
 	 * @return the main url
 	 */
 	public String getMainURL() {
@@ -36,8 +39,9 @@ public class EngineURL {
 
 	/**
 	 * Sets the main url.
-	 * 
-	 * @param mainURL the new main url
+	 *
+	 * @param mainURL
+	 *            the new main url
 	 */
 	public void setMainURL(String mainURL) {
 		this.mainURL = mainURL;
@@ -45,7 +49,7 @@ public class EngineURL {
 
 	/**
 	 * Gets the parameters.
-	 * 
+	 *
 	 * @return the parameters
 	 */
 	public Map getParameters() {
@@ -54,8 +58,9 @@ public class EngineURL {
 
 	/**
 	 * Sets the parameters.
-	 * 
-	 * @param parameters the new parameters
+	 *
+	 * @param parameters
+	 *            the new parameters
 	 */
 	public void setParameters(Map parameters) {
 		this.parameters = parameters;
@@ -63,18 +68,21 @@ public class EngineURL {
 
 	/**
 	 * Adds the parameter.
-	 * 
-	 * @param key the key
-	 * @param value the value
+	 *
+	 * @param key
+	 *            the key
+	 * @param value
+	 *            the value
 	 */
 	public void addParameter(String key, Object value) {
 		parameters.put(key, value);
 	}
-	
+
 	/**
 	 * Adds the parameters.
-	 * 
-	 * @param parameters the parameters
+	 *
+	 * @param parameters
+	 *            the parameters
 	 */
 	public void addParameters(Map parameters) {
 		this.parameters.putAll(parameters);
