6d500b8b9b0050c5b26afb3c273608052b6a49ac
Benedetto Milazzo <benedetto.milazzo@eng.it>
2016-02-26 09:04:10 +0000

    AngularJs Execution document: WIP

--- knowage/web-content/WEB-INF/jsp/tools/documentexecution/documentExecutionNg.jsp
+++ knowage/web-content/WEB-INF/jsp/tools/documentexecution/documentExecutionNg.jsp
@@ -15,79 +15,87 @@
 </head>
 
 <body class="bodyStyle" ng-app="documentExecutionModule">
-	<div layout="column" ng-controller="documentExecutionController as ctrl" 
-			ng-init="initSelectedRole()" ng-cloak>
+	<div layout="column" ng-controller="documentExecutionController as ctrl" ng-init="initSelectedRole()" ng-cloak>
 			
 		<md-toolbar class="miniheadimportexport">
             <div class="md-toolbar-tools" layout="row" layout-align="center center">
                 <i class="fa fa-file-text-o fa-2x"></i>
-                <h2 class="md-flex"> {{translate.load("sbi.generic.document");}}: <%= request.getParameter("OBJECT_NAME") %>
-                - {{translate.load("sbi.browser.defaultRole.role");}} {{selectedRole}} </h2>
+                <h2 class="md-flex">
+                	{{translate.load("sbi.generic.document")}}: <%= request.getParameter("OBJECT_NAME") %> - {{translate.load("sbi.browser.defaultRole.role")}} {{selectedRole}}
+                </h2>
                 
                 <span flex=""></span>
-                
-               <md-button class="toolbar-button-custom" title="Parameters" aria-label="Parameters" ng-click="toggleParametersPanel()" 
-               		ng-disabled="!showParametersPanel">
-					<i class="fa fa-cog header"></i>
+
+				<md-button class="toolbar-button-custom" title="Parameters"	aria-label="Parameters" 
+						ng-click="toggleParametersPanel()" ng-disabled="isParameterPanelDisabled()">
+					<i class="fa fa-cog header"></i> 
 				</md-button>
-                
-            </div>
+	
+			</div>
         </md-toolbar>
 
-
-
-		
 		<section layout="row" >
-			
-			
-				<md-input-container class="small counter">
+			<md-input-container class="small counter">
 				<md-select aria-label="aria-label" ng-model="selectedRole" ng-show="showSelectRoles">
-					<md-option ng-click="changeRole(role);" ng-repeat="role in roles" value="{{role}}">{{role}}</md-option>
+					<md-option ng-click="changeRole(role)" ng-repeat="role in roles" value="{{role}}">{{role|uppercase}}</md-option>
 				</md-select>
 			</md-input-container>
 			
-		<md-content layout-padding flex>
-			<iframe ng-src="{{result}}" style="overflow:hidden;height:100%;width:100%" height="100%"> </iframe>
-		</md-content>
+			<md-content layout-padding flex>
+				<iframe ng-src="{{documentUrl}}" layout-fill
+				<%--
+					style="overflow:hidden;height:100%;width:100%" height="100%"
+				--%>
+					> </iframe>
+			</md-content>
 		
-		<md-sidenav class="md-sidenav-right" md-component-id="right"  > <!-- md-is-locked-open="$mdMedia('gt-md')" -->
+			<md-sidenav class="md-sidenav-right" md-component-id="right" 
+					ng-class="{'md-locked-open': showParametersPanel}" md-is-locked-open="$mdMedia('gt-md')" 
+			> 
+			<!-- 
+			-->
+				<!--
 				<md-toolbar class="header" style="height: 75px;">
-					<h1 class="md-toolbar-tools" style="text-align:center; display:inline;">Parameters</h1>
+					<h1 class="md-toolbar-tools" style="text-align:center; display:inline;">
+						{{translate.load("sbi.execution.parametersselection.parameters")}}
+					</h1>
 					<div layout="row" layout-align="center center">
-						<md-button title="Execute Document" aria-label="Execute Document" class="toolbar-button-custom"
-								ng-click="executeDocument(selectedDocument)">
-							<i class="fa fa-play-circle" style="color:white"></i>
+						<md-button title="{{translate.load('sbi.execution.parametersselection.toolbar.next')}}"
+								aria-label="Execute Document" class="toolbar-button-custom"
+								ng-click="executeDocument()" ng-disabled="canExecuteDocument()">
+							<i class="fa fa-play-circle"></i>
 						</md-button>
 						
 						<md-button title="Edit Document" aria-label="Edit Document" class="toolbar-button-custom" 
-								ng-click="alert('Editing '+selectedDocument.name+'...')">
-							<i class="fa fa-pencil" style="color:white"></i>
+								ng-click="editDocument()">
+							<i class="fa fa-pencil"></i>
 						</md-button>
 						
-						
-						
 						<md-button title="Delete Document" aria-label="Delete Document" class="toolbar-button-custom" 
-								ng-click="alert('Deleting '+selectedDocument.name+'...')">
-							<i class="fa fa-trash-o" style="color:white"></i>
+								ng-click="deleteDocument()">
+							<i class="fa fa-trash-o"></i>
 						</md-button>
 					</div>
 				</md-toolbar>
+				-->
 				
 				<md-list>
 					<md-list-item layout="row" ng-repeat="param in documentParameters">
-						<md-input-container class="small counter"> <label>{{param.label}}</label>
-								<input class="input_class" ng-model="param.parameterToExecute" ng-required="param.mandatory" > 
+						<md-input-container class="small counter">
+							<label>{{param.label}}</label>
+							<input class="input_class" ng-model="param.parameterValue" ng-required="param.mandatory" > 
 						</md-input-container>
 					</md-list-item>
-					
 				</md-list>
+				
 				<!-- execute button -->
-				<md-button class="toolbar-button-custom" title="Execute"  ng-click="executeParameter()" ng-disabled="executeParameterDisabled()">
+				<md-button class="toolbar-button-custom md-raised" ng-hide="isExecuteParameterHidden()"
+						title="{{translate.load('sbi.execution.parametersselection.executionbutton.message')}}"  
+						ng-click="executeParameter()">
 					{{translate.load("sbi.execution.parametersselection.executionbutton.message")}}
 				</md-button>
 			</md-sidenav>
 		</section>
-		
 	</div>
 		
 	<script type="text/javascript">
@@ -96,12 +104,17 @@
 	angular.module('documentExecutionModule').factory('execProperties', function() {
 		var obj = {
 			roles: [<%for(Object roleObj : userRoles) out.print("'" + (String)roleObj + "',");%>],
-			executionInstance: {'OBJECT_ID' : '<%= request.getParameter("OBJECT_ID") %>', 'OBJECT_LABEL' : '<%= request.getParameter("OBJECT_LABEL") %> ',
-				'isFromCross' : false, 'isPossibleToComeBackToRolePage' : false}
+			executionInstance: {
+				'OBJECT_ID' : '<%= request.getParameter("OBJECT_ID") %>', 
+				'OBJECT_LABEL' : '<%= request.getParameter("OBJECT_LABEL") %> ',
+				'isFromCross' : false, 
+				'isPossibleToComeBackToRolePage' : false
+			}
 		};
 		return obj;
 	});
 	</script>
-	<script type="text/javascript" src="<%=urlBuilder.getResourceLink(request, "js/src/angular_1.4/tools/documentexecution/documentExecution.js")%>"></script>
+	<script type="text/javascript" 
+			src="<%=urlBuilder.getResourceLink(request, "js/src/angular_1.4/tools/documentexecution/documentExecution.js")%>"></script>
 </body>
 </html>
--- knowage/web-content/js/src/angular_1.4/tools/documentexecution/documentExecution.js
+++ knowage/web-content/js/src/angular_1.4/tools/documentexecution/documentExecution.js
@@ -22,7 +22,7 @@ function documentExecutionControllerFn(
 	$scope.roles = execProperties.roles;
 	$scope.selectedRole = "";
 	$scope.execContextId = "";
-	$scope.result="";
+	$scope.documentUrl="";
 	$scope.showSelectRoles=true;
 	$scope.translate = sbiModule_translate;
 	$scope.documentParameters = [];
@@ -31,9 +31,12 @@ function documentExecutionControllerFn(
 	$scope.initSelectedRole = function(){
 		console.log("initSelectedRole IN ");
 		if(execProperties.roles && execProperties.roles.length > 0) {
-			if(execProperties.roles.length==1){$scope.showSelectRoles=false;}			
-			$scope.selectedRole = execProperties.roles[0]
-			$scope.startExecutionProcess(execProperties.roles[0]);
+			if(execProperties.roles.length==1) {
+				$scope.showSelectRoles=false;
+			}
+			
+			$scope.selectedRole = execProperties.roles[0];
+			$scope.startExecutionProcess($scope.selectedRole);
 		}
 		console.log("initSelectedRole OUT ");
 	};
@@ -41,6 +44,7 @@ function documentExecutionControllerFn(
 	$scope.toggleParametersPanel = function(){
 		console.log('toggle ');
 		$mdSidenav('right').toggle();
+		$scope.showParametersPanel = $mdSidenav('right').isOpen();
 	};
 	
 	/*
@@ -48,14 +52,21 @@ function documentExecutionControllerFn(
 	 * Return the SBI_EXECUTION_ID code 
 	 */
 	$scope.startExecutionProcess = function(role) {
-		var params = "&OBJECT_ID="+execProperties.executionInstance.OBJECT_ID+"&OBJECT_LABEL="+execProperties.executionInstance.OBJECT_LABEL+
-			"&isFromCross="+execProperties.executionInstance.isFromCross+"&isPossibleToComeBackToRolePage="+execProperties.executionInstance.isPossibleToComeBackToRolePage+"&ROLE="+role;		
+		var params = 
+			"&OBJECT_ID=" + execProperties.executionInstance.OBJECT_ID
+			+ "&OBJECT_LABEL=" + execProperties.executionInstance.OBJECT_LABEL
+			+ "&isFromCross=" + execProperties.executionInstance.isFromCross
+			+ "&isPossibleToComeBackToRolePage=" + execProperties.executionInstance.isPossibleToComeBackToRolePage 
+			+ "&ROLE=" + role;		
 		$http.post(
-				sbiModule_config.contextName+"/servlet/AdapterHTTP?ACTION_NAME=" +
-				"START_EXECUTION_PROCESS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE"+ params
-				).success(function(data){
-					$scope.getParametersForExecutioin(role ,data.execContextId);
-				});
+				sbiModule_config.contextName + "/servlet/AdapterHTTP?ACTION_NAME=" +
+				"START_EXECUTION_PROCESS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE" + params
+		).success(function(data, status, headers, config){
+			$scope.getParametersForExecution(role, data.execContextId);
+		})
+		.error(function(data, status, headers, config) {
+			console.log("Error " + status + ": ", data);
+		});
 	};
 	
 	/*
@@ -64,74 +75,108 @@ function documentExecutionControllerFn(
 	 * exist - open parameters panel
 	 * no exist - get iframe url  
 	 */
-	$scope.getParametersForExecutioin = function(role, execContextId) {
-		var params = "&OBJECT_ID="+execProperties.executionInstance.OBJECT_ID+"&OBJECT_LABEL="+execProperties.executionInstance.OBJECT_LABEL+
-			"&isFromCross="+execProperties.executionInstance.isFromCross+"&isPossibleToComeBackToRolePage="+
-			execProperties.executionInstance.isPossibleToComeBackToRolePage+"&ROLE="+role+"&SBI_EXECUTION_ID="+execContextId;
+	$scope.getParametersForExecution = function(role, execContextId) {
+		var params = 
+			"&OBJECT_ID=" + execProperties.executionInstance.OBJECT_ID 
+			+ "&OBJECT_LABEL=" + execProperties.executionInstance.OBJECT_LABEL 
+			+ "&isFromCross=" + execProperties.executionInstance.isFromCross
+			+ "&isPossibleToComeBackToRolePage=" + execProperties.executionInstance.isPossibleToComeBackToRolePage 
+			+ "&ROLE=" + role + 
+			"&SBI_EXECUTION_ID=" + execContextId;
 		$http.post(
-				sbiModule_config.contextName+"/servlet/AdapterHTTP?ACTION_NAME=" +
-				"GET_PARAMETERS_FOR_EXECUTION_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE"+ params
-				).success(function(data){
-					//check paramitesr 
-					if(data.length>0){
-						//check default param control TODO
-						$scope.showParametersPanel=true;
-						$mdSidenav('right').toggle();
-						$scope.documentParameters = data;
-						$scope.getViewPoints(role ,execContextId); 
-					}else{
-						$scope.getURLForExecutioin(role ,execContextId,data);
-					}
-				});
+				sbiModule_config.contextName + "/servlet/AdapterHTTP?ACTION_NAME=" +
+				"GET_PARAMETERS_FOR_EXECUTION_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE" +  params
+		).success(function(data){
+
+			//check if document has paramiters 
+			if(data.length>0){
+				//check default param control TODO
+				$scope.showParametersPanel=true;
+				
+				if(!($mdSidenav('right').isOpen())) {
+//					$mdSidenav('right').toggle();
+					$mdSidenav('right').open();
+				}
+				$scope.documentParameters = data;
+				$scope.getViewPoints(role, execContextId); 
+//			}else{
+//				$scope.getURLForExecution(role ,execContextId,data);
+			}
+			$scope.getURLForExecution(role, execContextId, data);
+		});
 	};
 	
 	/*
 	 * GET VIEWPOINTS
-	 * return params saved
+	 * return saved params
 	 */
 	$scope.getViewPoints = function(role, execContextId) {
-		var params = "&OBJECT_ID="+execProperties.executionInstance.OBJECT_ID+"&OBJECT_LABEL="+execProperties.executionInstance.OBJECT_LABEL+
-			"&isFromCross="+execProperties.executionInstance.isFromCross+"&isPossibleToComeBackToRolePage="+
-			execProperties.executionInstance.isPossibleToComeBackToRolePage+"&ROLE="+role+"&SBI_EXECUTION_ID="+execContextId;
+		var params = 
+			"&OBJECT_ID=" + execProperties.executionInstance.OBJECT_I
+			+ "&OBJECT_LABEL=" + execProperties.executionInstance.OBJECT_LABEL 
+			+ "&isFromCross=" + execProperties.executionInstance.isFromCross 
+			+ "&isPossibleToComeBackToRolePage=" + execProperties.executionInstance.isPossibleToComeBackToRolePage 
+			+ "&ROLE=" + role 
+			+ "&SBI_EXECUTION_ID=" + execContextId;
+		
 		$http.post(
-				sbiModule_config.contextName+"/servlet/AdapterHTTP?ACTION_NAME=" +
-				"GET_VIEWPOINTS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE"+ params
-				).success(function(data){
-					$scope.getSubObject(role ,execContextId); //??
-					$scope.execContextId = execContextId;
-					console.log('GET VIEW POINTS return data ->', data);
-					//Handler data save
-				});
+				sbiModule_config.contextName + "/servlet/AdapterHTTP?ACTION_NAME=" +
+				"GET_VIEWPOINTS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE" +  params
+		).success(function(data){
+			$scope.getSubObject(role ,execContextId); //??
+			$scope.execContextId = execContextId;
+			console.log('GET VIEW POINTS return data ->', data);
+			//Handler data save
+		})
+		.error(function(data, status, headers, config) {
+			console.log("Error " + status + ": ", data);
+		});
 	};
 	
 	/*
 	 * GET SUBJECT OBJECT 
 	 */
 	$scope.getSubObject = function(role,execContextId) {
-		var params = "&OBJECT_ID="+execProperties.executionInstance.OBJECT_ID+"&OBJECT_LABEL="+execProperties.executionInstance.OBJECT_LABEL+
-			"&isFromCross="+execProperties.executionInstance.isFromCross+"&isPossibleToComeBackToRolePage="+
-			execProperties.executionInstance.isPossibleToComeBackToRolePage+"&ROLE="+role+"&SBI_EXECUTION_ID="+execContextId;		
+		var params =
+			"&OBJECT_ID=" + execProperties.executionInstance.OBJECT_ID
+			+ "&OBJECT_LABEL=" + execProperties.executionInstance.OBJECT_LABEL 
+			+ "&isFromCross=" + execProperties.executionInstance.isFromCross 
+			+ "&isPossibleToComeBackToRolePage=" + execProperties.executionInstance.isPossibleToComeBackToRolePage 
+			+ "&ROLE=" + role + "&SBI_EXECUTION_ID=" 
+			+ execContextId;
+		
 		$http.post(
-				sbiModule_config.contextName+"/servlet/AdapterHTTP?ACTION_NAME=" +
-				"GET_SUBOBJECTS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE"+ params
-				).success(function(data){
-					$scope.getSnapShots(role ,execContextId);
-				});
+				sbiModule_config.contextName + "/servlet/AdapterHTTP?ACTION_NAME="  + 
+				"GET_SUBOBJECTS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE" +  params
+		).success(function(data){
+			$scope.getSnapShots(role ,execContextId);
+		})
+		.error(function(data, status, headers, config) {
+			console.log("Error " + status + ": ", data);
+		});
 	};
 	
 	/*
 	 * GET SNAP SHOTS
 	 */
-	$scope.getSnapShots = function(role,execContextId) {
-		var params = "&OBJECT_ID="+execProperties.executionInstance.OBJECT_ID+"&OBJECT_LABEL="+execProperties.executionInstance.OBJECT_LABEL+
-			"&isFromCross="+execProperties.executionInstance.isFromCross+"&isPossibleToComeBackToRolePage="
-			+execProperties.executionInstance.isPossibleToComeBackToRolePage+"&ROLE="+role+"&SBI_EXECUTION_ID="+execContextId;	
+	$scope.getSnapShots = function(role, execContextId) {
+		var params = 
+			"&OBJECT_ID=" + execProperties.executionInstance.OBJECT_ID
+			+ "&OBJECT_LABEL=" + execProperties.executionInstance.OBJECT_LABEL 
+			+ "&isFromCross=" + execProperties.executionInstance.isFromCross
+			+ "&isPossibleToComeBackToRolePage=" + execProperties.executionInstance.isPossibleToComeBackToRolePage 
+			+ "&ROLE=" + role 
+			+ "&SBI_EXECUTION_ID=" + execContextId;
+		
 		$http.post(
-				sbiModule_config.contextName+"/servlet/AdapterHTTP?ACTION_NAME=" +
+				sbiModule_config.contextName + "/servlet/AdapterHTTP?ACTION_NAME=" +
 				"GET_SNAPSHOTS_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE"+ params
-				).success(function(data){
-					
-				});
+		).success(function(data){
+
+		})
+		.error(function(data, status, headers, config) {
+			console.log("Error " + status + ": ", data);
+		});
 	};
 	
 	/*
@@ -141,73 +186,124 @@ function documentExecutionControllerFn(
 	$scope.executeParameter = function(){
 		var jsonData =  {};
 		console.log("$scope.documentParameters -> ", $scope.documentParameters);
-		if($scope.documentParameters.length>0){
-			for(var i =0;i<=$scope.documentParameters.length-1;i++ ){
-				jsonData[$scope.documentParameters[i].id] = $scope.documentParameters[i].parameterToExecute;
-				jsonData[$scope.documentParameters[i].id+"_field_visible_description"] = $scope.documentParameters[i].parameterToExecute;
+		if($scope.documentParameters.length > 0){
+			for(var i = 0; i < $scope.documentParameters.length; i++ ){
+				jsonData[$scope.documentParameters[i].id] = $scope.documentParameters[i].parameterValue;
+				
+				jsonData[$scope.documentParameters[i].id + "_field_visible_description"] = 
+					$scope.documentParameters[i].parameterValue;
 			}
 		}
-		$scope.getURLForExecutioin($scope.selectedRole,  $scope.execContextId, jsonData)
-		$mdSidenav('right').toggle();
+		
+		$scope.getURLForExecution($scope.selectedRole, $scope.execContextId, jsonData);
+		if($mdSidenav('right').isOpen()) {
+			$mdSidenav('right').close();
+		}
+//		$mdSidenav('right').toggle();
 	}
 	
 	//chiedere a Benedetto
-	$scope.executeParameterDisabled = function(){
-		var disabled = false;
-		if($scope.documentParameters.length>0){
-			for(var i =0;i<=$scope.documentParameters.length-1;i++ ){
-				if($scope.documentParameters[i].mandatory && (typeof $scope.documentParameters[i].parameterToExecute === 'undefined') ){
-					disabled=true;
+	$scope.isExecuteParameterHidden = function(){
+//		var disabled = false;
+		if($scope.documentParameters.length > 0){
+			for(var i = 0; i < $scope.documentParameters.length; i++ ){
+				if($scope.documentParameters[i].mandatory 
+						&& (typeof $scope.documentParameters[i].parameterValue === 'undefined') ){
+					
+//					disabled = true;
+					return true;
 				}
 			}
 		}
-		return disabled;
+		
+		return false;
+//		return disabled;
 	}
 	
 	/*
 	 * GET URL DOCUMENT 
 	 * Load iframe URL
 	 */
-	$scope.getURLForExecutioin = function(role, execContextId, parameters) {
-		    var paramStr = (Object.keys(parameters).length >0) ? JSON.stringify(parameters) : '{}'; 
-		    var obj = new Object();
-		    obj.OBJECT_ID=execProperties.executionInstance.OBJECT_ID;
-			obj.OBJECT_LABEL=execProperties.executionInstance.OBJECT_LABEL;
-			obj.isFromCross=execProperties.executionInstance.isFromCross;
-			obj.isPossibleToComeBackToRolePage=execProperties.executionInstance.isPossibleToComeBackToRolePage;
-			obj.SBI_EXECUTION_ID=execContextId;
-			obj.PARAMETERS=paramStr;
-			obj.ROLE=role;			
-			$http({
-			    method: 'POST',
-			    url: sbiModule_config.contextName+"/servlet/AdapterHTTP?ACTION_NAME=" +
+	$scope.getURLForExecution = function(role, execContextId, parameters) {
+		var paramStr = (Object.keys(parameters).length > 0) ? 
+				JSON.stringify(parameters) : '{}'; 
+		var obj = new Object();
+		
+		obj.OBJECT_ID = execProperties.executionInstance.OBJECT_ID;
+		obj.OBJECT_LABEL = execProperties.executionInstance.OBJECT_LABEL;
+		obj.isFromCross = execProperties.executionInstance.isFromCross;
+		obj.isPossibleToComeBackToRolePage = execProperties.executionInstance.isPossibleToComeBackToRolePage;
+		obj.SBI_EXECUTION_ID = execContextId;
+		obj.PARAMETERS = paramStr;
+		obj.ROLE = role;
+		
+		$http({
+			method: 'POST',
+			url: sbiModule_config.contextName + "/servlet/AdapterHTTP?ACTION_NAME=" +
 				"GET_URL_FOR_EXECUTION_ACTION&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=TRUE",
-				transformRequest: function(obj) {
-			        var str = [];
-			        for(var p in obj){
-			        	str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-			        }
-			        return str.join("&");
-			    },
-			    data: obj,
-			    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-			}).success(function(data){
-				console.log(data);
-				if ('errors' in data){
-					if(data['errors'].length > 0 ){
-						var strErros='';
-						for(var i=0; i<=data['errors'].length-1;i++){
-							strErros=strErros + data['errors'][i] + ' \n';
-						}
-						alert(strErros);
-					}
-				}else{
-					$scope.result=data.url;
+			
+			transformRequest: function(obj) {
+				var str = [];
+				for(var p in obj){
+					str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
 				}
-			});
-	};	
-	$scope.changeRole = function(role){
-		$scope.startExecutionProcess(role);
+				return str.join("&");
+			},
+			data: obj,
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+		})
+		.success(function(data){
+			console.log(data);
+			if ('errors' in data){
+				if(data['errors'].length > 0 ){
+					var strErros='';
+					for(var i=0; i<=data['errors'].length-1;i++){
+						strErros=strErros + data['errors'][i] + ' \n';
+					}
+					alert(strErros);
+				}
+			}else{
+				$scope.documentUrl=data.url;
+			}
+		})
+		.error(function(data, status, headers, config) {
+			console.log("Error " + status + ": ", data);
+		});
 	};
+	
+	$scope.changeRole = function(role){
+		// $scope.selectedRole is overwritten by the ng-model attribute
+		
+		console.log("changeRole IN ");
+		//If new selected role is different from the previous one
+		if(role != $scope.selectedRole) { 
+			$scope.startExecutionProcess(role);
+		}
+		console.log("changeRole OUT ");
+	};
+	
+	$scope.canExecuteDocument = function(){
+	};
+	
+	$scope.isParameterPanelDisabled = function(){
+		return (!$scope.documentParameters || $scope.documentParameters.length == 0);
+	};
+	
+	
+	
+	$scope.executeDocument = function(){
+		console.log('Executing document -> ', execProperties);
+	};
+	
+	$scope.editDocument = function(){
+		alert('Editing document');
+		console.log('Editing document -> ', execProperties);
+	};
+	
+	$scope.deleteDocument = function(){
+		alert('Deleting document');
+		console.log('Deleting document -> ', execProperties);
+	};
+	
 	console.log("documentExecutionControllerFn OUT ");
 };
