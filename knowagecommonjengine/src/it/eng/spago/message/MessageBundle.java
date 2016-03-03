2aaa81a984146cac0f0cccc6bb9832d95fc32043
Alessandro Piovani <alessandro.piovani@eng.it>
2016-03-03 17:52:59 +0000

    Knowage License and Copyright comment header added

--- knowagecommonjengine/src/it/eng/spago/message/MessageBundle.java
+++ knowagecommonjengine/src/it/eng/spago/message/MessageBundle.java
@@ -1,24 +1,20 @@
-/**
-
- Copyright 2004, 2007 Engineering Ingegneria Informatica S.p.A.
-
- This file is part of Spago.
-
- Spago is free software; you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation; either version 2.1 of the License, or
- any later version.
-
- Spago is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with Spago; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
- **/
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ * 
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.eng.spago.message;
 
 import it.eng.spago.base.Constants;
