4adcb89ab1f2207ab7857432f8930fabd13f40af
Chiara Capobianco <chiara.capobianco@eng.it>
2016-03-04 08:58:39 +0000

    fixing bug after license

--- knowagedao/src/it/eng/spagobi/commons/dao/IExecuteOnTransaction.java
+++ knowagedao/src/it/eng/spagobi/commons/dao/IExecuteOnTransaction.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,15 +11,14 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.commons.dao;
 
 import org.hibernate.Session;
-import org.json.JSONException;
 
 public interface IExecuteOnTransaction<T> {
-	public T execute(Session session) throws JSONException;
+	public T execute(Session session) throws Exception;
 }
