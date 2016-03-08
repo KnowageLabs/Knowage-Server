216169d79b5b9fb1809220724e9fd13efa175b93
Alessandro Piovani <alessandro.piovani@eng.it>
2016-03-08 12:03:27 +0000

    Knowage License and Copyright comment header added again

--- knowagewhatifengine/WebContent/WEB-INF/jsp/commons/angular/angularImport.jsp
+++ knowagewhatifengine/WebContent/WEB-INF/jsp/commons/angular/angularImport.jsp
@@ -1,7 +1,19 @@
-<%-- 
-	This files must be use in every 
-	SpagoBI${pageContext.request.contextPath}/Knowage page that 
-	makes use of AngularJS  
+<%--
+Knowage, Open Source Business Intelligence suite
+Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ 
+Knowage is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Knowage is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+ 
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --%>
 
 <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" />
--- knowagewhatifengine/src/mondrian/olap4j/MondrianOlap4jMember.java
+++ knowagewhatifengine/src/mondrian/olap4j/MondrianOlap4jMember.java
@@ -1,11 +1,19 @@
 /*
-// This software is subject to the terms of the Eclipse Public License v1.0
-// Agreement, available at the following URL:
-// http://www.eclipse.org/legal/epl-v10.html.
-// You must accept the terms of that agreement to use this software.
-//
-// Copyright (C) 2007-2012 Pentaho
-// All Rights Reserved.
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ * 
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package mondrian.olap4j;
 
