2b0a20279a749b117f86ec7624c2283f15320b5b
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-03-18 13:24:58 +0000

    fix

--- knowagewhatifengine/src/it/eng/spagobi/engines/whatif/calculatedmember/MDXFormulaHandler.java
+++ knowagewhatifengine/src/it/eng/spagobi/engines/whatif/calculatedmember/MDXFormulaHandler.java
@@ -1,9 +1,25 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.eng.spagobi.engines.whatif.calculatedmember;
 
 import it.eng.spagobi.engines.whatif.WhatIfEngineConfig;
 
 import java.io.File;
-import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -11,17 +27,31 @@ import javax.xml.bind.Unmarshaller;
 
 public class MDXFormulaHandler {
 
-	private String xmlPath;
+	private static String xmlPath = WhatIfEngineConfig.getInstance().getEngineResourcePath() + "Olap/formulas.xml";
+	private static File xmlFile;
 
-	public static List<MDXFormula> getFormulas() throws JAXBException {
+	public MDXFormulaHandler() {
 
-		String b = WhatIfEngineConfig.getInstance().getEngineResourcePath();
-		File file = new File(b + "Olap/formulas.xml");
-		JAXBContext jc = JAXBContext.newInstance(MDXFormulas.class);
-		Unmarshaller unmarshaller = jc.createUnmarshaller();
-		MDXFormulas formulas = (MDXFormulas) unmarshaller.unmarshal(file);
+	}
 
-		return formulas.getFormulas();
+	private static boolean loadFile() {
+
+		xmlFile = new File(xmlPath);
+
+		return xmlFile.exists();
+
+	}
+
+	public static MDXFormulas getFormulas() throws JAXBException {
+		MDXFormulas formulas = new MDXFormulas();
+
+		if (loadFile()) {
+			JAXBContext jc = JAXBContext.newInstance(MDXFormulas.class);
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			formulas = (MDXFormulas) unmarshaller.unmarshal(xmlFile);
+		}
+
+		return formulas;
 	};
 
 }
