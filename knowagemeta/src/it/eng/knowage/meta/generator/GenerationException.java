480bbf691496f96e7073b975e96e70e337584c49
Antonella Giachino <antonella.giachino@eng.it>
2016-03-24 13:25:06 +0000

    Changed license references

--- knowagemeta/src/it/eng/knowage/meta/generator/GenerationException.java
+++ knowagemeta/src/it/eng/knowage/meta/generator/GenerationException.java
@@ -1,16 +1,24 @@
-/**
- SpagoBI, the Open Source Business Intelligence suite
-
- Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- This Source Code Form is subject to the terms of the Mozilla Public
- License, v. 2.0. If a copy of the MPL was not distributed with this file,
- You can obtain one at http://mozilla.org/MPL/2.0/.
- 
-**/
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.eng.knowage.meta.generator;
 
 /**
-* @author Andrea Gioia (andrea.gioia@eng.it)
+ * @author Andrea Gioia (andrea.gioia@eng.it)
  *
  */
 public class GenerationException extends RuntimeException {
@@ -20,7 +28,7 @@ public class GenerationException extends RuntimeException {
 	public GenerationException(String msg) {
 		super(msg);
 	}
-	
+
 	public GenerationException(String msg, Throwable t) {
 		super(msg, t);
 	}
