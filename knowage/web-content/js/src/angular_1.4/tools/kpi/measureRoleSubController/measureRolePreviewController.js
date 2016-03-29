ce2ecf7f91274675f52a4147e93552ccec13902e
Gianluca Ulivo <gianluca.ulivo@eng..it>
2016-03-29 14:49:27 +0000

    rename module name

--- knowage/web-content/js/src/angular_1.4/tools/kpi/measureRoleSubController/measureRolePreviewController.js
+++
@@ -1,5 +0,0 @@
-angular.module('measureRoleManager').controller('measureRolePreviewController', [ '$scope','sbiModule_translate' ,'sbiModule_restServices',measureRolePreviewControllerFunction ]);
-
-function measureRolePreviewControllerFunction($scope,sbiModule_translate,sbiModule_restServices){
-	
-}
