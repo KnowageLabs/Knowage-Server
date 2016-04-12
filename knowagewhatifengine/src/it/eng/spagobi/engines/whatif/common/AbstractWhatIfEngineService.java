a8453fed3788d1d177f3d21fba83d474ae7ea768
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-04-12 06:54:30 +0000

    fix

--- knowagewhatifengine/src/it/eng/spagobi/engines/whatif/common/AbstractWhatIfEngineService.java
+++ knowagewhatifengine/src/it/eng/spagobi/engines/whatif/common/AbstractWhatIfEngineService.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -39,6 +39,7 @@ import javax.ws.rs.core.Context;
 import org.apache.log4j.Logger;
 import org.olap4j.metadata.Member;
 import org.pivot4j.PivotModel;
+import org.pivot4j.sort.SortCriteria;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
@@ -69,9 +70,12 @@ public class AbstractWhatIfEngineService extends AbstractEngineRestService {
 			// adds the calculated fields before rendering the model
 			sbiModel.applyCal();
 			serializedModel = serialize(sbiModel);
+			SortCriteria sortCriteria = model.getSortCriteria();
 
 			// restore the query without calculated fields
 			sbiModel.restoreQuery();
+			model.setSortCriteria(sortCriteria);
+
 		} catch (SerializationException e) {
 			logger.error("Error serializing the pivot", e);
 			throw new SpagoBIEngineRuntimeException("Error serializing the pivot", e);
