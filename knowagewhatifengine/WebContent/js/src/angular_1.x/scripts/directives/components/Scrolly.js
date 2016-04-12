19c4b9f71b6eeef99654bdb12c1fd8233f176478
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-04-12 06:59:58 +0000

    fix

--- knowagewhatifengine/WebContent/js/src/angular_1.x/scripts/directives/components/Scrolly.js
+++ knowagewhatifengine/WebContent/js/src/angular_1.x/scripts/directives/components/Scrolly.js
@@ -1,24 +1,74 @@
 angular.module('scrolly_directive',[])
-	.directive('scrolly', function () {
+	.directive('scrolly',['$window', function ($window) {
 	    return {
 	        restrict: 'A',
 	        link: function (scope, element, attrs) {
 	        	var ready = false;
 	            var raw = element[0];
 	            console.log('loading directive');
+	            scope.tableHeight = raw.offsetHeight;
+	            console.log(scope.tableHeight);
+	            scope.scrollTo = function(posX,posY) {
+		    		console.log(scope.modelConfig);
+	        		raw.scrollTop = posX*100;
+	        		raw.scrollLeft = posY*100;
+	        	 
+	        	};
+	        	
+	        	scope.resize = function(){
+	        		 scope.tableHeight = raw.offsetHeight;
+	            	 var corner = document.getElementsByClassName("x-pivot-corner")[0];
+	            	console.log(corner+' corner');
+	            	if(corner){
+	            		 scope.modelConfig.rowsSet = Math.round(raw.offsetHeight/43)-Math.round(corner.offsetHeight/43)-1;
+	            		 console.log(corner.offsetHeight/43);
+	            	}else{
+	            		scope.modelConfig.rowsSet =Math.round(raw.offsetHeight/43)-2;
+	            	}
+	            	
+	            	 console.log(scope.modelConfig.rowsSet);
+	            	 if(scope.modelConfig.rowsSet<1){
+	            		 scope.modelConfig.rowsSet = 1;
+	            	 }
+	            	 scope.sendModelConfig(scope.modelConfig);
+	        	}
+	           
+	        	
+	               
+	            angular.element($window).bind('resize', function(){
+	                
+	            	scope.resize();
+	              });
+	                
+	               
+	            
 	                
 	            element.bind('scroll', function () {
 	                
 	                console.log(raw.scrollTop+"gore");
 	                console.log(raw.scrollLeft+"levo");
-	    scope.modelConfig.startRow = Math.round(raw.scrollTop/100);
-	    scope.modelConfig.startColumn = Math.round(raw.scrollLeft/100);
-	    scope.$apply();
-	    console.log(scope.modelConfig);
-	    scope.sendModelConfig(scope.modelConfig);
 	                
+	                var startRow = Math.round((raw.scrollTop)/100);
+	                var startColumn =  Math.round(raw.scrollLeft/100);
+	               if(scope.modelConfig.startRow!=startRow){
+	            	   scope.modelConfig.startRow = startRow;
+	            	  
+	           	    	scope.$apply();
+	           	    
+	           	    scope.sendModelConfig(scope.modelConfig);
+	               }
+	               if(scope.modelConfig.startColumn!=startColumn){
+	            	
+	            	   scope.modelConfig.startColumn = startColumn;
+	           	    	scope.$apply();
+	           	    
+	           	    scope.sendModelConfig(scope.modelConfig);
+	               }
+	   
+	    
+	    
 	               
 	            });
 	        }
 	    };
-	});
+	}]);
