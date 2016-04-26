a1dfd889e265b6ff7f1d036a41da285ed2152b2f
Salvatore Lupo <salvatore.lupo@eng.it>
2016-04-26 11:31:58 +0000

    newKpi Alert

--- knowage/src/it/eng/spagobi/tools/alert/AlertService.java
+++ knowage/src/it/eng/spagobi/tools/alert/AlertService.java
@@ -1,8 +1,9 @@
 package it.eng.spagobi.tools.alert;
 
-import it.eng.spagobi.kpi.bo.AlertListener;
 import it.eng.spagobi.services.rest.annotations.ManageAuthorization;
 import it.eng.spagobi.services.serialization.JsonConverter;
+import it.eng.spagobi.tools.alert.bo.AlertAction;
+import it.eng.spagobi.tools.alert.bo.AlertListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +23,22 @@ import javax.ws.rs.core.Response;
 public class AlertService {
 
 	static final List<AlertListener> mockListeners = new ArrayList<>();
+	static final List<AlertAction> mockActions = new ArrayList<>();
 	{
 		mockListeners.clear();
 		AlertListener l = new AlertListener();
 		l.setId(1);
-		l.setName("kpi listener");
-		l.setClassName("it.eng.spagobi.kpi.alert.KpiListener");
+		l.setName("KPI Listener");
+		l.setClassName("it.eng.spagobi.tools.alert.listener.KpiListener");
 		l.setTemplate("/restful-services/publish?PUBLISHER=/WEB-INF/jsp/tools/alert/listeners/kpiListener.jsp");
 		mockListeners.add(l);
+
+		mockActions.clear();
+		AlertAction a = new AlertAction();
+		a.setName("Send mail");
+		a.setClassName("it.eng.spagobi.tools.alert.action.SendMail");
+		a.setTemplate("/restful-services/publish?PUBLISHER=/WEB-INF/jsp/tools/alert/actions/sendMail.jsp");
+		mockActions.add(a);
 	}
 
 	@GET
@@ -41,10 +50,26 @@ public class AlertService {
 	}
 
 	@GET
-	@Path("/{idListener}/loadTemplate")
-	public Response loadTemplate(@PathParam("idListener") Integer idListener) {
+	@Path("/listAction")
+	public Response listAction() {
+		// TODO call dao service
+		List<AlertAction> actions = mockActions;
+		return Response.ok(JsonConverter.objectToJson(actions, actions.getClass())).build();
+	}
+
+	@GET
+	@Path("/{idListener}/loadListenerTemplate")
+	public Response loadListenerTemplate(@PathParam("idListener") Integer idListener) {
 		// TODO call dao service
 		int i = mockListeners.indexOf(new AlertListener(idListener));
+		return Response.ok(JsonConverter.objectToJson(mockListeners.get(i), AlertListener.class)).build();
+	}
+
+	@GET
+	@Path("/{idAction}/loadActionTemplate")
+	public Response loadActionTemplate(@PathParam("idListener") Integer idListener) {
+		// TODO call dao service
+		int i = mockActions.indexOf(new AlertAction(idListener));
 		return Response.ok(JsonConverter.objectToJson(mockListeners.get(i), AlertListener.class)).build();
 	}
 
