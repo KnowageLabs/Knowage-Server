302dfab6f99af3dfc9f594c1a96de5336fbd6ae5
anddamic <anddamic@eng.it>
2016-05-17 09:32:32 +0000

    fix semaphore visualization for IE

--- knowage/web-content/js/src/angular_1.4/tools/kpi/directive/kpiSemaphoreIndicator/kpiSemaphoreIndicator.js
+++ knowage/web-content/js/src/angular_1.4/tools/kpi/directive/kpiSemaphoreIndicator/kpiSemaphoreIndicator.js
@@ -3,8 +3,8 @@ angular.module('kpi_semaphore_indicator', ['ngMaterial'])
 	return {
 		template:'<span layout="row"  layout-align="center center">'+
 				'<span class="fa-stack">'+
-					'<i class="fa  fa-stack-1x scorecardSemaphoreBackIcon" ng-class="getClass(indicatorColor,true)" style="color : black"></i>'+
-					'<i class="fa  fa-stack-1x scorecardSemaphoreFrontIcon" ng-class="getClass(indicatorColor,false)" style="color : {{indicatorColor || \'GRAY\'}}"></i>'+
+					'<i class="fa  fa-stack-1x scorecardSemaphoreBackIcon" ng-class="getClass(indicatorColor,true)" ng-style="{color : black}"></i>'+
+					'<i class="fa  fa-stack-1x scorecardSemaphoreFrontIcon" ng-class="getClass(indicatorColor,false)" ng-style="{color : indicatorColor || \'GRAY\'}"></i>'+
 				'</span>'+
 				'<span>{{indicatorValue}}</span>'+
 			'</span>',
