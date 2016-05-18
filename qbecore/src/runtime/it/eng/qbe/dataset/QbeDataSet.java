a6ed0968758bf1a739a47a1afd7add5126c65e4f
Antonella Giachino <antonella.giachino@eng.it>
2016-05-18 10:45:52 +0000

    Managed relations between qbe dataset and business class

--- qbecore/src/runtime/it/eng/qbe/dataset/QbeDataSet.java
+++ qbecore/src/runtime/it/eng/qbe/dataset/QbeDataSet.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -332,7 +332,7 @@ public class QbeDataSet extends ConfigurableDataSet {
 
 	/**
 	 * Get the driver name (hibernate or jpa). It checks if the passed jar file contains the persistence.xml in the META-INF folder
-	 * 
+	 *
 	 * @param jarFile
 	 *            a jar file with the model definition
 	 * @return jpa if the persistence provder is JPA o hibernate otherwise
@@ -455,6 +455,11 @@ public class QbeDataSet extends ConfigurableDataSet {
 	@Override
 	public String getDsType() {
 		return DS_TYPE;
+	}
+
+	public it.eng.qbe.datasource.IDataSource getQbeDataSourceFromStmt() {
+		init();
+		return ((AbstractQbeDataSet) ds).getStatement().getDataSource();
 	}
 
 }
