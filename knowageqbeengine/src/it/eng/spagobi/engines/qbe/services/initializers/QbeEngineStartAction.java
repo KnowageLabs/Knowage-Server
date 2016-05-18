142a597c8e08d22a116fda5313f0fe9f2d8e78a7
Alessandro Portosa <alessandro.portosa@eng.it>
2016-05-18 12:02:16 +0000

    Profiling the Registry QBE document with userfunctionality RegistryDataEntry

--- knowageqbeengine/src/it/eng/spagobi/engines/qbe/services/initializers/QbeEngineStartAction.java
+++ knowageqbeengine/src/it/eng/spagobi/engines/qbe/services/initializers/QbeEngineStartAction.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,13 +11,14 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.engines.qbe.services.initializers;
 
 import it.eng.spago.base.SourceBean;
+import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.engines.qbe.QbeEngine;
 import it.eng.spagobi.engines.qbe.QbeEngineAnalysisState;
 import it.eng.spagobi.engines.qbe.QbeEngineInstance;
@@ -30,6 +31,7 @@ import it.eng.spagobi.utilities.assertion.Assert;
 import it.eng.spagobi.utilities.engines.AbstractEngineStartAction;
 import it.eng.spagobi.utilities.engines.EngineConstants;
 import it.eng.spagobi.utilities.engines.SpagoBIEngineStartupException;
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 import java.util.Locale;
 import java.util.Map;
@@ -117,6 +119,9 @@ public class QbeEngineStartAction extends AbstractEngineStartAction {
 			// CHECKS WHETHER IF IT IS A QBE DOCUMENT OR REGISTRY, BY LOOKING AT THE TEMPLATE
 			RegistryConfiguration registryConf = qbeEngineInstance.getRegistryConfiguration();
 			if (registryConf != null) {
+				if (!getUserProfile().isAbleToExecuteAction(SpagoBIConstants.REGISTRY_DATA_ENTRY)) {
+					throw new SpagoBIRuntimeException("It is not allowed to use the Registry document.");
+				}
 				logger.debug("Registry document");
 				getServiceResponse().setAttribute("DOCTYPE", "REGISTRY");
 				Assert.assertNotNull(registryConf, "Registry configuration not found, check document's template");
@@ -219,10 +224,10 @@ public class QbeEngineStartAction extends AbstractEngineStartAction {
 
 	@Override
 	public Map getEnv() {
-		
+
 		IDataSource dataSource = this.getDataSource();
 		Map env = super.getEnv();
-		
+
 		if (dataSource == null || dataSource.checkIsReadOnly()) {
 			logger.debug("Getting datasource for writing, since the datasource is not defined or it is read-only");
 			IDataSource datasourceForWriting = this.getDataSourceForWriting();
@@ -233,5 +238,4 @@ public class QbeEngineStartAction extends AbstractEngineStartAction {
 
 		return env;
 	}
-
 }
