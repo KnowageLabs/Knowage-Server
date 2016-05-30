733e4b1d52a8451d9683790a82c009c31f42f028
Davide Zerbetto <davide.zerbetto@eng.it>
2016-05-30 14:02:23 +0000

    Hiding capability to start SmartFilter designer from a Qbe document, that is deprecated

--- knowageqbeengine/WebContent/js/spagobi/formtemplate/DocumentTemplateBuilder.js
+++ knowageqbeengine/WebContent/js/spagobi/formtemplate/DocumentTemplateBuilder.js
@@ -56,24 +56,6 @@ Sbi.formtemplate.DocumentTemplateBuilder = function(config) {
 				isAbsolute: false,
 				baseUrl:{restServicesPath: 'restful-services'}});
 	
-	this.services['getDocuments'] = this.services['getDocuments'] || Sbi.config.remoteServiceRegistry.getServiceUrl({
-		serviceName: 'FILTER_FOLDER_CONTENT_ACTION'
-		, baseParams: {
-			typeFilter: 'EQUALS_TO'
-			, valueFilter: 'DATAMART'
-			, columnFilter: 'TYPE'
-		}
-	});
-	
-	this.services['execFormBuilder'] = this.services['execFormBuilder'] || Sbi.config.serviceRegistry.getServiceUrl({
-		serviceName: 'FORM_ENGINE_START_ACTION'
-		, baseParams: {
-			MODALITY: 'EDIT'
-			, NEW_SESSION: 'TRUE'
-			, formDocumentId: this.formDocumentId
-		}
-	});
-	
 	this.services['execFormBuilderDataset'] = this.services['execFormBuilderDataset'] || Sbi.config.serviceRegistry.getServiceUrl({
 		serviceName: 'FORM_ENGINE_FROM_DATASET_START_ACTION'
 		, baseParams: {
@@ -89,7 +71,7 @@ Sbi.formtemplate.DocumentTemplateBuilder = function(config) {
 	Ext.apply(c, {
 		closable: false
 		, border: false
-		, items: [this.infoPanel, this.selectStart]
+		, items: [this.selectStart]
 	});
 	
 	// constructor
@@ -105,8 +87,6 @@ Sbi.formtemplate.DocumentTemplateBuilder = function(config) {
 Ext.extend(Sbi.formtemplate.DocumentTemplateBuilder, Ext.Panel, {
     
 	services: null
-	, infoPanel: null
-	, datamartSelectPage: null
 	, datasetSelectPage: null
 	, selectStart: null
 	
@@ -121,110 +101,15 @@ Ext.extend(Sbi.formtemplate.DocumentTemplateBuilder, Ext.Panel, {
 	// --------------------------------------------------------------------------------
 	
 	, init: function() {
-		this.initInfoPanel();
-		this.initDatamartSelectPage();
 		this.initDatasetSelectPage();
 		this.selectStart = new Ext.Panel({
 			bodyStyle: 'padding:10px',
 			layout: 'form',
 			border: false,
-			items: [this.datamartSelectPage, this.datasetSelectPage]
+			items: [this.datasetSelectPage]
 		});
 	}
 
-	,
-	initInfoPanel : function () {
-		this.infoPanel = new Ext.Panel({
-			html: '<div class="info">' + LN('sbi.formtemplate.documenttemplatebuilder.selectsourceinfo.msg') + '</div>',
-			border: false
-		});
-	}
-
-	, initDatamartSelectPage: function() {
-		
-		this.proxy = new Ext.data.ScriptTagProxy({
-	        url: this.services['getDocuments'],
-	        method: 'GET'
-	    });
-		
-		this.proxy.onRead = function(action, trans, res) {
-	        var result;
-	        try {
-	        	var t, fc = res.folderContent; 
-	        	for (var i = 0; i < fc.length; i++) {
-	        		if(fc[i].title === 'Documents') {
-	        			t = fc[i];
-	        			break;
-	        		}
-	        	}
-	        	t = t.samples;	        
-	        	result = trans.reader.readRecords(t);
-	        }catch(e){
-	            // @deprecated: fire loadexception
-	            this.fireEvent("loadexception", this, trans, res, e);
-
-	            this.fireEvent('exception', this, 'response', action, trans, res, e);
-	            trans.callback.call(trans.scope||window, null, trans.arg, false);
-	            return;
-	        }
-	        if (result.success === false) {
-	            // @deprecated: fire old loadexception for backwards-compat.
-	            this.fireEvent('loadexception', this, trans, res);
-
-	            this.fireEvent('exception', this, 'remote', action, trans, res, null);
-	        } else {
-	            this.fireEvent("load", this, res, trans.arg);
-	        }
-	        trans.callback.call(trans.scope||window, result, trans.arg, result.success);
-	    };
-
-	    var store = new Ext.data.Store({
-	        autoLoad: false,
-	        proxy: this.proxy,
-		    reader: new Ext.data.JsonReader({id: 'id'}, [
-	            {name:'id'},
-	            {name:'name'},
-	            {name:'description'}
-		    ])
-		});
-	  
-	    this.documentsCombo = new Ext.form.ComboBox({
-    	   	tpl: '<tpl for="."><div ext:qtip="{name}: {description}" class="x-combo-list-item">{name}</div></tpl>',	
-    	   	editable  : false,
-    	   	fieldLabel : LN('sbi.formtemplate.documenttemplatebuilder.documentfield.label'),
-    	   	forceSelection : true,
-    	   	//mode : 'local',
-    	   	name : 'scope',
-    	   	store : store,
-    	   	displayField:'name',
-    	    valueField:'id',
-    	    emptyText: LN('sbi.formtemplate.documenttemplatebuilder.documentfield.emptytext'),
-    	    typeAhead: true,
-    	    triggerAction: 'all',
-    	    selectOnFocus:true
-    	});
-		
-		this.submitBtn = new Ext.Button({
-			text: LN('sbi.formtemplate.documenttemplatebuilder.startwithdocument'),
-			disabled: false,
-	        hidden: false,
-	        handler: function() {			
-				var docId = this.documentsCombo.getValue();
-				this.openFormBuilder(docId);
-			}, 
-			scope: this
-	    });
-		
-		this.datamartSelectPage = new Ext.Panel({
-			bodyStyle: 'padding:10px;margin: 5px;',
-			layout: 'form',
-			items: [this.documentsCombo, this.submitBtn]
-		});	
-		
-
-		
-		//alert('alert');
-	}
 	, initDatasetSelectPage: function() {
 
 		var storeDS = new Ext.data.Store({
@@ -277,23 +162,6 @@ Ext.extend(Sbi.formtemplate.DocumentTemplateBuilder, Ext.Panel, {
 			items: [this.datasetCombo, this.submitDSBtn]
 		});		
 
-	}
-	
-	
-	, openFormBuilder: function(docId) {
-		var form = document.getElementById('submit-form');
-		if(!form) {
-			var dh = Ext.DomHelper;
-			form = dh.append(Ext.getBody(), {
-			    id: 'submit-form'
-			    , tag: 'form'
-			    , method: 'post'
-			    , cls: 'submit-form'
-			});
-		}
-		
-		form.action = this.services['execFormBuilder'] + '&document=' + docId;
-		form.submit();
 	}
 	
 	, openFormBuilderDS: function(datasetLabel) {
