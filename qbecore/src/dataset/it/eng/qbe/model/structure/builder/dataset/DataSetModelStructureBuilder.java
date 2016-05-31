b375121e663a0a816a14d3d7869c7997e5de7661
Nikola Simovic <nikola.simovic@eng.it>
2016-05-31 15:54:18 +0000

    get dataset type, use alias instead of name for qbe dataset type

--- qbecore/src/dataset/it/eng/qbe/model/structure/builder/dataset/DataSetModelStructureBuilder.java
+++ qbecore/src/dataset/it/eng/qbe/model/structure/builder/dataset/DataSetModelStructureBuilder.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -44,179 +44,178 @@ public class DataSetModelStructureBuilder extends AbstractModelStructureBuilder 
 	private static final String FIELD_TYPE_PROPERTY = "type";
 	private static transient Logger logger = Logger.getLogger(DataSetModelStructureBuilder.class);
 
-
 	IModelStructurePropertiesInitializer propertiesInitializer;
 
 	public DataSetModelStructureBuilder(DataSetDataSource dataSource) {
-		if(dataSource == null) {
+		if (dataSource == null) {
 			throw new IllegalArgumentException("DataSource parameter cannot be null");
 		}
-		setDataSource( dataSource );
+		setDataSource(dataSource);
 		propertiesInitializer = new SimpleModelStructurePropertiesInitializer(dataSource);
 	}
-	
 
+	@Override
 	public IModelStructure build() {
 		ModelStructure modelStructure;
 		String modelName;
-		
+
 		logger.debug("IN");
-		
+
 		try {
 			modelStructure = new ModelStructure();
-	
+
 			modelName = getDataSource().getConfiguration().getModelName();
-			Assert.assertNotNull(getDataSource(), "datasource cannot be null");	
-			
-			propertiesInitializer.addProperties(modelStructure);	
+			Assert.assertNotNull(getDataSource(), "datasource cannot be null");
+
+			propertiesInitializer.addProperties(modelStructure);
 			Map calculatedFields = getDataSource().getConfiguration().loadCalculatedFields();
-			if(calculatedFields!=null){
+			if (calculatedFields != null) {
 				modelStructure.setCalculatedFields(calculatedFields);
 			}
-					
+
 			List<IDataSet> rootentities = getDataSource().getRootEntities();
-			logger.debug("The root entity names are ["+ rootentities + "] ");
-			
-			for(int i=0;i< rootentities.size(); i++) {
-				IDataSet entity =  rootentities.get(i);
-				logger.debug("Adding entity type [" +entity.getName() + "] to model structure");
-				addEntity(modelStructure, modelName, entity);	
-				logger.info("Entity type [" +entity.getName() + "] succesfully added to model structure");
-			}		
-			
-			
+			logger.debug("The root entity names are [" + rootentities + "] ");
+
+			for (int i = 0; i < rootentities.size(); i++) {
+				IDataSet entity = rootentities.get(i);
+				logger.debug("Adding entity type [" + entity.getName() + "] to model structure");
+				addEntity(modelStructure, modelName, entity);
+				logger.info("Entity type [" + entity.getName() + "] succesfully added to model structure");
+			}
+
 			addRelationshipsBetweenRootEntities(modelStructure);
-			
+
 			logger.info("Model structure for model [" + modelName + "] succesfully built");
-			
+
 			return modelStructure;
-		} 
-		catch (Exception e){
+		} catch (Exception e) {
 			logger.debug("Impossible to build model structure", e);
 			throw new RuntimeException("Impossible to build model structure", e);
-		}
-		catch(Throwable t) {
+		} catch (Throwable t) {
 			throw new RuntimeException("Impossible to build model structure", t);
-		
+
 		} finally {
 			logger.debug("OUT");
 		}
 	}
-	
 
-
-	
-	private void addEntity (IModelStructure modelStructure, String modelName, IDataSet entity){
+	private void addEntity(IModelStructure modelStructure, String modelName, IDataSet entity) {
 
 		String entityLabel = entity.getName();
 		String tableName = entity.getTableNameForReading();
-		
+		String dsType = entity.getDsType();
+
 		IModelEntity dataMartEntity = modelStructure.addRootEntity(modelName, entityLabel, null, null, entityLabel);
 		dataMartEntity.getProperties().put("label", entityLabel);
 		dataMartEntity.getProperties().put("tableName", tableName);
-		
-		// the query name is used when building the SQL statement, because we need to know the name of the actual table that contains the data of the dataset
+
+		// the query name is used when building the SQL statement, because we
+		// need to know the name of the actual table that contains the data of
+		// the dataset
 		// i.e. we need to know the persistence table name
 		dataMartEntity.getProperties().put("queryName", entity.getTableNameForReading());
-		
-		propertiesInitializer.addProperties(dataMartEntity);
-		dataMartEntity.getProperties().put(FIELD_TYPE_PROPERTY,"cube");
-		
-		addNormalFields(dataMartEntity, entity);
-		addCalculatedFields(dataMartEntity);		
-	}
 
+		propertiesInitializer.addProperties(dataMartEntity);
+		dataMartEntity.getProperties().put(FIELD_TYPE_PROPERTY, "cube");
+
+		addNormalFields(dataMartEntity, entity);
+		addCalculatedFields(dataMartEntity);
+	}
 
 	/**
 	 * This method adds the normal fields to the datamart entry structure
-	 * @param dataMartEntity:  the datamart structure to complete
+	 *
+	 * @param dataMartEntity
+	 *            : the datamart structure to complete
 	 */
-	public void addNormalFields(IModelEntity dataMartEntity, IDataSet entity) {		
-		logger.debug("Adding the field "+dataMartEntity.getName());
+	public void addNormalFields(IModelEntity dataMartEntity, IDataSet entity) {
+		logger.debug("Adding the field " + dataMartEntity.getName());
 
 		IMetaData datasetMetadata = entity.getMetadata();
-		
-		
-		for(int i=0; i<datasetMetadata.getFieldCount(); i++) {
-			IFieldMetaData fieldMetadata = datasetMetadata.getFieldMeta(i);
-			addField(fieldMetadata, dataMartEntity, "");	
-		}	
-		
-		
-		logger.debug("Field "+dataMartEntity.getName()+" added");
-	}
-	
 
-	private void addField(IFieldMetaData fieldMetadata, IModelEntity dataMartEntity, String keyPrefix){
-		
-//		String fieldName = fieldMetadata.getAlias();
-//		if(fieldName==null || fieldName.equals("")){
-//			fieldName = fieldMetadata.getName();
-//		}
+		for (int i = 0; i < datasetMetadata.getFieldCount(); i++) {
+			IFieldMetaData fieldMetadata = datasetMetadata.getFieldMeta(i);
+			addField(fieldMetadata, dataMartEntity, "", entity);
+		}
+
+		logger.debug("Field " + dataMartEntity.getName() + " added");
+	}
+
+	private void addField(IFieldMetaData fieldMetadata, IModelEntity dataMartEntity, String keyPrefix, IDataSet entity) {
+
+		// String fieldName = fieldMetadata.getAlias();
+		// if(fieldName==null || fieldName.equals("")){
+		// fieldName = fieldMetadata.getName();
+		// }
 		String fieldUniqueName = fieldMetadata.getName();
 		String alias = fieldMetadata.getAlias();
 		if (alias == null || alias.equals("")) {
 			alias = fieldMetadata.getName();
 		}
-		
+
 		// TODO: SCALE
 		int scale = 0;
 		Object precision = (fieldMetadata.getProperty(IFieldMetaData.DECIMALPRECISION));
-		
-		int precisionInt =0;
-		if(precision!=null){
-			if(precision instanceof String){
-				precisionInt = (new Integer((String)precision)).intValue();
-			}else{
-				precisionInt = (Integer)precision;
+
+		int precisionInt = 0;
+		if (precision != null) {
+			if (precision instanceof String) {
+				precisionInt = (new Integer((String) precision)).intValue();
+			} else {
+				precisionInt = (Integer) precision;
 			}
 		}
 
-		IModelField datamartField = dataMartEntity.addNormalField(keyPrefix + fieldUniqueName);
+		IModelField datamartField;
+
+		if (entity.getDsType().equals("it.eng.qbe.dataset.qbedataset")) {
+			datamartField = dataMartEntity.addNormalField(keyPrefix + alias);
+			datamartField.setName(alias);
+		} else {
+			datamartField = dataMartEntity.addNormalField(keyPrefix + fieldUniqueName);
+			datamartField.setName(fieldUniqueName);
+		}
+
 		datamartField.setType(fieldMetadata.getType().getName());
-		datamartField.setName(fieldUniqueName);
 		datamartField.getProperties().put("label", alias);
 		datamartField.setPrecision(precisionInt);
 		datamartField.setLength(scale);
 		propertiesInitializer.addProperties(datamartField);
-		if(fieldMetadata.getFieldType()!=null){
+		if (fieldMetadata.getFieldType() != null) {
 			datamartField.getProperties().put(FIELD_TYPE_PROPERTY, (fieldMetadata.getFieldType().name()).toLowerCase());
 		}
-		
+
 	}
-	
 
 	private void addCalculatedFields(IModelEntity dataMartEntity) {
-		logger.debug("Adding the calculated field "+dataMartEntity.getName());
+		logger.debug("Adding the calculated field " + dataMartEntity.getName());
 		List calculatedFileds;
 		ModelCalculatedField calculatedField;
-		
-		calculatedFileds = dataMartEntity.getStructure().getCalculatedFieldsByEntity(dataMartEntity.getUniqueName()); 
-		if(calculatedFileds != null) {
-			for(int i = 0; i < calculatedFileds.size(); i++) {
-				calculatedField = (ModelCalculatedField)calculatedFileds.get(i);
+
+		calculatedFileds = dataMartEntity.getStructure().getCalculatedFieldsByEntity(dataMartEntity.getUniqueName());
+		if (calculatedFileds != null) {
+			for (int i = 0; i < calculatedFileds.size(); i++) {
+				calculatedField = (ModelCalculatedField) calculatedFileds.get(i);
 				dataMartEntity.addCalculatedField(calculatedField);
 				propertiesInitializer.addProperties(calculatedField);
 			}
 		}
-		logger.debug("Added the calculated field "+dataMartEntity.getName());
+		logger.debug("Added the calculated field " + dataMartEntity.getName());
 	}
-	
-	
-	
+
 	/**
 	 * @return the DataSetDataSource
 	 */
 	public DataSetDataSource getDataSource() {
-		return (DataSetDataSource)dataSource;
+		return (DataSetDataSource) dataSource;
 	}
 
 	/**
-	 * @param DataSetDataSource the datasource to set
+	 * @param DataSetDataSource
+	 *            the datasource to set
 	 */
 	public void setDataSource(DataSetDataSource dataSource) {
 		this.dataSource = dataSource;
 	}
-
 
 }
--- qbecore/src/runtime/it/eng/qbe/dataset/FederationUtils.java
+++ qbecore/src/runtime/it/eng/qbe/dataset/FederationUtils.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,18 +31,19 @@ import org.json.JSONObject;
 
 public class FederationUtils {
 
-	
 	private static transient Logger logger = Logger.getLogger(FederationUtils.class);
-	
+
 	/**
-	 * Here we get the relationships configuration and we replace the name of the dataset with the name of the cached dataset. This because the qbe is working wit the cached
+	 * Here we get the relationships configuration and we replace the name of
+	 * the dataset with the name of the cached dataset. This because the qbe is
+	 * working wit the cached
 	 */
-	public static void adjustRelationName(JSONObject relations, Map<String, String> mapNameTable){
+	public static void adjustRelationName(JSONObject relations, Map<String, String> mapNameTable) {
 		logger.debug("Starting adjusting the relationsip definition");
 		try {
 			JSONArray relationshipArray = relations.getJSONArray("relationships");
-			if(relationshipArray!=null){
-				for(int y=0; y<relationshipArray.length(); y++){
+			if (relationshipArray != null) {
+				for (int y = 0; y < relationshipArray.length(); y++) {
 					JSONObject jo = relationshipArray.getJSONObject(y);
 					jo.getJSONObject("sourceTable").put("name", mapNameTable.get(jo.getJSONObject("sourceTable").get("name")));
 					jo.getJSONObject("sourceTable").put("className", mapNameTable.get(jo.getJSONObject("sourceTable").get("className")));
@@ -51,33 +52,35 @@ public class FederationUtils {
 				}
 			}
 		} catch (Exception e) {
-			logger.error("Error adjusting the relationship definition for the federated dataset",e);
-			throw new SpagoBIRuntimeException("Error adjusting the relationship definition for the federated dataset",e);
+			logger.error("Error adjusting the relationship definition for the federated dataset", e);
+			throw new SpagoBIRuntimeException("Error adjusting the relationship definition for the federated dataset", e);
 		}
 
 	}
-	
+
 	/**
-	 * Here we get the relationships configuration and we replace the name of the dataset with the name of the cached dataset. This because the qbe is working wit the cached
+	 * Here we get the relationships configuration and we replace the name of
+	 * the dataset with the name of the cached dataset. This because the qbe is
+	 * working wit the cached
 	 */
-	public static void adjustRelationName(JSONArray relationshipArray, JSONObject mapNameTable){
+	public static void adjustRelationName(JSONArray relationshipArray, JSONObject mapNameTable) {
 		logger.debug("Starting adjusting the relationsip definition");
 		try {
-			
-			if(relationshipArray!=null){
+
+			if (relationshipArray != null) {
 				relationshipArray = relationshipArray.getJSONArray(0);
-				for(int y=0; y<relationshipArray.length(); y++){
+				for (int y = 0; y < relationshipArray.length(); y++) {
 					JSONObject jo = relationshipArray.getJSONObject(y);
-					jo.getJSONObject("sourceTable").put("name", mapNameTable.get((String)jo.getJSONObject("sourceTable").get("name")));
-					jo.getJSONObject("sourceTable").put("className", mapNameTable.get((String)jo.getJSONObject("sourceTable").get("className")));
-					jo.getJSONObject("destinationTable").put("name", mapNameTable.get((String)jo.getJSONObject("destinationTable").get("name")));
-					jo.getJSONObject("destinationTable").put("className", mapNameTable.get((String)jo.getJSONObject("destinationTable").get("className")));
+					jo.getJSONObject("sourceTable").put("name", mapNameTable.get((String) jo.getJSONObject("sourceTable").get("name")));
+					jo.getJSONObject("sourceTable").put("className", mapNameTable.get((String) jo.getJSONObject("sourceTable").get("className")));
+					jo.getJSONObject("destinationTable").put("name", mapNameTable.get((String) jo.getJSONObject("destinationTable").get("name")));
+					jo.getJSONObject("destinationTable").put("className", mapNameTable.get((String) jo.getJSONObject("destinationTable").get("className")));
 				}
 			}
-			
+
 		} catch (Exception e) {
-			logger.error("Error adjusting the relationship definition for the federated dataset",e);
-			throw new SpagoBIRuntimeException("Error adjusting the relationship definition for the federated dataset",e);
+			logger.error("Error adjusting the relationship definition for the federated dataset", e);
+			throw new SpagoBIRuntimeException("Error adjusting the relationship definition for the federated dataset", e);
 		}
 
 	}
@@ -85,39 +88,40 @@ public class FederationUtils {
 	/**
 	 * Invokes the service that persits the datasets
 	 */
-	public static JSONObject createDatasetsOnCache(JSONObject datasetLabelsIndexes, String userId){
-		//dave in cache the derived datasets
+	public static JSONObject createDatasetsOnCache(JSONObject datasetLabelsIndexes, String userId) {
+		// dave in cache the derived datasets
 		logger.debug("Saving the datasets on cache");
 		DataSetPersister dsp = new DataSetPersister();
 		JSONObject datasetPersistedLabels = null;
 		try {
-			datasetPersistedLabels = dsp.cacheDataSets(datasetLabelsIndexes, userId );
+			datasetPersistedLabels = dsp.cacheDataSets(datasetLabelsIndexes, userId);
 		} catch (Exception e1) {
-			logger.error("Error executing the service that persist the datasets on the cache",e1);
-			throw new SpagoBIRuntimeException("Error executing the service that persist the datasets on the cache",e1);
+			logger.error("Error executing the service that persist the datasets on the cache", e1);
+			throw new SpagoBIRuntimeException("Error executing the service that persist the datasets on the cache", e1);
 		}
 		return datasetPersistedLabels;
 	}
-	
-
 
 	/**
 	 * Creates a jdbc dataset on the cached table
+	 *
 	 * @param cachedTable
 	 * @param dataSet
 	 * @param cachedDataSource
 	 * @return
 	 */
-	public static IDataSet createDatasetOnCache(String cachedTable, IDataSet dataSet, IDataSource cachedDataSource){
-		logger.debug("Creating the cached dataset for the dataSet "+dataSet.getLabel());
+	public static IDataSet createDatasetOnCache(String cachedTable, IDataSet dataSet, IDataSource cachedDataSource) {
+		logger.debug("Creating the cached dataset for the dataSet " + dataSet.getLabel());
 		JDBCDataSet dataset = new JDBCDataSet();
+		dataset.setDsType(dataSet.getClass().getName().toLowerCase());
 		dataset.setDataSource(cachedDataSource);
 
 		String query = "select * from " + cachedTable;
-		logger.debug("The query for the dataset "+dataSet.getLabel()+" is "+query);
+		logger.debug("The query for the dataset " + dataSet.getLabel() + " is " + query);
 
-		dataset.setLabel(cachedTable);		
-		dataset.setName(dataSet.getLabel());//the label because we need it for the joins
+		dataset.setLabel(cachedTable);
+		dataset.setName(dataSet.getLabel());// the label because we need it for
+											// the joins
 		dataset.setDescription(dataSet.getDescription());
 		dataset.setQuery(query);
 		dataset.setPersisted(true);
@@ -128,13 +132,14 @@ public class FederationUtils {
 		dataset.setDsMetadata(dataSet.getDsMetadata());
 		return dataset;
 	}
-	
+
 	/**
 	 * Prefix for federated dataset built automatically
+	 *
 	 * @return
 	 */
-	public static String getDatasetFederationLabelSuffix(){
+	public static String getDatasetFederationLabelSuffix() {
 		return "_from_ds_";
 	}
-	
+
 }
