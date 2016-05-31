2933c7b582c5a72b08e06f1207bc0b4ac502b595
Nikola Simovic <nikola.simovic@eng.it>
2016-05-31 09:46:25 +0000

    FAVORITES - translate mapping, licences, tooltip on md card.

--- knowage/web-content/js/src/angular_1.4/tools/workspace/scripts/directive/components/favouritesViewWorkspace.js
+++ knowage/web-content/js/src/angular_1.4/tools/workspace/scripts/directive/components/favouritesViewWorkspace.js
@@ -1,3 +1,20 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 angular
 	.module('favourites_view_workspace', [])
 
@@ -14,7 +31,7 @@ angular
 		  };
 	})
 
-function favouritesController($scope,sbiModule_restServices,sbiModule_translate,sbiModule_user,$documentViewer){
+function favouritesController($scope,sbiModule_restServices,sbiModule_translate,sbiModule_user,$documentViewer,$mdDialog){
 
 	$scope.loadFavoriteDocumentExecutionsForUser =function(){
 		sbiModule_restServices.promiseGet("2.0/favorites","")
@@ -28,11 +45,20 @@ function favouritesController($scope,sbiModule_restServices,sbiModule_translate,
 	}
 
 	$scope.deleteFavoriteDocumentExecutionById = function(doc) {
-		sbiModule_restServices.promiseDelete("2.0/favorites",doc.id)
-		.then(function(response) {
-			$scope.loadFavoriteDocumentExecutionsForUser();
-		},function(response) {
-			sbiModule_restServices.errorHandler(response.data,sbiModule_translate.load('sbi.browser.folder.load.error'));
+		var confirm = $mdDialog.confirm()
+		.title(sbiModule_translate.load("sbi.workspace.favorites.delete.confirm.dialog"))
+		.content(sbiModule_translate.load("sbi.workspace.favorites.delete.confirm.delete"))
+		.ariaLabel('delete Document') 
+		.ok(sbiModule_translate.load("sbi.general.yes"))
+		.cancel(sbiModule_translate.load("sbi.general.No"));
+			$mdDialog.show(confirm).then(function() {
+				sbiModule_restServices.promiseDelete("2.0/favorites",doc.id)
+				.then(function(response) {
+					$scope.loadFavoriteDocumentExecutionsForUser();
+					$scope.selectDocument(undefined);
+				},function(response) {
+					sbiModule_restServices.errorHandler(response.data,sbiModule_translate.load('sbi.browser.folder.load.error'));
+				});
 		});
 	}
 	
--- knowage/web-content/js/src/angular_1.4/tools/workspace/scripts/directive/favorite-view/favoriteView.js
+++ knowage/web-content/js/src/angular_1.4/tools/workspace/scripts/directive/favorite-view/favoriteView.js
@@ -1,3 +1,20 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 angular.module('favorite_view', ['ngMaterial'])
 .directive('favoriteView', function() {
 	return {
@@ -26,10 +43,12 @@ angular.module('favorite_view', ['ngMaterial'])
 	}
 });
 
-function favoriteViewControllerFunction($scope){
+function favoriteViewControllerFunction($scope,sbiModule_translate){
 	
 	$scope.clickDocument=function(item){
 		
 		 $scope.selectDocumentAction({doc: item});
 	}
+	
+	$scope.translate=sbiModule_translate;
 }
