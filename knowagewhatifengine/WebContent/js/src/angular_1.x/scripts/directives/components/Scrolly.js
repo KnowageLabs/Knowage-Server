644da494c45641237dcec28506b4de57694af80f
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-06-08 13:59:22 +0000

    fix

--- knowagewhatifengine/WebContent/js/src/angular_1.x/scripts/directives/components/Scrolly.js
+++ knowagewhatifengine/WebContent/js/src/angular_1.x/scripts/directives/components/Scrolly.js
@@ -4,11 +4,20 @@ angular.module('scrolly_directive',[])
 	        restrict: 'A',
 	        link: function (scope, element, attrs) {
 	        	var ready = false;
-	        	
 	            var raw = element[0];
-	            scope.$watch('tableHeight', function() {
-	                
-	            });
+	            
+	            var startRow = null;
+	            var startColumn = null;
+	            
+	            var table = null;
+        		var thead = null;
+        		var bodyRows = [];
+        		var bodyColumns = [];
+        		var newRowSet = null;
+        		var newColumnSet = null;
+        		var newTableHeight =null;
+        		var newTableWeight = null;
+	            
 	           
 	            scope.tableHeight = raw.offsetHeight;
 	            scope.tableWeight = raw.offsetWidth;
@@ -19,80 +28,76 @@ angular.module('scrolly_directive',[])
 	        	 
 	        	};
 	        	
-	        	  element.bind('scroll', function () {
-	                 scope.isScrolling = true;
-	                 
-	              });
+	        	  
 	        	
 	        	
 	        	scope.resize = function(){
 	        		
 	        		scope.tableHeight = raw.offsetHeight;
 		            scope.tableWeight = raw.offsetWidth;
-	            	 var table = document.getElementsByClassName("x-pivot-table")[0];
+		            
+	            	table = document.getElementsByClassName("pivot-table")[0];
+	            	
+	            	
 	            	 if(table){
-	            		 var thead = table.getElementsByTagName("thead")[0];
-		            	 var bodyRows = table.getElementsByTagName("tbody")[0].children;
-		            	 var bodyColumns = bodyRows[0].children;
+	            		 
+	            		  thead = table.getElementsByTagName("thead")[0];
+		            	  bodyRows = table.getElementsByTagName("tbody")[0].children;
+		            	  bodyColumns = bodyRows[0].children;
 	            	 
-	            	 
-	            	 
-	            	 var newRowSet = scope.modelConfig.rowsSet;
-	            	 var newColumnSet = scope.modelConfig.columnSet; 
+		            	  newRowSet = scope.modelConfig.rowsSet;
+		            	  newColumnSet = scope.modelConfig.columnSet; 
 	            	
 	            	
-	            	//Setting new number of rows if table height is bigger than div 
-	            	if(table.offsetHeight>raw.offsetHeight-25){
+		            	  //Setting new number of rows if table height is bigger than div 
+		            	  if(table.offsetHeight>raw.offsetHeight-25){
 	            		
-	            		
-	            		
-	            		var newTableHeight = thead.offsetHeight;
-	            		
-	            		for ( newRowSet = 0; newRowSet < bodyRows.length; newRowSet++) {
+		            		  newTableHeight = thead.offsetHeight;
+		            		  newRowSet = 0;
+		            		  
+		            		  	for ( var i = 0; i < bodyRows.length; i++) {
 							
-	            			
-	            			
-	            			if((newTableHeight+bodyRows[newRowSet].offsetHeight)>(raw.parentElement.offsetHeight-25)){
+		            		  		if((newTableHeight+bodyRows[newRowSet].offsetHeight)>(raw.parentElement.offsetHeight-25)){
 	            					            				
-	            				break;
-	            			}else{
-	            				newTableHeight = newTableHeight+bodyRows[newRowSet].offsetHeight;
-	            			}
+		            		  			break;
+		            		  		}else{
+		            		  			
+		            		  			newRowSet++;
+		            		  			newTableHeight = newTableHeight+bodyRows[newRowSet].offsetHeight;
 	            				
-						}
+		            		  		}
+	            				
+		            		  	}
 	            		
 	            	
-	            	}	
-	            	if(table.offsetHeight<raw.offsetHeight-70&&
-	            			scope.modelConfig.rowCount>newRowSet){
-	            		
+		            	  }
+		            	  
+		            	  if(table.offsetHeight<raw.offsetHeight-70&&
+		            		scope.modelConfig.rowCount>newRowSet){
 	            		
 	            			newRowSet = 50;
-	            		
-	            			
-	            			
-	            		
-	            	}	
+
+		            	  }	
 	            	
-	            	if(table.offsetWidth<raw.offsetWidth-50&&
+		            	  if(table.offsetWidth<raw.offsetWidth-50&&
 	            			scope.modelConfig.columnCount>newColumnSet){
-	            		if(bodyColumns[newColumnSet]){
+		            		  
+		            		  if(bodyColumns[newColumnSet]){
 	            			
-	            		
-	            		if(table.offsetWidth+bodyColumns[newColumnSet].offsetWidth<raw.offsetWidth-50){
+		            			  if(table.offsetWidth+bodyColumns[newColumnSet].offsetWidth<raw.offsetWidth-50){
 	            			
 		            			newColumnSet = 50;
 		            		
 	            			
-	            		}
+		            			  }
 	            		
-	            		}
+		            		  }
 	            			
 	            		
-	            	}	
+		            	  }	
 	            	
-	            		if(table.offsetWidth>raw.parentElement.offsetWidth-25){
-	            			
+	            	
+	            		if(table.offsetWidth>raw.parentElement.offsetWidth-50){
 	            			
 		            		var ajSize = 0;
 		            		var headerCount = 0;
@@ -103,8 +108,9 @@ angular.module('scrolly_directive',[])
 		            			if(bodyColumns[newColumnSet].nodeName==='TH'){
 		            				headerCount++;
 		            			}
-		            			if((ajSize+bodyColumns[newColumnSet].offsetWidth)>(raw.parentElement.offsetWidth-25)){
-		            					            				
+		            			
+		            			if((ajSize+bodyColumns[newColumnSet].offsetWidth)>(raw.parentElement.offsetWidth-50)){
+		            				           				
 		            				break;
 		            			}else{
 		            				ajSize = ajSize+bodyColumns[newColumnSet].offsetWidth;
@@ -116,84 +122,112 @@ angular.module('scrolly_directive',[])
 	            		
 		            	
 		            	
-		            	 if(newRowSet<1){
+		            if(newRowSet<1){
 		            		
-		            		 newRowSet =1;
-		            	 }
-		            	 if(newColumnSet<1){
-		            		
-		            		 newColumnSet =1;
-		            	 }
+		            	newRowSet =1;
+		            	
+		            }
 		            	 
-		            	 if(scope.modelConfig.rowsSet!=newRowSet||scope.modelConfig.columnSet!=newColumnSet){
-		            		 if(scope.ready){
-		            			 scope.modelConfig.rowsSet=newRowSet;
-			            		 scope.modelConfig.columnSet=newColumnSet;
-			            		 scope.sendModelConfig(scope.modelConfig);
-			            		 console.log("sending modelconfig from resize!!!!!!!!!!!!");
-		            		 }
+		            if(newColumnSet<1){
+		            		
+		            	newColumnSet =1;
+		            	 
+		            }
+		            	 
+		            if(scope.modelConfig.rowsSet!=newRowSet||scope.modelConfig.columnSet!=newColumnSet){
+		            	
+		            	if(scope.ready){
+		            		
+		            		scope.modelConfig.rowsSet=newRowSet;
+			            	scope.modelConfig.columnSet=newColumnSet;
+			            	scope.sendModelConfig(scope.modelConfig);
+			            		
+		            	}
 		            		 
-		            	 }
+		            }
 		            	 
 	            	 
 	        	} 
+	            	  //table = null;
+	            	  thead = null;
+	            	  bodyRows.length = 0;
+	            	  bodyColumns.length = 0;
+	            	  newRowSet = null;
+	            	  newColumnSet = null;
+	            	  newTableHeight =null;
 	        	}
 	           
 	        	scope.interval = $interval(
 	        			function(){
+	        				
+	        				scope.scroll();
 	        				if(scope.ready){
 	        					scope.resize();
 	        				}
-	        				if(!scope.isScrolling){
-	        					scope.scroll();
-	        				}
 	        				
-	        				scope.isScrolling = false;
+	        					
+	        				
+	        				
+	        				//scope.isScrolling = false;
 	        			
 	        			},100);
+	        	
+	        	
+	        	
 	               
 	        	scope.scroll = function () {
-	                
-	              
 	        		
-	                var startRow = Math.round((raw.scrollTop)/100);
-	                var startColumn =  Math.round(raw.scrollLeft/100);
+	        		
+	                
+	                
+	                startRow = Math.round((raw.scrollTop)/100);
+	                startColumn =  Math.round(raw.scrollLeft/100);
+	                
+	                
 	                if(scope.modelConfig){
 	                	
-	                
-	               
-	               if(scope.modelConfig.startRow!=startRow){
+	                	if(scope.modelConfig.startRow!=startRow){
+	                		
+	                		if(table){
+	        		   			table.remove();
+	        		   		}
 	           
 	            		   scope.modelConfig.startRow = startRow;
-	            	  
-	            	   
-	            	   
-	            	   scope.showLoadingMask = false;
+	            		   scope.showLoadingMask = false;
 	           	    	
-	           	  //  if(scope.tableSubsets[scope.modelConfig.startRow]!=undefined){
-	           	  //  	scope.table = $sce.trustAsHtml(scope.tableSubsets[scope.modelConfig.startRow]);
-	           	 //   }else{
-	           	    	scope.sendModelConfig(scope.modelConfig);
-	           	//    }
+	            		   	if(scope.tableSubsets!=null&&
+	            		   		scope.tableSubsets[scope.modelConfig.startRow]!=undefined&&
+	            		   		scope.tableSubsets[scope.modelConfig.startRow]!=null){
+	            		   		
+	            		   		
+	            		   		scope.table = $sce.trustAsHtml(scope.tableSubsets[scope.modelConfig.startRow]);
+	            		   		
+	            		   	}else{
+	            		   		
+	            		   		scope.sendModelConfig(scope.modelConfig);
+	            		   	}
 	           	    
-	               }
-	               if(scope.modelConfig.startColumn!=startColumn){
+	                	}
+	                	
+	                	if(scope.modelConfig.startColumn!=startColumn){
 	            	
-	            	
-	            		   scope.modelConfig.startColumn = startColumn;
-	            	  
-	           	    	
-	            	   scope.showLoadingMask = false;
-	            	   scope.sendModelConfig(scope.modelConfig);
+	                		scope.modelConfig.startColumn = startColumn;
+	                		scope.showLoadingMask = false;
+	                		scope.sendModelConfig(scope.modelConfig);
 	           	    
-	               }
+	                	}
 	   
 	    
-	        	}
+	                }
+	                
+	                startRow = null;
+	                startColumn =null;
 	               
 	            }
 	                
 	          
+	        	
+	        	
 	        }
 	    };
 	}]);
