3396ace6d1a361b8122f3942edc97f326c45de90
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-06-08 13:56:46 +0000

    fix

--- knowagewhatifengine/WebContent/js/src/angular_1.x/scripts/directives/components/cell.js
+++ knowagewhatifengine/WebContent/js/src/angular_1.x/scripts/directives/components/cell.js
@@ -5,16 +5,39 @@ angular.module('cell_directive',[])
 	        link: function (scope, element, attrs) {
 	            
 	            scope.selectedCell = {};
+	            var cell = {};
+	            cell.id = attrs.id;
+	            cell.measurename = attrs.measurename;
+	            cell.ordinal = attrs.ordinal;
+	            
+	            
+	            
+	            var ondbClick = function(){
+	            	scope.makeEditable(cell.id,cell.measurename);
+	            }
+	            
+	            var onClick =function(){
+	            		
+	            	scope.selectedCell = cell;
+			            
+		        	 console.log(cell);
+		        	 console.log(scope.selectedCell);
+	            		
+	            }
 	           
-	         element.bind('click', function () {
+	         element.bind('click', onClick);
+	         
+	         element.bind('dblclick', ondbClick);
+	         
+	         element.on('$destroy', function () {
 	                
 	        	 	
-	        	 	scope.selectedCell.id = element[0].attributes['id'].value;
-		            scope.selectedCell.measureName = element[0].attributes['measureName'].value;
-		            scope.selectedCell.ordinal = element[0].attributes['ordinal'].value;
-		            scope.selectedCell.value = element[0].attributes['value'].value
-		            
-	        	 console.log(scope.selectedCell);
+	        	 cell =null;
+	        	 
+		         element.unbind('dblclick',ondbClick);  
+		         element.unbind('click',onClick); 
+		         element.detach();
+	        	 console.log("lllllllllllllllllllllllllllllllll");
 	               
 	            });
 	        }
