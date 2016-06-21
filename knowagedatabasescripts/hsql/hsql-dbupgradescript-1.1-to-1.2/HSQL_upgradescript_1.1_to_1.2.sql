4c9113b9e5b9fbcbfa8c8488795404808fd225ec
Antonella Giachino <antonella.giachino@eng.it>
2016-06-21 12:44:51 +0000

    commented delete of worksheet obj because shouldn't exist this type of documents.

--- knowagedatabasescripts/hsql/hsql-dbupgradescript-1.1-to-1.2/HSQL_upgradescript_1.1_to_1.2.sql
+++ knowagedatabasescripts/hsql/hsql-dbupgradescript-1.1-to-1.2/HSQL_upgradescript_1.1_to_1.2.sql
@@ -5,11 +5,11 @@ ALTER TABLE  SBI_GEO_MAPS ADD COLUMN LEVEL INTEGER;
 ALTER TABLE  SBI_GEO_MAPS ADD COLUMN MEMBER_NAME VARCHAR(100);
 
 
-SET FOREIGN_KEY_CHECKS=0;
-delete from SBI_USER_FUNC  where name = 'CreateWorksheetFromDatasetUserFunctionality';
-DELETE FROM SBI_OBJECTS WHERE ENGINE_ID = (select engine_id from SBI_ENGINES  where name = 'Worksheet Engine');
-delete from SBI_ENGINES  where name = 'Worksheet Engine';
-delete from SBI_DOMAINS where value_cd = 'WORKSHEET';
-SET FOREIGN_KEY_CHECKS=1;
+-- SET FOREIGN_KEY_CHECKS=0;
+-- delete from SBI_USER_FUNC  where name = 'CreateWorksheetFromDatasetUserFunctionality';
+-- DELETE FROM SBI_OBJECTS WHERE ENGINE_ID = (select engine_id from SBI_ENGINES  where name = 'Worksheet Engine');
+-- delete from SBI_ENGINES  where name = 'Worksheet Engine';
+-- delete from SBI_DOMAINS where value_cd = 'WORKSHEET';
+-- SET FOREIGN_KEY_CHECKS=1;
 
 ALTER TABLE SBI_DATA_SET ADD COLUMN IS_PERSISTED_HDFS BOOLEAN DEFAULT FALSE BEFORE PERSIST_TABLE_NAME;
