a21cbfed6303a46e2fe6f570ac775f6d3bbb62ba
Francesco Lucchi <francesco.lucchi@eng.it>
2016-07-29 14:12:36 +0000

    fixed SQL Server scripts (and deleted old not-working upgrade scripts)

--- knowagedatabasescripts/sqlserver/SQLServer_drop_quartz_tables.sql
+++ knowagedatabasescripts/sqlserver/SQLServer_drop_quartz_tables.sql
@@ -1,13 +1,12 @@
-
-drop table qrtz_job_listeners;
-drop table qrtz_trigger_listeners;
-drop table qrtz_fired_triggers;
-DROP TABLE QRTZ_PAUSED_TRIGGER_GRPS;
-DROP TABLE QRTZ_SCHEDULER_STATE;
-DROP TABLE QRTZ_LOCKS;
-drop table qrtz_simple_triggers;
-drop table qrtz_cron_triggers;
-DROP TABLE QRTZ_BLOB_TRIGGERS;
-drop table qrtz_triggers;
-drop table qrtz_job_details;
-drop table qrtz_calendars;
+DROP TABLE qrtz_job_listeners;
+DROP TABLE qrtz_trigger_listeners;
+DROP TABLE qrtz_fired_triggers;
+DROP TABLE qrtz_paused_trigger_grps;
+DROP TABLE qrtz_scheduler_state;
+DROP TABLE qrtz_locks;
+DROP TABLE qrtz_simple_triggers;
+DROP TABLE qrtz_cron_triggers;
+DROP TABLE qrtz_blob_triggers;
+DROP TABLE qrtz_triggers;
+DROP TABLE qrtz_job_details;
+DROP TABLE qrtz_calendars;
