f1bea7396ac1351b7fc3094e03b9831544e0327b
Nikola Simovic <nikola.simovic@eng.it>
2016-07-29 15:49:17 +0000

    Hardcoded buttons for right toolbar

---
+++ knowagewhatifengine/src/it/eng/spagobi/engines/whatif/toolbarbuttons/SbiToolbarButton.java
@@ -0,0 +1,67 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.eng.spagobi.engines.whatif.toolbarbuttons;
+
+public class SbiToolbarButton {
+
+	private String name;
+	private boolean visible;
+	private boolean clicked;
+	private String category;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public boolean isClicked() {
+		return clicked;
+	}
+
+	public void setClicked(boolean clicked) {
+		this.clicked = clicked;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public SbiToolbarButton(String name, boolean visible, boolean clicked, String category) {
+		super();
+		this.name = name;
+		this.visible = visible;
+		this.clicked = clicked;
+		this.category = category;
+	}
+
+}
---
+++ knowagewhatifengine/src/it/eng/spagobi/engines/whatif/toolbarbuttons/ToolbarButtonManager.java
@@ -0,0 +1,53 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.eng.spagobi.engines.whatif.toolbarbuttons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToolbarButtonManager {
+
+	private static List<SbiToolbarButton> listOfToolbarButtons;
+
+	private static void init() {
+		listOfToolbarButtons = new ArrayList<SbiToolbarButton>();
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_DRILL_THROUGH", false, false, ToolbarButtonCategory.DRILL_ON_DATA.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_MDX", false, false, ToolbarButtonCategory.OLAP_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_EDIT_MDX", false, false, ToolbarButtonCategory.OLAP_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_FATHER_MEMBERS", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_CC", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_HIDE_SPANS", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_SORTING_SETTINGS", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_SORTING", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_SHOW_PROPERTIES", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_HIDE_EMPTY", false, false, ToolbarButtonCategory.TABLE_FUNCTIONS.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_SAVE_NEW", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_UNDO", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_VERSION_MANAGER", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_EXPORT_OUTPUT", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_SAVE_SUBOBJECT", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_EDITABLE_EXCEL_EXPORT", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_ALGORITHMS", false, false, ToolbarButtonCategory.WHAT_IF.toString()));
+		listOfToolbarButtons.add(new SbiToolbarButton("BUTTON_FLUSH_CACHE", false, false, ToolbarButtonCategory.OLAP_FUNCTIONS.toString()));
+	}
+
+	public static List<SbiToolbarButton> loadAllToolbarButtons() {
+		init();
+		return listOfToolbarButtons;
+	}
+}
