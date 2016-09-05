c5a06ddd050d1be5375aeaf46b3f7561bedf0489
Salvatore Lupo <salvatore.lupo@eng.it>
2016-09-05 09:49:13 +0000

    cockpit 3 - save

--- knowage/src/it/eng/spagobi/engines/drivers/cockpit/CockpitDriver.java
+++ knowage/src/it/eng/spagobi/engines/drivers/cockpit/CockpitDriver.java
@@ -17,9 +17,15 @@
  */
 package it.eng.spagobi.engines.drivers.cockpit;
 
+import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
+import it.eng.spagobi.engines.config.bo.Engine;
+import it.eng.spagobi.engines.drivers.EngineURL;
+import it.eng.spagobi.engines.drivers.exceptions.InvalidOperationRequest;
 import it.eng.spagobi.engines.drivers.generic.GenericDriver;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
@@ -38,13 +44,22 @@ public class CockpitDriver extends GenericDriver {
 		JSONObject templateContent = getTemplateAsJsonObject(contentTemplate);
 
 		// get datasets from template
-
-		JSONObject storesConf = (JSONObject) templateContent.get("storesConf");
-		JSONArray stores = (JSONArray) storesConf.get("stores");
+		JSONArray stores = null;
+		String dsFieldName = null;
+		if (templateContent.has("configuration")) {
+			// new cockpit
+			stores = templateContent.getJSONObject("configuration").getJSONArray("datasets");
+			dsFieldName = "dsLabel";
+		} else {
+			// old cockpit
+			JSONObject storeConfJSON = templateContent.getJSONObject("templateContent").getJSONObject("storesConf");
+			stores = storeConfJSON.getJSONArray("stores");
+			dsFieldName = "storeId";
+		}
 
 		for (int i = 0; i < stores.length(); i++) {
 			JSONObject store = stores.getJSONObject(i);
-			String dsLabel = store.getString("storeId");
+			String dsLabel = store.getString(dsFieldName);
 			datasetsLabels.add(dsLabel);
 		}
 
@@ -52,4 +67,47 @@ public class CockpitDriver extends GenericDriver {
 		return datasetsLabels;
 	}
 
+	@Override
+	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
+		logger.debug("IN");
+		BIObject obj = null;
+		try {
+			obj = (BIObject) biobject;
+		} catch (ClassCastException cce) {
+			logger.error("The input object is not a BIObject type", cce);
+			return null;
+		}
+		Engine engine = obj.getEngine();
+		String url = engine.getUrl();
+		HashMap parameters = new HashMap();
+		String documentId = obj.getId().toString();
+		parameters.put("document", documentId);
+		applySecurity(parameters, profile);
+		EngineURL engineURL = new EngineURL(url.replace("/execute", "/edit"), parameters);
+		logger.debug("OUT");
+		return engineURL;
+		// return new EngineURL("pippo.jsp", new HashMap<>());
+	}
+
+	@Override
+	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile) throws InvalidOperationRequest {
+		// TODO Auto-generated method stub
+		logger.debug("IN");
+		BIObject obj = null;
+		try {
+			obj = (BIObject) biobject;
+		} catch (ClassCastException cce) {
+			logger.error("The input object is not a BIObject type", cce);
+			return null;
+		}
+		Engine engine = obj.getEngine();
+		String url = engine.getUrl();
+		HashMap parameters = new HashMap();
+		String documentId = obj.getId().toString();
+		parameters.put("document", documentId);
+		applySecurity(parameters, profile);
+		EngineURL engineURL = new EngineURL(url.replace("/execute", "/edit"), parameters);
+		logger.debug("OUT");
+		return engineURL;
+	}
 }
