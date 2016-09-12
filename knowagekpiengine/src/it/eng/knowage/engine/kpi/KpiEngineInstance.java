f83f48ba1f29da4c1fb6bf113ff2de6363c3e0c8
Salvatore Lupo <salvatore.lupo@eng.it>
2016-09-12 15:40:35 +0000

    refactoring: from now "country" and "language" parameters are called "knowage_sys_country" and "knowage_sys_language" (KNOWAGE-1153)

--- knowagekpiengine/src/it/eng/knowage/engine/kpi/KpiEngineInstance.java
+++ knowagekpiengine/src/it/eng/knowage/engine/kpi/KpiEngineInstance.java
@@ -17,14 +17,6 @@
  */
 package it.eng.knowage.engine.kpi;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import org.json.JSONObject;
-
 import it.eng.qbe.datasource.IDataSource;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.commons.bo.UserProfile;
@@ -42,6 +34,14 @@ import it.eng.spagobi.utilities.engines.SpagoBIEngineException;
 import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 import it.eng.spagobi.utilities.json.JSONUtils;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.json.JSONObject;
+
 /**
  * @author
  */
@@ -52,7 +52,7 @@ public class KpiEngineInstance extends AbstractEngineInstance {
 
 	// ENVIRONMENT VARIABLES
 	private final String[] lstEnvVariables = { "SBI_EXECUTION_ID", "SBICONTEXT", "SBI_COUNTRY", "SBI_LANGUAGE", "SBI_SPAGO_CONTROLLER", "SBI_EXECUTION_ROLE",
-			"SBI_HOST", "country", "language", "user_id", "DOCUMENT_ID", "DOCUMENT_LABEL", "DOCUMENT_NAME", "DOCUMENT_IS_PUBLIC", "DOCUMENT_COMMUNITIES",
+			"SBI_HOST", COUNTRY, LANGUAGE, "user_id", "DOCUMENT_ID", "DOCUMENT_LABEL", "DOCUMENT_NAME", "DOCUMENT_IS_PUBLIC", "DOCUMENT_COMMUNITIES",
 			"DOCUMENT_DESCRIPTION", "SPAGOBI_AUDIT_ID", "DOCUMENT_USER", "DOCUMENT_IS_VISIBLE", "DOCUMENT_AUTHOR", "DOCUMENT_FUNCTIONALITIES",
 			"DOCUMENT_VERSION" };
 
