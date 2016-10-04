116f7a1b1f0f42cd9f9c538f4443a807821b23fa
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-10-04 17:03:39 +0000

    adding phantomjs binary path

--- knowagewhatifengine/ProtractorTest/test/e2e/dragan/conf.js
+++ knowagewhatifengine/ProtractorTest/test/e2e/dragan/conf.js
@@ -3,7 +3,7 @@ exports.config = {
   specs: ['angularTest.js'],
  capabilities: {
 	    'browserName': 'phantomjs',
-	    //'phantomjs.binary.path': require('C:\Users\dpirkovic\AppData\Roaming\npm\node_modules\phantomjs-prebuild\phantom\bin\phantomjs.exe').path
+	    'phantomjs.binary.path': '/home/spagobi/continuousintegration/software/node_modules/phantomjs-prebuilt/bin/phantomjs'
 	  },
   onPrepare: function() {
 	  // The require statement must be down here, since jasmine-reporters
