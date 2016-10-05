c0f2d319d0ab929d63797a4257776124b17a79a7
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-10-05 12:04:31 +0000

    fixing path

--- knowagewhatifengine/ProtractorTest/test/e2e/dragan/conf.js
+++ knowagewhatifengine/ProtractorTest/test/e2e/dragan/conf.js
@@ -3,7 +3,7 @@ exports.config = {
   specs: ['angularTest.js'],
  capabilities: {
 	    'browserName': 'phantomjs',
-	    'phantomjs.binary.path': '/home/spagobi/continuousintegration/software/node_modules/phantomjs-prebuilt/bin/phantomjs'
+	    'phantomjs.binary.path': '/home/spagobi/continuousintegration/software/node_modules/phantomjs-prebuilt/bin'
 	  },
   onPrepare: function() {
 	  // The require statement must be down here, since jasmine-reporters
