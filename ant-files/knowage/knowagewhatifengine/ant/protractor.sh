63334608613fd332f9fd78c59cc5bbaecb7369f4
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-10-06 13:33:45 +0000

    fix

--- ant-files/knowage/knowagewhatifengine/ant/protractor.sh
+++ ant-files/knowage/knowagewhatifengine/ant/protractor.sh
@@ -1,12 +1,12 @@
 # start selenium
-/home/spagobi/continuousintegration/software/node_modules/protractor/bin/webdriver-manager start > /dev/null 2>&1 &
+/home/spagobi/continuousintegration/software/node_modules/protractor/bin/webdriver-manager start  &
 
 # wait until selenium is up
-while ! curl http://localhost:4444/wd/hub/status &>/dev/null; do :; done
+while ! curl http://localhost:4444/wd/hub/status &; do :; done
 
 # run protractor tests
 cd /home/spagobi/jenkins/workspace/knowage/knowagewhatifengine/ProtractorTest/test/e2e/dragan
 /home/spagobi/continuousintegration/software/node_modules/protractor/bin/protractor conf.js
 
 # stop selenium
-curl -s -L http://localhost:4444/selenium-server/driver?cmd=shutDownSeleniumServer > /dev/null 2>&1
+curl -s -L http://localhost:4444/selenium-server/driver?cmd=shutDownSeleniumServer 
