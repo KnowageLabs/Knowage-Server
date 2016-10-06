e5421ff2c0731e13a616ca478140141c1025066d
Dragan Pirkovic <dragan.pirkovic@eng.it>
2016-10-06 12:38:16 +0000

    fix

--- ant-files/knowage/knowagewhatifengine/ant/protractor.sh
+++ ant-files/knowage/knowagewhatifengine/ant/protractor.sh
@@ -5,7 +5,7 @@
 while ! curl http://localhost:4444/wd/hub/status &>/dev/null; do :; done
 
 # run protractor tests
-#cd /home/spagobi/jenkins/workspace/knowage/knowagewhatifengine/ProtractorTest/test/e2e/dragan
+cd /home/spagobi/jenkins/workspace/knowage/knowagewhatifengine/ProtractorTest/test/e2e/dragan
 /home/spagobi/continuousintegration/software/node_modules/protractor/bin/protractor conf.js
 
 # stop selenium
