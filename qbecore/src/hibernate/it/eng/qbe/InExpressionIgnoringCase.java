9740928f17a4150fa5b7eb68ca12d2ed16a23e33
Salvatore Lupo <salvatore.lupo@eng.it>
2016-10-17 13:00:22 +0000

    hibernate - fixed handling case in InExpression

--- qbecore/src/hibernate/it/eng/qbe/InExpressionIgnoringCase.java
+++ qbecore/src/hibernate/it/eng/qbe/InExpressionIgnoringCase.java
@@ -9,6 +9,7 @@ import org.hibernate.EntityMode;
 import org.hibernate.HibernateException;
 import org.hibernate.criterion.CriteriaQuery;
 import org.hibernate.criterion.Criterion;
+import org.hibernate.engine.SessionFactoryImplementor;
 import org.hibernate.engine.TypedValue;
 import org.hibernate.type.CompositeType;
 import org.hibernate.type.Type;
@@ -16,7 +17,7 @@ import org.hibernate.util.StringHelper;
 
 /**
  * Code taken from see org.hibernate.criterion.InExpression
- * 
+ *
  */
 public class InExpressionIgnoringCase implements Criterion {
 
@@ -25,21 +26,23 @@ public class InExpressionIgnoringCase implements Criterion {
 
 	public InExpressionIgnoringCase(String propertyName, Object[] values) {
 		this.propertyName = propertyName;
-		this.values = values;
+		this.values = toLower(values);
 	}
 
 	public InExpressionIgnoringCase(String propertyName, Collection values) {
 		this.propertyName = propertyName;
-		this.values = values.toArray();
+		this.values = toLower(values);
 	}
 
 	@Override
 	public String toSqlString(Criteria criteria, CriteriaQuery criteriaQuery) throws HibernateException {
 		String[] columns = criteriaQuery.findColumns(propertyName, criteria);
-		final String[] wrappedLowerColumns = wrapLower(columns);
+		SessionFactoryImplementor factory = criteriaQuery.getFactory();
+		String lowerFunction = factory.getDialect().getLowercaseFunction();
+		final String[] wrappedLowerColumns = wrapLower(columns, lowerFunction);
 		if (criteriaQuery.getFactory().getDialect().supportsRowValueConstructorSyntaxInInList() || columns.length <= 1) {
 
-			String singleValueParam = StringHelper.repeat("lower(?), ", columns.length - 1) + "?";
+			String singleValueParam = StringHelper.repeat(lowerFunction + "(?), ", columns.length - 1) + "?";
 			if (columns.length > 1) {
 				singleValueParam = '(' + singleValueParam + ')';
 			}
@@ -50,7 +53,7 @@ public class InExpressionIgnoringCase implements Criterion {
 			}
 			return cols + " in (" + params + ')';
 		} else {
-			String cols = " ( " + StringHelper.join(" = lower(?) and ", wrappedLowerColumns) + "= lower(?) ) ";
+			String cols = " ( " + StringHelper.join(" = " + lowerFunction + "(?) and ", wrappedLowerColumns) + "= " + lowerFunction + "(?) ) ";
 			cols = values.length > 0 ? StringHelper.repeat(cols + "or ", values.length - 1) + cols : "";
 			cols = " ( " + cols + " ) ";
 			return cols;
@@ -83,12 +86,38 @@ public class InExpressionIgnoringCase implements Criterion {
 		return propertyName + " in (" + StringHelper.toString(values) + ')';
 	}
 
-	private String[] wrapLower(final String[] columns) {
+	private String[] wrapLower(final String[] columns, String lowerFunction) {
 		final String[] wrappedColumns = new String[columns.length];
 		for (int i = 0; i < columns.length; i++) {
-			wrappedColumns[i] = "lower(" + columns[i] + ")";
+			wrappedColumns[i] = lowerFunction + "(" + columns[i] + ")";
 		}
 		return wrappedColumns;
 	}
 
+	private Object[] toLower(Object[] values) {
+		Object[] ret = new Object[values.length];
+		for (int i = 0; i < values.length; i++) {
+			Object v = values[i];
+			if (v instanceof String) {
+				ret[i] = v.toString().toLowerCase();
+			} else {
+				ret[i] = v;
+			}
+		}
+		return ret;
+	}
+
+	private Object[] toLower(Collection values) {
+		Object[] ret = new Object[values.size()];
+		int i = 0;
+		for (Object v : values) {
+			if (v instanceof String) {
+				ret[i] = v.toString().toLowerCase();
+			} else {
+				ret[i] = v;
+			}
+			i++;
+		}
+		return ret;
+	}
 }
