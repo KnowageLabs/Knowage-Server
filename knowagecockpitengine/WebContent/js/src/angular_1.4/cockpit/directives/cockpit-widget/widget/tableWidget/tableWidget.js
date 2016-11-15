9d6b48740d94a42185026a86fe564ade53b6bc77
Alessandro Piovani <alessandro.piovani@eng.it>
2016-11-15 16:34:35 +0000

    alias replaced with aliasToShow in selectRow function

--- knowagecockpitengine/WebContent/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/tableWidget/tableWidget.js
+++ knowagecockpitengine/WebContent/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/tableWidget/tableWidget.js
@@ -149,21 +149,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 					
 				}	
 				
-				if(angular.equals($scope.ngModel.content.columnSelectedOfDataset[i].alias,columnName)){
+				if(angular.equals($scope.ngModel.content.columnSelectedOfDataset[i].aliasToShow,columnName)){
 					if($scope.ngModel.content.columnSelectedOfDataset[i].fieldType=="MEASURE")
 					{
 						return;
 					}
 					else
 					{
-
-
-						if(angular.equals($scope.ngModel.content.columnSelectedOfDataset[i].alias,columnName)){
 							if($scope.ngModel.content.columnSelectedOfDataset[i].isCalculated){
 								return;
 							}
-						}
-
 					}	
 				}
 			}
