f6d3a475e237c635283572f3f4bd408e72d1b55c
Davide Zerbetto <davide.zerbetto@eng.it>
2016-12-05 15:56:04 +0000

    RESOLVED KNOWAGE-1692: When cross navigating (old cross navigation mechanism), spaces in drivers' values are lost https://production.eng.it/jira/browse/KNOWAGE-1692

--- knowage/web-content/js/src/angular_1.4/tools/documentexecution/documentExecution.js
+++ knowage/web-content/js/src/angular_1.4/tools/documentexecution/documentExecution.js
@@ -500,7 +500,7 @@
 
 //from executed document, call this function to exec old cross navigation method
 var execCrossNavigation=function(frameid, doclabel, params, subobjid, title, target){
-	var jsonEncodedParams=params?JSON.parse('{"' + decodeURI(params).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"').replace(/\s/g,'') + '"}'):{};
+	var jsonEncodedParams=params?JSON.parse('{"' + decodeURI(params).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}'):{};
 	angular.element(frameElement).scope().$parent.internalNavigateTo(jsonEncodedParams,doclabel);
 };
 
