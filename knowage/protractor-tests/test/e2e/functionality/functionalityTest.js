ee6823de28b857cfb4ecdac871d9aedebd91de3c
Radmila Selakovic <radmila.selakovic@eng.it>
2016-12-23 15:45:34 +0000

    update

--- knowage/protractor-tests/test/e2e/functionality/functionalityTest.js
+++ knowage/protractor-tests/test/e2e/functionality/functionalityTest.js
@@ -5,8 +5,10 @@ describe('functionality-test', function() {
 	});
 
 	it('should try to login', function() {
+	
 	browser.ignoreSynchronization=true;
 	 browser.get('https://spagobitest:48081/knowage');
+		browser.sleep(10000);
 	 browser.driver.findElement(by.id('password')).sendKeys('test_functionality_admin');
 	 browser.driver.findElement(by.id('username')).sendKeys('test_functionality_admin');
 	 browser.driver.findElement(by.css('.btn-signin')).click();
