083c573c95aa1ad70d5137e4ce05fc75da9db1a3
Davide Zerbetto <davide.zerbetto@eng.it>
2017-01-26 11:15:02 +0000

    Fixed log trace

--- knowage/src/it/eng/spagobi/commons/filters/ProfileFilter.java
+++ knowage/src/it/eng/spagobi/commons/filters/ProfileFilter.java
@@ -156,7 +156,7 @@ public class ProfileFilter implements Filter {
 				chain.doFilter(request, response);
 			}
 		} catch (Exception e) {
-			logger.error(e);
+			logger.error("Error while service execution", e);
 		} finally {
 			// since TenantManager uses a ThreadLocal, we must clean after
 			// request processed in each case
