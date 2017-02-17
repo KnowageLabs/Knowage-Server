ab392bcf443613bbb4642b7332be214da815c36b
Francesco Lucchi <francesco.lucchi@eng.it>
2017-02-17 17:12:00 +0000

    Add DDL scripts for calendar for MySQL

---
+++ knowagedatabasescripts/mysql/MySQL_drop_calendar.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS a_t_ass_cal_cfg_attrib CASCADE ;
+DROP TABLE IF EXISTS a_t_cal_attributes CASCADE ;
+DROP TABLE IF EXISTS a_t_cal_cfg CASCADE ;
+DROP TABLE IF EXISTS a_t_cal_att_domain CASCADE ;
+DROP TABLE IF EXISTS a_t_calendar CASCADE ;
+DROP TABLE IF EXISTS a_d_time_by_day CASCADE ;
+DROP TABLE IF EXISTS hibernate_sequences CASCADE ;
