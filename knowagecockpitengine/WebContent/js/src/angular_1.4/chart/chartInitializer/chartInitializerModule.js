c2f2cbe8c305232d235e9f3838d0faca23deb415
Dragan Pirkovic <dragan.pirkovic@eng.it>
2017-03-29 14:06:21 +0000

    adding a service to chartIntializer module

--- knowagecockpitengine/WebContent/js/src/angular_1.4/chart/chartInitializer/chartInitializerModule.js
+++ knowagecockpitengine/WebContent/js/src/angular_1.4/chart/chartInitializer/chartInitializerModule.js
@@ -16,4 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-angular.module('chartInitializer',['JsonChartTemplateServiceModule']);
+angular.module('chartInitializer',['JsonChartTemplateServiceModule','chartengine.settings']);
