d795743d1792d40d00d132981227c3e7e9515257
Dragan Pirkovic <dragan.pirkovic@eng.it>
2017-05-22 10:36:46 +0000

    refactoring

--- knowagecockpitengine/WebContent/js/src/angular_1.4/chart/chartRenderer/directives/chartRenderer.js
+++ knowagecockpitengine/WebContent/js/src/angular_1.4/chart/chartRenderer/directives/chartRenderer.js
@@ -21,7 +21,7 @@ angular.module('chartRendererModule')
 	
 	return{
 		restrict:'E',
-		template:'<div ng-show="noLib">no library impl</div>',
+		template:'<div ></div>',
 		scope:{
 			
 			chartLibNamesConfig:'=',
@@ -59,7 +59,7 @@ angular.module('chartRendererModule')
 				scope.chartConf;
 				scope.chartTemplate;
 				scope.chartInitializer;
-				scope.noLib = false;
+			
 				
 				
 				
@@ -139,11 +139,12 @@ angular.module('chartRendererModule')
 				
 				var lib = getChartExecutionLib(scope.chartTemplate);
 				if(lib){
+					scope.noLib = false;
 					scope.chartInitializer = chartInitializerRetriver.getChartInitializer(lib);
 					scope.loadChart(scope.chartTemplate,scope.datasetLabel,data);
-					scope.noLib = false;
+					
 				}else{
-					scope.noLib = true;
+					element[0].innerHTML = "no library implementation";
 				}
 				
 				
@@ -180,12 +181,15 @@ angular.module('chartRendererModule')
 			if(!scope.widgetData){
 				var lib = getChartExecutionLib(scope.chartTemplate);
 				if(lib){
+					
 					scope.chartInitializer = chartInitializerRetriver.getChartInitializer(lib);
 					
 					scope.loadChart(scope.chartTemplate,scope.datasetLabel,undefined);
-					scope.noLib = false;
+					
+				}else{
+					element[0].innerHTML = "no library implementation";
 				}
-				scope.noLib = true;
+				
 			}
 			
 			
