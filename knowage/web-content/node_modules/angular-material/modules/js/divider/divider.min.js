260294079c71c37e44211bcdf5587fa1e15048d2
Dragan Pirkovic <dragan.pirkovic@eng.it>
2017-06-21 09:45:06 +0000

    angular material files

---
+++ knowage/web-content/node_modules/angular-material/modules/js/divider/divider.min.js
@@ -0,0 +1,7 @@
+/*!
+ * AngularJS Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v1.1.3-master-471c225
+ */
+!function(i,e,n){"use strict";function r(i){return{restrict:"E",link:i}}r.$inject=["$mdTheming"],e.module("material.components.divider",["material.core"]).directive("mdDivider",r)}(window,window.angular);
