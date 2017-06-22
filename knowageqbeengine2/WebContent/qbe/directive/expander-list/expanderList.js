ea45b388db4f341b952720d7f6b28a15f9a6e739
Davide Vernassa <davide.vernassa@eng.it>
2017-06-22 14:47:04 +0000

    changed colors behaviour

--- knowageqbeengine2/WebContent/qbe/directive/expander-list/expanderList.js
+++ knowageqbeengine2/WebContent/qbe/directive/expander-list/expanderList.js
@@ -1,17 +1,17 @@
 /**
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Knowage is distributed in the hope that it will be useful, 
+ * Knowage is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -42,9 +42,14 @@ angular.module('qbe_expander_list', ['ngDraggable'])
                     for (var k in scope.ngModel) {
                         if (scope.ngModel.hasOwnProperty(k)) {
                             for (var j in scope.ngModel[k]) {
-                                var color = { "background-color": scope.colors[scope.usedColorIndex] };
+                                var color = scope.colors[scope.usedColorIndex];
                                 scope.usedColorIndex++;
                                 scope.ngModel[k][j].color = color;
+                                if (scope.ngModel[k][j].children) {
+                                    for (var y in scope.ngModel[k][j].children) {
+                                        scope.ngModel[k][j].children[y].color = color;
+                                    }
+                                }
                             }
                         }
                     }
