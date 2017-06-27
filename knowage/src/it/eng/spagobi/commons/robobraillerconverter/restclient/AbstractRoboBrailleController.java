c00e943a37411b5467dfb222f2d1eaa4d0f411b3
Davide Vernassa <davide.vernassa@eng.it>
2017-06-27 15:57:18 +0000

    proxy resolution in robobraille

--- knowage/src/it/eng/spagobi/commons/robobraillerconverter/restclient/AbstractRoboBrailleController.java
+++ knowage/src/it/eng/spagobi/commons/robobraillerconverter/restclient/AbstractRoboBrailleController.java
@@ -16,63 +16,59 @@ import it.eng.spagobi.commons.robobraillerconverter.jobmanager.JobManager;
 import it.eng.spagobi.commons.robobraillerconverter.restclient.conf.ConfigInstance;
 import it.eng.spagobi.commons.robobraillerconverter.restclient.conf.RobobrailleConfiguration;
 import it.eng.spagobi.tools.dataset.ckan.utils.CKANUtils;
-import it.eng.spagobi.utilities.engines.rest.SimpleRestClient;
 
-public abstract class AbstractRoboBrailleController  implements JobManager {
-	
+public abstract class AbstractRoboBrailleController implements JobManager {
+
 	static protected Logger logger = Logger.getLogger(AbstractRoboBrailleController.class);
 	protected HttpClient httpClient;
 	protected HawkClient hawkClient;
 	protected ClientExecutor executor;
 	private RobobrailleConfiguration robobrailleConfiguration;
-	
+
 	public AbstractRoboBrailleController() {
-		robobrailleConfiguration  = ConfigInstance.getRobobrailleConfiguration();
+		robobrailleConfiguration = ConfigInstance.getRobobrailleConfiguration();
 		setHawkClient();
 		setHttpClient();
-		
+
 	}
-	
-	
-	private void setHttpClient(){
-		
-		String proxyHost = System.getProperty("http.proxyHost");
-		String proxyPort = System.getProperty("http.proxyPort");
+
+	private void setHttpClient() {
+
+		String proxyHost = System.getProperty("http.robobrailleProxyHost");
+		String proxyPort = System.getProperty("http.robobrailleProxyPort");
 		int proxyPortInt = CKANUtils.portAsInteger(proxyPort);
 		String proxyUsername = System.getProperty("http.proxyUsername");
 		String proxyPassword = System.getProperty("http.proxyPassword");
-		
+
 		httpClient = new HttpClient();
 		httpClient.setTimeout(10000);
-		if(proxyHost != null && proxyPortInt > 0){
-			httpClient.getHostConfiguration().setProxy(proxyHost,proxyPortInt);
+		if (proxyHost != null && proxyPortInt > 0 && proxyUsername != null && proxyUsername != "") {
+			httpClient.getHostConfiguration().setProxy(proxyHost, proxyPortInt);
 			HttpState state = new HttpState();
-			state.setProxyCredentials(null, null, new UsernamePasswordCredentials(proxyUsername,proxyPassword ));
+			state.setProxyCredentials(null, null, new UsernamePasswordCredentials(proxyUsername, proxyPassword));
 			httpClient.setState(state);
-		}else{
+		} else {
 			logger.debug("Setting proxy without authentication");
 			httpClient.getHostConfiguration().setProxy(proxyHost, proxyPortInt);
 			logger.debug("Proxy without authentication set");
 		}
-		
-		httpClient.getHostConfiguration().setHost(robobrailleConfiguration.getHost(), 
-																			robobrailleConfiguration.getPort(), 
-																			robobrailleConfiguration.getProtocol());
+
+		httpClient.getHostConfiguration().setHost(robobrailleConfiguration.getHost(), robobrailleConfiguration.getPort(),
+				robobrailleConfiguration.getProtocol());
 		executor = new ApacheHttpClientExecutor(httpClient);
-		}
+	}
 
 	private void setHawkClient() {
 		Builder builder = new HawkCredentials.Builder();
-	
+
 		builder.keyId(robobrailleConfiguration.getId());
 		builder.key(robobrailleConfiguration.getKey());
 		builder.algorithm(Algorithm.parse(robobrailleConfiguration.getAlgorithm()));
-	
-		HawkCredentials hawkCredentials = builder.build();
-	
-		hawkClient = new HawkClient.Builder().credentials(hawkCredentials).build();
-	
-	}
- 
-}
 
+		HawkCredentials hawkCredentials = builder.build();
+
+		hawkClient = new HawkClient.Builder().credentials(hawkCredentials).build();
+
+	}
+
+}
