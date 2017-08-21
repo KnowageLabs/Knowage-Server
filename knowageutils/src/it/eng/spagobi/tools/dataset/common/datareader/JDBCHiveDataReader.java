4b3389b1a7206534b8008d8a99a9456e9d16c7b6
Francesco Lucchi <francesco.lucchi@eng.it>
2017-08-21 09:45:57 +0000

    Fix management of column types for compatibility with Vertica DB

--- knowageutils/src/it/eng/spagobi/tools/dataset/common/datareader/JDBCHiveDataReader.java
+++ knowageutils/src/it/eng/spagobi/tools/dataset/common/datareader/JDBCHiveDataReader.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -41,117 +41,126 @@ import org.apache.log4j.Logger;
 public class JDBCHiveDataReader extends AbstractDataReader {
 
 	private static transient Logger logger = Logger.getLogger(JDBCHiveDataReader.class);
-    
-	
-	public JDBCHiveDataReader() { }
-	
-	public boolean isOffsetSupported() {return true;}	
-	public boolean isFetchSizeSupported() {return true;}	
-	public boolean isMaxResultsSupported() {return true;}
-    
-    public IDataStore read(Object data) throws EMFUserError, EMFInternalError {
-    	DataStore dataStore = null;
+
+	public JDBCHiveDataReader() {
+	}
+
+	@Override
+	public boolean isOffsetSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isFetchSizeSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isMaxResultsSupported() {
+		return true;
+	}
+
+	@Override
+	public IDataStore read(Object data) throws EMFUserError, EMFInternalError {
+		DataStore dataStore = null;
 		MetaData dataStoreMeta;
-    	int columnCount;
-    	int columnIndex;
+		int columnCount;
+		int columnIndex;
 		ResultSet rs;
 
-    	FieldMetadata fieldMeta;
-    	
+		FieldMetadata fieldMeta;
+
 		logger.debug("IN");
 
-		rs = (ResultSet) data;  
+		rs = (ResultSet) data;
 
-		
 		dataStore = new DataStore();
 		dataStoreMeta = new MetaData();
 		dataStore.setMetaData(dataStoreMeta);
 
-		try {				
+		try {
 			logger.debug("Reading data ...");
-    		if(getOffset() > 0) {
-    			logger.debug("Offset is equal to [" + getOffset() + "]");
-    			int i=0;
-     			while(i != getOffset()){
-     				rs.next();
-     				i++;
-     			}
-    			
-    		} else {
-    			logger.debug("Offset not set");
-    		}
-    		
-    		long maxRecToParse = Long.MAX_VALUE;
-    		if(getFetchSize() > 0) {
-    			maxRecToParse = getFetchSize();
-    			logger.debug("FetchSize is equal to [" + maxRecToParse + "]");
-    		} else {
-    			logger.debug("FetchSize not set");
-    		}
-    		int recCount = 0;
-    		int resultNumber = 0;
-    		while ((recCount < maxRecToParse) && rs.next()) {
-    			IRecord record = new Record(dataStore);
-            	logger.debug("Reading metadata ...");
-            	columnCount = rs.getMetaData().getColumnCount();
-            	
-        		for(columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-            		fieldMeta = new FieldMetadata();
-            		
-    				Object columnValue = rs.getObject(columnIndex);
-    				IField field = new Field( columnValue );
+			if (getOffset() > 0) {
+				logger.debug("Offset is equal to [" + getOffset() + "]");
+				int i = 0;
+				while (i != getOffset()) {
+					rs.next();
+					i++;
+				}
 
-    				record.appendField( field );					
-					
-            		String fieldName = rs.getMetaData().getColumnLabel(columnIndex);
-            		
-            		logger.debug("Field [" + columnIndex + "] name is equal to [" + fieldName + "]");
-            		if(dataStore.getMetaData().getFieldIndex(fieldName) == -1){
-                		fieldMeta.setName( fieldName );
-                		Class tpeClass = String.class;
-                		try {
-                			tpeClass = getType(rs.getMetaData().getColumnTypeName(columnIndex));
+			} else {
+				logger.debug("Offset not set");
+			}
+
+			long maxRecToParse = Long.MAX_VALUE;
+			if (getFetchSize() > 0) {
+				maxRecToParse = getFetchSize();
+				logger.debug("FetchSize is equal to [" + maxRecToParse + "]");
+			} else {
+				logger.debug("FetchSize not set");
+			}
+			int recCount = 0;
+			int resultNumber = 0;
+			while ((recCount < maxRecToParse) && rs.next()) {
+				IRecord record = new Record(dataStore);
+				logger.debug("Reading metadata ...");
+				columnCount = rs.getMetaData().getColumnCount();
+
+				for (columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+					fieldMeta = new FieldMetadata();
+
+					Object columnValue = rs.getObject(columnIndex);
+					IField field = new Field(columnValue);
+
+					record.appendField(field);
+
+					String fieldName = rs.getMetaData().getColumnLabel(columnIndex);
+
+					logger.debug("Field [" + columnIndex + "] name is equal to [" + fieldName + "]");
+					if (dataStore.getMetaData().getFieldIndex(fieldName) == -1) {
+						fieldMeta.setName(fieldName);
+						Class tpeClass = String.class;
+						try {
+							tpeClass = getType(rs.getMetaData().getColumnTypeName(columnIndex));
 						} catch (Exception e) {
-							logger.error("Can not read the type of the the clumn with index ["+columnIndex+"] and name ["+fieldName+"]",e);
+							logger.error("Can not read the type of the the clumn with index [" + columnIndex + "] and name [" + fieldName + "]", e);
 						}
-                		fieldMeta.setType(tpeClass );
-                		dataStore.getMetaData().addFiedMeta(fieldMeta);
-            		}
-            	}    
-        		
-    			dataStore.appendRecord(record);
-    			recCount++;
-    		}
-    		logger.debug("Readed [" + recCount+ "] records");
-    		logger.debug("Data readed succcesfully");
-    		
-    		if (this.isCalculateResultNumberEnabled()) {
-    			logger.debug("Calculation of result set number is enabled");
-    			resultNumber = getResultNumber(rs, maxRecToParse, recCount);
-    			dataStore.getMetaData().setProperty("resultNumber", new Integer(resultNumber));
-    		} else {
-    			logger.debug("Calculation of result set number is NOT enabled");
-    		}
+						fieldMeta.setType(tpeClass);
+						dataStore.getMetaData().addFiedMeta(fieldMeta);
+					}
+				}
 
-				
+				dataStore.appendRecord(record);
+				recCount++;
+			}
+			logger.debug("Readed [" + recCount + "] records");
+			logger.debug("Data readed succcesfully");
+
+			if (this.isCalculateResultNumberEnabled()) {
+				logger.debug("Calculation of result set number is enabled");
+				resultNumber = getResultNumber(rs, maxRecToParse, recCount);
+				dataStore.getMetaData().setProperty("resultNumber", new Integer(resultNumber));
+			} else {
+				logger.debug("Calculation of result set number is NOT enabled");
+			}
+
 		} catch (SQLException e) {
 			logger.error("An unexpected error occured while reading resultset", e);
-		}finally {
-    		logger.debug("OUT");
-    	}
-		
+		} finally {
+			logger.debug("OUT");
+		}
+
 		return dataStore;
-    }
-    
-	private int getResultNumber(ResultSet rs, long maxRecToParse, int recCount)
-			throws SQLException {
+	}
+
+	private int getResultNumber(ResultSet rs, long maxRecToParse, int recCount) throws SQLException {
 		logger.debug("IN");
-		
+
 		int toReturn = recCount;
 		int remaining = 0;
-		
-		logger.debug("resultset type [" + rs.getType() + "] (" + (rs.getType()  == ResultSet.TYPE_FORWARD_ONLY) + ")");
-		if (rs.getType()  == ResultSet.TYPE_FORWARD_ONLY) {
+
+		logger.debug("resultset type [" + rs.getType() + "] (" + (rs.getType() == ResultSet.TYPE_FORWARD_ONLY) + ")");
+		if (rs.getType() == ResultSet.TYPE_FORWARD_ONLY) {
 
 			int recordsReaded = 0;
 			if (recCount < maxRecToParse) {
@@ -159,7 +168,7 @@ public class JDBCHiveDataReader extends AbstractDataReader {
 				recordsReaded = recCount;
 			} else {
 				recordsReaded = getFetchSize();
-				//recordsCount = rs.getRow();
+				// recordsCount = rs.getRow();
 				while (rs.next()) {
 					remaining++;
 					// do nothing, just scroll result set
@@ -168,54 +177,52 @@ public class JDBCHiveDataReader extends AbstractDataReader {
 
 			toReturn = getOffset() + recordsReaded + remaining;
 		}
-		
+
 		logger.debug("Reading total record numeber is equal to [" + toReturn + "]");
 		logger.debug("OUT " + toReturn);
 		return toReturn;
 	}
-	
-	private Class getType(String type) throws SpagoBIEngineException{
+
+	private Class getType(String type) throws SpagoBIEngineException {
 		type = type.toUpperCase();
-		if(type.equals("STRING") ||type.equals("CHAR") || type.equals("VARCHAR") || type.equals("LONGVARCHAR")){
+		if (type.equals("STRING") || type.equals("CHAR") || type.equals("VARCHAR") || type.equals("LONGVARCHAR")) {
 			return String.class;
-		}else if(type.equals("NUMERIC") || type.equals("DECIMAL")){
+		} else if (type.equals("NUMERIC") || type.equals("DECIMAL")) {
 			return java.math.BigDecimal.class;
-		}else if(type.equals("BIT") || type.equals("BOOLEAN") ){
+		} else if (type.equals("BIT") || type.equals("BOOLEAN")) {
 			return Boolean.class;
-		}else if(type.equals("TINYINT")){
+		} else if (type.equals("TINYINT")) {
 			return Byte.class;
-		}else if(type.equals("SMALLINT")){
+		} else if (type.equals("SMALLINT")) {
 			return Short.class;
-		}else if(type.equals("INT")){
-			return Integer.class; 
-		}else if(type.equals("BIGINT")){
+		} else if (type.equals("INT") || type.equals("INTEGER")) {
+			return Integer.class;
+		} else if (type.equals("BIGINT")) {
 			return Long.class;
-		}else if(type.equals("REAL")){
+		} else if (type.equals("REAL")) {
 			return Float.class;
-		}else if(type.equals("FLOAT") || type.equals("DOUBLE")){
+		} else if (type.equals("FLOAT") || type.equals("DOUBLE") || type.equals("DOUBLE PRECISION")) {
 			return Double.class;
-		}else if(type.equals("BINARY") || type.equals("VARBINARY") || type.equals("LONGVARBINARY")){
+		} else if (type.equals("BINARY") || type.equals("VARBINARY") || type.equals("LONGVARBINARY")) {
 			return String.class;
-		}else if(type.equals("DATE") || type.equals("TIME")){
+		} else if (type.equals("DATE") || type.equals("TIME")) {
 			return java.sql.Date.class;
-		}else if(type.equals("TIME")){
+		} else if (type.equals("TIME")) {
 			return java.sql.Time.class;
-		}else if(type.equals("TIMESTAMP")){
+		} else if (type.equals("TIMESTAMP")) {
 			return java.sql.Timestamp.class;
-		}else if(type.equals("ARRAY")){
+		} else if (type.equals("ARRAY")) {
 			return String.class;
-		}else if(type.equals("STRUCT")){
+		} else if (type.equals("STRUCT")) {
 			return String.class;
-		}else if(type.equals("REF")){
+		} else if (type.equals("REF")) {
 			return Ref.class;
-		}else if(type.equals("DATALINK")){
+		} else if (type.equals("DATALINK")) {
 			return java.net.URL.class;
-		}else if(type.equals("MAP")){
+		} else if (type.equals("MAP")) {
 			return String.class;
 		}
-		throw new SpagoBIEngineException("Can not find a java type for ["+type+"]");
+		throw new SpagoBIEngineException("Can not find a java type for [" + type + "]");
 	}
 
-	
 }
-
