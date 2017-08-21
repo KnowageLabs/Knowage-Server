6aa3eb8f63491bd6f7f2ffd7a00e100f862159e3
Francesco Lucchi <francesco.lucchi@eng.it>
2017-08-21 08:47:33 +0000

    Fix loading of config values for PDF front/back page

--- knowageutils/src/it/eng/knowage/export/pdf/PDFCreator.java
+++ knowageutils/src/it/eng/knowage/export/pdf/PDFCreator.java
@@ -243,13 +243,13 @@ public abstract class PDFCreator {
 
 	private static InputStream getFrontpage() throws FileNotFoundException {
 		String path = SingletonConfig.getInstance().getConfigValue(ConfigurationConstants.DOCUMENT_EXPORTING_PDF_FRONT_PAGE);
-		return path.isEmpty() ? Thread.currentThread().getContextClassLoader().getResourceAsStream(PDFCreator.DEFAULT_FRONT_PAGE_RESOURCE_PATH)
-				: new FileInputStream(path);
+		return (path == null || path.isEmpty()) ? Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(PDFCreator.DEFAULT_FRONT_PAGE_RESOURCE_PATH) : new FileInputStream(path);
 	}
 
 	private static InputStream getBackpage() throws FileNotFoundException {
 		String path = SingletonConfig.getInstance().getConfigValue(ConfigurationConstants.DOCUMENT_EXPORTING_PDF_BACK_PAGE);
-		return path.isEmpty() ? Thread.currentThread().getContextClassLoader().getResourceAsStream(PDFCreator.DEFAULT_BACK_PAGE_RESOURCE_PATH)
-				: new FileInputStream(path);
+		return (path == null || path.isEmpty()) ? Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(PDFCreator.DEFAULT_BACK_PAGE_RESOURCE_PATH) : new FileInputStream(path);
 	}
 }
