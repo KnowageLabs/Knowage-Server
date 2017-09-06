96b3b224bb7cd6bbc2db59dbb9a47153770c2bb2
Alessandro Portosa <alessandro.portosa@eng.it>
2017-09-06 15:27:33 +0000

    Alert now uses NGSIv2, where the KPI label is the entity id

--- knowage/src/it/eng/spagobi/tools/alert/action/NotifyContextBroker.java
+++ knowage/src/it/eng/spagobi/tools/alert/action/NotifyContextBroker.java
@@ -1,23 +1,26 @@
 package it.eng.spagobi.tools.alert.action;
 
-import it.eng.spagobi.commons.metadata.SbiHibernateModel;
-import it.eng.spagobi.tools.alert.exception.AlertActionException;
-import it.eng.spagobi.tools.alert.job.AbstractAlertAction;
-import it.eng.spagobi.tools.alert.listener.KpiListener;
-import it.eng.spagobi.utilities.rest.RestUtilities;
-import it.eng.spagobi.utilities.rest.RestUtilities.HttpMethod;
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.log4j.Logger;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import it.eng.spagobi.commons.metadata.SbiHibernateModel;
+import it.eng.spagobi.security.hmacfilter.HMACSecurityException;
+import it.eng.spagobi.tools.alert.exception.AlertActionException;
+import it.eng.spagobi.tools.alert.job.AbstractAlertAction;
+import it.eng.spagobi.tools.alert.listener.KpiListener;
+import it.eng.spagobi.utilities.assertion.Assert;
+import it.eng.spagobi.utilities.rest.RestUtilities;
+import it.eng.spagobi.utilities.rest.RestUtilities.HttpMethod;
 
 public class NotifyContextBroker extends AbstractAlertAction {
 
@@ -26,104 +29,78 @@ public class NotifyContextBroker extends AbstractAlertAction {
 	@Override
 	public void executeAction(String jsonOptions, Map<String, String> externalParameters) throws AlertActionException {
 		logger.debug("IN");
-		JSONObject result = new JSONObject();
-		String url = null;
-		String type = null;
 		try {
 			JSONObject temp = new JSONObject(new JSONObject(jsonOptions).getString("jsonActionParameters"));
-			logger.debug("get json action parameters: " + temp);
+			logger.debug("Get JSON action parameters: " + temp);
 
-			url = temp.optString("contextBrokerUrl");
-			type = temp.optString("contextBrokerType");
+			String createUrl = temp.optString("contextBrokerUrl");
+			String entityType = temp.optString("contextBrokerType");
 
-			logger.debug("url: " + url + ", type: " + type);
+			logger.debug("Url: " + createUrl + ", Entity type: " + entityType);
 
-		} catch (JSONException e) {
-			logger.error("Error in recovering context broker settings ");
-			return;
-		}
-
-		// Get KPI Info
-		JSONObject kpiInfo = null;
-		if (externalParameters != null && !externalParameters.isEmpty()) {
-			for (Entry<String, String> entry : externalParameters.entrySet()) {
-				if (entry.getKey().equals(KpiListener.KPI_INFO_PLACEHOLDER)) {
-					String jsonString = entry.getValue();
-					try {
+			// Get KPI Info
+			JSONObject kpiInfo = null;
+			if (externalParameters != null && !externalParameters.isEmpty()) {
+				for (Entry<String, String> entry : externalParameters.entrySet()) {
+					if (entry.getKey().equals(KpiListener.KPI_INFO_PLACEHOLDER)) {
+						String jsonString = entry.getValue();
 						kpiInfo = new JSONObject(jsonString);
-					} catch (JSONException e) {
-						logger.error("No JsonObject from string " + jsonString + " could be parsed");
-						return;
+						break;
 					}
 				}
 			}
-		}
 
-		try {
+			Assert.assertNotNull(kpiInfo, "Impossible to obtain KPI info.");
+			Assert.assertTrue(kpiInfo.length() == 1, "Invalid length for kpiInfo. It must be equals to 1.");
 
-			JSONObject jsonToSend = createJSONObject(type, kpiInfo);
+			kpiInfo = kpiInfo.getJSONObject("kpiId");
+			JSONObject jsonToSend = createJSONObject(kpiInfo);
+			String entityId = kpiInfo.getString("kpiLabel");
+			String updateUrl = createUrl.endsWith("/") ? (createUrl + entityId + "/attrs") : (createUrl + "/" + entityId + "/attrs");
+			logger.debug("Update url: " + updateUrl);
 
-			Map<String, String> headersMap = new HashMap<String, String>();
+			Map<String, String> headersMap = new HashMap<>();
 			headersMap.put("Content-Type", "application/json");
 			headersMap.put("Accept", "application/json");
 
-			RestUtilities.Response response = RestUtilities.makeRequest(HttpMethod.Post, url, headersMap, jsonToSend.toString(), null);
+			RestUtilities.Response response = RestUtilities.makeRequest(HttpMethod.Post, updateUrl, headersMap, jsonToSend.toString(), null);
 			int statusCode = response.getStatusCode();
-			logger.debug("KPI object sent, status code returned " + statusCode);
+			if (statusCode == HttpStatus.SC_NOT_FOUND) {
+				logger.debug("The requested entity has not been found. This can be the first update, thus an entity will be created.");
+				jsonToSend.put("id", entityId);
+				jsonToSend.put("type", entityType);
+				response = RestUtilities.makeRequest(HttpMethod.Post, createUrl, headersMap, jsonToSend.toString(), null);
+				Assert.assertTrue(response.getStatusCode() == HttpStatus.SC_CREATED,
+						"Impossible to create entity " + entityId + ".\n Status code is not 201 but " + response.getStatusCode());
+			} else {
+				if (statusCode == HttpStatus.SC_NO_CONTENT) {
+					logger.debug("KPI object sent, status code returned " + statusCode);
+				} else {
+					throw new HttpException("Status code: " + statusCode + ".\n" + response.getResponseBody());
+				}
+			}
 
-		} catch (Exception e) {
-			logger.error("Error while sending schedule to context broker", e);
-			return;
+		} catch (JSONException | HMACSecurityException | IOException e) {
+			throw new AlertActionException("Error while sending schedule to context broker", e);
 		}
 
 	}
 
-	private JSONObject buildValueJsonObject(String name, String type, String value) throws JSONException {
+	private JSONObject buildValueJsonObject(String type, String value) throws JSONException {
 		JSONObject valueJson = new JSONObject();
-		valueJson.put("name", name);
-		valueJson.put("type", type);
 		valueJson.put("value", value);
+		valueJson.put("type", type);
 		return valueJson;
 	}
 
-	private JSONObject createJSONObject(String contextBrokerType, JSONObject kpiInfo) throws JSONException {
+	private JSONObject createJSONObject(JSONObject kpiInfo) throws JSONException {
 		logger.debug("IN");
 
 		JSONObject jsonToSend = new JSONObject();
 
-		JSONArray contextElementsJsonArray = new JSONArray();
-
-		// for each KPI
-		int i = 0;
-		for (Iterator it = kpiInfo.keys(); it.hasNext();) {
-			String key = (String) it.next();
-
-			JSONObject contextElementsJsonObj = new JSONObject();
-			contextElementsJsonObj.put("type", contextBrokerType);
-			contextElementsJsonObj.put("isPattern", "false");
-
-			JSONObject containedInfo = kpiInfo.getJSONObject(key);
-			String kpiId = containedInfo.optString("kpiId");
-			String kpiLabel = containedInfo.optString("kpiLabel");
-			String computedValue = containedInfo.optString("computedValue");
-			// String manualValue = containedInfo.optString("manualValue");
-
-			contextElementsJsonObj.put("id", kpiId);
-
-			JSONArray jsonAttributes = new JSONArray();
-			JSONObject valueJson = buildValueJsonObject("value", "double", computedValue);
-			JSONObject nameJson = buildValueJsonObject("label", "string", kpiLabel);
-
-			jsonAttributes.put(0, valueJson);
-			jsonAttributes.put(1, nameJson);
-			contextElementsJsonObj.put("attributes", jsonAttributes);
-
-			contextElementsJsonArray.put(i, contextElementsJsonObj);
-			i++;
-		}
-
-		jsonToSend.put("contextElements", contextElementsJsonArray);
-		jsonToSend.put("updateAction", "APPEND");
+		String computedValue = kpiInfo.optString("computedValue");
+		JSONObject valueJson = buildValueJsonObject("Double", computedValue);
+		jsonToSend.put("value", valueJson);
 
 		logger.debug("Returning value parsed in JSON: " + jsonToSend.toString());
 
@@ -134,7 +111,7 @@ public class NotifyContextBroker extends AbstractAlertAction {
 
 	@Override
 	public List<SbiHibernateModel> exportAction(String actionParams) {
-		return new ArrayList<SbiHibernateModel>();
+		return new ArrayList<>();
 	}
 
 }
