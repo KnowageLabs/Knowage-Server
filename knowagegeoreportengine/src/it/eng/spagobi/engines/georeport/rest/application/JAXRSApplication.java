716941db1ba1879bfd1db4a9acba647388564d34
Davide Zerbetto <davide.zerbetto@eng.it>
2017-09-11 09:07:37 +0000

    Moving to Resteasy 3

--- knowagegeoreportengine/src/it/eng/spagobi/engines/georeport/rest/application/JAXRSApplication.java
+++ knowagegeoreportengine/src/it/eng/spagobi/engines/georeport/rest/application/JAXRSApplication.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,28 +11,17 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package it.eng.spagobi.engines.georeport.interceptor;
+package it.eng.spagobi.engines.georeport.rest.application;
 
-import it.eng.spagobi.services.rest.ExternalEngineSecurityServerInterceptor;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
-import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
-import javax.ws.rs.ext.Provider;
-
-
-/**
- * The org.jboss.resteasy.spi.interception.PreProcessInterceptor runs after a JAX-RS resource method is found to invoke on, but before the actual invocation
- * happens
- *
- * Similar to SpagoBIAccessFilter but designed for REST services
- *
- */
-@Provider
-@Priority(Priorities.AUTHENTICATION)
-public class SecurityServerInterceptor extends ExternalEngineSecurityServerInterceptor {
-
+@ApplicationPath("/api")
+public class JAXRSApplication extends Application {
 }
+
+
