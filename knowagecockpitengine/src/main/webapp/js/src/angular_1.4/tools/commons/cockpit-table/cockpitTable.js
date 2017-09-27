555f638bcd88e51079e38958b512492b092ecbe0
Davide Vernassa <davide.vernassa@eng.it>
2017-09-27 09:26:39 +0000

    fixed sorting arrow in cockpit table

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/tools/commons/cockpit-table/cockpitTable.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/tools/commons/cockpit-table/cockpitTable.js
@@ -52,6 +52,7 @@
 
                     //returning if the order is ASC or DESC
                     scope.getSortingOrderAsBoolean = function(){
+                    	debugger;
                     	if(scope.settings.pagination.frontEnd && scope.settings.sortingOrder){
                     		return scope.settings.sortingOrder.toUpperCase() == 'DESC';
                     	}
