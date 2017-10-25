8c361054b72ad0ea6e7887eba31f47dee68080ca
Dragan Pirkovic <dragan.pirkovic@eng.it>
2017-10-25 14:49:09 +0000

    adding button back and it's click function

--- knowagewhatifengine/src/main/webapp/js/src/angular_1.x/scripts/directives/components/OlapDesigner.js
+++ knowagewhatifengine/src/main/webapp/js/src/angular_1.x/scripts/directives/components/OlapDesigner.js
@@ -145,6 +145,20 @@ $scope.saveMDX = function(){
 			
 		});			
 		};
+		
+		/**
+		  * Close the wizard
+		  */
+	$scope.closeOlapTemplate = function(){
+		 
+		 	if(sbiModule_config.externalBasePath == 'null'){
+		 		sbiModule_config.externalBasePath = '/knowage';
+		 	}
+			 var url= sbiModule_config.protocol+"://"+sbiModule_config.host+":"+sbiModule_config.port+sbiModule_config.externalBasePath;
+			 url+= "/servlet/AdapterHTTP?PAGE=DetailBIObjectPage&SBI_ENVIRONMENT=DOCBROWSER&LIGHT_NAVIGATOR_DISABLED=FALSE&MESSAGEDET=DETAIL_SELECT&OBJECT_ID="+sbiModule_docInfo.id;
+			 window.parent.location.href=url;
+
+		 }
 
 };
 
