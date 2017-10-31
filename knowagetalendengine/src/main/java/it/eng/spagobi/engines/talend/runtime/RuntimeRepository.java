b0497ef4be1b87a76016d8fb901055788a9fec7a
Giulio Gavardi <giulio.gavardi@eng.it>
2017-10-31 10:39:20 +0000

    can deploy Talend job to knowage using normal job deploy

--- knowagetalendengine/src/main/java/it/eng/spagobi/engines/talend/runtime/RuntimeRepository.java
+++ knowagetalendengine/src/main/java/it/eng/spagobi/engines/talend/runtime/RuntimeRepository.java
@@ -1,27 +1,21 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engines.talend.runtime;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 import it.eng.spagobi.engines.talend.exception.ContextNotFoundException;
 import it.eng.spagobi.engines.talend.exception.JobExecutionException;
 import it.eng.spagobi.engines.talend.exception.JobNotFoundException;
 import it.eng.spagobi.engines.talend.utils.TalendScriptAccessUtils;
 import it.eng.spagobi.engines.talend.utils.ZipUtils;
-import it.eng.spagobi.utilities.callbacks.audit.AuditAccessUtils;
-
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Properties;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
  * @author Andrea Gioia
@@ -29,59 +23,68 @@ import javax.servlet.http.HttpSession;
  */
 public class RuntimeRepository {
 	private File rootDir;
-	
+
 	/**
 	 * Instantiates a new runtime repository.
-	 * 
-	 * @param rootDir the root dir
+	 *
+	 * @param rootDir
+	 *            the root dir
 	 */
 	public RuntimeRepository(File rootDir) {
 		this.rootDir = rootDir;
 	}
-	
+
 	/**
 	 * Deploy job.
-	 * 
-	 * @param jobDeploymentDescriptor the job deployment descriptor
-	 * @param executableJobFiles the executable job files
+	 *
+	 * @param jobDeploymentDescriptor
+	 *            the job deployment descriptor
+	 * @param executableJobFiles
+	 *            the executable job files
 	 */
 	public void deployJob(JobDeploymentDescriptor jobDeploymentDescriptor, ZipFile executableJobFiles) {
 		File jobsDir = new File(rootDir, jobDeploymentDescriptor.getLanguage().toLowerCase());
-		File projectDir = new File(jobsDir, jobDeploymentDescriptor.getProject());		
-		ZipUtils.unzipSkipFirstLevel(executableJobFiles, projectDir);		
+		File projectDir = new File(jobsDir, jobDeploymentDescriptor.getProject());
+		ZipUtils.unzipSkipFirstLevel(executableJobFiles, projectDir);
 	}
-	
+
 	/**
 	 * Run job.
-	 * 
-	 * @param job the job
-	 * @param env the environment
-
-	 * 
-	 * @throws JobNotFoundException the job not found exception
-	 * @throws ContextNotFoundException the context not found exception
-	 * @throws JobExecutionException the job execution exception
+	 *
+	 * @param job
+	 *            the job
+	 * @param env
+	 *            the environment
+	 *
+	 *
+	 * @throws JobNotFoundException
+	 *             the job not found exception
+	 * @throws ContextNotFoundException
+	 *             the context not found exception
+	 * @throws JobExecutionException
+	 *             the job execution exception
 	 */
 	public void runJob(Job job, Map env) throws JobNotFoundException, ContextNotFoundException, JobExecutionException {
 		IJobRunner jobRunner;
-		
-		jobRunner = getJobRunner( job.getLanguage() );
-		if(jobRunner != null) {
-			jobRunner.run(job, env);
-		}		
+
+		jobRunner = getJobRunner(job.getLanguage());
+		// if(jobRunner != null) {
+		jobRunner.run(job, env);
+		// }
 	}
-	
+
 	/**
 	 * Gets the job runner.
-	 * 
-	 * @param jobLanguage the job language
-	 * 
+	 *
+	 * @param jobLanguage
+	 *            the job language
+	 *
 	 * @return the job runner
 	 */
 	public IJobRunner getJobRunner(String jobLanguage) {
-		if(jobLanguage.equalsIgnoreCase("java")) {
+		if (jobLanguage.equalsIgnoreCase("java")) {
 			return new JavaJobRunner(this);
-		} else if(jobLanguage.equalsIgnoreCase("perl")) {
+		} else if (jobLanguage.equalsIgnoreCase("perl")) {
 			return new PerlJobRunner(this);
 		} else {
 			return null;
@@ -90,7 +93,7 @@ public class RuntimeRepository {
 
 	/**
 	 * Gets the root dir.
-	 * 
+	 *
 	 * @return the root dir
 	 */
 	public File getRootDir() {
@@ -99,18 +102,20 @@ public class RuntimeRepository {
 
 	/**
 	 * Sets the root dir.
-	 * 
-	 * @param rootDir the new root dir
+	 *
+	 * @param rootDir
+	 *            the new root dir
 	 */
 	public void setRootDir(File rootDir) {
 		this.rootDir = rootDir;
 	}
-	
+
 	/**
 	 * Gets the executable job project dir.
-	 * 
-	 * @param job the job
-	 * 
+	 *
+	 * @param job
+	 *            the job
+	 *
 	 * @return the executable job project dir
 	 */
 	public File getExecutableJobProjectDir(Job job) {
@@ -118,49 +123,55 @@ public class RuntimeRepository {
 		File projectDir = new File(jobsDir, job.getProject());
 		return projectDir;
 	}
-	
+
 	/**
 	 * Gets the executable job dir.
-	 * 
-	 * @param job the job
-	 * 
+	 *
+	 * @param job
+	 *            the job
+	 *
 	 * @return the executable job dir
 	 */
 	public File getExecutableJobDir(Job job) {
 		File jobDir = new File(getExecutableJobProjectDir(job), job.getName());
 		return jobDir;
 	}
-	
+
 	/**
 	 * Gets the executable job file.
-	 * 
-	 * @param job the job
-	 * 
+	 *
+	 * @param job
+	 *            the job
+	 *
 	 * @return the executable job file
 	 */
 	public File getExecutableJobFile(Job job) {
-		File jobExecutableFile = new File(getExecutableJobDir(job), TalendScriptAccessUtils.getExecutableFileName(job));	
+		File jobExecutableFile = new File(getExecutableJobDir(job), TalendScriptAccessUtils.getExecutableFileName(job));
 		return jobExecutableFile;
 	}
-	
+
 	/**
 	 * Contains job.
-	 * 
-	 * @param job the job
-	 * 
+	 *
+	 * @param job
+	 *            the job
+	 *
 	 * @return true, if successful
 	 */
 	public boolean containsJob(Job job) {
 		return getExecutableJobFile(job).exists();
 	}
-	
+
 	/**
 	 * The main method.
-	 * 
-	 * @param args the arguments
-	 * 
-	 * @throws ZipException the zip exception
-	 * @throws IOException Signals that an I/O exception has occurred.
+	 *
+	 * @param args
+	 *            the arguments
+	 *
+	 * @throws ZipException
+	 *             the zip exception
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
 	 */
 	public static void main(String[] args) throws ZipException, IOException {
 		File rootDir = new File("C:\\Prototipi\\SpagoBI-Demo-1.9.2\\webapps\\SpagoBITalendEngine\\RuntimeRepository");
