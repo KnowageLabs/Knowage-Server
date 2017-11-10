ff1805983500237ba8f6103c95eab517027ab0c9
Alessandro Portosa <alessandro.portosa@eng.it>
2017-11-10 16:05:33 +0000

    Update version to 6.1.2-SNAPSHOT

--- knowageutils/src/main/java/it/eng/knowage/wapp/VersionInfo.java
+++ knowageutils/src/main/java/it/eng/knowage/wapp/VersionInfo.java
@@ -22,16 +22,16 @@ public final class VersionInfo {
 	
 	public static final int MAJOR = 6;
 	public static final int MINOR = 1;
-	public static final int PATCH = 1;
+	public static final int PATCH = 2;
 	public static final String VERSION_SEPARATOR = ".";	
 	// this can be "-next", "-rc" "-snapshot" or any optional string
-	public static final String OPTIONAL = "";
+	public static final String OPTIONAL = "-SNAPSHOT";
 	
 	public static final String COMPLETE_VERSION = VersionInfo.MAJOR + VersionInfo.VERSION_SEPARATOR + VersionInfo.MINOR + VersionInfo.VERSION_SEPARATOR + VersionInfo.PATCH + VersionInfo.OPTIONAL;
 	
-	public static final String YEAR = "2017";
-	public static final String MONTH = "11";
-	public static final String DAY = "10";
+	public static final String YEAR = "yyyy";
+	public static final String MONTH = "MM";
+	public static final String DAY = "dd";
 	public static final String DATE_SEPARATOR = "/";
 	
 	public static final String RELEASE_DATE = VersionInfo.YEAR + VersionInfo.DATE_SEPARATOR + VersionInfo.MONTH + VersionInfo.DATE_SEPARATOR + VersionInfo.DAY;
