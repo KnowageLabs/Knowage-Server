feb16da65fec8035d867408a79e128f717cc4c5a
Alberto Ghedin <alberto.ghedin.eng.it>
2017-11-30 10:53:48 +0000

    add credential

--- knowageutils/src/main/java/it/eng/spagobi/tools/dataset/common/dataproxy/MongoDataProxy.java
+++ knowageutils/src/main/java/it/eng/spagobi/tools/dataset/common/dataproxy/MongoDataProxy.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,20 +11,14 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.tools.dataset.common.dataproxy;
 
-import it.eng.spago.error.EMFUserError;
-import it.eng.spagobi.tools.dataset.common.datareader.IDataReader;
-import it.eng.spagobi.tools.dataset.common.datareader.MongoDataReader;
-import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
-import it.eng.spagobi.tools.datasource.bo.IDataSource;
-import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
-
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 
@@ -32,6 +26,14 @@ import com.mongodb.CommandResult;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+
+import it.eng.spago.error.EMFUserError;
+import it.eng.spagobi.tools.dataset.common.datareader.IDataReader;
+import it.eng.spagobi.tools.dataset.common.datareader.MongoDataReader;
+import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
+import it.eng.spagobi.tools.datasource.bo.IDataSource;
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 public class MongoDataProxy extends AbstractDataProxy {
 
@@ -94,6 +96,7 @@ public class MongoDataProxy extends AbstractDataProxy {
 		return load(dataReader);
 	}
 
+	@Override
 	public IDataStore load(IDataReader dataReader) {
 		logger.debug("IN");
 
@@ -133,17 +136,20 @@ public class MongoDataProxy extends AbstractDataProxy {
 		logger.debug("Connection URL: " + databaseUrl);
 		logger.debug("Database name: " + databaseName);
 
-		if (dataSource.getUser() != null && dataSource.getPwd() != null && dataSource.getUser().length() > 0 && dataSource.getPwd().length() > 0) {
-			MongoCredential credential = MongoCredential.createMongoCRCredential(dataSource.getUser(), databaseName, dataSource.getPwd().toCharArray());
-			// mongoClient = new MongoClient(new ServerAddress(clientUrl),
-			// Arrays.asList(credential));
-			logger.error("No autentication available yet");
-			throw new SpagoBIRuntimeException("No autentication available yet");
-		}
-
 		try {
+
+			logger.debug("Connected to mongodb");
+			if (dataSource.getUser() != null && dataSource.getPwd() != null && dataSource.getUser().length() > 0 && dataSource.getPwd().length() > 0) {
+				MongoCredential credential = MongoCredential.createMongoCRCredential(dataSource.getUser(), databaseName, dataSource.getPwd().toCharArray());
+				 mongoClient = new MongoClient(new ServerAddress(clientUrl),
+				 Arrays.asList(credential));
+			}else {
+				mongoClient = new MongoClient(databaseUrl);
+			}
+
+
 			logger.debug("Connecting to mongodb");
-			mongoClient = new MongoClient(databaseUrl);
+
 
 		} catch (UnknownHostException e) {
 			logger.error("Error connectiong to the MongoClient", e);
