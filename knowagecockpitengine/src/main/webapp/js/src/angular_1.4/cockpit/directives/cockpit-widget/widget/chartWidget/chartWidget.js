9d7907110d8478f4d22b0749f061ad453c6e8049
Radmila Selakovic <radmila.selakovic@eng.it>
2018-02-14 17:12:20 +0000

    RESOLVED KNOWAGE-2939 Cockpit: Extrernal cross navigation doesn't start from a chart https://production.eng.it/jira/browse/KNOWAGE-2939

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/chartWidget/chartWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/chartWidget/chartWidget.js
@@ -776,8 +776,14 @@ function cockpitChartWidgetControllerFunction(
 		var event= item.select != undefined ? item.select : item;
 		var crossParameters= createCrossParameters(item);
 		var chartType = $scope.ngModel.content.chartTemplate.CHART.type;
-		$scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLORCopy = angular.copy($scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLOR)
-		$scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLOR[0].value = item.point.color;
+		if(chartType.toLowerCase()=="pie"){
+			$scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLORCopy = angular.copy($scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLOR)
+			if($scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLOR[0]){
+				$scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLOR[0].value = item.point.color;
+			} else {
+				$scope.ngModel.content.chartTemplate.CHART.COLORPALETTE.COLOR.push({"value":item.point.color})
+			}
+		}
 		if($scope.ngModel.cliccable==false){
 			console.log("widget is not cliccable")
 			return;
