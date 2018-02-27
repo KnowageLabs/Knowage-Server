397a001e9c3bd3e94ed6ccdd8117203625a45b05
Filip Milosavljevic <filip.milosavljevic@eng.it>
2018-02-27 16:47:46 +0000

    added interceptors for error handling

---
+++ knowageqbeengine3/src/main/webapp/registry/services/interceptors/httpInterceptor.js
@@ -0,0 +1,16 @@
+(function () {
+
+	 angular.module('BlankApp')
+    .factory('httpInterceptor', function($q,$injector) {
+
+    	return{
+    	 'responseError': function(rejection) {
+    		 var sbiMsg = $injector.get('sbiModule_messaging');
+    		var msg = sbiMsg.showErrorMessage(rejection.data.errors[0].message,'Failure!!!' );
+    	      return $q.reject(msg);
+    	    }
+    	}
+    	return $q.reject(msg);
+       	   });
+
+})();
