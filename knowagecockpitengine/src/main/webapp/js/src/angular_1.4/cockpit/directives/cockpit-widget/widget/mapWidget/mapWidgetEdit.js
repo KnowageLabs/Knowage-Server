6877125278851f9378eb99eff80f8d0c38e26013
Davide Vernassa <davide.vernassa@eng.it>
2018-03-05 08:12:44 +0000

    advancements on map widget edit dialog

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/mapWidget/mapWidgetEdit.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/mapWidget/mapWidgetEdit.js
@@ -15,59 +15,59 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-(function() {
-	angular
-		.module('cockpitModule')
-		.controller('mapWidgetEditControllerFunction', MapWidgetEditControllerFunction)
+angular
+	.module('cockpitModule')
+	.controller('mapWidgetEditControllerFunction',mapWidgetEditControllerFunction)
 
-	function MapWidgetEditControllerFunction(
-			$scope,
-			finishEdit,
-			model,
-			sbiModule_translate,
-			$mdDialog,
-			mdPanelRef,
-			$mdToast,
-			$timeout,
-			cockpitModule_generalServices){
-			
-		$scope.translate=sbiModule_translate;
-		$scope.newModel = angular.copy(model);
-			
-		//get templates location
-	  	$scope.getTemplateUrl = function(template){
-	  		return cockpitModule_generalServices.getTemplateUrl('mapWidget',template);
-	  	}
-	  	
-	  	$scope.addLayer = function(ev) {
-	  		$mdDialog.show({
-				controller: function ($scope,$mdDialog) {
-					
-					$scope.add = function(){
-						$mdDialog.hide();
-					}
-					$scope.cancel = function(){
-						$mdDialog.cancel();
-					}
-				},
-				scope: $scope,
-				preserveScope:true,
-		      templateUrl: $scope.getTemplateUrl('mapWidgetAddLayerDialog'),
-		      targetEvent: ev,
-		      clickOutsideToClose:true,
-		      locals: {  }
-		    })
-		    .then(function() {});
-	  	}
-		$scope.saveConfiguration=function(){
-			 mdPanelRef.close();
-			 angular.copy($scope.newModel,model);
-			 finishEdit.resolve();
- 	  	}
+function mapWidgetEditControllerFunction($scope,finishEdit,model,sbiModule_translate,$mdDialog,mdPanelRef,$location){
+	$scope.translate=sbiModule_translate;
+	$scope.newModel = angular.copy(model);
+	
+	//get templates location
+  	$scope.basePath = $location.$$absUrl.substring(0,$location.$$absUrl.indexOf('api/'));
+  	$scope.templatesUrl = 'js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/mapWidget/templates/';
+  	$scope.getTemplateUrl = function(template){
+  		return $scope.basePath + $scope.templatesUrl + template +'.html';
+  	}
+  	
+  	$scope.expandRow = function(layer,content){
+  		if(layer.expanded && layer.expanded==content){
+  			delete layer.expanded;
+  		}else{
+  			layer.expanded = content;
+  		}
+  	}
+  	
+  	$scope.addLayer = function(ev) {
+  		$mdDialog.show({
+			controller: function ($scope,$mdDialog) {
+				
+				$scope.add = function(){
+					$mdDialog.hide();
+				}
+				$scope.cancel = function(){
+					$mdDialog.cancel();
+				}
+			},
+			scope: $scope,
+			preserveScope:true,
+	      templateUrl: $scope.getTemplateUrl('mapWidgetAddLayerDialog'),
+	      targetEvent: ev,
+	      clickOutsideToClose:true,
+	      locals: {  }
+	    })
+	    .then(function() {
 
-		$scope.cancelConfiguration=function(){
- 	  		mdPanelRef.close();
- 	  		finishEdit.reject();
- 	  	}
-	}
-})();
+	    });
+  	}
+	$scope.saveConfiguration=function(){
+		 mdPanelRef.close();
+		 angular.copy($scope.newModel,model);
+		 finishEdit.resolve();
+  	}
+
+	$scope.cancelConfiguration=function(){
+  		mdPanelRef.close();
+  		finishEdit.reject();
+  	}
+}
