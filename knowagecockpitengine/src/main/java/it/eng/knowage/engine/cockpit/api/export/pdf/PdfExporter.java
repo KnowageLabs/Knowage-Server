8fce48a93ad6a4c5c64171c6f96afd555ce105a3
Alessandro Portosa <alessandro.portosa@eng.it>
2018-03-12 16:33:50 +0000

    Fix issue with merge

--- knowagecockpitengine/src/main/java/it/eng/knowage/engine/cockpit/api/export/pdf/PdfExporter.java
+++ knowagecockpitengine/src/main/java/it/eng/knowage/engine/cockpit/api/export/pdf/PdfExporter.java
@@ -37,6 +37,7 @@ import org.json.JSONObject;
 import it.eng.knowage.export.pdf.ExportDetails;
 import it.eng.knowage.export.pdf.FrontpageDetails;
 import it.eng.knowage.export.pdf.PDFCreator;
+import it.eng.knowage.export.pdf.PageNumbering;
 import it.eng.knowage.slimerjs.wrapper.DeleteOnCloseFileInputStream;
 import it.eng.knowage.slimerjs.wrapper.SlimerJS;
 import it.eng.knowage.slimerjs.wrapper.SlimerJSConstants;
@@ -80,17 +81,15 @@ public class PdfExporter {
 
 		BIObject document = DAOFactory.getBIObjectDAO().loadBIObjectById(documentId);
 		int sheetCount = getSheetCount(document);
-		int sheetHeight = getSheetHeight(document);
-		RenderOptions renderOptionsWithFixedHeight = renderOptions.withDimensions(renderOptions.getDimensions().withHeight(sheetHeight));
 		URL url = new URL(requestUrl);
 		Map<String, String> authenticationHeaders = new HashMap<>(1);
 		String encodedUserId = Base64.encodeBase64String(userId.getBytes("UTF-8"));
 		authenticationHeaders.put("Authorization", "Direct " + encodedUserId);
-		List<InputStream> images = SlimerJS.render(url, sheetCount, renderOptionsWithFixedHeight);
+		List<InputStream> images = SlimerJS.render(url, sheetCount, renderOptions);
 		PDFCreator.createPDF(images, output, pdfFrontPage, pdfBackPage);
 
-		// PageNumbering pageNumbering = new PageNumbering(!pdfFrontPage, true, !pdfBackPage);
-		ExportDetails details = new ExportDetails(getFrontpageDetails(pdfFrontPage, document), null);
+		PageNumbering pageNumbering = new PageNumbering(!pdfFrontPage, true, !pdfBackPage);
+		ExportDetails details = new ExportDetails(getFrontpageDetails(pdfFrontPage, document), pageNumbering);
 		PDFCreator.addInformation(output, details);
 		try (InputStream is = new DeleteOnCloseFileInputStream(output.toFile())) {
 			return IOUtils.toByteArray(is);
@@ -99,16 +98,16 @@ public class PdfExporter {
 
 	private int getSheetCount(BIObject document) {
 		try {
+			ObjTemplate objTemplate = document.getActiveTemplate();
+			if (objTemplate == null) {
+				throw new SpagoBIRuntimeException("Unable to get template for document with id [" + documentId + "]");
+			}
 			int numOfPages = 0;
 			switch (document.getEngineLabel()) {
 			case "knowagechartengine":
 				numOfPages = 1;
 				return numOfPages;
 			case "knowagecockpitengine":
-				ObjTemplate objTemplate = document.getActiveTemplate();
-				if (objTemplate == null) {
-					throw new SpagoBIRuntimeException("Unable to get template for document with id [" + documentId + "]");
-				}
 				String templateString = new String(objTemplate.getContent());
 				JSONObject template = new JSONObject(templateString);
 				JSONArray sheets = template.getJSONArray("sheets");
@@ -119,44 +118,6 @@ public class PdfExporter {
 				return numOfPages;
 			}
 
-		} catch (EMFAbstractError e) {
-			throw new SpagoBIRuntimeException("Unable to get template for document with id [" + documentId + "]");
-		} catch (JSONException e) {
-			throw new SpagoBIRuntimeException("Invalid template for document with id [" + documentId + "]", e);
-		}
-	}
-
-	private int getSheetHeight(BIObject document) {
-		try {
-			int sheetHeight = Integer.valueOf(renderOptions.getDimensions().getHeight());
-			switch (document.getEngineLabel()) {
-			case "knowagechartengine":
-				break;
-			case "knowagecockpitengine":
-				ObjTemplate objTemplate = document.getActiveTemplate();
-				if (objTemplate == null) {
-					throw new SpagoBIRuntimeException("Unable to get template for document with id [" + documentId + "]");
-				}
-				String templateString = new String(objTemplate.getContent());
-				JSONObject template = new JSONObject(templateString);
-				JSONArray sheets = template.getJSONArray("sheets");
-				int sheetLabelHeigth = (sheets.length() > 0) ? 48 : 0;
-				for (int sheetIndex = 0; sheetIndex < sheets.length(); sheetIndex++) {
-					JSONObject sheet = (JSONObject) sheets.get(sheetIndex);
-					if (sheet.has("widgets")) {
-						JSONArray widgets = sheet.getJSONArray("widgets");
-						for (int widgetIndex = 0; widgetIndex < widgets.length(); widgetIndex++) {
-							JSONObject widget = (JSONObject) widgets.get(widgetIndex);
-							int row = widget.getInt("row");
-							int sizeY = widget.getInt("sizeY");
-							int widgetHeight = (row + sizeY) * 30 + sheetLabelHeigth; // scaling by cockpitModule_gridsterOptions.rowHeight
-							sheetHeight = Math.max(sheetHeight, widgetHeight);
-						}
-					}
-				}
-				break;
-			}
-			return sheetHeight;
 		} catch (EMFAbstractError e) {
 			throw new SpagoBIRuntimeException("Unable to get template for document with id [" + documentId + "]");
 		} catch (JSONException e) {
