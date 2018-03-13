d5058d9012a129081b9ef310e03710ed4d4a5bd1
Davide Zerbetto <davide.zerbetto@eng.it>
2018-03-13 15:13:45 +0000

    Bug fix: authentication through JavaScript SDK was not succeeding

--- knowage-core/src/main/java/it/eng/spagobi/commons/services/LoginActionWeb.java
+++ knowage-core/src/main/java/it/eng/spagobi/commons/services/LoginActionWeb.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,11 +11,17 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.commons.services;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import it.eng.spago.base.Constants;
 import it.eng.spago.base.RequestContainer;
@@ -31,6 +37,7 @@ import it.eng.spagobi.commons.utilities.SpagoBIServiceExceptionHandler;
 import it.eng.spagobi.commons.utilities.StringUtilities;
 import it.eng.spagobi.commons.utilities.UserUtilities;
 import it.eng.spagobi.commons.utilities.messages.MessageBuilder;
+import it.eng.spagobi.services.security.bo.SpagoBIUserProfile;
 import it.eng.spagobi.services.security.service.ISecurityServiceSupplier;
 import it.eng.spagobi.services.security.service.SecurityServiceSupplierFactory;
 import it.eng.spagobi.utilities.assertion.Assert;
@@ -38,13 +45,6 @@ import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
 import it.eng.spagobi.utilities.service.AbstractBaseHttpAction;
 import it.eng.spagobi.utilities.service.JSONSuccess;
 import it.eng.spagobi.wapp.services.ChangeTheme;
-import it.eng.spagobi.wapp.util.MenuUtilities;
-
-import java.io.IOException;
-import java.util.Locale;
-
-import org.apache.log4j.Logger;
-import org.json.JSONObject;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it) This action is used by SpagoBISDK module.
@@ -115,6 +115,7 @@ public class LoginActionWeb extends AbstractBaseHttpAction {
 			logger.debug("Configuration parameter [" + SSO_ACTIVE + "] is equals to [" + isSSOActive + "]");
 			logger.info("SSO is " + (isSSOActive ? "enabled" : "disabled"));
 
+			String userUniqueIdentifier = null;
 			// If SSO is not active, check username and password, i.e. performs the authentication;
 			// instead, if SSO is active, the authentication mechanism is provided by the SSO itself, so SpagoBI does not make
 			// any authentication, just creates the user profile object and puts it into Spago permanent container
@@ -124,26 +125,34 @@ public class LoginActionWeb extends AbstractBaseHttpAction {
 				Assert.assertNotNull(usr, "User identifier not spicified");
 				Assert.assertNotNull(usr, "Password not spicified");
 
-				if (supplier.checkAuthentication(usr, pwd) == null) {
+				SpagoBIUserProfile userProfile = null;
+				userProfile = supplier.checkAuthentication(usr, pwd);
+
+				if (userProfile == null) {
 					logger.warn("An attempt to authenticate with wrong credential has made [" + usr + "/" + pwd + "]");
 					AuditLogUtilities.updateAudit(getHttpRequest(), profile, "LOGIN", null, "KO");
 					throw new SpagoBIServiceException(SERVICE_NAME, "An attempt to authenticate with wrong credential has made [" + usr + "/" + pwd + "]");
 				}
 
+				// authentication was successful, we get the user unique identifier
+				userUniqueIdentifier = userProfile.getUniqueIdentifier();
+
 			} else {
-				usr = UserUtilities.getUserId(this.getHttpRequest());
+				// in case SSO is enabled, we get user unique identifier from request
+				userUniqueIdentifier = UserUtilities.getUserId(this.getHttpRequest());
 				AuditLogUtilities.updateAudit(getHttpRequest(), profile, "LOGIN", null, "KO");
 				Assert.assertNotNull(usr, "User identifier not found. Cannot build user profile object");
 
 			}
 
-			logger.info("User [" + usr + "] has been autheticated succesfully");
+			logger.info("User with unique identifier [" + userUniqueIdentifier + "] has been autheticated succesfully");
 
-			profile = UserUtilities.getUserProfile(usr);
+			profile = UserUtilities.getUserProfile(userUniqueIdentifier);
 			AuditLogUtilities.updateAudit(getHttpRequest(), profile, "LOGIN", null, "OK");
-			Assert.assertNotNull(profile, "Impossible to load profile for the user [" + usr + "]");
+			Assert.assertNotNull(profile, "Impossible to load profile for the user [" + userUniqueIdentifier + "]");
 
-			logger.info("User [" + usr + "] profile has been loaded succesfully");
+			String userId = (String) ((UserProfile) profile).getUserId();
+			logger.info("User [" + ((UserProfile) profile).getUserId() + "] profile has been loaded succesfully");
 
 			// Propagate user profile
 			getSessionContainer().getPermanentContainer().setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
@@ -167,29 +176,18 @@ public class LoginActionWeb extends AbstractBaseHttpAction {
 				getSessionContainer().getPermanentContainer().setAttribute(Constants.USER_COUNTRY, locale.getCountry());
 			}
 
-			String username = (String) ((UserProfile) profile).getUserId();
-			if (!UserUtilities.userFunctionalityRootExists(username)) {
-				logger.debug("Funcitonality root not yet exists for " + username);
-				UserUtilities.createUserFunctionalityRoot(profile);
-			} else {
-				logger.debug("Funcitonality root already exists for " + username);
-			}
-
-			MenuUtilities.getMenuItems(request, response, profile);
-			logger.debug("Menu items for user [" + username + "] succesfully loaded");
-
 			try {
 				JSONObject results = new JSONObject();
-				results.put("username", username);
+				results.put("username", userId);
 				results.put("userid", profile.getUserUniqueIdentifier());
-				logger.debug("Response for [" + username + "] succesfully built");
+				logger.debug("Response for [" + userId + "] succesfully built");
 				writeBackToClient(new JSONSuccess(results, callback));
-				logger.debug("Response for [" + username + "] succesfully written back to user");
+				logger.debug("Response for [" + userId + "] succesfully written back to user");
 			} catch (IOException e) {
 				AuditLogUtilities.updateAudit(getHttpRequest(), profile, "LOGIN", null, "KO");
 				throw new SpagoBIServiceException("Impossible to write back the responce to the client", e);
 			}
-		} catch (Throwable t) {
+		} catch (Exception t) {
 			try {
 				AuditLogUtilities.updateAudit(getHttpRequest(), profile, "LOGIN", null, "KO");
 			} catch (Exception e) {
