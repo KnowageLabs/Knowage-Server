405f0c3ea4dedcb921412cbe63017ed72ed92240
Giulio Gavardi <giulio.gavardi@eng.it>
2018-04-06 14:37:30 +0000

    hide menu if contained document cannot be seen

--- knowage-core/src/main/java/it/eng/spagobi/wapp/util/MenuAccessVerifier.java
+++ knowage-core/src/main/java/it/eng/spagobi/wapp/util/MenuAccessVerifier.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,57 +11,78 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.wapp.util;
 
-
-import it.eng.spago.error.EMFInternalError;
-import it.eng.spago.security.IEngUserProfile;
-import it.eng.spagobi.commons.bo.Role;
-import it.eng.spagobi.commons.bo.UserProfile;
-import it.eng.spagobi.wapp.bo.Menu;
-
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.apache.log4j.Logger;
+
+import it.eng.spago.error.EMFInternalError;
+import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
+import it.eng.spagobi.analiticalmodel.document.dao.IBIObjectDAO;
+import it.eng.spagobi.commons.bo.Role;
+import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.commons.dao.DAOFactory;
+import it.eng.spagobi.commons.utilities.ObjectsAccessVerifier;
+import it.eng.spagobi.wapp.bo.Menu;
+
 public class MenuAccessVerifier {
 
-
+	public static transient Logger logger = Logger.getLogger(MenuAccessVerifier.class);
 
 	public static boolean canView(Menu menu, IEngUserProfile profile) {
-
-		Role[] menuRoles=menu.getRoles();
-		Collection profileRoles=null;
+		logger.debug("IN");
+		Role[] menuRoles = menu.getRoles();
+		Collection profileRoles = null;
 
 		try {
-			profileRoles = ((UserProfile)profile).getRolesForUse();
+			profileRoles = ((UserProfile) profile).getRolesForUse();
 		} catch (EMFInternalError e) {
 			return false;
 		}
 
-
-		boolean found=false;
+		boolean found = false;
 		for (Iterator iterator = profileRoles.iterator(); iterator.hasNext() && !found;) {
 			String profileRole = (String) iterator.next();
-			for(int i=0;i<menuRoles.length && !found;i++){
-				Role menuRole=menuRoles[i];
-				String menuRoleName=menuRole.getName();
+			for (int i = 0; i < menuRoles.length && !found; i++) {
+				Role menuRole = menuRoles[i];
+				String menuRoleName = menuRole.getName();
 
-				if(menuRoleName.equals(profileRole)){
-					found=true;
+				if (menuRoleName.equals(profileRole)) {
+					found = true;
 				}
 			}
 
 		}
+		if (found == true) {
+			// if menu contains a document that is not visible to user do not show menu
+			Integer objId = menu.getObjId();
+			if (objId != null) {
+				try {
+					IBIObjectDAO dao = DAOFactory.getBIObjectDAO();
+					dao.setUserProfile(profile);
+					BIObject obj = dao.loadBIObjectById(objId);
+					if (obj == null)
+						found = false;
+					else
+						found = ObjectsAccessVerifier.canSee(obj, profile);
+				} catch (Exception e) {
+					logger.error("error in evaluating menu visibility by Object contained", e);
+					return false;
+				}
+
+			}
+		}
+		logger.debug("OUT");
+
 		return found;
 
-
 	}
-
-
 
 }
