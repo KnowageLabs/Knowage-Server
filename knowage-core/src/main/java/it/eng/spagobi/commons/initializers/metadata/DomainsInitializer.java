568f59b94355feb70ee865cb255282435aff4acf
Alessandro Portosa <alessandro.portosa@eng.it>
2018-04-13 13:20:05 +0000

    Add KPI/Scorecard/Target management also for DEV_ROLE

--- knowage-core/src/main/java/it/eng/spagobi/commons/initializers/metadata/DomainsInitializer.java
+++ knowage-core/src/main/java/it/eng/spagobi/commons/initializers/metadata/DomainsInitializer.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,15 +11,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.commons.initializers.metadata;
-
-import it.eng.spago.base.SourceBean;
-import it.eng.spagobi.commons.metadata.SbiDomains;
-import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -28,7 +24,10 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+
+import it.eng.spago.base.SourceBean;
+import it.eng.spagobi.commons.metadata.SbiDomains;
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
@@ -42,7 +41,8 @@ public class DomainsInitializer extends SpagoBIInitializer {
 		targetComponentName = "Domains";
 		configurationFileName = "it/eng/spagobi/commons/initializers/metadata/config/domains.xml";
 	}
-	
+
+	@Override
 	public void init(SourceBean config, Session hibernateSession) {
 		logger.debug("IN");
 		try {
@@ -54,7 +54,7 @@ public class DomainsInitializer extends SpagoBIInitializer {
 				writeDomains(hibernateSession);
 			} else {
 				logger.debug("Domains table is already populated, only missing domains will be populated");
-				writeMissingDomains(hibernateSession);				
+				writeMissingDomains(hibernateSession);
 			}
 		} catch (Throwable t) {
 			throw new SpagoBIRuntimeException("Ab unexpected error occured while initializeng Domains", t);
@@ -62,7 +62,7 @@ public class DomainsInitializer extends SpagoBIInitializer {
 			logger.debug("OUT");
 		}
 	}
-	
+
 	private void writeDomains(Session aSession) throws Exception {
 		logger.debug("IN");
 		SourceBean domainsSB = getConfiguration();
@@ -82,12 +82,13 @@ public class DomainsInitializer extends SpagoBIInitializer {
 			aDomain.setValueCd((String) aDomainSB.getAttribute("valueCd"));
 			aDomain.setValueNm((String) aDomainSB.getAttribute("valueNm"));
 			aDomain.setValueDs((String) aDomainSB.getAttribute("valueDs"));
-			logger.debug("Inserting Domain with valueCd = [" + aDomainSB.getAttribute("valueCd") + "], domainCd = [" + aDomainSB.getAttribute("domainCd") + "] ...");
+			logger.debug(
+					"Inserting Domain with valueCd = [" + aDomainSB.getAttribute("valueCd") + "], domainCd = [" + aDomainSB.getAttribute("domainCd") + "] ...");
 			aSession.save(aDomain);
 		}
 		logger.debug("OUT");
 	}
-	
+
 	private void writeMissingDomains(Session aSession) throws Exception {
 		logger.debug("IN");
 		SourceBean domainsSB = getConfiguration();
@@ -98,66 +99,60 @@ public class DomainsInitializer extends SpagoBIInitializer {
 		if (domainsList == null || domainsList.isEmpty()) {
 			throw new Exception("No predefined domains found!!!");
 		}
-		
+
 		List alreadyExamined = new ArrayList();
 		Iterator it = domainsList.iterator();
 		while (it.hasNext()) {
 			SourceBean aDomainSB = (SourceBean) it.next();
-			if(!alreadyExamined.contains(aDomainSB)){
-				
-			String domainCd = (String) aDomainSB.getAttribute("domainCd");
-			if (domainCd == null || domainCd.equals("")) {
-				logger.error("No predefined domains code found!!!");
-				throw new Exception("No predefined domains code found!!!");
-			}
-			//Retrieving all the domains in the DB with the specified domain Code
-			logger.debug("Retrieving all the domains in the DB with the specified domain Code");
-			String hql = "from SbiDomains where domainCd = '"+domainCd+"'";
-			Query hqlQuery = aSession.createQuery(hql);
-			List result = hqlQuery.list();
-			
-			logger.debug("Retrieving all the domains in the XML file with the specified domain Code");
-			//Retrieving all the domains in the XML file with the specified domain Code
-			List domainsXmlList = domainsSB.getFilteredSourceBeanAttributeAsList("DOMAIN", "domainCd", domainCd);
-			
-			logger.debug("Retrieving all the domains in the XML file with the specified domain Code");
-			//Checking if the domains in the DB are less than the ones in the xml file
-			if(result.size() < domainsXmlList.size()){
-				//Less domains in the DB than in the XML file, will add new ones
-				logger.debug("Less domains in the DB than in the XML file, will add new ones");
-				addMissingDomains(aSession,result,domainsXmlList);
-			}
-			//Removing form the list of XML domains the ones already checked
-			logger.debug("Adding to the list of XML domains already checked");
-			alreadyExamined.addAll(domainsXmlList);
+			if (!alreadyExamined.contains(aDomainSB)) {
+
+				String domainCd = (String) aDomainSB.getAttribute("domainCd");
+				if (domainCd == null || domainCd.equals("")) {
+					logger.error("No predefined domains code found!!!");
+					throw new Exception("No predefined domains code found!!!");
+				}
+				// Retrieving all the domains in the DB with the specified domain Code
+				logger.debug("Retrieving all the domains in the DB with the specified domain Code");
+				String hql = "from SbiDomains where domainCd = '" + domainCd + "'";
+				Query hqlQuery = aSession.createQuery(hql);
+				List result = hqlQuery.list();
+
+				logger.debug("Retrieving all the domains in the XML file with the specified domain Code");
+				// Retrieving all the domains in the XML file with the specified domain Code
+				List domainsXmlList = domainsSB.getFilteredSourceBeanAttributeAsList("DOMAIN", "domainCd", domainCd);
+
+				addMissingDomains(aSession, result, domainsXmlList);
+				// Removing form the list of XML domains the ones already checked
+				logger.debug("Adding to the list of XML domains already checked");
+				alreadyExamined.addAll(domainsXmlList);
 			}
 		}
 		logger.debug("OUT");
 	}
-	
-	private void addMissingDomains(Session aSession, List dbDomains, List xmlDomains){
+
+	private void addMissingDomains(Session aSession, List dbDomains, List xmlDomains) {
 		logger.debug("IN");
-		
+
 		Iterator it2 = xmlDomains.iterator();
-		while(it2.hasNext()){
+		while (it2.hasNext()) {
 			boolean existsInDb = false;
 			SourceBean aDomainSB = (SourceBean) it2.next();
 			String valueCdXml = (String) aDomainSB.getAttribute("valueCd");
-			logger.debug("Retrieved valueCd of XML Domain: "+valueCdXml);
-			
+			logger.debug("Retrieved valueCd of XML Domain: " + valueCdXml);
+
 			Iterator it = dbDomains.iterator();
 			while (it.hasNext()) {
-				SbiDomains d = (SbiDomains)it.next();
+				SbiDomains d = (SbiDomains) it.next();
 				String valueCd = d.getValueCd();
-				logger.debug("Retrieved valueCd of DB Domain: "+valueCd);
-				
-				if(valueCdXml.equalsIgnoreCase(valueCd)){
+				logger.debug("Retrieved valueCd of DB Domain: " + valueCd);
+
+				if (valueCdXml.equalsIgnoreCase(valueCd)) {
 					existsInDb = true;
 					logger.debug("Domain already exists in the DB");
 					break;
-				}				
-			}	
-			if(!existsInDb){
+				}
+			}
+			if (!existsInDb) {
 				logger.debug("Domain doesn't exist in the DB");
 				SbiDomains aDomain = new SbiDomains();
 				aDomain.setDomainCd((String) aDomainSB.getAttribute("domainCd"));
@@ -166,11 +161,12 @@ public class DomainsInitializer extends SpagoBIInitializer {
 				aDomain.setValueNm((String) aDomainSB.getAttribute("valueNm"));
 				aDomain.setValueDs((String) aDomainSB.getAttribute("valueDs"));
 				logger.debug("New Domain ready to be iserted in the DB");
-				logger.debug("Inserting Domain with valueCd = [" + aDomainSB.getAttribute("valueCd") + "], domainCd = [" + aDomainSB.getAttribute("domainCd") + "] ...");
+				logger.debug("Inserting Domain with valueCd = [" + aDomainSB.getAttribute("valueCd") + "], domainCd = [" + aDomainSB.getAttribute("domainCd")
+						+ "] ...");
 				aSession.save(aDomain);
 				logger.debug("New Domain iserted in the DB");
 			}
-		}		
+		}
 		logger.debug("OUT");
 	}
 
