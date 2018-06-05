39c9c02554b2a03589ceb4fd4d7b154c34150edc
Francesco Lucchi <francesco.lucchi@eng.it>
2018-06-05 15:08:26 +0000

    Fix schema loading from Vertica database

--- knowageutils/src/main/java/it/eng/spagobi/utilities/database/VerticaDataBase.java
+++ knowageutils/src/main/java/it/eng/spagobi/utilities/database/VerticaDataBase.java
@@ -19,7 +19,9 @@
 package it.eng.spagobi.utilities.database;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import it.eng.spagobi.tools.datasource.bo.IDataSource;
 
@@ -31,7 +33,18 @@ public class VerticaDataBase extends AbstractDataBase implements MetaDataBase {
 
 	@Override
 	public String getSchema(Connection conn) throws SQLException {
-		return conn.getSchema();
+		Statement stmt = conn.createStatement();
+		try {
+			ResultSet rs = stmt.executeQuery("SELECT CURRENT_SCHEMA");
+			try {
+				rs.next();
+				return rs.getString(1);
+			} finally {
+				rs.close();
+			}
+		} finally {
+			stmt.close();
+		}
 	}
 
 	@Override
