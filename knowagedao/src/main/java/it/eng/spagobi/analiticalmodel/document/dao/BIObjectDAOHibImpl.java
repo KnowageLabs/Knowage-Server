100c87c74c698a636fb9b397732d729b475bd0a4
Antonella Giachino <antonella.giachino@eng.it>
2018-06-18 12:54:59 +0000

    Fixed bug KNOWAGE-2855:"When clone a document the output parameters are lost"

--- knowagedao/src/main/java/it/eng/spagobi/analiticalmodel/document/dao/BIObjectDAOHibImpl.java
+++ knowagedao/src/main/java/it/eng/spagobi/analiticalmodel/document/dao/BIObjectDAOHibImpl.java
@@ -1774,6 +1774,7 @@ public class BIObjectDAOHibImpl extends AbstractHibernateDAO implements IBIObjec
 		outp.setFormatCode(hiObjPar.getFormatCode());
 		outp.setFormatValue(hiObjPar.getFormatValue());
 		outp.setBiObjectId(hiObjPar.getBiobjId());
+		outp.setIsUserDefined(hiObjPar.getIsUserDefined());
 
 		return outp;
 	}
