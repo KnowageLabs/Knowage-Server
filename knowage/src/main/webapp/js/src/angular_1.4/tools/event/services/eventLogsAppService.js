eb6c33ff9d3ac210ba6f5a69c604befa8b9b8147
Marko Kostić <marko.kostic@eng.it>
2018-06-22 09:12:59 +0000

    eventLogsAppService.js

--- knowage/src/main/webapp/js/src/angular_1.4/tools/event/services/eventLogsAppService.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/event/services/eventLogsAppService.js
@@ -1,19 +1,57 @@
 (function(){
 
 
-var app = angular.module("eventModule").service("eventService", function (){
+	var app = angular.module("eventModule");
+	
+	app.service("eventService", function (){
+		
+		var allEvents = [{         
 
-})
+                "id" : 1,
+
+                "user" : "biuser",
+                
+                "date" : "17/04/2018",
+
+                "type" : "type1",
+
+                "desc" : "someText"
+            },
+
+            {         
+
+                "id" : 2,
+
+                "user" : "biadmin",
+
+                "date" : "17/04/2018",
+
+                "type" : "type2",
+
+                "desc" : "someText"
+            },
+
+            {         
+
+                "id" : 3,
+
+                "user" : "biuser",
+
+                "date" : "17/04/2018",
+
+                "type" : "type3",
+
+                "desc" : "someText"
+
+            }
+        ]
+		
+		this.getAllEvents = function() {
+			
+			return allEvents;
+		}
+
+	});
 
 
-
-
-
-
-
-
-
-
-
-}
-())
+}());
