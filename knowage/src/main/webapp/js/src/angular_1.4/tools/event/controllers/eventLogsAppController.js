f76830aa6502894fcf63725de5b989989c4776ce
Miloš Šešić <milos.sesic@eng.it>
2018-07-04 15:02:56 +0000

    IN PROGRESS KNOWAGE-3209 https://production.eng.it/jira/browse/KNOWAGE-3209 As an admin, I'd like to see the entire list of events that were registered by the platform

--- knowage/src/main/webapp/js/src/angular_1.4/tools/event/controllers/eventLogsAppController.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/event/controllers/eventLogsAppController.js
@@ -3,46 +3,43 @@
 var app = angular.module("eventModule").controller("eventController",["$scope","eventService","sbiModule_messaging", function ($scope,eventService,sbiModule_messaging){
 
 	$scope.eventSelectModel = ["SCHEDULER", "ETL", "COMMONJ", "DATA_MINING"]
-$scope.pageChangedFun = function(itemsPerPage, currentPageNumber) {
-		
+	$scope.pageChangedFun = function(itemsPerPage, currentPageNumber) {
+
 		$scope.fetchsize = itemsPerPage;
-		
+
 		$scope.offset = 0;
-		
-		
+
 		if(currentPageNumber > 1) {
 			$scope.offset = (currentPageNumber -1) * $scope.fetchsize;
 		}
-		
+
 		$scope.filter = {
 			offset: $scope.offset,
 			fetchsize: itemsPerPage
 		}
-		
+
 		if($scope.startDate != undefined) {
 			$scope.filter.startDate = $scope.startDate;
 		}
-		
+
 		if($scope.endDate != undefined) {
 			$scope.filter.endDate = $scope.endDate;
 		}
-		
+
 		if($scope.type != undefined) {
 			$scope.filter.type = $scope.type;
 		}
-		
-		
+
+
 		eventService.getAllEvents($scope.filter).then(function(response) {
-			
 			$scope.events = response.data.results;
 			$scope.totalItemCountt = response.data.total;
-			
+
 		}, function(response) {
-			
 			sbiModule_messaging.showErrorMessage(response.data.errors[0].message, "Error");
-			
+
 		});
-		
+
 	}
 
 	$scope.showDetail = false;
@@ -56,16 +53,24 @@ $scope.pageChangedFun = function(itemsPerPage, currentPageNumber) {
 
 	$scope.searchEvents = function (){
 
-		var startDateFormat = moment($scope.startDate).format("YYYY-MM-DD HH:mm:ss");
-		var endDateFormat = moment($scope.endDate).format("YYYY-MM-DD HH:mm:ss");
-		
-		var filter = {
+		if($scope.startDate){
+			var startDateFormat = moment($scope.startDate).format("YYYY-MM-DD HH:mm:ss");
+			$scope.filter.startDate = startDateFormat;
 
-			startDate:startDateFormat,
-			endDate:endDateFormat,
-			type:$scope.type
 		}
-		eventService.getAllEvents(filter)
+
+		if($scope.endDate){
+			var endDateFormat = moment($scope.endDate).format("YYYY-MM-DD HH:mm:ss");
+			$scope.filter.endDate = endDateFormat;
+		}
+
+		if($scope.type){
+			var type = $scope.type;
+			$scope.filter.type = type;
+
+		}
+
+		eventService.getAllEvents($scope.filter)
 		.then(function(response){
 			$scope.events=response.data.results;
 		},	  function(response){
