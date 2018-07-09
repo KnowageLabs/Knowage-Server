6e7e36eee71ce521d55c292689779f49a53c83c6
Dragan Pirkovic <dragan.pirkovic@eng.it>
2018-07-09 23:47:15 +0000

    bug knowage-2973 deleting federation definition as a final user only if it is owner

---
+++ knowage-core/src/main/java/it/eng/spagobi/dataset/federation/exceptions/FederationDefinitionAccessException.java
@@ -0,0 +1,41 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.eng.spagobi.dataset.federation.exceptions;
+
+import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
+
+public class FederationDefinitionAccessException extends SpagoBIRuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FederationDefinitionAccessException(String message) {
+		super(message);
+
+	}
+
+	public FederationDefinitionAccessException(String message, Throwable ex) {
+		super(message, ex);
+	}
+
+	public FederationDefinitionAccessException(Throwable ex) {
+		super(ex);
+	}
+}
