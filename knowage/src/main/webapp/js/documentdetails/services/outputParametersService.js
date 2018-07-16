8ad82ae04f90e412d78eb4ad011078727aaeed40
Andrijana Predojevic <andrijana.predojevic@eng.it>
2018-07-16 15:39:22 +0000

    Refactored code and added service for each controller

--- knowage/src/main/webapp/js/documentdetails/services/outputParametersService.js
+++ knowage/src/main/webapp/js/documentdetails/services/outputParametersService.js
@@ -1,22 +1,59 @@
-(function () {
-    angular.module('OutputParametersModule', [])
-    	   .service('outputParametersService', ['$location','resourceService', function($location,resourceService) {
-    		   var documentData = {}
-    		   var crudService = resourceService;
-    		   documentData.documentInfoObject =$location.search();
-    		   documentData.requiredPath = "2.0/documents1";    		   
-			   documentData.changedOutputParameters = [];			   
-    		   documentData.outputParametersForDeleting = [];
+/**
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-    		   if( documentAndInfo.document){
-    			   documentData.document = documentAndInfo.document;
-    			   documentData.drivers = documentAndInfo.drivers
-    		   } else {
-    			   documentData.document = {};
-    			   documentData.drivers = [];
-    		   }    		   
+(function () {
+    angular.module('OutputParametersModule', ['sbiModule'])
+    	   .service('outputParametersService', ['resourceService', 'DocumentService', 'sbiModule_translate', 'sbiModule_messaging', function(resourceService,DocumentService,sbiModule_translate,sbiModule_messaging) {
+    		   var outputResource = {};
+    		   var documentService = DocumentService;
+    		   var requiredPath = "2.0/documents1";    		   
+    		   outputResource.changedOutputParameters = [];			   
+    		   outputResource.outputParametersForDeleting = [];
+
+    		   outputResource.persistOutputParameters = function() {
+    	        	for(var i = 0; i < outputResource.changedOutputParameters.length; i++) {
+    	        		if(!outputResource.changedOutputParameters[i].id) {
+    	        			delete outputResource.changedOutputParameters[i].$$hashKey;
+    	        			var outputParametersPostBasePath = documentService.document.id + '/outputparameters';
+    	        			resourceService.post(requiredPath, outputParametersPostBasePath, outputResource.changedOutputParameters[i]);
+    	        		} else {
+    	        			delete outputResource.changedOutputParameters[i].$$hashKey;
+    	        			var outputParametersPutBasePath = documentService.document.id + "/outputparameters/" + outputResource.changedOutputParameters[i].id;
+    	        			resourceService.put(requiredPath, outputParametersPutBasePath, outputResource.changedOutputParameters[i]);
+    	        		}
+    	        	}
+    	        };
+    	        
+    	        var deleteOutputParameterById = function(outputParameter) {
+    	        	 var basePath = documentService.document.id + "/" + 'outputparameters';
+    	        	 var basePathWithId = basePath + "/" + outputParameter.id;
+    	        	 resourceService.delete(requiredPath, basePathWithId).then(function(response) {
+    	        		 sbiModule_messaging.showSuccessMessage(sbiModule_translate.load("sbi.documentdetails.toast.deleted"), 'Success!');
+    	        	 });
+    	         };
+
+    	         outputResource.deleteOutputParameters = function() {
+    	        	 for(var i = 0; i < outputResource.outputParametersForDeleting.length; i++) {
+    	        		 deleteOutputParameterById(outputResource.outputParametersForDeleting[i]);
+    	        	 }
+    	         };
     		   
-    		   return documentData;
+    		   return outputResource;
     	   }]);
 
 })();
--- knowage/src/main/webapp/js/documentdetails/services/templateService.js
+++ knowage/src/main/webapp/js/documentdetails/services/templateService.js
@@ -1,27 +1,73 @@
-(function () {
-    angular.module('TemplateModule', [])
-    	   .service('templateService', ['$location','resourceService', function($location,resourceService) {
-    		   var documentData = {};    		   
-    		   var crudService = resourceService;
-    		   documentData.documentInfoObject =$location.search();
-    		   documentData.templates = "templates";
-    		   documentData.requiredPath = "2.0/documents1";    		   
-    		   documentData.template= documentAndInfo.template;    		   
-			   documentData.listOfTemplates = [];
-    		   documentData.changedTemplates = [];
-    		   documentData.changedTemplate = {};
-    		   documentData.file = {};    		  
-    		   documentData.templatesForDeleting = [];
+/**
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-    		   if( documentAndInfo.document){
-    			   documentData.document = documentAndInfo.document;
-    			   documentData.drivers = documentAndInfo.drivers
-    		   } else {
-    			   documentData.document = {};
-    			   documentData.drivers = [];
-    		   }
+(function () {
+    angular.module('TemplateModule', ['sbiModule'])
+    	   .service('templateService', ['resourceService', 'DocumentService', 'multipartForm', 'sbiModule_translate', 'sbiModule_messaging', function(resourceService, DocumentService, multipartForm, sbiModule_translate, sbiModule_messaging) {
+    		   var templateResource = {};
+    		   var documentService = DocumentService;
+    		   var requiredPath = "2.0/documents1";   		   
+    		   templateResource.listOfTemplates = [];
+    		   templateResource.changedTemplates = [];
+    		   templateResource.changedTemplate = {};
+    		   templateResource.file = {};    		  
+    		   templateResource.templatesForDeleting = [];
+    		   var templateUploadBasePath = document.id + '/templates';
+
+    		   templateResource.uploadTemplate = function() {
+    	        	if(templateResource.file.file) {
+    	        		var templateUploadBasePath = documentService.document.id + '/templates';
+    	        		multipartForm.post(requiredPath +"/"+ templateUploadBasePath, templateResource.file).then(function(response){
+    	        			getAllTemplates();
+    	  	      	  });
+    	        	}
+    	        };
+    	        
+    	        var getAllTemplates = function() {
+    	        	var templateBasePath = documentService.document.id + '/templates';
+    	        	resourceService.get(requiredPath, templateBasePath).then(function(response) {
+    	        		templateResource.listOfTemplates = response.data;
+    	        	});
+    	        }
+    	        
+    	        templateResource.setActiveTemplate = function() {
+    	        	if(templateResource.changedTemplate.id) {
+    					var templateModifyBasePath = documentService.document.id + "/templates/" + templateResource.changedTemplate.id;
+    	    			resourceService.put(requiredPath, templateModifyBasePath);
+    				}
+    	        };
+    	        
+    	        var deleteTemplateById = function(template) {
+    	          	 var basePath = documentService.document.id + "/" + 'templates';
+    	          	 var basePathWithId = basePath + "/" + template.id;
+    	          	 resourceService.delete(requiredPath, basePathWithId).then(function(response) {
+    	          		 sbiModule_messaging.showSuccessMessage(sbiModule_translate.load("sbi.documentdetails.toast.deleted"), 'Success!');
+    	          	 });
+    	           };
+
+    	           templateResource.deleteTemplates = function() {
+    	          	 for(var i = 0; i < templateResource.templatesForDeleting.length; i++) {
+    	          		 deleteTemplateById(templateResource.templatesForDeleting[i]);
+    	          	 }
+    	           };
+
     		   
-    		   return documentData;
+    		   return templateResource;
     	   }]);
 
 })();
