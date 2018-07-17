7e70cf9d2cd9c8d93accb25e4eb2cf2245fbe3b9
Radmila Selakovic <radmila.selakovic@eng.it>
2018-07-17 13:47:03 +0000

    RESOLVED KNOWAGE-3342 bar/line chart: the scale factor is not working https://production.eng.it/jira/browse/KNOWAGE-3342

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/chartWidget/chartWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/chartWidget/chartWidget.js
@@ -217,7 +217,7 @@ function cockpitChartWidgetControllerFunction(
 		if($scope.ngModel.content.chartTemplate.CHART.type == "SCATTER" || $scope.ngModel.content.chartTemplate.CHART.type == "BAR" || $scope.ngModel.content.chartTemplate.CHART.type == "LINE"){
 	    	  for (var i = 0; i < $scope.ngModel.content.chartTemplate.CHART.VALUES.SERIE.length; i++) {
 	    		  for (var j = 0; j < $scope.ngModel.content.chartTemplate.CHART.AXES_LIST.AXIS.length; j++) {
-						if($scope.ngModel.content.chartTemplate.CHART.VALUES.SERIE[i].axis == $scope.ngModel.content.chartTemplate.CHART.AXES_LIST.AXIS[j].alias){
+						if($scope.ngModel.content.chartTemplate.CHART.VALUES.SERIE[i].axis == $scope.ngModel.content.chartTemplate.CHART.AXES_LIST.AXIS[j].alias && $scope.ngModel.content.chartTemplate.CHART.AXES_LIST.AXIS[j].labels){
 							$scope.ngModel.content.chartTemplate.CHART.VALUES.SERIE[i].scaleFactor = $scope.ngModel.content.chartTemplate.CHART.AXES_LIST.AXIS[j].labels.scaleFactor
 						}
 		    	  }
