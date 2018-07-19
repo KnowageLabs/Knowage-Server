03b2c57a1cc5124eddddb1535fbd3e3f9091bd31
Dragan Pirkovic <dragan.pirkovic@eng.it>
2018-07-19 08:10:22 +0000

    fix

--- knowagedatabasescripts/oracle/oracle-dbupgradescript-6.1_to_6.2/ORA_upgradescript_6.1_to_6.2.sql
+++ knowagedatabasescripts/oracle/oracle-dbupgradescript-6.1_to_6.2/ORA_upgradescript_6.1_to_6.2.sql
@@ -1,4 +1,4 @@
-INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS, USER_IN) values ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'), 'Date', 'Date', 'DS_META_VALUE', 'Dataset Metadata Value', 'Dataset Metadata Value', '');
+INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS, USER_IN,TIME_IN) values ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'), 'Date', 'Date', 'DS_META_VALUE', 'Dataset Metadata Value', 'Dataset Metadata Value', ' ',TIMESTAMP);
 UPDATE hibernate_sequences SET next_val = (SELECT MAX(VALUE_ID) + 1 FROM SBI_DOMAINS) WHERE sequence_name = 'SBI_DOMAINS';
 
 UPDATE SBI_FEDERATION_DEFINITION SET NAME = LABEL WHERE NAME IS NULL;
@@ -8,7 +8,7 @@ ALTER TABLE SBI_EXT_ROLES ADD IS_PUBLIC SMALLINT NULL;
 
 ALTER TABLE SBI_DATA_SOURCE ADD JDBC_ADVANCED_CONFIGURATION	VARCHAR2(4000);
 
-INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS, USER_IN) values ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'), 'Solr', 'SbiSolrDataSet', 'DATA_SET_TYPE', 'Data Set Type', 'SbiSolrDataSet', '');
+INSERT into SBI_DOMAINS (VALUE_ID, VALUE_CD, VALUE_NM, DOMAIN_CD, DOMAIN_NM,VALUE_DS, USER_IN,TIME_IN) values ((SELECT next_val FROM hibernate_sequences WHERE sequence_name = 'SBI_DOMAINS'), 'Solr', 'SbiSolrDataSet', 'DATA_SET_TYPE', 'Data Set Type', 'SbiSolrDataSet', ' ',TIMESTAMP);
 UPDATE hibernate_sequences SET next_val = (SELECT MAX(VALUE_ID) + 1 FROM SBI_DOMAINS) WHERE sequence_name = 'SBI_DOMAINS';
 
 DELETE FROM SBI_PRODUCT_TYPE_ENGINE WHERE ENGINE_ID = (
