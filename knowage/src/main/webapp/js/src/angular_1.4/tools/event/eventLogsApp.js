897143f753920798bdaf6f72f86156375b663cbc
Davide Vernassa <davide.vernassa@eng.it>
2018-08-08 15:53:43 +0000

    unified layout in new event log management

--- knowage/src/main/webapp/js/src/angular_1.4/tools/event/eventLogsApp.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/event/eventLogsApp.js
@@ -1,5 +1,4 @@
 (function(){
-
-angular.module("eventModule",["angular-list-detail", "angular_table"]);
-
+	agGrid.initialiseAgGridWithAngular1(angular);
+	angular.module("eventModule",["angular-list-detail", "angular_table","agGrid"]);
 }());
