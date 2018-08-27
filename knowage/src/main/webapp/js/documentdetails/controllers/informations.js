e2ffed6764a9d1a5438c33443a41233c7c306205
Filip Milosavljevic <filip.milosavljevic@eng.it>
2018-08-27 10:53:48 +0000

    bug fixes

--- knowage/src/main/webapp/js/documentdetails/controllers/informations.js
+++ knowage/src/main/webapp/js/documentdetails/controllers/informations.js
@@ -103,7 +103,9 @@ angular
              }
              var setCheckedFolder = function(folders){
             	 for(var i = 0; i < folders.length; i++){
-            		 if(self.document.functionalities && self.document.functionalities.indexOf(folders[i].path) != -1)
+            		 if(!self.document.functionalities && documentAndInfo.folderId == folders[i].id){
+            			 folders[i].checked = true;
+            		 }else if (self.document.functionalities && self.document.functionalities.indexOf(folders[i].path) != -1)
             			 folders[i].checked = true;
             	 }
             	 return folders;
