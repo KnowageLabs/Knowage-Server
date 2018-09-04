2f80308a0ef12766c3f48044def8773c654e1e0c
Marko Kostić <marko.kostic@eng.it>
2018-09-04 08:25:00 +0000

    Refactored

--- knowage/src/main/webapp/js/src/angular_1.4/tools/workspace/scripts/directive/components/modelsViewWorkspace.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/workspace/scripts/directive/components/modelsViewWorkspace.js
@@ -97,7 +97,7 @@
 //		}
 		}
 		$scope.tableColumnsFederation = [{"label":"Label","name":"label"},{"label":"Name","name":"name"}];
-		$scope.tableColumnsModels = [{"label":"Name","name":"name","type":"text"}, {"label":"Description","name":"description","type":"text"},{"type": "buttons", "buttons": [{"name": "Open business model in QBE", "icon": "fa fa-search", "action": $scope.showQbeModel, "visible":true}]}];
+		$scope.tableColumnsModels = [{"label":"Name","name":"name","type":"text"}, {"label":"Description","name":"description","type":"text"},{"type": "buttons", "buttons": [{"name": "Open business model in QBE", "icon": "fa fa-search", "action": $scope.showQbeModel, "visible":function(row){return row.visible}}]}];
 
 		$scope.showModelInfo = false;
 		$scope.idsOfFederationDefinitionsUsediNFederatedDatasets = [];
