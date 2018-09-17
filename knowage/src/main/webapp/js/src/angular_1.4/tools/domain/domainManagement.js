536604db3ed724282c805b1158060c598ea79570
Davide Vernassa <davide.vernassa@eng.it>
2018-09-17 13:21:04 +0000

    FIXED ISSUE KNOWAGE-3090:Configuration/Domain management are really slow when showing the detail section https://production.eng.it/jira/browse/KNOWAGE-3090

--- knowage/src/main/webapp/js/src/angular_1.4/tools/domain/domainManagement.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/domain/domainManagement.js
@@ -1,14 +1,30 @@
+/*
+Knowage, Open Source Business Intelligence suite
+Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
 
-var app = angular.module('domainManagementApp', ['angular_table','ngMaterial', 'ngMessages', 'ui.tree', 'angularUtils.directives.dirPagination', 'angular_list', 'angular-list-detail', 'sbiModule', 'angularXRegExp']);
-app.config(['$mdThemingProvider', function($mdThemingProvider) {
-    $mdThemingProvider.theme('knowage')
-    $mdThemingProvider.setDefaultTheme('knowage');
-}]);
+Knowage is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-app.controller('Controller', ['$angularListDetail', 'sbiModule_messaging', 'sbiModule_translate','sbiModule_restServices', 'kn_regex', '$scope', '$q', '$log', '$mdDialog', 'sbiModule_config', manageDomainFucntion ]);
+Knowage is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+agGrid.initialiseAgGridWithAngular1(angular);
 
-function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_translate, sbiModule_restServices, kn_regex, $scope, $q, $log,  $mdDialog,sbiModule_config) {
+angular.module('domainManagementApp', ['angular_table','ngMaterial', 'ngMessages', 'angular_list', 'angular-list-detail', 'sbiModule','agGrid'])
+	.config(['$mdThemingProvider', function($mdThemingProvider) {
+	    $mdThemingProvider.theme('knowage')
+	    $mdThemingProvider.setDefaultTheme('knowage');
+	}])
+	.controller('Controller', manageDomainFucntion);
+
+function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_translate, sbiModule_restServices, kn_regex, $scope, $q,  $mdDialog,sbiModule_config, $timeout) {
 
 	$scope.path = "2.0/domains";
 	var headers = {
@@ -16,9 +32,41 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
 	};
 	$scope.translate = sbiModule_translate;
 	$scope.message = sbiModule_messaging;
-	$scope.data=[]
+	$scope.data=[];
 	$scope.itemSelected= {};
+	$scope.columns = [{"headerName":"valueCd","field":"valueCd"},
+		{"headerName":"valueName","field":"valueName"},
+		{"headerName":"domainCode","field":"domainCode"},
+		{"headerName":"domainName","field":"domainName"},
+		{"headerName":"valueDescription","field":"valueDescription",editable:true,cellEditor: "agLargeTextCellEditor"},
+		{"headerName":"",cellRenderer: buttonRenderer,"field":"valueId","cellStyle":{"text-align": "right","display":"inline-flex","justify-content":"flex-end","border":"none"},
+			suppressSorting:true,suppressFilter:true,width: 100,suppressSizeToFit:true, tooltip: false}];
 	$scope.regex = kn_regex;
+	
+	function buttonRenderer(params){
+		return 	'<md-button class="md-icon-button" ng-click="editRow(\''+params.data.valueId+'\')"><md-icon md-font-icon="fa fa-pencil"></md-icon></md-button>'+
+				'<md-button class="md-icon-button" ng-click="deleteRow(\''+params.data.valueId+'\')"><md-icon md-font-icon="fa fa-trash"></md-icon></md-button>';
+	}
+	
+	$scope.gridOptions = {
+			angularCompileRows: true,
+            enableColResize: false,
+            enableFilter: true,
+            enableSorting: true,
+            pagination: true,
+            paginationAutoPageSize: true,
+            onGridSizeChanged: resizeColumns,
+            defaultColDef: {
+            	suppressMovable: true,
+            	tooltip: function (params) {
+                    return params.value;
+                },
+            }
+	};
+	
+	function resizeColumns(){
+		$scope.gridOptions.api.sizeColumnsToFit();
+	}
 
 	var rowDefault = {
 		valueId : "",
@@ -29,23 +77,6 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
 		valueDescription : "Description Default"
 	};
 
-	$scope.domainSpeedMenu = [{
-    	label: $scope.translate.load('sbi.generic.edit'),
-    	icon:'fa fa-pencil',
-    	color:'#153E7E',
-    	action:function(item,event){
-    		$scope.editRow(item);
-    	}
-	},{
-    	label: $scope.translate.load('sbi.generic.delete'),
-    	icon:'fa fa-trash',
-    	color:'#153E7E',
-    	action:function(item,event){
-    		$scope.deleteRow(item);
-    	}
-	}];
-
-
 	sbiModule_restServices.promiseGet($scope.path, "", null)
 		.then(function(response) {
 				if (response.data.errors != undefined){
@@ -53,6 +84,9 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
 					return;
 				}
 				$scope.data = response.data;
+				$scope.gridOptions.api.setColumnDefs($scope.columns);
+				$scope.gridOptions.api.setRowData($scope.data);
+					$scope.gridOptions.api.sizeColumnsToFit();
 			}, function(data, status, headers, config){
 				$scope.message.showErrorMessage($scope.translate.load('sbi.generic.error.msg') + data);
 		});
@@ -67,9 +101,18 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
 		$scope.domainForm.$setPristine();
 		$scope.domainForm.$setUntouched();
 	}
+	
+	$scope.getRowFromId = function(id){
+		for(var r in $scope.data){
+			if($scope.data[r].valueId==id){
+				return $scope.data[r];
+			}
+		}
+		return false;
+	}
 
-	$scope.editRow = function(item) {
-		$scope.domain = angular.copy(item);
+	$scope.editRow = function(id) {
+		$scope.domain = angular.copy($scope.getRowFromId(id));
 		$angularListDetail.goToDetail();
 	}
 
@@ -111,6 +154,7 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
 				item.valueId = response.data;
 				$scope.data.splice(0, 0, item);
 				$angularListDetail.goToList();
+				$scope.gridOptions.api.setRowData($scope.data);
 				$scope.domain = {};
 				$scope.message.showSuccessMessage($scope.translate.load('sbi.generic.operationSucceded'));
 			}
@@ -129,7 +173,7 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
 		});
 	}
 
-	$scope.deleteRow = function(item) {
+	$scope.deleteRow = function(id) {
 		var confirm = $mdDialog.confirm()
 				        .title($scope.translate.load('sbi.generic.delete'))
 				        .content($scope.translate.load('sbi.generic.confirmDelete'))
@@ -139,7 +183,7 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
         $mdDialog
 	  	  	.show(confirm)
 	  	  	.then(function(){
-  	  			var rowsSelected = item;
+  	  			var rowsSelected = $scope.getRowFromId(id);
   	  			if (rowsSelected.valueId !== undefined) {
   	  				var idx = $scope.indexOf($scope.data, rowsSelected);
   	  				if (idx>=0){
@@ -150,6 +194,7 @@ function manageDomainFucntion($angularListDetail,sbiModule_messaging, sbiModule_
   	  								return;
   	  							}
   	  							$scope.oldValue = $scope.data.splice(idx, 1);
+  	  							$scope.gridOptions.api.setRowData($scope.data);
   	  							$scope.message.showSuccessMessage($scope.translate.load('sbi.generic.operationSucceded'));
   	  						},function(data,status){
   	  							$scope.message.showErrorMessage($scope.translate.load('sbi.generic.error.msg') + data);
