a5a3719884bce9bb8d8d82ebcfff7a29f0c058b6
Marko Kostic <marko.kostic@eng.it>
2018-10-22 09:16:53 +0200

    [IN PROGRESS] Knowage-3504 As a technical user, when definining a business model, I'd like to define custom SQL dynamic filters on an entity (i.e. a table), exploiting analytical drivers

--- knowagemeta/src/main/webapp/js/src/meta/services/businessViewFilterService.js
+++ knowagemeta/src/main/webapp/js/src/meta/services/businessViewFilterService.js
@@ -1,47 +1,64 @@
 (function() {
 	
-	angular.module('metaManager').service('businessViewFilterService', ['sbiModule_restServices', 'sbiModule_config', 'sbiModule_translate', '$q',
-		function(sbiModule_restServices, sbiModule_config, sbiModule_translate, $q) {
+	angular.module('metaManager').service('businessViewFilterService', ['sbiModule_restServices', 'sbiModule_config', 'sbiModule_translate',
+		function(sbiModule_restServices, sbiModule_config, sbiModule_translate) {
 			
-			var objToReturn = {};
-			
-			objToReturn.bmDrivers = [];
-		
-			objToReturn.obj = {
-				input: ""
-			}
-			
-			objToReturn.getBusinessModelDrivers = function() {
-				var reqPath = bmId + "/drivers";
-				var endPath = "2.0/businessmodels";
-				var deferred = $q.defer();
+			return{
 				
-				sbiModule_restServices.alterContextPath(sbiModule_config.externalBasePath);
-				sbiModule_restServices.promiseGet(endPath, reqPath)
-					.then(function(response) {
-						objToReturn.bmDrivers = response.data;
-						deferred.resolve(objToReturn.bmDrivers);
+				sqlExpression:"",
+				
+				getBusinessModelDrivers : function(businessModelId) {
+					var reqPath = businessModelId + "/drivers";
+					var endPath = "2.0/businessmodels";
+					var bmDrivers = [];
+					
+					sbiModule_restServices.alterContextPath(sbiModule_config.externalBasePath);
+					var promise = sbiModule_restServices.promiseGet(endPath, reqPath);
+						promise.then(function(response) {
+							bmDrivers = response.data;
 						
-					}, function(response) {
-						deferred.reject(response);
-						//sbiModule_restServices.errorHandler(response.data, sbiModule_translate.load("sbi.meta.model.business.filter.failureMessage"));
-					});
+						}, function(response) {
+							
+							sbiModule_restServices.errorHandler(response.data, sbiModule_translate.load("sbi.meta.model.business.filter.failureMessage"));
+						});
+					
+						return promise;
+				},
 				
-					return deferred.promise;
-			}
-			
-			objToReturn.checkIfDriversExist = function(sqlExpression) {
+				areDriversFromExpressionValid : function() {
+					
+					var expressionDrivers = this.extractDriversFromExpression(this.sqlExpression);
+					return this.isBusinessModelContainsDrivers(bmId, expressionDrivers);
+					
+				},
 				
-				var pattern = /\$p{(.+)}/g;
-				var result = sqlExpression.match(pattern);
-				var res = pattern.exec(sqlExpression);
-				result = res[1];
 				
-				var listDrivers = bmDrivers;
+				contains : function(sourceArray, destinationArray) {
+					return false;
+				},
 				
-			}
-			
-			return objToReturn;
+				isBusinessModelContainsDrivers: function(bmId, drivers) {
+					
+					
+					var promise = this.getBusinessModelDrivers(bmId);
+					promise.then(function(response) {
+							businessModelDrivers = response.data;
+							return this.contains(drivers, businessModelDrivers)
+						})
+					return promise;
+				},
+				
+				extractDriversFromExpression : function(expression) {
+//					var pattern = /\$p{(.+)}/g;
+//					var result = expression.match(pattern);
+//					var res = pattern.exec(expression);
+//					result = res[1];
+					var drivers = [];
+					drivers.push("dragan");
+					drivers.push("marko");
+				}
+				
+			}	
 	}]);
 	
 }());
