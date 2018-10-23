3d4393d6e02076c2fafdbd354cdd09d05f196eae
Andrijana Predojevic <Andrijana.Predojevic@eng.it>
2018-10-23 17:40:07 +0200

    [Solved KNOWAGE-3358] Events for ETL jobs are not registered

--- knowage-core/src/main/java/it/eng/spagobi/services/event/service/EventServiceImpl.java
+++ knowage-core/src/main/java/it/eng/spagobi/services/event/service/EventServiceImpl.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,16 +11,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.services.event.service;
-
-import it.eng.spagobi.engines.drivers.handlers.IRolesHandler;
-import it.eng.spagobi.events.EventsManager;
-import it.eng.spagobi.services.common.AbstractServiceImpl;
-import it.eng.spagobi.services.security.exceptions.SecurityException;
 
 import java.util.List;
 
@@ -29,64 +24,78 @@ import org.apache.log4j.Logger;
 import com.jamonapi.Monitor;
 import com.jamonapi.MonitorFactory;
 
+import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.commons.utilities.UserUtilities;
+import it.eng.spagobi.engines.drivers.handlers.IRolesHandler;
+import it.eng.spagobi.events.EventsManager;
+import it.eng.spagobi.services.common.AbstractServiceImpl;
+import it.eng.spagobi.services.security.exceptions.SecurityException;
+
 public class EventServiceImpl extends AbstractServiceImpl {
-    static private Logger logger = Logger.getLogger(EventServiceImpl.class);
+	static private Logger logger = Logger.getLogger(EventServiceImpl.class);
 
-    /**
-     * Fire event.
-     * 
-     * @param token the token
-     * @param user the user
-     * @param description the description
-     * @param parameters the parameters
-     * @param rolesHandler the roles handler
-     * @param presentationHandler the presentation handler
-     * 
-     * @return the string
-     */
-    public String fireEvent(String token, String user, String description,
-	    String parameters, String rolesHandler, String presentationHandler) {
-	logger.debug("IN");
-	Monitor monitor =MonitorFactory.start("spagobi.service.event.fireEvent");
-	try {
-	    validateTicket(token, user);
-	    this.setTenantByUserId(user);
-	    return fireEvent(user, description, parameters,rolesHandler, presentationHandler);
-	} catch (SecurityException e) {
-	    logger.error("SecurityException", e);
-	    return null;
-	} finally {
-		this.unsetTenant();
-	    monitor.stop();
-	    logger.debug("OUT");
-	}	
+	/**
+	 * Fire event.
+	 * 
+	 * @param token
+	 *            the token
+	 * @param user
+	 *            the user
+	 * @param description
+	 *            the description
+	 * @param parameters
+	 *            the parameters
+	 * @param rolesHandler
+	 *            the roles handler
+	 * @param presentationHandler
+	 *            the presentation handler
+	 * 
+	 * @return the string
+	 */
+	public String fireEvent(String token, String user, String description, String parameters, String rolesHandler, String presentationHandler) {
+		logger.debug("IN");
+		Monitor monitor = MonitorFactory.start("spagobi.service.event.fireEvent");
+		try {
+			validateTicket(token, user);
+			this.setTenantByUserId(user);
+			UserProfile profile = (UserProfile) UserUtilities.getUserProfile(user);
+			return fireEvent(profile, description, parameters, rolesHandler, presentationHandler);
+		} catch (SecurityException e) {
+			logger.error("SecurityException", e);
+			return null;
+		} catch (Exception e) {
+			logger.error("Error while firing event", e);
+			return null;
+		} finally {
+			this.unsetTenant();
+			monitor.stop();
+			logger.debug("OUT");
+		}
 
-    }
-
-    private String fireEvent(String user, String description,
-	    String parameters, String rolesHandler, String presentationHandler) {
-	logger.debug("IN");
-	String returnValue = null;
-
-	try {
-	    if (user != null) {
-
-		IRolesHandler rolesHandlerClass = (IRolesHandler) Class.forName(rolesHandler).newInstance();
-		List roles = rolesHandlerClass.calculateRoles(parameters);
-		Integer id = EventsManager.getInstance().registerEvent(user,description, parameters, roles, presentationHandler);
-		returnValue = id.toString();
-		logger.debug("Service executed succesfully");
-	    } else {
-		logger.warn("User is NULL");
-	    }
-	    return returnValue;
-	} catch (Exception e) {
-		logger.error("Impossible to fire event [" + description + "]",e);
-
-	}finally{
-	    logger.debug("OUT");
 	}
-	return null;
-    }
+
+	private String fireEvent(UserProfile profile, String description, String parameters, String rolesHandler, String presentationHandler) {
+		logger.debug("IN");
+		String returnValue = null;
+
+		try {
+			if (profile != null) {
+				IRolesHandler rolesHandlerClass = (IRolesHandler) Class.forName(rolesHandler).newInstance();
+				List roles = rolesHandlerClass.calculateRoles(parameters);
+				Integer id = EventsManager.getInstance().registerEvent(profile.getUserId().toString(), description, parameters, roles, presentationHandler);
+				returnValue = id.toString();
+				logger.debug("Service executed succesfully");
+			} else {
+				logger.warn("User is NULL");
+			}
+			return returnValue;
+		} catch (Exception e) {
+			logger.error("Impossible to fire event [" + description + "]", e);
+
+		} finally {
+			logger.debug("OUT");
+		}
+		return null;
+	}
 
 }
