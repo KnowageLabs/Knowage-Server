b34ada9fe28fb001e75d1cbfcec3fcaeac4726e8
Marko Kostic <marko.kostic@eng.it>
2018-11-29 11:15:54 +0100

    [REFACTORED] - Modified file filters.js

--- knowageqbeengine/src/main/webapp/qbe/services/filters/filters.js
+++ knowageqbeengine/src/main/webapp/qbe/services/filters/filters.js
@@ -38,24 +38,24 @@ filters.service('filters_service',function(sbiModule_action,sbiModule_translate)
 	this.aggFunctions = [ "NONE", "SUM", "MIN", "MAX", "AVG", "COUNT", "COUNT_DISTINCT" ];
 
 	this.getOperators = [
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.equals.to"),value:"EQUALS TO"},
-		        // {name:sbiModule_translate.load("kn.qbe.filters.operators.not.equals.to"),value:"NOT EQUALS TO"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.equals.to"),value:"EQUALS TO"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.equals.to"),value:"NOT EQUALS TO"},
 		         {name:sbiModule_translate.load("kn.qbe.filters.operators.greater.than"),value:"GREATER THAN"},
 		         {name:sbiModule_translate.load("kn.qbe.filters.operators.equals.or.greater.than"),value:"EQUALS OR GREATER THAN"},
 		         {name:sbiModule_translate.load("kn.qbe.filters.operators.less.than"),value:"LESS THAN"},
 		         {name:sbiModule_translate.load("kn.qbe.filters.operators.equals.or.less.than"),value:"EQUALS OR LESS THAN"}
-		        // {name:sbiModule_translate.load("kn.qbe.filters.operators.starts.with"),value:"STARTS WITH"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.not.starts.with"),value:"NOT STARTS WITH"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.ends.with"),value:"ENDS WITH"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.not.ends.with"),value:"NOT ENDS WITH"},
-		        // {name:sbiModule_translate.load("kn.qbe.filters.operators.contains"),value:"CONTAINS"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.not.contains"),value:"NOT CONTAINS"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.between"),value:"BETWEEN"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.not.between"),value:"NOT BETWEEN"},
-		        // {name:sbiModule_translate.load("kn.qbe.filters.operators.in"),value:"IN"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.not.in"),value:"NOT IN"},
-		         //{name:sbiModule_translate.load("kn.qbe.filters.operators.is.null"),value:"IS NULL"},
-		        // {name:sbiModule_translate.load("kn.qbe.filters.operators.not.null"),value:"NOT NULL"}
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.starts.with"),value:"STARTS WITH"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.starts.with"),value:"NOT STARTS WITH"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.ends.with"),value:"ENDS WITH"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.ends.with"),value:"NOT ENDS WITH"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.contains"),value:"CONTAINS"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.contains"),value:"NOT CONTAINS"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.between"),value:"BETWEEN"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.between"),value:"NOT BETWEEN"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.in"),value:"IN"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.in"),value:"NOT IN"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.is.null"),value:"IS NULL"},
+		         {name:sbiModule_translate.load("kn.qbe.filters.operators.not.null"),value:"NOT NULL"}
 		         ];
 
 	this.getSpatialOperators = [
