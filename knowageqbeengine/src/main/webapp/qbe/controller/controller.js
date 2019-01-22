3fb0641f5cadb43755b41e2464682ddd6dd9956b
Filip Milosavljevic <filip.milosavljevic@eng.it>
2019-01-22 18:16:23 +0100

    Save Dataset modifications from workspace as user

--- knowageqbeengine/src/main/webapp/qbe/controller/controller.js
+++ knowageqbeengine/src/main/webapp/qbe/controller/controller.js
@@ -64,7 +64,41 @@ function qbeFunction($scope,$rootScope,$filter,entity_service,query_service,filt
 	$scope.selectedRelationsService = selectedEntitiesRelationshipsService;
 	$scope.queryEntitiesService = queryEntitiesService;
 	var comunicator = windowCommunicationService;
+<<<<<<< Updated upstream
 
+=======
+	comunicator.sendMessage("Hi from qbe");
+
+	var consoleHandler = {}
+	consoleHandler.handleMessage = function(message){
+		if(message === 'close'){
+			var saveObj = {};
+			saveObj.qbeQuery = {catalogue: {queries: [$scope.editQueryObj]}};
+			saveObj.pars = $scope.pars;
+			comunicator.sendMessage(saveObj);
+		}
+		if(message === 'saveDS'){
+			var saveObj = {};
+			saveObj.name = "workspace"
+			saveObj.qbeQuery = {catalogue: {queries: [$scope.editQueryObj]}};
+			comunicator.sendMessage(saveObj);
+		}
+
+		console.log(message)
+	}
+	comunicator.addMessageHandler(consoleHandler);
+
+//	var workspaceDShandler = {}
+//	workspaceDShandler.handleMessage = function(message){
+//		if(message === 'saveDS'){
+//			var saveObj = {};
+//			saveObj.qbeQuery = {catalogue: {queries: [$scope.editQueryObj]}};;
+//			comunicator.sendMessage(saveObj);
+//		}
+//		console.log(message)
+//	}
+//	comunicator.addMessageHandler(workspaceDShandler);
+>>>>>>> Stashed changes
 	formulaService.getCustomFormulas().then(function(response) {
 		$scope.customFormulas = [];
 		if(response.data.data){
