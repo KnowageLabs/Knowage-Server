4f7cdbf314f414dce903104d310dce642c4ce11d
PredragJosipovic <predrag.josipovic@eng.it>
2019-01-23 16:24:17 +0100

    [FIXED KNOWAGE-3783] - Error reading date fiels  - Fixes persisting File Dataset when some of Columns are Dates
    (cherry picked from commit fa5c7ccf694d2b84707d566a416e536a72d0c5f4)

--- jdbccore/src/main/java/it/eng/spagobi/tools/dataset/persist/PersistedTableHelper.java
+++ jdbccore/src/main/java/it/eng/spagobi/tools/dataset/persist/PersistedTableHelper.java
@@ -37,7 +37,7 @@ public class PersistedTableHelper {
 	private static transient Logger logger = Logger.getLogger(PersistedTableHelper.class);
 	
 	public static void addField(PreparedStatement insertStatement, int fieldIndex, Object fieldValue, String fieldMetaName, String fieldMetaTypeName,
-								boolean isfieldMetaFieldTypeMeasure, Map<String, Integer> columnSizes) {
+			boolean isfieldMetaFieldTypeMeasure, Map<String, Integer> columnSizes) {
 		try {
 			if (fieldValue == null) {
 				insertStatement.setObject(fieldIndex + 1, null);
@@ -88,6 +88,15 @@ public class PersistedTableHelper {
 			} else if (fieldMetaTypeName.contains("Date")) {
 				if (fieldValue instanceof java.sql.Date) {
 					insertStatement.setDate(fieldIndex + 1, (Date) fieldValue);
+				} else if (fieldValue instanceof java.util.Date) {
+					// JDK 8 version
+					/*
+					 * Instant instant = date.toInstant(); ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault()); LocalDate localDate =
+					 * zdt.toLocalDate();
+					 */
+					DateTime dateTime = new DateTime(fieldValue, DateTimeZone.getDefault());
+					java.sql.Date sqlDate = new java.sql.Date(dateTime.getMillis());
+					insertStatement.setDate(fieldIndex + 1, sqlDate);
 				} else {
 					java.util.Date date = new java.util.Date(fieldValue.toString());
 					// JDK 8 version
