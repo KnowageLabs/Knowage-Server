b59e0779edd7b7758d5cd48e682f2f98e6d9bf81
Davide Vernassa <davide.vernassa@eng.it>
2019-03-27 17:10:14 +0100

    Update htmlWidget.js

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/htmlWidget/htmlWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/htmlWidget/htmlWidget.js
@@ -378,7 +378,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		$scope.ifConditionReplacer = function(match, p1, row, aggr, precision){
 			var columnInfo = $scope.getColumnFromName(p1,aggr ? $scope.aggregationDataset : $scope.htmlDataset ,aggr);
 			if(aggr){
-				p1 = $scope.aggregationDataset && $scope.aggregationDataset.rows[0] && $scope.aggregationDataset.rows[0][columnInfo.name] != "" && typeof($scope.aggregationDataset.rows[0][columnInfo.name])!='undefined' ? $scope.aggregationDataset.rows[0][columnInfo.name] : null;
+				p1 = $scope.aggregationDataset && $scope.aggregationDataset.rows[0] && $scope.aggregationDataset.rows[0][columnInfo.name] !== "" && typeof($scope.aggregationDataset.rows[0][columnInfo.name])!='undefined' ? $scope.aggregationDataset.rows[0][columnInfo.name] : null;
 			}
 			else if($scope.htmlDataset && $scope.htmlDataset.rows[row||0] && typeof($scope.htmlDataset.rows[row||0][columnInfo.name])!='undefined' && $scope.htmlDataset.rows[row||0][columnInfo.name] != ""){
 				p1 = columnInfo.type == 'string' ? '\''+$scope.htmlDataset.rows[row||0][columnInfo.name]+'\'' : $scope.htmlDataset.rows[row||0][columnInfo.name];
@@ -395,7 +395,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		$scope.replacer = function(match, p1, row, aggr, precision,format) {
 			var columnInfo = $scope.getColumnFromName(p1,aggr ? $scope.aggregationDataset : $scope.htmlDataset ,aggr);
 			if(aggr){
-				p1 = $scope.aggregationDataset && $scope.aggregationDataset.rows[0] && $scope.aggregationDataset.rows[0][columnInfo.name] != "" && typeof($scope.aggregationDataset.rows[0][columnInfo.name])!='undefined' ? $scope.aggregationDataset.rows[0][columnInfo.name] : null;
+				p1 = $scope.aggregationDataset && $scope.aggregationDataset.rows[0] && $scope.aggregationDataset.rows[0][columnInfo.name] !== "" && typeof($scope.aggregationDataset.rows[0][columnInfo.name])!='undefined' ? $scope.aggregationDataset.rows[0][columnInfo.name] : null;
 			}else if($scope.htmlDataset && $scope.htmlDataset.rows[row||0] && typeof($scope.htmlDataset.rows[row||0][columnInfo.name])!='undefined' && $scope.htmlDataset.rows[row||0][columnInfo.name] != ""){
 				p1 = $scope.htmlDataset.rows[row||0][columnInfo.name];
 			}else {
