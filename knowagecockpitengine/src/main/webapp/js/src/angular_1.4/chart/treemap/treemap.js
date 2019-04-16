7f9beb9375e77fefcf103a76025ac6dce7ede023
Milos Sesic <milos.sesic@eng.it>
2019-04-16 17:20:25 +0200

    [RESOLVED KNOWAGE-3973] TREEMAP: Cannot create treemap chart
    (cherry picked from commit baeb39b8e8e9777dd4d299f2fcf3f38d1ef89085)

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/chart/treemap/treemap.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/chart/treemap/treemap.js
@@ -244,7 +244,8 @@ function prepareChartConfForTreemap(chartConf,handleCockpitSelection,handleCross
 		 }
 	}
 
-    if(colorStops.length >= 2 && colorStops[1][0] && colorStops[0][0]){
+    if(colorStops.length >= 2 && colorStops[1][0] != undefined && colorStops[0][0] != undefined){
+
         distance = colorStops[1][0] - colorStops[0][0];
     }
 
