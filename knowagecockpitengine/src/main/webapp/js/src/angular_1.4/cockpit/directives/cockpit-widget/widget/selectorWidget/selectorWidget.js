ca74e553ad8195b8dde3d42818a3e4b2877c5a2c
Francesco Lucchi <francesco.lucchi@eng.it>
2019-04-16 12:05:24 +0200

    Fix active values filtering on selector widget

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/selectorWidget/selectorWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/selectorWidget/selectorWidget.js
@@ -442,6 +442,7 @@ angular.module('cockpitModule')
 
 	    $scope.deleteSelections = function(item){
 	    	var reloadAss=false;
+	    	var associatedDatasets = [];
 	    	var reloadFilt=[];
 
 	    	if(item.aggregated){
@@ -453,6 +454,7 @@ angular.module('cockpitModule')
 						if(selection){
 							delete selection[key];
 							reloadAss=true;
+							associatedDatasets.push(item.ds);
 						}
 					}
 				}
@@ -472,7 +474,7 @@ angular.module('cockpitModule')
 			}
 
 			if(reloadAss){
-				$scope.cockpitModule_widgetSelection.getAssociations(true);
+				$scope.cockpitModule_widgetSelection.getAssociations(true,undefined,undefined,associatedDatasets);
 			}
 
 			cockpitModule_widgetSelection.removeTimestampedSelection(item.ds, item.columnName);
