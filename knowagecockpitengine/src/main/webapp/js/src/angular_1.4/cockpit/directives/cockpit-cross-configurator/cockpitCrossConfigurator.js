0fe3c6d77e437ca7e44ca7209a4f5afd62a39c99
Davide Vernassa <davide.vernassa@eng.it>
2019-05-15 17:17:50 +0200

    Added missing translation and added columns management in cross navigation

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-cross-configurator/cockpitCrossConfigurator.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-cross-configurator/cockpitCrossConfigurator.js
@@ -165,6 +165,13 @@ function cockpitCrossConfiguratorControllerFunction($scope,sbiModule_translate,c
 			chart = $scope.localModel.chartTemplate;
 		}
 		$scope.chartProperties=cockpitModule_crossServices.getChartParameters(chart.type, chart);
+	}else if($scope.crossAdvancedTable){
+		if($scope.$parent.newModel && $scope.$parent.newModel.dataset!=undefined && $scope.$parent.newModel.dataset.dsId != undefined){
+			   angular.copy(cockpitModule_datasetServices.getDatasetById($scope.$parent.newModel.dataset.dsId), $scope.localDataset);
+		   }else{
+			   if($scope.$parent.newModel) $scope.$parent.newModel.dataset= {};
+			   else $scope.$parent.newModel = {dataset:{}};
+		   }
 	}else{
 	   if($scope.model && $scope.model.dataset!=undefined && $scope.model.dataset.dsId != undefined){
 		   angular.copy(cockpitModule_datasetServices.getDatasetById($scope.model.dataset.dsId), $scope.localDataset);
