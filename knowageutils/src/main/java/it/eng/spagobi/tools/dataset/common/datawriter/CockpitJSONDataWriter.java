27160214098e1c82ba11b8b2836df1bdb28c19d0
Francesco Lucchi <francesco.lucchi@eng.it>
2019-06-03 18:25:25 +0200

    Add support for Oracle data types to datasetore serialization

--- knowageutils/src/main/java/it/eng/spagobi/tools/dataset/common/datawriter/CockpitJSONDataWriter.java
+++ knowageutils/src/main/java/it/eng/spagobi/tools/dataset/common/datawriter/CockpitJSONDataWriter.java
@@ -25,29 +25,50 @@ public class CockpitJSONDataWriter extends JSONDataWriter {
 
 		Object value = field.getValue();
 		if (value != null) {
-			if (Timestamp.class.isAssignableFrom(fieldMetaData.getType())) {
+			Class type = fieldMetaData.getType();
+			String typeName = type.getName();
+			if (Timestamp.class.isAssignableFrom(type)) {
 				result = TIMESTAMP_FORMATTER.format(value);
-			} else if (Time.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if ("oracle.sql.TIMESTAMP".equals(typeName)) {
+				String s = value.toString();
+				int year = Integer.parseInt(s.substring(0, 4)) - 1900;
+				int month = Integer.parseInt(s.substring(5, 7)) - 1;
+				int date = Integer.parseInt(s.substring(8, 10));
+				int hour = Integer.parseInt(s.substring(11, 13));
+				int minute = Integer.parseInt(s.substring(14, 16));
+				int second = Integer.parseInt(s.substring(17, 19));
+				String nanoString = s.substring(20);
+				int nano = Integer.parseInt(nanoString) * (int) Math.pow(10, 9 - nanoString.length());
+				Timestamp timestamp = new Timestamp(year, month, date, hour, minute, second, nano);
+				result = TIMESTAMP_FORMATTER.format(timestamp);
+			} else if ("oracle.sql.DATE".equals(typeName)) {
+				String s = value.toString();
+				int year = Integer.parseInt(s.substring(0, 4)) - 1900;
+				int month = Integer.parseInt(s.substring(5, 7)) - 1;
+				int date = Integer.parseInt(s.substring(8, 10));
+				Timestamp timestamp = new Timestamp(year, month, date, 0, 0, 0, 0);
+				result = TIMESTAMP_FORMATTER.format(timestamp);
+			} else if (Time.class.isAssignableFrom(type)) {
 				result = CACHE_TIMEONLY_FORMATTER.format(value);
-			} else if (Date.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Date.class.isAssignableFrom(type)) {
 				result = DATE_FORMATTER.format(value);
-			} else if (Boolean.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Boolean.class.isAssignableFrom(type)) {
 				result = Boolean.valueOf(value.toString());
-			} else if (Byte.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Byte.class.isAssignableFrom(type)) {
 				result = Byte.valueOf(value.toString());
-			} else if (Short.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Short.class.isAssignableFrom(type)) {
 				result = Short.valueOf(value.toString());
-			} else if (Integer.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Integer.class.isAssignableFrom(type)) {
 				result = Integer.valueOf(value.toString());
-			} else if (Long.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Long.class.isAssignableFrom(type)) {
 				result = Long.valueOf(value.toString());
-			} else if (BigInteger.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (BigInteger.class.isAssignableFrom(type)) {
 				result = new BigInteger(value.toString());
-			} else if (Float.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Float.class.isAssignableFrom(type)) {
 				result = Float.valueOf(value.toString());
-			} else if (Double.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (Double.class.isAssignableFrom(type)) {
 				result = Double.valueOf(value.toString());
-			} else if (BigDecimal.class.isAssignableFrom(fieldMetaData.getType())) {
+			} else if (BigDecimal.class.isAssignableFrom(type)) {
 				result = new BigDecimal(value.toString());
 			} else {
 				result = value.toString();
