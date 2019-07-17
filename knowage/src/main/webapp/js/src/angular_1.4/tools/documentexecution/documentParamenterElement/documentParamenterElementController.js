0ec9087f89dc32d1094dd2ef613126260aa96016
Davide Vernassa <davide.vernassa@eng.it>
2019-07-17 12:15:54 +0200

    [KNOWAGE-4233, KNOWAGE-4234] Moved selected values on top when present

--- knowage/src/main/webapp/js/src/angular_1.4/tools/documentexecution/documentParamenterElement/documentParamenterElementController.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/documentexecution/documentParamenterElement/documentParamenterElementController.js
@@ -539,8 +539,10 @@
 				                    return params.value;
 				                },
 				            },
-				            columnDefs: paramDialogCtrl.columns
+				            columnDefs: paramDialogCtrl.columns,
+				            postSort : postSort
 						}
+						
 						
 						paramDialogCtrl.filterDataset = function(){
 							var tempParametersList = $filter('filter')(paramDialogCtrl.tableData,paramDialogCtrl.paramSearchText);
@@ -548,25 +550,41 @@
 						}
 						
 						function resizeColumns(){
-							paramDialogCtrl.lookoutGridOptions.api.sizeColumnsToFit();
+							if(paramDialogCtrl.lookoutGridOptions.api) paramDialogCtrl.lookoutGridOptions.api.sizeColumnsToFit();
+						}
+						
+						function postSort(nodes){
+
+						    function move(toIndex, fromIndex) {
+						    	nodes.splice(toIndex, 0, nodes.splice(fromIndex, 1)[0]);
+						    }
+
+						    var nextInsertPos = 0;
+						    for (var i = 0; i < nodes.length; i++) {
+						        if (nodes[i].selected) {
+						            move(nextInsertPos, i)
+						            nextInsertPos++;
+						        }
+						    }
 						}
 
 						sbiModule_restServices.post($scope.executionParameters,$scope.parametersPath, objPost)
-											  .success(function(data, status, headers, config) {
-													if(data.errors && data.errors[0]){
-														sbiModule_messaging.showWarningMessage(data.errors[0].message, 'Warning');
-													}
-													else if(data.status=="OK"){
-														paramDialogCtrl.tableData = data.result.root;
-														paramDialogCtrl.lookoutGridOptions.api.setRowData(data.result.root);
-														if(parameter.parameterValue && parameter.parameterValue.length>0){
-															paramDialogCtrl.lookoutGridOptions.api.forEachNode( function(rowNode, index) {
-																if(parameter.parameterValue.indexOf(rowNode.data.value) > -1 ) rowNode.setSelected(true);
-															});
-														}
-														paramDialogCtrl.selectedTableItems = paramDialogCtrl.initSelectedTableItems();
-													}
-								});
+						  .then(function(response) {
+								if(response.data.errors && response.data.errors[0]){
+									sbiModule_messaging.showWarningMessage(response.data.errors[0].message, 'Warning');
+								}
+								else if(response.data.status=="OK"){
+									paramDialogCtrl.tableData = response.data.result.root;
+									paramDialogCtrl.lookoutGridOptions.api.setRowData(response.data.result.root);
+									if(parameter.parameterValue && parameter.parameterValue.length>0){
+										paramDialogCtrl.lookoutGridOptions.api.forEachNode( function(rowNode, index) {
+											if(parameter.parameterValue.indexOf(rowNode.data.value) > -1 ) rowNode.setSelected(true);
+										});
+									}
+									paramDialogCtrl.lookoutGridOptions.api.refreshClientSideRowModel('sort');
+									paramDialogCtrl.selectedTableItems = paramDialogCtrl.initSelectedTableItems();
+								}
+						  });
 
 						paramDialogCtrl.initSelectedTableItems = function() {
 							var isMultivalue = paramDialogCtrl.tempParameter.multivalue;
