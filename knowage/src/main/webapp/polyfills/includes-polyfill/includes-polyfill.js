2a353c11c556b33ad11799a13df5d9f37a572e37
Radmila Selakovic <radmila.selakovic@eng.it>
2019-07-31 17:29:45 +0200

    [CLOSES KNOWAGE-4296] adding polyfill for includes for Strings

--- knowage/src/main/webapp/polyfills/includes-polyfill/includes-polyfill.js
+++ knowage/src/main/webapp/polyfills/includes-polyfill/includes-polyfill.js
@@ -29,4 +29,20 @@ if (!Array.prototype.includes) {
     }
     return false;
   };
+
+}
+
+if (!String.prototype.includes) {
+	String.prototype.includes = function(search, start) {
+	    'use strict';
+	    if (typeof start !== 'number') {
+	    	start = 0;
+	    }
+
+	    if (start + search.length > this.length) {
+	    	return false;
+	    } else {
+	    	return this.indexOf(search, start) !== -1;
+		}
+	};
 }
