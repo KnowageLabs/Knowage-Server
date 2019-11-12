6aa1b71eb23beeacc8632f10639998a766787ea4
Marco Libanori <marco.libanori@eng.it>
2019-11-12 10:49:20 +0100

    Add programmatically initialization of Hibernate/Quartz dialect

--- knowage-core/src/main/java/it/eng/spagobi/tools/scheduler/init/QuartzInitializer.java
+++ knowage-core/src/main/java/it/eng/spagobi/tools/scheduler/init/QuartzInitializer.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,55 +11,125 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.tools.scheduler.init;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+import org.quartz.Scheduler;
+import org.quartz.impl.StdSchedulerFactory;
 
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.init.InitializerIFace;
 import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.commons.utilities.SpagoBITracer;
 
-import java.io.InputStream;
-import java.util.Properties;
-
-import org.quartz.Scheduler;
-import org.quartz.impl.StdSchedulerFactory;
-
-
 public class QuartzInitializer implements InitializerIFace {
-	
+
+	private static final String JDBC_HSQLDB = "jdbc:hsqldb";
+	private static final String JDBC_INGRES = "jdbc:ingres";
+	private static final String JDBC_ORACLE = "jdbc:oracle";
+	private static final String JDBC_POSTGRESQL = "jdbc:postgresql";
+	private static final String JDBC_MYSQL = "jdbc:mysql";
+	private static final String JDBC_SQLSERVER = "jdbc:sqlserver";
+
+	public static transient Logger logger = Logger.getLogger(QuartzInitializer.class);
+
 	private SourceBean _config = null;
 
-	
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 *
 	 * @see it.eng.spago.init.InitializerIFace#init(it.eng.spago.base.SourceBean)
 	 */
+	@Override
 	public void init(SourceBean config) {
 		StdSchedulerFactory stdSchedFact = new StdSchedulerFactory();
-	    Properties properties = new Properties();
-	    try{
-	    	Thread currThread = Thread.currentThread();
-	    	ClassLoader classLoader = currThread.getContextClassLoader();
-	    	InputStream propIs = classLoader.getResourceAsStream("quartz.properties");
-	        properties.load(propIs);
-	        stdSchedFact.initialize(properties);
-	        Scheduler sched = stdSchedFact.getScheduler();
-	        sched.start();
-	    } catch (Exception e) {
-	    	SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), 
-	    			            "init", "Error while initializing scheduler " + e);
-	    }
+		Properties properties = new Properties();
+		try {
+			Thread currThread = Thread.currentThread();
+			ClassLoader classLoader = currThread.getContextClassLoader();
+			InputStream propIs = classLoader.getResourceAsStream("quartz.properties");
+			properties.load(propIs);
+
+			String jndiUrl = properties.getProperty("org.quartz.dataSource.quartz.jndiURL");
+
+			if (jndiUrl == null) {
+				throw new IllegalStateException("No value for property org.quartz.dataSource.quartz.jndiURL");
+			}
+
+			Connection connection = null;
+			String driverDelegateClass = null;
+			try {
+				InitialContext ctx = new InitialContext();
+				DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/knowage");
+				connection = ds.getConnection();
+				DatabaseMetaData metaData = connection.getMetaData();
+				String url = metaData.getURL();
+
+				if (url.startsWith(JDBC_MYSQL)) {
+					driverDelegateClass = "org.quartz.impl.jdbcjobstore.StdJDBCDelegate";
+				} else if (url.startsWith(JDBC_SQLSERVER)) {
+					driverDelegateClass = "org.quartz.impl.jdbcjobstore.MSSQLDelegate";
+				} else if (url.startsWith(JDBC_POSTGRESQL)) {
+					driverDelegateClass = "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate";
+				} else if (url.startsWith(JDBC_ORACLE)) {
+					// TODO : Oracle in WEBLOGIC org.quartz.impl.jdbcjobstore.oracle.weblogic.WebLogicOracleDelegate
+					driverDelegateClass = "org.quartz.impl.jdbcjobstore.oracle.OracleDelegate";
+				} else if (url.startsWith(JDBC_INGRES)) {
+					driverDelegateClass = "org.quartz.impl.jdbcjobstore.StdJDBCDelegate";
+				} else if (url.startsWith(JDBC_HSQLDB)) {
+					driverDelegateClass = "org.quartz.impl.jdbcjobstore.HSQLDBDelegate";
+				} else {
+					throw new IllegalStateException("No Hibernate's dialect for URL: " + url);
+				}
+
+			} catch (Exception e) {
+				logger.error("Error determining Hibernate's dialect", e);
+			} finally {
+				if (connection != null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						// Yes, it's mute!
+					}
+				}
+			}
+
+			if (driverDelegateClass == null) {
+				throw new IllegalStateException("Cannot determine driver delegate class. See previous log.");
+			}
+
+			logger.info("Quartz will be initialized with the delegate: " + driverDelegateClass);
+			properties.put("org.quartz.jobStore.driverDelegateClass", driverDelegateClass);
+
+			stdSchedFact.initialize(properties);
+			Scheduler sched = stdSchedFact.getScheduler();
+			sched.start();
+		} catch (Exception e) {
+			SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), "init", "Error while initializing scheduler " + e);
+		}
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 *
 	 * @see it.eng.spago.init.InitializerIFace#getConfig()
 	 */
+	@Override
 	public SourceBean getConfig() {
 		return _config;
 	}
 
-	
 }
--- knowagedao/src/main/java/it/eng/spagobi/commons/utilities/HibernateSessionManager.java
+++ knowagedao/src/main/java/it/eng/spagobi/commons/utilities/HibernateSessionManager.java
@@ -19,6 +19,11 @@ package it.eng.spagobi.commons.utilities;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
@@ -30,13 +35,77 @@ import it.eng.spagobi.commons.dao.DAOConfig;
 
 public class HibernateSessionManager {
 
+	private static final String DIALECT_DB2 = "org.hibernate.dialect.DB2400Dialect";
+	private static final String DIALECT_HSQL = "org.hibernate.dialect.HSQLDialect";
+	private static final String DIALECT_INGRES = "org.hibernate.dialect.IngresDialect";
+	private static final String DIALECT_ORACLE = "org.hibernate.dialect.Oracle9Dialect";
+	private static final String DIALECT_POSTGRE = "org.hibernate.dialect.PostgreSQLDialect";
+	private static final String DIALECT_SQLSERVER = "org.hibernate.dialect.SQLServerDialect";
+	private static final String DIALECT_MYSQL = "org.hibernate.dialect.MySQLDialect";
+
+	private static final String JDBC_DB2 = "jdbc:db2";
+	private static final String JDBC_HSQLDB = "jdbc:hsqldb";
+	private static final String JDBC_INGRES = "jdbc:ingres";
+	private static final String JDBC_ORACLE = "jdbc:oracle";
+	private static final String JDBC_POSTGRESQL = "jdbc:postgresql";
+	private static final String JDBC_MYSQL = "jdbc:mysql";
+	private static final String JDBC_SQLSERVER = "jdbc:sqlserver";
+
 	public static transient Logger logger = Logger.getLogger(HibernateSessionManager.class);
 
 	private static SessionFactory sessionFactory;
 
 	private static void initSessionFactory() {
 		logger.info("Initializing hibernate Session Factory Described by [" + DAOConfig.getHibernateConfigurationFile() + "]");
+
+		Connection connection = null;
+		String hibernateDialect = null;
+		try {
+			InitialContext ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/knowage");
+			connection = ds.getConnection();
+			DatabaseMetaData metaData = connection.getMetaData();
+			String url = metaData.getURL();
+
+			if (url.startsWith(JDBC_MYSQL)) {
+				hibernateDialect = DIALECT_MYSQL;
+			} else if (url.startsWith(JDBC_SQLSERVER)) {
+				hibernateDialect = DIALECT_SQLSERVER;
+			} else if (url.startsWith(JDBC_POSTGRESQL)) {
+				hibernateDialect = DIALECT_POSTGRE;
+			} else if (url.startsWith(JDBC_ORACLE)) {
+				hibernateDialect = DIALECT_ORACLE;
+			} else if (url.startsWith(JDBC_INGRES)) {
+				hibernateDialect = DIALECT_INGRES;
+			} else if (url.startsWith(JDBC_HSQLDB)) {
+				hibernateDialect = DIALECT_HSQL;
+			} else if (url.startsWith(JDBC_DB2)) {
+				hibernateDialect = DIALECT_DB2;
+			} else {
+				throw new IllegalStateException("No Hibernate's dialect for URL: " + url);
+			}
+
+		} catch (Exception e) {
+			logger.error("Error determining Hibernate's dialect", e);
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// Yes, it's mute!
+				}
+			}
+		}
+
+		if (hibernateDialect == null) {
+			throw new IllegalStateException("Cannot determine Hibernate's dialect. See previous log.");
+		}
+
 		Configuration conf = new Configuration();
+
+		logger.info("Session manager will be initialized with the dialect: " + hibernateDialect);
+		conf.setProperty("hibernate.dialect", hibernateDialect);
+
 		File hibernateConfigurationFileFile = DAOConfig.getHibernateConfigurationFileFile();
 		if (hibernateConfigurationFileFile != null) {
 			// for testing
