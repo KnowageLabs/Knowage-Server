8e32a465693fe8b28a0015e5f99e09259c550435
Andrijana Predojevic <Andrijana.Predojevic@eng.it>
2019-12-10 17:00:36 +0100

    [SOLVED KNOWAGE-4696] - Hierarchic cockpit parameters JSON array is not correctly created

--- knowage/src/main/webapp/js/src/angular_1.4/tools/documentexecution/utils/documentExecutionExportService.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/documentexecution/utils/documentExecutionExportService.js
@@ -535,8 +535,8 @@
 				}
 
 
-			} 
-			
+			}
+
 			var body = {
 					user_id: sbiModule_user.userUniqueIdentifier,
 					outputType: exportType,
@@ -544,10 +544,16 @@
 					DOCUMENT_LABEL: execProperties.executionInstance.OBJECT_LABEL,
 					SBI_COUNTRY: sbiModule_config.curr_country,
 					SBI_LANGUAGE: sbiModule_config.curr_language,
-					parametersDataArray : execProperties.parametersData.documentParameters,
+					parametersDataArray : angular.copy(execProperties.parametersData.documentParameters),
 					widgetsPivot : widgetsPivot
 
 			};
+
+
+			for (var i = 0; i < body.parametersDataArray.length; i++){
+				if(body.parametersDataArray[i].children)
+					delete body.parametersDataArray[i].children;
+			}
 
 			for (var parameter in parameters) {
 				if (parameters.hasOwnProperty(parameter)) {
@@ -555,7 +561,7 @@
 				}
 			}
 
-		
+
 			var aggregations = documentFrame.window.angular.element(document).find('iframe').contents().find('body').scope().cockpitModule_template.configuration.aggregations;
 			var filters = documentFrame.window.angular.element(document).find('iframe').contents().find('body').scope().cockpitModule_template.configuration.filters;
 			var cockpitSelections = {};
