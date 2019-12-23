13a5d30bbfd953ae1f0206a22a87add788e6f0a7
Dragan Pirkovic <dragan.pirkovic@eng.it>
2019-12-23 16:49:43 +0100

    [BUG FIXED] KNOWAGE-4731
    (cherry picked from commit c44f50c35edad74cabe3c8cdea231443342eef3e)

--- knowageqbeengine/src/main/webapp/js/src/qbe/directive/calculated-field-editor/calculatedFieldEditor.js
+++ knowageqbeengine/src/main/webapp/js/src/qbe/directive/calculated-field-editor/calculatedFieldEditor.js
@@ -42,7 +42,7 @@ angular.module('qbe_calculated_field_editor', ['ngSanitize', 'ui.codemirror'])
 		};
 	})
 
-.directive('qbeCalculatedFieldEditor', function($sce, $mdDialog, $mdMenu, $mdSidenav, $timeout,sbiModule_translate) {
+.directive('qbeCalculatedFieldEditor', function($sce, $mdDialog, $mdMenu, $mdSidenav, $timeout,sbiModule_translate,formatDate) {
     return {
         restrict: 'E',
         scope: {
@@ -59,7 +59,7 @@ angular.module('qbe_calculated_field_editor', ['ngSanitize', 'ui.codemirror'])
             $scope.availableFormulaTypes = [];
             $scope.availableTypes = ['NUMBER','STRING','DATE'];
             $scope.availableNatures = ['MEASURE','ATTRIBUTE'];
-            $scope.dateFormats = ['LLLL','llll','LLL','lll','DD/MM/YYYY HH:mm:SS','DD/MM/YYYY HH:mm','LL','ll','L','l']
+            $scope.dateFormats = formatDate.dateFormats;
             $scope.calculatedField.fieldType = "inline.calculated.field";
 
             //fix for codemirror to refresh when opened
--- knowageqbeengine/src/main/webapp/js/src/qbe/services/format/formatModule.js
+++ knowageqbeengine/src/main/webapp/js/src/qbe/services/format/formatModule.js
@@ -15,24 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/**
- * Dependencies for the Workspace main controller:
- * 		- document_viewer: Directive that provides possibility to execute a document in separate
- * 		iframe (window) that has a button for closing the executed document. When user do that,
- * 		the iframe closes and we are having the initial page (the one from which we wished to
- * 		execute the document).
- */
-
-angular
-	.module('services',
-			[	'entities',
-				'queries',
-				'filters',
-				'saveservice',
-				'formulas',
-				'parameters',
-				'selectedEntitiesRelationshipsModule',
-				'expressions',
-				'formatModule'
-				]);
+(function(){
+	angular.module("formatModule",["ngMaterial"])
+})()
