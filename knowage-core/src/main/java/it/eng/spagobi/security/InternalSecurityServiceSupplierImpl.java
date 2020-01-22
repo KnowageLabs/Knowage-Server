bbb1d731222099706dc8c7b9d5de5defaf5ffd9d
dzerbett <davide.zerbetto@eng.it>
2020-01-22 18:28:19 +0100

    [IN PROGRESS KNOWAGE-4776] Moving public user check and creation from internal security connector to UserUtilities class
    (cherry picked from commit cbca76f602a3fae980a33f6a4316c71c15e5257d)

--- knowage-core/src/main/java/it/eng/spagobi/security/InternalSecurityServiceSupplierImpl.java
+++ knowage-core/src/main/java/it/eng/spagobi/security/InternalSecurityServiceSupplierImpl.java
@@ -30,7 +30,6 @@ import it.eng.spago.error.EMFUserError;
 import it.eng.spagobi.commons.dao.DAOFactory;
 import it.eng.spagobi.commons.dao.IRoleDAO;
 import it.eng.spagobi.commons.metadata.SbiExtRoles;
-import it.eng.spagobi.profiling.PublicProfile;
 import it.eng.spagobi.profiling.bean.SbiUser;
 import it.eng.spagobi.profiling.bean.SbiUserAttributes;
 import it.eng.spagobi.services.common.JWTSsoService;
@@ -123,13 +122,6 @@ public class InternalSecurityServiceSupplierImpl implements ISecurityServiceSupp
 		String userId = JWTSsoService.jwtToken2userId(jwtToken);
 		logger.debug("userId: " + userId);
 		SpagoBIUserProfile profile = null;
-
-		// check if user is public then create public profile
-		if (PublicProfile.isPublicUser(jwtToken)) {
-			profile = PublicProfile.createPublicUserProfile(userId);
-			logger.debug("Built public profile");
-			return profile;
-		}
 
 		SbiUser user = DAOFactory.getSbiUserDAO().loadSbiUserByUserId(userId);
 
--- knowagedao/src/main/java/it/eng/spagobi/commons/utilities/UserUtilities.java
+++ knowagedao/src/main/java/it/eng/spagobi/commons/utilities/UserUtilities.java
@@ -62,7 +62,9 @@ import it.eng.spagobi.commons.metadata.SbiTenant;
 import it.eng.spagobi.dao.exception.DAORuntimeException;
 import it.eng.spagobi.engines.config.bo.Engine;
 import it.eng.spagobi.engines.config.dao.IEngineDAO;
+import it.eng.spagobi.profiling.PublicProfile;
 import it.eng.spagobi.profiling.bean.SbiAccessibilityPreferences;
+import it.eng.spagobi.services.common.JWTSsoService;
 import it.eng.spagobi.services.common.SsoServiceFactory;
 import it.eng.spagobi.services.common.SsoServiceInterface;
 import it.eng.spagobi.services.security.bo.SpagoBIUserProfile;
@@ -208,7 +210,13 @@ public class UserUtilities {
 			try {
 				UserProfile profile;
 				if (UserProfile.isSchedulerUser(userId)) {
+					logger.debug("User [" + userId + "] has been recognized as a scheduler user.");
 					profile = UserProfile.createSchedulerUserProfile(userId);
+				} else if (PublicProfile.isPublicUser(userId)) {
+					logger.debug("User [" + userId + "] has been recognized as a public user.");
+					String decodedUserId = JWTSsoService.jwtToken2userId(userId);
+					SpagoBIUserProfile user = PublicProfile.createPublicUserProfile(decodedUserId);
+					profile = new UserProfile(user);
 				} else {
 					ISecurityServiceSupplier supplier = createISecurityServiceSupplier();
 					SpagoBIUserProfile user = supplier.createUserProfile(userId);
