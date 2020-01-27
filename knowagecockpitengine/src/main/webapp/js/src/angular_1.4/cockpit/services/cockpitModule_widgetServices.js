6374d24956fcf5b339f65316127c87724be73b4a
matmassa <matteo.massarotto@gmail.com>
2020-01-27 10:57:44 +0100

    [Knowage-4787] - removed lastTimestampedSelection check

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/services/cockpitModule_widgetServices.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/services/cockpitModule_widgetServices.js
@@ -394,16 +394,8 @@ angular.module("cockpitModule").service("cockpitModule_widgetServices",function(
                         }
                     }else{
                         var data = {};
-                        if(!cockpitModule_widgetSelection.isLastTimestampedSelection(config.dataset.label, config.content.selectedColumn.name)){
-                        	data.activeValues = wi.loadDatasetRecords(config, options, false);
-                        }
-                        else {
-
-                        	if (angular.equals(cockpitModule_widgetSelection.getLastTimestampedSelection().values,  config.activeValues)) {
-                        		data.activeValues = wi.loadDatasetRecords(config, options, false);
-                        	}
-                        	else data.activeValues = config.activeValues;
-                        }
+                     	data.activeValues = wi.loadDatasetRecords(config, options, false);
+                        
                         $rootScope.$broadcast("WIDGET_EVENT"+config.id,"REFRESH",{element:element,width:width,height:height,data:data,nature:nature});
                     }
                 }
