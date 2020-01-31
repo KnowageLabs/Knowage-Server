037f3590d2ba1e42ff4a2f4dad92f504ff2f093c
Marco Libanori <marco.libanori@eng.it>
2020-01-31 14:01:21 +0100

    [KNOWAGE-4784] Merging all the versions of SingletonConfigCache

--- knowagecommonjengine/src/main/java/it/eng/spagobi/commons/SingletonConfigCache.java
+++ knowagecommonjengine/src/main/java/it/eng/spagobi/commons/SingletonConfigCache.java
@@ -17,7 +17,12 @@
  */
 package it.eng.spagobi.commons;
 
+import it.eng.spagobi.commons.bo.Config;
+import it.eng.spagobi.commons.dao.DAOFactory;
+import it.eng.spagobi.commons.dao.IConfigDAO;
+
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
@@ -26,33 +31,36 @@ import org.apache.log4j.Logger;
  * implementation.
  * 
  * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
  */
 public class SingletonConfigCache implements ISingletonConfigCache {
 
-	private static transient Logger logger = Logger.getLogger(SingletonConfigCache.class);
+	private static Logger logger = Logger.getLogger(SingletonConfigCache.class);
 	private final HashMap<String, String> cache = new HashMap<String, String>();
 
 	public SingletonConfigCache() {
 		logger.debug("IN");
 
-		// IConfigDAO dao = null;
-		// try {
-		// dao = DAOFactory.getSbiConfigDAO();
-		// List<Config> allConfig = dao.loadAllConfigParameters();
-		// if (allConfig.size() == 0)
-		// logger.error("The table sbi_config is EMPTY");
-		// for (Config config : allConfig) {
-		// cache.put(config.getLabel(), config.getValueCheck());
-		// logger.info("Add: " + config.getLabel() + " / " + config.getValueCheck());
-		// }
-		// } catch (Exception e) {
-		// logger.error("Impossible to load configuration for full kpi engine", e);
-		// e.printStackTrace();
-		// } finally {
-		// logger.debug("OUT");
-		// }
+		IConfigDAO dao = null;  
+		try {
+			dao = DAOFactory.getSbiConfigDAO();
+			List<Config> allConfig = dao.loadAllConfigParameters();
+			if (allConfig.isEmpty()) {
+				logger.error("The table sbi_config is EMPTY");
+			}
+			for (Config config: allConfig ) {
+				cache.put(config.getLabel(), config.getValueCheck());
+				logger.info("Add: "+config.getLabel() +" / "+config.getValueCheck());
+			}
+			
+		} catch (Throwable e) {
+			logger.error("Impossible to load configuration for report engine",e);
+		} finally {
+			logger.debug("OUT");
+		}
 	}
 
+	@Override
 	public String get(String key) {
 		if (cache.get(key) == null) {
 			logger.info("The property '" + key + "' doens't have any value assigned, check SBI_CONFIG table");
