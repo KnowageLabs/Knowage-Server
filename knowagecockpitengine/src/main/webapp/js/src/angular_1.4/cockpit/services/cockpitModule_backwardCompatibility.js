7f37cab8ecc4d3b5df3a6e28d538e41a13768884
Marco Libanori <marco.libanori@eng.it>
2020-02-04 10:40:46 +0100

    [KNOWAGE-3957] Add layout option to crosstable's style panel

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/services/cockpitModule_backwardCompatibility.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/services/cockpitModule_backwardCompatibility.js
@@ -72,6 +72,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 						}
 					}
 				}
+				if(model.type=='static-pivot-table') {
+					if (!model.content.style) {
+						model.content.style = {};
+					}
+					if (!model.content.style.generic) {
+						model.content.style.generic = {};
+					}
+					model.content.style.generic['layout'] = 'auto';
+				}
 			}
 			//to version 6.4
 			if(!self.compareVersion("6.4.3",model.knowageVersion)){
@@ -106,15 +115,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 				}
 				if(model.type=='selector'){
 					if(model.settings && model.settings.modalityValue == 'multiValue' && model.settings.modalityPresent == 'COMBOBOX') model.settings.modalityValue = 'multiDropdown';
-				}
-				if(model.type=='static-pivot-table') {
-					if (!model.content.style) {
-						model.content.style = {};
-					}
-					if (!model.content.style.generic) {
-						model.content.style.generic = {};
-					}
-					model.content.style.generic['layout'] = 'auto';
 				}
 			}
 
