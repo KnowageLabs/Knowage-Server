d4d1d3061878dc2cf0c7d95b57e423c1785e30ec
Radmila Selakovic <radmila.selakovic@eng.it>
2020-02-12 10:43:15 +0100

    fixing issie on orme env
    (cherry picked from commit 746acec08287f254a7b432276d50fe1b0b1c8803)

--- knowagecockpitengine/src/main/java/it/eng/spagobi/engine/chart/util/DataSetTransformer.java
+++ knowagecockpitengine/src/main/java/it/eng/spagobi/engine/chart/util/DataSetTransformer.java
@@ -846,7 +846,8 @@ public class DataSetTransformer {
 		if (drillOrder != null) {
 			for (String key : drillOrder.keySet()) {
 				Map<String, String> keyMapper = (Map<String, String>) drillOrder.get(key);
-				if (!keyMapper.get("orderColumn").equals("") && !keyMapper.get("orderColumn").equals(categorieColumns.get("column"))) {
+				if (!keyMapper.get("orderColumn").equals("") && !keyMapper.get("orderColumn").equals(categorieColumns.get("column"))
+						&& !drillOrder.containsKey(keyMapper.get("orderColumn"))) {
 					dataColumnsMapper.remove(keyMapper.get("orderColumn").toLowerCase());
 				}
 			}
