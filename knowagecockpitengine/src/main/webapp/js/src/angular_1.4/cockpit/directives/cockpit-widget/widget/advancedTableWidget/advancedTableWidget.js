c7877b32b65918a1e548ef90b715cb517fb0f87a
Davide Vernassa <davide.vernassa@eng.it>
2020-03-02 14:35:50 +0100

    [KNOWAGE-4839] - Added border radius in groups definitiondefinition

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/advancedTableWidget/advancedTableWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/advancedTableWidget/advancedTableWidget.js
@@ -190,13 +190,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 									if(columnGroups[group.name]) {
 										columns[columnGroups[group.name]].children.push(tempCol);
 									}else {
+										columnGroups[group.name] = columns.length;
 										columns.push({
 									        headerName: group.name,
 									        headerGroupComponent: CustomHeaderGroupRenderer,
 									        headerParams: group,
 									        children: [tempCol]
 									    });
-									    columnGroups[group.name] = c;
 									}
 								}
 							})
