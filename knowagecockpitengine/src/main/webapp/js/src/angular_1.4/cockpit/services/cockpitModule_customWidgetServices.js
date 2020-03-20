9597c262f98cc324bcc37de86a8d9a7af562ff42
Radmila Selakovic <radmila.selakovic@eng.it>
2020-03-20 11:29:01 +0100

    [IN PROGRESS KNOWAGE-4935] adding first api functions

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/services/cockpitModule_customWidgetServices.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/services/cockpitModule_customWidgetServices.js
@@ -57,5 +57,102 @@ angular.module("cockpitModule").service("cockpitModule_customWidgetServices",fun
 		return columnSelectedOfDataset;
 	}
 
+	this.transformDataStore = function (datastore){
+		var newDataStore = {};
+		newDataStore.metaData = datastore.metaData;
+		newDataStore.results = datastore.results;
+		newDataStore.rows = [];
+
+		for(var i=0; i<datastore.rows.length; i++){
+			var obj = {};
+			for(var j=1; j<datastore.metaData.fields.length; j++){
+				if(datastore.rows[i][datastore.metaData.fields[j].name]!=undefined){
+					obj[datastore.metaData.fields[j].header] = datastore.rows[i][datastore.metaData.fields[j].name];
+				}
+			}
+			newDataStore.rows.push(obj);
+		}
+		return newDataStore;
+	}
+
+	this.getDataArray = function (getDataArrayFn,dataStore){
+		var newDataStore = this.transformDataStore(dataStore) ;
+		var dataArray = [];
+		for(var i=0; i<newDataStore.rows.length; i++){
+			var dataObj = getDataArrayFn(newDataStore.rows[i]);
+			dataArray.push(dataObj)
+		}
+		return dataArray;
+
+	}
+
+	this.getColumn = function (categoryName, dataStore){
+
+		var categArray = [];
+		var fields = dataStore.metaData.fields;
+		var categoryColumn = dataStore.metaData.fields;
+		for(var i=1; i<fields.length; i++){
+
+			if(fields[i].header==categoryName){
+				categoryColumn = fields[i].name;
+			}
+		}
+		for(var i=0; i<dataStore.rows.length; i++){
+			var dataObj = dataStore.rows[i][categoryColumn];
+			categArray.push(dataObj)
+		}
+
+		categArray = categArray.filter(function(item, pos) {
+			return categArray.indexOf(item) == pos;
+		})
+		return categArray;
+
+	}
+
+	this.getSeriesAndData = function (getDataArrayFn,column,dataStore){
+
+		var newDataStore = this.transformDataStore(dataStore);
+		var seriesMap = {};
+		for(var i=0; i<newDataStore.rows.length; i++){
+			if(seriesMap[newDataStore.rows[i][column]]==undefined){
+				seriesMap[newDataStore.rows[i][column]] = []
+			}
+
+			seriesMap[newDataStore.rows[i][column]].push(getDataArrayFn(newDataStore.rows[i]))
+		}
+		var series = []
+		for (var property in seriesMap) {
+			var serieObj = {};
+			serieObj.name = property;
+			serieObj.id = property;
+			serieObj.data = seriesMap[property];
+			series.push(serieObj)
+		}
+		return series;
+	}
+
+	var sortAsc = function (array){
+		array.sort(function(a, b){
+		    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+		    if (nameA < nameB) //sort string ascending
+		        return -1
+		    if (nameA > nameB)
+		        return 1
+		    return 0 //default return value (no sorting)
+		})
+	}
+
+	var sortDesc = function (array){
+		array.sort(function(a, b){
+		    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+		    if (nameA > nameB) //sort string ascending
+		        return -1
+		    if (nameA < nameB)
+		        return 1
+		    return 0 //default return value (no sorting)
+		})
+	}
+
+
 });
 
