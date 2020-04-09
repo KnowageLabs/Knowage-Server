8c8919208cf4ae69e2f1833f5a96e4252541586e
Davide Vernassa <davide.vernassa@eng.it>
2020-04-09 18:01:57 +0200

    [KNOWAGE-4949] - Added dynamic value in crossnavigation

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/cockpitWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/cockpitWidget.js
@@ -855,7 +855,6 @@ cockpitModule_templateServices.getDatasetUsetByWidgetWithParams();
 
 			// enter cross navigation mode
 			var doCross = false;
-
 			var nameToCheckForCross = columnName;
 			if(columnName != undefined){
 				// check if selected column has been renamed by an alias, in that
@@ -957,7 +956,7 @@ cockpitModule_templateServices.getDatasetUsetByWidgetWithParams();
 								var objToAdd = {};
 								objToAdd[par] = valToAdd;
 								otherOutputParameters.push(objToAdd);
-							}else if(model.type == 'text' || model.type == 'html'){
+							}else if(model.type == 'text' || model.type == 'html' || model.type == 'customchart'){
 								var OBJ = {};
 								OBJ[par] = columnValue;
 								otherOutputParameters.push(OBJ);
