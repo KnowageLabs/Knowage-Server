20b0c63235f08ead2ca785a99af691093dd42211
Marco Balestri <marco.balestri@eng.it>
2020-04-15 17:24:12 +0200

    [KNOWAGE-4976] Added parameters resolution in dataset request

--- Knowage-R/utils.R
+++ Knowage-R/utils.R
@@ -19,15 +19,32 @@ resolve_drivers <- function(script, drivers){
   script
 }
 
-resolve_parameters <- function(script, parameters){
+build_parameters <- function(parameters){
+  to_return <- list()
   parameters_df <- as.data.frame(parameters)
-  print("BEFORE")
   for(i in 1:nrow(parameters_df)) {
     row <- parameters_df[i,]
-    print("ROW")
-    print(parameters_df)
+    name <- row[["name"]]
+    value <- row[["value"]]
+    type <- row[["type"]]
+    if(value == "") {
+      value <- row[["defaultValue"]]
+    }
+    if(type == "Number") {
+      value <- as.numeric(value)
+    }
+    to_return[[name]] <- value
   }
-  print("AFTER")
+  to_return
+}
+
+resolve_parameters <- function(script, parameters){
+  for(name in names(parameters)) {
+    value <- parameters[[name]]
+    original <- paste0('\\$P\\{', name , '\\}')
+    final <- paste0('parameters_[[\\"', name, '\\"]]')
+    script <- gsub(original,final,script)
+  }
   script
 }
 
