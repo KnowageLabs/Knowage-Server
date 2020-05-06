6281566d5f44727680aca35f8e18ffb703bc2d25
matmassa <matteo.massarotto@gmail.com>
2020-05-06 14:40:16 +0200

    [KNOWAGE-5114] - No delimiter usage for Postgres data column names

--- knowageutils/src/main/java/it/eng/spagobi/tools/dataset/metasql/query/visitor/PostgreSqlSelectQueryVisitor.java
+++ knowageutils/src/main/java/it/eng/spagobi/tools/dataset/metasql/query/visitor/PostgreSqlSelectQueryVisitor.java
@@ -18,12 +18,56 @@
 
 package it.eng.spagobi.tools.dataset.metasql.query.visitor;
 
+import it.eng.spagobi.commons.utilities.StringUtilities;
+import it.eng.spagobi.tools.dataset.common.query.AggregationFunctions;
+import it.eng.spagobi.tools.dataset.common.query.IAggregationFunction;
+import it.eng.spagobi.tools.dataset.metasql.query.item.Projection;
+import it.eng.spagobi.utilities.database.AbstractDataBase;
 import it.eng.spagobi.utilities.database.IDataBase;
 
 public class PostgreSqlSelectQueryVisitor extends AbstractSelectQueryVisitor {
 
 	public PostgreSqlSelectQueryVisitor(IDataBase database) {
 		super(database);
+	}
+
+	/**
+	 * @param projection
+	 * @param useAlias   enables the output of an alias. Column name is used as alias if related flag is true.
+	 */
+	@Override
+	protected void append(Projection projection, boolean useAlias) {
+		String aliasDelimiter = database.getAliasDelimiter();
+		IAggregationFunction aggregationFunction = projection.getAggregationFunction();
+
+		String name = projection.getName();
+		String columnName = isCalculatedColumn(name) ? name.replace(AbstractDataBase.STANDARD_ALIAS_DELIMITER, aliasDelimiter) : name;
+
+		boolean isValidAggregationFunction = aggregationFunction != null && !aggregationFunction.getName().equals(AggregationFunctions.NONE);
+		if (!isValidAggregationFunction) {
+			queryBuilder.append(columnName);
+		} else {
+			queryBuilder.append(aggregationFunction.apply(columnName));
+		}
+
+		String alias = projection.getAlias();
+		if (useAlias) {
+			if (StringUtilities.isNotEmpty(alias) && !alias.equals(name)) {
+				queryBuilder.append(" ");
+				queryBuilder.append(aliasPrefix);
+				queryBuilder.append(" ");
+				queryBuilder.append(aliasDelimiter);
+				queryBuilder.append(alias);
+				queryBuilder.append(aliasDelimiter);
+			} else if (useNameAsAlias || isValidAggregationFunction) {
+				queryBuilder.append(" ");
+				queryBuilder.append(aliasPrefix);
+				queryBuilder.append(" ");
+				queryBuilder.append(aliasDelimiter);
+				queryBuilder.append(name);
+				queryBuilder.append(aliasDelimiter);
+			}
+		}
 	}
 
 }
