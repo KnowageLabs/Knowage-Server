5f599334811b70768dc8cbd9a364f3b7d8e56675
Marco Libanori <marco.libanori@eng.it>
2020-07-09 16:26:01 +0200

    [KNOWAGE-5283] Fix cover graph validation in QBE

--- qbecore/src/main/java/it/eng/qbe/statement/graph/validator/AbstractGraphValidator.java
+++ qbecore/src/main/java/it/eng/qbe/statement/graph/validator/AbstractGraphValidator.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,33 +11,43 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.qbe.statement.graph.validator;
 
-import it.eng.qbe.model.structure.IModelEntity;
-
 import java.util.Set;
 
 import org.jgrapht.Graph;
 
+import it.eng.qbe.model.structure.IModelEntity;
+
 public abstract class AbstractGraphValidator implements IGraphValidator {
 
-	
+
+	@Override
 	public boolean isValid(Graph G, Set<IModelEntity> unjoinedEntities){
 
-		
+
 		// the empty graph is always valid
-		if((unjoinedEntities==null || unjoinedEntities.size()==0) && (G==null || G.vertexSet().size()==0)){
+		Set<?> vertexSet = G.vertexSet();
+		if((unjoinedEntities == null || unjoinedEntities.isEmpty()) && (G == null || vertexSet.isEmpty())){
 			return true;
 		}
-		
+
+		/*
+		 * Check if the graph contains at least all the
+		 * required entities.
+		 */
+		if (!vertexSet.isEmpty() && !vertexSet.containsAll(unjoinedEntities)) {
+			return false;
+		}
+
 		return validate(G, unjoinedEntities);
-		
+
 	}
 
 	public abstract boolean validate(Graph G, Set<IModelEntity> unjoinedEntities);
-	
+
 }
