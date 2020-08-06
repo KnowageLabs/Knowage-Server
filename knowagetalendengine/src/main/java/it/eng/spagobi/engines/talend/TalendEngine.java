c36fabeb9e93945b47d1e80bfe1ec2827e11d458
matmassa <matteo.massarotto@gmail.com>
2020-08-06 13:30:32 +0200

    [KNOWAGE-3971] - Talend engine changed runtime job path retrieval

--- knowagetalendengine/src/main/java/it/eng/spagobi/engines/talend/TalendEngine.java
+++ knowagetalendengine/src/main/java/it/eng/spagobi/engines/talend/TalendEngine.java
@@ -1,11 +1,9 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engines.talend;
-
-
 
 import java.io.File;
 
@@ -16,49 +14,31 @@ import it.eng.spagobi.utilities.engines.SpagoBIEngineException;
  * @author Andrea Gioia
  *
  */
-public class TalendEngine  {
-	
+public class TalendEngine {
+
 	private static TalendEngineVersion version;
-	private static TalendEngineConfig config;	
-		
-	
-	private static RuntimeRepository runtimeRepository;
-		
-	
-	static { 
+	private static TalendEngineConfig config;
+
+	static {
 		TalendEngine.version = TalendEngineVersion.getInstance();
 		TalendEngine.config = TalendEngineConfig.getInstance();
-		
-		File rrRootDir = TalendEngineConfig.getInstance().getRuntimeRepositoryRootDir();
-		TalendEngine.setRuntimeRepository( new RuntimeRepository(rrRootDir) );		
 	}
-	
-	
+
 	public static RuntimeRepository getRuntimeRepository() throws SpagoBIEngineException {
-		if(runtimeRepository == null || !runtimeRepository.getRootDir().exists()) {
-			throw new SpagoBIEngineException("Runtime-Repository not available",
-					"repository.not.available");
+		File rrRootDir = TalendEngineConfig.getInstance().getRuntimeRepositoryRootDir();
+		RuntimeRepository runtimeRepository = new RuntimeRepository(rrRootDir);
+		if (!runtimeRepository.getRootDir().exists()) {
+			throw new SpagoBIEngineException("Runtime-Repository not available", "repository.not.available");
 		}
-		return TalendEngine.runtimeRepository;
+		return runtimeRepository;
 	}
-
-
-	private static void setRuntimeRepository(RuntimeRepository runtimeRepository) {
-		TalendEngine.runtimeRepository = runtimeRepository;
-	}
-
 
 	public static TalendEngineVersion getVersion() {
 		return TalendEngine.version;
 	}
 
-
 	public static TalendEngineConfig getConfig() {
 		return TalendEngine.config;
 	}
 
-	
-	
-	
-	
 }
