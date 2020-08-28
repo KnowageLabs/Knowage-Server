2a81e516a9771c0f4650a46af90c84c3fd0f92c1
Marco Balestri <marco.balestri@eng.it>
2020-08-28 12:01:07 +0200

    [KNOWAGE-5349] cherry-pick from master

--- knowageutils/src/main/java/it/eng/spagobi/tools/dataset/bo/PythonDataSet.java
+++ knowageutils/src/main/java/it/eng/spagobi/tools/dataset/bo/PythonDataSet.java
@@ -18,6 +18,7 @@
 package it.eng.spagobi.tools.dataset.bo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -37,6 +38,7 @@ import it.eng.spagobi.tools.dataset.common.dataproxy.PythonDataProxy;
 import it.eng.spagobi.tools.dataset.common.datareader.JSONPathDataReader;
 import it.eng.spagobi.tools.dataset.common.datareader.JSONPathDataReader.JSONPathAttribute;
 import it.eng.spagobi.tools.dataset.constants.PythonDataSetConstants;
+import it.eng.spagobi.tools.dataset.exceptions.ParametersNotValorizedException;
 import it.eng.spagobi.tools.datasource.bo.IDataSource;
 import it.eng.spagobi.user.UserProfileManager;
 import it.eng.spagobi.utilities.Helper;
@@ -124,11 +126,14 @@ public class PythonDataSet extends ConfigurableDataSet {
 			requestHeaders = getRequestHeadersPropMap(PythonDataSetConstants.REST_REQUEST_HEADERS, jsonConf);
 			JSONObject pythonEnv = new JSONObject(getProp(PythonDataSetConstants.PYTHON_ENVIRONMENT, jsonConf, false));
 			String label = pythonEnv.get("label").toString();
-			String pythonAddress = SingletonConfig.getInstance().getConfigValue(label);
-			if (pythonDatasetType != null && pythonDatasetType.equals("r"))
-				restAddress = "http://" + pythonAddress + "/dataset"; // R engine does not support https
-			else
-				restAddress = "https://" + pythonAddress + "/dataset";
+			if ("python".equals(pythonDatasetType)) {
+				pythonAddress = PythonUtils.getPythonAddress(label);
+			} else if ("r".equals(pythonDatasetType)) {
+				pythonAddress = RUtils.getRAddress(label);
+			} else {
+				throw new IllegalStateException("Invalid Python Dataset Type: " + pythonDatasetType);
+			}
+			restAddress = pythonAddress.replaceAll("/+$", "") + "/dataset";
 
 		} catch (Exception e) {
 			throw new ConfigurationException("Problems in configuration of data proxy", e);
@@ -260,4 +265,149 @@ public class PythonDataSet extends ConfigurableDataSet {
 		return getUserProfile() != null ? getUserProfile() : UserProfileManager.getProfile();
 	}
 
+	/*
+	 * These snippets of code come from a common utilities logic, now the logic behind parameters handling is inside each type of dataset
+	 */
+	@Override
+	public void setParametersMap(Map<String, String> paramValues) throws JSONException {
+		List<JSONObject> parameters = getDataSetParameters();
+		if (parameters.size() > paramValues.size()) {
+			String parameterNotValorizedStr = getParametersNotValorized(parameters, paramValues);
+			throw new ParametersNotValorizedException("The following parameters have no value [" + parameterNotValorizedStr + "]");
+		}
+
+		if (paramValues.size() > 0) {
+			for (String paramName : paramValues.keySet()) {
+				for (int i = 0; i < parameters.size(); i++) {
+					JSONObject parameter = parameters.get(i);
+					if (paramName.equals(parameter.optString("namePar"))) {
+						String[] values = getValuesAsArray(paramValues, paramName, parameter);
+						List<String> encapsulatedValues = encapsulateValues(parameter, values);
+						paramValues.put(paramName, org.apache.commons.lang3.StringUtils.join(encapsulatedValues, ","));
+						break;
+					}
+				}
+			}
+			setParamsMap(paramValues);
+		}
+	}
+
+	private static String getParametersNotValorized(List<JSONObject> parameters, Map<String, String> parametersValues) throws JSONException {
+		String toReturn = "";
+
+		for (Iterator<JSONObject> iterator = parameters.iterator(); iterator.hasNext();) {
+			JSONObject parameter = iterator.next();
+			String parameterName = parameter.getString("namePar");
+			if (parametersValues.get(parameterName) == null) {
+				toReturn += parameterName;
+				if (iterator.hasNext()) {
+					toReturn += ", ";
+				}
+			}
+		}
+		return toReturn;
+	}
+
+	private String[] getValuesAsArray(Map<String, String> paramValues, String paramName, JSONObject parameter) {
+		boolean isMultiValue = parameter.optBoolean("multiValuePar");
+		String paramValue = paramValues.get(paramName);
+		String[] values = null;
+		if (isMultiValue) {
+			List<String> list = new ArrayList<String>();
+			boolean paramValueConsumed = false;
+			try {
+				JSONArray jsonArray = new JSONArray(paramValue);
+				for (int j = 0; j < jsonArray.length(); j++) {
+					list.add(jsonArray.getString(j));
+				}
+				paramValueConsumed = true;
+			} catch (JSONException e) {
+				paramValueConsumed = false;
+			}
+			if (!paramValueConsumed) {
+				list.add(paramValue);
+			}
+			values = list.toArray(new String[0]);
+		} else {
+			values = Arrays.asList(paramValue).toArray(new String[0]);
+		}
+		return values;
+	}
+
+	/**
+	 * Encapsulate values into SQL values.
+	 *
+	 * For every type of data except string, the method convert the values to strings.
+	 *
+	 * With strings we can have two case:
+	 * <ul>
+	 * <li>String that starts and ends with single quote</li>
+	 * <li>String that doesn't start and end with single quote</li>
+	 * </ul>
+	 *
+	 * In the first case, FE are sending us SQL values that probably contain JSON escape (e.g., a JSON value like 'this string contains a \' in it').
+	 *
+	 * In the second case, FE are sending us standard not-SQL-escaded string ( e.g., a string like "this string contains a ' in it"). In this second case this
+	 * method escapes single quote and duplicates them as requested by SQL.
+	 *
+	 * @param parameter Original parameter JSON metadata
+	 * @param values    Actual values of parameters
+	 * @return List of encapsulated values as strings
+	 */
+	private List<String> encapsulateValues(JSONObject parameter, String[] values) {
+		String typePar = parameter.optString("typePar");
+		boolean isString = "string".equalsIgnoreCase(typePar);
+		String delim = isString ? "'" : "";
+
+		List<String> newValues = new ArrayList<>();
+		for (int j = 0; j < values.length; j++) {
+			String value = values[j].trim();
+			if (!value.isEmpty()) {
+				if (value.startsWith(delim) && value.endsWith(delim)) {
+					if (value.contains("','")) {
+						value = value.substring(1, value.length() - 1);
+						String[] valuesArray = value.split("','");
+						String newValuesFromArray = "";
+						for (int i = 0; i < valuesArray.length; i++) {
+							String temp = valuesArray[i];
+							if (!delim.isEmpty() && temp.startsWith(delim) && temp.endsWith(delim))
+								temp = temp.substring(1, temp.length() - 1);
+							temp = temp.replaceAll("'", "''");
+							if (i == 0) {
+								if (!delim.isEmpty() && temp.startsWith(delim) && temp.endsWith(delim)) {
+									temp = temp.substring(1, temp.length() - 1);
+								}
+								newValuesFromArray = (temp);
+							}
+
+							else {
+								if (!delim.isEmpty() && temp.startsWith(delim) && temp.endsWith(delim)) {
+									temp = temp.substring(1, temp.length() - 1);
+								}
+								newValuesFromArray = newValuesFromArray + "," + (temp);
+							}
+
+						}
+						newValues.add(newValuesFromArray);
+					} else {
+						if (isString) {
+							value = value.substring(1, value.length() - 1);
+							value = value.replaceAll("'", "''");
+						}
+						if (!delim.isEmpty() && value.startsWith(delim) && value.endsWith(delim)) {
+							value = value.substring(1, value.length() - 1);
+						}
+						newValues.add(value);
+					}
+				} else {
+					if (isString) {
+						// Duplicate single quote to transform it into an escaped SQL single quote
+						value = value.replaceAll("'", "''");
+					}
+					newValues.add(value);
+				}
+			}
+		}
+		return newValues;
+	}
 }
