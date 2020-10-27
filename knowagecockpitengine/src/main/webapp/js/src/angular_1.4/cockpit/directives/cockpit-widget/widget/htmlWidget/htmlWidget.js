762a397defd0b3568f140dd0381b56724b150add
Davide Vernassa <davide.vernassa@eng.it>
2020-10-27 17:07:17 +0100

    [KNOWAGE-5530] - Added multivalue selection in kn-selection html tag

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/htmlWidget/htmlWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/htmlWidget/htmlWidget.js
@@ -371,7 +371,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 				  if (allElements[j] && allElements[j].hasAttribute("kn-selection-column")){
 					  	var columnSelectionLabel = allElements[j].getAttribute("kn-selection-column");
 					  	var columnSelectionValue = allElements[j].getAttribute("kn-selection-value");
-					  	allElements[j].setAttribute("ng-click", columnSelectionValue ? "select('"+ columnSelectionLabel +"','"+columnSelectionValue +"')" : "select('"+ columnSelectionLabel +"')");
+					  	if(columnSelectionValue.charAt(0) != "[") columnSelectionValue = "'"+columnSelectionValue+"'";
+					  	allElements[j].setAttribute("ng-click", columnSelectionValue ? "select('"+ columnSelectionLabel +"',"+columnSelectionValue +")" : "select('"+ columnSelectionLabel +"')");
 				    }
 				  j++;
 
