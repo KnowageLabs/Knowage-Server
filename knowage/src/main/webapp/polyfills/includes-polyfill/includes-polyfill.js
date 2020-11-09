999322442e4c88ee97d75a2e85af71f14e894e9c
Davide Zerbetto <davide.zerbetto@eng.it>
2020-11-09 10:50:55 +0100

    [Closes KNOWAGE-5556] Storing currently selected members in a local variable

--- knowage/src/main/webapp/polyfills/includes-polyfill/includes-polyfill.js
+++ knowage/src/main/webapp/polyfills/includes-polyfill/includes-polyfill.js
@@ -32,6 +32,30 @@ if (!Array.prototype.includes) {
 
 }
 
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     'use strict';
+     if (this == null) {
+       throw new TypeError('Array.prototype.find called on null or undefined');
+     }
+     if (typeof predicate !== 'function') {
+       throw new TypeError('predicate must be a function');
+     }
+     var list = Object(this);
+     var length = list.length >>> 0;
+     var thisArg = arguments[1];
+
+     for (var i = 0; i !== length; i++) {
+       if (predicate.call(thisArg, this[i], i, list)) {
+         return this[i];
+       }
+     }
+     return undefined;
+    }
+  });
+}
+
 if (!String.prototype.includes) {
 	String.prototype.includes = function(search, start) {
 	    'use strict';
