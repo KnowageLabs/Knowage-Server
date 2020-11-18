194f47edaa1a31e4776a6f8f20d76639f3a05ef7
Marco Libanori <marco.libanori@eng.it>
2020-11-18 17:15:04 +0100

    Add support to BigQuery's timestmap on JS

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/factory/cockpitModule_generalOptions.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/factory/cockpitModule_generalOptions.js
@@ -131,7 +131,8 @@ angular.module('cockpitModule').factory('cockpitModule_generalOptions',function(
 			"java.sql.Date": {label:'date',icon:'fa fa-calendar'},
 			"java.sql.Time": {label:'time',icon:'fa fa-clock-o'},
 			"oracle.sql.BLOB": {label:'blob',icon:'fa fa-archive'},
-			"oracle.sql.CLOB": {label:'clob',icon:'fa fa-archive'}
+			"oracle.sql.CLOB": {label:'clob',icon:'fa fa-archive'},
+			"com.simba.googlebigquery.dsi.dataengine.utilities.TimestampTz": {label:'timestamp',icon:'fa fa-calendar'}
 		},
 		 dateFormat: [
 			 {label:sbiModule_translate.load("sbi.cockpit.date.format.long"),value:'dd EEEE yyyy HH:mm:ss',hint:'September 03 2010 12:05:08 PM'},
