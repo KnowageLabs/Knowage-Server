7b789ee8e623a092ec6b789e0f62c21fe93fdde6
Marco Libanori <marco.libanori@eng.it>
2020-12-14 17:34:15 +0100

    [KNOWAGE-5658] Fix validation on rating

--- knowagedao/src/main/java/it/eng/spagobi/analiticalmodel/document/dao/BIObjectRatingDAOHibImpl.java
+++ knowagedao/src/main/java/it/eng/spagobi/analiticalmodel/document/dao/BIObjectRatingDAOHibImpl.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,19 +11,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.analiticalmodel.document.dao;
-
-import it.eng.spago.error.EMFErrorSeverity;
-import it.eng.spago.error.EMFUserError;
-import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
-import it.eng.spagobi.analiticalmodel.document.metadata.SbiObjects;
-import it.eng.spagobi.analiticalmodel.document.metadata.SbiObjectsRating;
-import it.eng.spagobi.analiticalmodel.document.metadata.SbiObjectsRatingId;
-import it.eng.spagobi.commons.dao.AbstractHibernateDAO;
 
 import java.util.Iterator;
 import java.util.List;
@@ -34,39 +26,126 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class BIObjectRatingDAOHibImpl extends AbstractHibernateDAO implements
-		IBIObjectRating {
-    
-    static private Logger logger = Logger.getLogger(BIObjectRatingDAOHibImpl.class);
-    
-    /* (non-Javadoc)
-     * @see it.eng.spagobi.analiticalmodel.document.dao.IBIObjectRating#voteBIObject(it.eng.spagobi.analiticalmodel.document.bo.BIObject, java.lang.String, java.lang.String)
-     */
-    public void voteBIObject(BIObject obj,String userid, String rating) throws EMFUserError{
-    	logger.debug("IN");
+import it.eng.spago.error.EMFErrorSeverity;
+import it.eng.spago.error.EMFUserError;
+import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
+import it.eng.spagobi.analiticalmodel.document.metadata.SbiObjects;
+import it.eng.spagobi.analiticalmodel.document.metadata.SbiObjectsRating;
+import it.eng.spagobi.analiticalmodel.document.metadata.SbiObjectsRatingId;
+import it.eng.spagobi.commons.dao.AbstractHibernateDAO;
+
+public class BIObjectRatingDAOHibImpl extends AbstractHibernateDAO implements IBIObjectRating {
+
+	static private Logger logger = Logger.getLogger(BIObjectRatingDAOHibImpl.class);
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see it.eng.spagobi.analiticalmodel.document.dao.IBIObjectRating#calculateBIObjectRating(it.eng.spagobi.analiticalmodel.document.bo.BIObject)
+	 */
+	@Override
+	public Double calculateBIObjectRating(BIObject obj) throws EMFUserError {
+		Double rating = new Double(0);
+
 		Session aSession = null;
 		Transaction tx = null;
 		try {
 			aSession = getSession();
 			tx = aSession.beginTransaction();
-				
-			SbiObjects hibBIObject = (SbiObjects)aSession.load(SbiObjects.class,  obj.getId());
+			// String hql = "from SbiObjectsRating s where s.id.objId = " + obj.getId();
+			String hql = "from SbiObjectsRating s where s.id.objId = ?";
+			Query query = aSession.createQuery(hql);
+			query.setInteger(0, obj.getId().intValue());
+			List l = query.list();
+			double totalVotes = 0;
+			double sumVotes = 0;
+
+			Iterator it = l.iterator();
+			while (it.hasNext()) {
+				SbiObjectsRating temp = (SbiObjectsRating) it.next();
+				Integer rat = temp.getRating();
+				sumVotes = sumVotes + rat.doubleValue();
+				totalVotes++;
+			}
+			if (totalVotes != 0) {
+				rating = new Double(sumVotes / totalVotes);
+			}
+			tx.commit();
+			return rating;
+		} catch (HibernateException he) {
+			logger.error(he);
+			if (tx != null)
+				tx.rollback();
+			throw new EMFUserError(EMFErrorSeverity.ERROR, 100);
+		} finally {
+			if (aSession != null) {
+				if (aSession.isOpen())
+					aSession.close();
+			}
+		}
+
+	}
+
+	private SbiObjectsRating loadBIObjectRatingById(BIObject obj, String userid) throws EMFUserError {
+
+		SbiObjectsRating hibBIObjectsRating = new SbiObjectsRating();
+		Session aSession = null;
+		Transaction tx = null;
+		try {
+			aSession = getSession();
+			tx = aSession.beginTransaction();
+
+			// String hql = " from SbiObjectsRating s where " +
+			// " s.id.objId = "+ obj.getId()+ " and s.id.userId = '"+ userid +"'";
+
+			String hql = " from SbiObjectsRating s where " + " s.id.objId = ? and s.id.userId = ?";
+
+			Query hqlQuery = aSession.createQuery(hql);
+			hqlQuery.setInteger(0, obj.getId().intValue());
+			hqlQuery.setString(1, userid);
+			hibBIObjectsRating = (SbiObjectsRating) hqlQuery.uniqueResult();
+			tx.commit();
+			return hibBIObjectsRating;
+		} catch (HibernateException he) {
+			logger.error(he);
+			if (tx != null)
+				tx.rollback();
+			throw new EMFUserError(EMFErrorSeverity.ERROR, 100);
+		} finally {
+			if (aSession != null) {
+				if (aSession.isOpen())
+					aSession.close();
+			}
+		}
+
+	}
+
+	@Override
+	public void voteBIObject(BIObject obj, String userid, int rating) throws EMFUserError {
+		logger.debug("IN");
+		Session aSession = null;
+		Transaction tx = null;
+		try {
+			aSession = getSession();
+			tx = aSession.beginTransaction();
+
+			SbiObjects hibBIObject = (SbiObjects) aSession.load(SbiObjects.class, obj.getId());
 			SbiObjectsRating hibBIObjectsRating = new SbiObjectsRating();
-			
+
 			hibBIObjectsRating = loadBIObjectRatingById(obj, userid);
 			Integer newRating = new Integer(rating);
-			if(newRating!= null && newRating.intValue()> 5){
+			if (newRating != null && newRating.intValue() > 5) {
 				newRating = new Integer(5);
-			}else if(newRating!= null && newRating.intValue()< 0){
+			} else if (newRating != null && newRating.intValue() < 0) {
 				newRating = new Integer(0);
 			}
-			if (hibBIObjectsRating != null){
-				
+			if (hibBIObjectsRating != null) {
+
 				hibBIObjectsRating.setRating(newRating);
 				updateSbiCommonInfo4Update(hibBIObjectsRating);
 				aSession.update(hibBIObjectsRating);
 
-			}else {
+			} else {
 				SbiObjectsRating hibBIObjectsRating1 = new SbiObjectsRating();
 				SbiObjectsRatingId hibBIObjectsRatingId1 = new SbiObjectsRatingId();
 				hibBIObjectsRatingId1.setObjId(obj.getId());
@@ -74,16 +153,15 @@ public class BIObjectRatingDAOHibImpl extends AbstractHibernateDAO implements
 				hibBIObjectsRating1.setId(hibBIObjectsRatingId1);
 				hibBIObjectsRating1.setRating(newRating);
 				hibBIObjectsRating1.setSbiObjects(hibBIObject);
-				hibBIObjectsRating = hibBIObjectsRating1 ;
+				hibBIObjectsRating = hibBIObjectsRating1;
 				updateSbiCommonInfo4Insert(hibBIObjectsRating);
 				aSession.save(hibBIObjectsRating);
-				
-				
-			}		
-					
+
+			}
+
 			tx.commit();
 		} catch (HibernateException he) {
-			logger.error("Error while inserting the Distribution List with name " , he);
+			logger.error("Error while inserting the Distribution List with name ", he);
 
 			if (tx != null)
 				tx.rollback();
@@ -91,92 +169,77 @@ public class BIObjectRatingDAOHibImpl extends AbstractHibernateDAO implements
 			throw new EMFUserError(EMFErrorSeverity.ERROR, 9100);
 
 		} finally {
-			if (aSession!=null){
-				if (aSession.isOpen()) aSession.close();
+			if (aSession != null) {
+				if (aSession.isOpen())
+					aSession.close();
 				logger.debug("OUT");
 			}
 		}
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see it.eng.spagobi.analiticalmodel.document.dao.IBIObjectRating#calculateBIObjectRating(it.eng.spagobi.analiticalmodel.document.bo.BIObject)
-     */
-    public Double calculateBIObjectRating(BIObject obj) throws EMFUserError{
-    	Double rating = new Double(0);
-    
-    	
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see it.eng.spagobi.analiticalmodel.document.dao.IBIObjectRating#voteBIObject(it.eng.spagobi.analiticalmodel.document.bo.BIObject, java.lang.String,
+	 * java.lang.String)
+	 */
+	@Override
+	public void voteBIObject(BIObject obj, String userid, String rating) throws EMFUserError {
+		logger.debug("IN");
 		Session aSession = null;
 		Transaction tx = null;
 		try {
 			aSession = getSession();
 			tx = aSession.beginTransaction();
-			//String hql = "from SbiObjectsRating s where s.id.objId = " + obj.getId();
-			String hql = "from SbiObjectsRating s where s.id.objId = ?";
-			Query query = aSession.createQuery(hql);
-			query.setInteger(0,  obj.getId().intValue());
-			List l = query.list();
-			double totalVotes = 0 ;
-			double sumVotes = 0 ;
-			
-		    Iterator it = l.iterator();
-		    while(it.hasNext()){
-		    	SbiObjectsRating temp = (SbiObjectsRating)it.next();
-		    	Integer rat = temp.getRating();
-		    	sumVotes = sumVotes + rat.doubleValue();
-		    	totalVotes ++ ;
-		    }
-		    if (totalVotes != 0){
-		    	rating = new Double (sumVotes / totalVotes);
-		    }
+
+			SbiObjects hibBIObject = (SbiObjects) aSession.load(SbiObjects.class, obj.getId());
+			SbiObjectsRating hibBIObjectsRating = new SbiObjectsRating();
+
+			hibBIObjectsRating = loadBIObjectRatingById(obj, userid);
+			Integer newRating = new Integer(rating);
+			if (newRating != null && newRating.intValue() > 5) {
+				newRating = new Integer(5);
+			} else if (newRating != null && newRating.intValue() < 0) {
+				newRating = new Integer(0);
+			}
+			if (hibBIObjectsRating != null) {
+
+				hibBIObjectsRating.setRating(newRating);
+				updateSbiCommonInfo4Update(hibBIObjectsRating);
+				aSession.update(hibBIObjectsRating);
+
+			} else {
+				SbiObjectsRating hibBIObjectsRating1 = new SbiObjectsRating();
+				SbiObjectsRatingId hibBIObjectsRatingId1 = new SbiObjectsRatingId();
+				hibBIObjectsRatingId1.setObjId(obj.getId());
+				hibBIObjectsRatingId1.setUserId(userid);
+				hibBIObjectsRating1.setId(hibBIObjectsRatingId1);
+				hibBIObjectsRating1.setRating(newRating);
+				hibBIObjectsRating1.setSbiObjects(hibBIObject);
+				hibBIObjectsRating = hibBIObjectsRating1;
+				updateSbiCommonInfo4Insert(hibBIObjectsRating);
+				aSession.save(hibBIObjectsRating);
+
+			}
+
 			tx.commit();
-			return rating ;
 		} catch (HibernateException he) {
-			logger.error(he);
+			logger.error("Error while inserting the Distribution List with name ", he);
+
 			if (tx != null)
 				tx.rollback();
-			throw new EMFUserError(EMFErrorSeverity.ERROR, 100);
+
+			throw new EMFUserError(EMFErrorSeverity.ERROR, 9100);
+
 		} finally {
-			if (aSession!=null){
-				if (aSession.isOpen()) aSession.close();
+			if (aSession != null) {
+				if (aSession.isOpen())
+					aSession.close();
+				logger.debug("OUT");
 			}
 		}
-    	
-    }
-    
-    private SbiObjectsRating loadBIObjectRatingById(BIObject obj, String userid) throws EMFUserError {
 
-    	SbiObjectsRating hibBIObjectsRating = new SbiObjectsRating();
-		Session aSession = null;
-		Transaction tx = null;
-		try {
-			aSession = getSession();
-			tx = aSession.beginTransaction();
-			
-			//String hql = " from SbiObjectsRating s where " +
-			//             " s.id.objId = "+  obj.getId()+ " and s.id.userId = '"+ userid +"'";
-			
-			String hql = " from SbiObjectsRating s where " +
-            " s.id.objId = ? and s.id.userId = ?";
-			
-			Query hqlQuery = aSession.createQuery(hql);
-			hqlQuery.setInteger(0,  obj.getId().intValue());
-			hqlQuery.setString(1, userid);
-			hibBIObjectsRating = (SbiObjectsRating)hqlQuery.uniqueResult();
-			tx.commit();
-			return hibBIObjectsRating ;
-		} catch (HibernateException he) {
-			logger.error(he);
-			if (tx != null)
-				tx.rollback();
-			throw new EMFUserError(EMFErrorSeverity.ERROR, 100);
-		} finally {
-			if (aSession!=null){
-				if (aSession.isOpen()) aSession.close();
-			}
-		}
-    	
-    }
-
+	}
 
 }
--- knowagedao/src/main/java/it/eng/spagobi/analiticalmodel/document/dao/IBIObjectRating.java
+++ knowagedao/src/main/java/it/eng/spagobi/analiticalmodel/document/dao/IBIObjectRating.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,27 +22,40 @@ import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
 import it.eng.spagobi.commons.dao.ISpagoBIDao;
 
 public interface IBIObjectRating extends ISpagoBIDao{
-	
-	
+
+
 	/**
 	 * Implements the query to insert a rating for a BI Object.
-	 * 
+	 *
 	 * @param obj the obj
 	 * @param userid the userid
 	 * @param rating the rating
-	 * 
+	 *
+	 * @throws EMFUserError If an Exception occurred
+	 * @deprecated Use {@link #voteBIObject(BIObject, String, int)}
+	 * TODO ML : Delete
+	 */
+	@Deprecated
+	public void voteBIObject(BIObject obj,String userid, String rating) throws EMFUserError;
+
+	/**
+	 * Implements the query to insert a rating for a BI Object.
+	 *
+	 * @param obj the obj
+	 * @param userid the userid
+	 * @param rating the rating
+	 *
 	 * @throws EMFUserError If an Exception occurred
 	 */
-	
-	public void voteBIObject(BIObject obj,String userid, String rating) throws EMFUserError;
-	
+	public void voteBIObject(BIObject obj, String userid, int rating) throws EMFUserError;
+
 	/**
 	 * Implements the query to calculate the medium rating for a BI Object.
-	 * 
+	 *
 	 * @param obj the obj
-	 * 
+	 *
 	 * @return The BI object medium rating
-	 * 
+	 *
 	 * @throws EMFUserError If an Exception occurred
 	 */
 	public Double calculateBIObjectRating(BIObject obj) throws EMFUserError;
