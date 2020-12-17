1045cdccda5c439cc0d75e87ac3ba8cb4efd2103
Marco Libanori <marco.libanori@eng.it>
2020-12-17 17:21:28 +0100

    Remove slimerjs-wrapper project

--- slimerjs-wrapper/src/main/java/it/eng/knowage/slimerjs/wrapper/enums/RenderFormat.java
+++
@@ -1,8 +0,0 @@
-package it.eng.knowage.slimerjs.wrapper.enums;
-
-/**
- * The render formats supported by the SlimersJS binaries
- */
-public enum RenderFormat {
-	PNG, JPEG, JPG
-}
--- slimerjs-wrapper/src/main/java/it/eng/knowage/slimerjs/wrapper/enums/SizeUnit.java
+++
@@ -1,8 +0,0 @@
-package it.eng.knowage.slimerjs.wrapper.enums;
-
-/**
- * The units supported by the SlimersJS binaries
- */
-public enum SizeUnit {
-	mm, cm, in, px
-}
