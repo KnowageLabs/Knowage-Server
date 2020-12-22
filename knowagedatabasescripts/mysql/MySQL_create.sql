70947e0dcd710745fe399d589cb21495d80bf37e
Marco Balestri <marco.balestri@eng.it>
2020-12-22 16:27:02 +0100

    Added db update scripts for new function catalog

--- knowagedatabasescripts/mysql/MySQL_create.sql
+++ knowagedatabasescripts/mysql/MySQL_create.sql
@@ -3339,3 +3339,48 @@ CREATE TABLE SBI_NEWS_ROLES (
 
 ALTER TABLE SBI_DOSSIER_ACTIVITY ADD DOC longblob NULL; 
 ALTER TABLE SBI_DOSSIER_ACTIVITY ADD CONFIG varchar(4000) NULL;
+
+-- CATALOG FUNCTION TABLES UPDATE
+
+-- START
+
+-- CLEAN old tables
+DELETE FROM knowage.sbi_catalog_function;
+DELETE FROM knowage.sbi_function_input_variable;
+
+-- DROP old tables
+DROP TABLE IF EXISTS knowage.sbi_function_output, knowage.sbi_function_input_dataset, knowage.sbi_function_input_file;
+
+-- ------------------------- TABLE sbi_catalog_function -------------------------
+ALTER TABLE knowage.sbi_catalog_function
+ADD (BENCHMARKS text(4000), FAMILY text(30), ONLINE_SCRIPT text(4000), OFFLINE_SCRIPT_TRAIN text(4000), OFFLINE_SCRIPT_USE text(4000)),
+DROP SCRIPT, DROP URL, DROP REMOTE;
+
+-- ----------------------- TABLE sbi_function_input_column -----------------------
+CREATE TABLE IF NOT EXISTS knowage.sbi_function_input_column LIKE knowage.sbi_function_input_variable;
+  
+ALTER TABLE knowage.sbi_function_input_column
+CHANGE VAR_NAME COL_NAME varchar(100),
+CHANGE VAR_VALUE COL_TYPE varchar(100);
+
+-- ---------------------- TABLE sbi_function_input_variable ---------------------
+ALTER TABLE knowage.sbi_function_input_variable
+ADD (VAR_TYPE varchar(100));
+
+-- ---------------------- TABLE sbi_function_output_column -----------------------
+CREATE TABLE IF NOT EXISTS knowage.sbi_function_output_column LIKE knowage.sbi_function_input_column;
+
+ALTER TABLE knowage.sbi_function_output_column
+ADD (COL_FIELD_TYPE varchar(100));
+
+-- --------------------------- TABLE sbi_obj_function ---------------------------
+CREATE TABLE IF NOT EXISTS knowage.sbi_obj_function LIKE knowage.sbi_obj_data_set;
+
+ALTER TABLE knowage.sbi_obj_function
+DROP IS_DETAIL,
+CHANGE BIOBJ_DS_ID BIOBJ_FUNCTION_ID int,
+CHANGE DS_ID FUNCTION_ID int,
+ADD FOREIGN KEY (BIOBJ_ID) REFERENCES knowage.sbi_objects(BIOBJ_ID),
+ADD FOREIGN KEY (FUNCTION_ID) REFERENCES knowage.sbi_catalog_function(FUNCTION_ID);
+
+-- END
