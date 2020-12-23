6ecd11d86c91dbd4d99f2fb41cd1930f27284ed4
Marco Balestri <marco.balestri@eng.it>
2020-12-23 11:48:53 +0100

    Updated oracle scripts for functions catalog

--- knowagedatabasescripts/mysql/mysql-dbupgradescript-7.3_to_7.4/MySQL_upgradescript_7.3_to_7.4.sql
+++ knowagedatabasescripts/mysql/mysql-dbupgradescript-7.3_to_7.4/MySQL_upgradescript_7.3_to_7.4.sql
@@ -4,42 +4,42 @@
 -- START
 
 -- CLEAN old tables
-DELETE FROM knowage.sbi_catalog_function;
-DELETE FROM knowage.sbi_function_input_variable;
+DELETE FROM sbi_catalog_function;
+DELETE FROM sbi_function_input_variable;
 
 -- DROP old tables
-DROP TABLE IF EXISTS knowage.sbi_function_output, knowage.sbi_function_input_dataset, knowage.sbi_function_input_file;
+DROP TABLE IF EXISTS sbi_function_output, sbi_function_input_dataset, sbi_function_input_file;
 
 -- ------------------------- TABLE sbi_catalog_function -------------------------
-ALTER TABLE knowage.sbi_catalog_function
+ALTER TABLE sbi_catalog_function
 ADD (BENCHMARKS text(4000), FAMILY text(30), ONLINE_SCRIPT text(4000), OFFLINE_SCRIPT_TRAIN text(4000), OFFLINE_SCRIPT_USE text(4000)),
 DROP SCRIPT, DROP URL, DROP REMOTE;
 
 -- ----------------------- TABLE sbi_function_input_column -----------------------
-CREATE TABLE IF NOT EXISTS knowage.sbi_function_input_column LIKE knowage.sbi_function_input_variable;
+CREATE TABLE IF NOT EXISTS sbi_function_input_column LIKE .sbi_function_input_variable;
   
-ALTER TABLE knowage.sbi_function_input_column
+ALTER TABLE sbi_function_input_column
 CHANGE VAR_NAME COL_NAME varchar(100),
 CHANGE VAR_VALUE COL_TYPE varchar(100);
 
 -- ---------------------- TABLE sbi_function_input_variable ---------------------
-ALTER TABLE knowage.sbi_function_input_variable
+ALTER TABLE sbi_function_input_variable
 ADD (VAR_TYPE varchar(100));
 
 -- ---------------------- TABLE sbi_function_output_column -----------------------
-CREATE TABLE IF NOT EXISTS knowage.sbi_function_output_column LIKE knowage.sbi_function_input_column;
+CREATE TABLE IF NOT EXISTS sbi_function_output_column LIKE sbi_function_input_column;
 
-ALTER TABLE knowage.sbi_function_output_column
+ALTER TABLE sbi_function_output_column
 ADD (COL_FIELD_TYPE varchar(100));
 
 -- --------------------------- TABLE sbi_obj_function ---------------------------
-CREATE TABLE IF NOT EXISTS knowage.sbi_obj_function LIKE knowage.sbi_obj_data_set;
+CREATE TABLE IF NOT EXISTS sbi_obj_function LIKE sbi_obj_data_set;
 
-ALTER TABLE knowage.sbi_obj_function
+ALTER TABLE sbi_obj_function
 DROP IS_DETAIL,
 CHANGE BIOBJ_DS_ID BIOBJ_FUNCTION_ID int,
 CHANGE DS_ID FUNCTION_ID int,
-ADD FOREIGN KEY (BIOBJ_ID) REFERENCES knowage.sbi_objects(BIOBJ_ID),
-ADD FOREIGN KEY (FUNCTION_ID) REFERENCES knowage.sbi_catalog_function(FUNCTION_ID);
+ADD FOREIGN KEY (BIOBJ_ID) REFERENCES sbi_objects(BIOBJ_ID),
+ADD FOREIGN KEY (FUNCTION_ID) REFERENCES sbi_catalog_function(FUNCTION_ID);
 
 -- END
