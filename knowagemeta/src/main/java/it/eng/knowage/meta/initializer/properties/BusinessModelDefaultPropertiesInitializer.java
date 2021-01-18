2db93172a30e8641369dcb088cc26263123b9ba7
Alberto Nale <alberto.nale@eng.it>
2021-01-18 09:25:57 +0100

    Formatting

--- knowagemeta/src/main/java/it/eng/knowage/meta/initializer/properties/BusinessModelDefaultPropertiesInitializer.java
+++ knowagemeta/src/main/java/it/eng/knowage/meta/initializer/properties/BusinessModelDefaultPropertiesInitializer.java
@@ -32,141 +32,142 @@ import it.eng.knowage.meta.model.business.SimpleBusinessColumn;
 
 /**
  * @deprecated
- * 
+ *
  * @author Andrea Gioia (andrea.gioia@eng.it)
  *
  */
+@Deprecated
 public class BusinessModelDefaultPropertiesInitializer implements IPropertiesInitializer {
-	
+
 	// Model property names
 	public static final String MODEL_PACKAGE = "structural.package";
-	
+
 	// Table property names
-	
-	//...
-	
+
+	// ...
+
 	// Column property names
-	
+
 	public static final String COLUMN_AGGTYPE = "structural.aggtype";
 	public static final String COLUMN_DATATYPE = "structural.datatype";
-	
+
 	public static final String COLUMN_ALIGNMENT = "style.alignment";
-	
+
 	public static final String COLUMN_PHYSICAL_TABLE = "physical.physicaltable";
-	
+
 	// Relationship property names
-	
-	
+
 	static public ModelFactory FACTORY = ModelFactory.eINSTANCE;
-	
+
+	@Override
 	public void addProperties(ModelObject o) {
-		
-		if(o instanceof BusinessModel) {
-			initModelProperties((BusinessModel)o);
-		} else if(o instanceof BusinessTable) {
-			initTableProperties((BusinessTable)o);
-		} else if(o instanceof BusinessColumn) {
-			initColumnProperties((SimpleBusinessColumn)o);
-		} else if(o instanceof BusinessIdentifier) {
-			initIdentifierProperties((BusinessIdentifier)o);
-		} else if(o instanceof BusinessRelationship) {
-			initRelationshipProperties((BusinessRelationship)o);
+
+		if (o instanceof BusinessModel) {
+			initModelProperties((BusinessModel) o);
+		} else if (o instanceof BusinessTable) {
+			initTableProperties((BusinessTable) o);
+		} else if (o instanceof BusinessColumn) {
+			initColumnProperties((SimpleBusinessColumn) o);
+		} else if (o instanceof BusinessIdentifier) {
+			initIdentifierProperties((BusinessIdentifier) o);
+		} else if (o instanceof BusinessRelationship) {
+			initRelationshipProperties((BusinessRelationship) o);
 		} else {
-			
+
 		}
 	}
-	
+
 	private void initModelProperties(BusinessModel o) {
-        ModelPropertyCategory structuralCategory;
-        ModelPropertyType propertyType;
-        ModelProperty property;
-       
-        // if doesn't exist create 'structural' category
-        structuralCategory =  o.getParentModel().getPropertyCategory("Structural");
-        if(structuralCategory == null) {
-            structuralCategory = FACTORY.createModelPropertyCategory();
-            structuralCategory.setName("Structural");
-            structuralCategory.setDescription("Structural properties");
-            o.getParentModel().getPropertyCategories().add(structuralCategory);	
-        }    
-        
-        // Create the new property type and add it to the structural category
-        propertyType = null;
-       
-        if(o.getParentModel() != null) {
-        	propertyType = o.getParentModel().getPropertyType(MODEL_PACKAGE);
-        }
-        if(propertyType == null) {
-            propertyType = FACTORY.createModelPropertyType();
-            propertyType.setId( MODEL_PACKAGE );
-            propertyType.setName("Package Name");
-            propertyType.setDescription("Package name used in JPA mapping");
-            propertyType.setCategory(structuralCategory);
-            propertyType.setDefaultValue("it.eng.knowage.meta");
-            
-            if(o.getParentModel() != null) {
-            	o.getParentModel().getPropertyTypes().add(propertyType);
-            }
-        }
-       
-        // add a property of type MODEL_PACKAGE to the model object
-        property = FACTORY.createModelProperty();
-        property.setPropertyType(propertyType);
-        o.getProperties().put(property.getPropertyType().getId(), property);
-            
+		ModelPropertyCategory structuralCategory;
+		ModelPropertyType propertyType;
+		ModelProperty property;
+
+		// if doesn't exist create 'structural' category
+		structuralCategory = o.getParentModel().getPropertyCategory("Structural");
+		if (structuralCategory == null) {
+			structuralCategory = FACTORY.createModelPropertyCategory();
+			structuralCategory.setName("Structural");
+			structuralCategory.setDescription("Structural properties");
+			o.getParentModel().getPropertyCategories().add(structuralCategory);
+		}
+
+		// Create the new property type and add it to the structural category
+		propertyType = null;
+
+		if (o.getParentModel() != null) {
+			propertyType = o.getParentModel().getPropertyType(MODEL_PACKAGE);
+		}
+		if (propertyType == null) {
+			propertyType = FACTORY.createModelPropertyType();
+			propertyType.setId(MODEL_PACKAGE);
+			propertyType.setName("Package Name");
+			propertyType.setDescription("Package name used in JPA mapping");
+			propertyType.setCategory(structuralCategory);
+			propertyType.setDefaultValue("it.eng.knowage.meta");
+
+			if (o.getParentModel() != null) {
+				o.getParentModel().getPropertyTypes().add(propertyType);
+			}
+		}
+
+		// add a property of type MODEL_PACKAGE to the model object
+		property = FACTORY.createModelProperty();
+		property.setPropertyType(propertyType);
+		o.getProperties().put(property.getPropertyType().getId(), property);
+
 	}
 
 	private void initTableProperties(BusinessTable o) {
-		
+
 	}
-	
+
 	private void initColumnProperties(SimpleBusinessColumn o) {
 		Model rootModel;
 		ModelPropertyType propertyType;
 		ModelProperty property;
 		ModelPropertyCategory structuralCategory, styleCategory, otherCategory;
-		
+
 		rootModel = null;
-		
-		if(o.getTable() != null && o.getTable().getModel() != null) {
+
+		if (o.getTable() != null && o.getTable().getModel() != null) {
 			rootModel = o.getTable().getModel().getParentModel();
 		}
-		
-		//**** Structural Category ****
-		structuralCategory =  o.getTable().getModel().getParentModel().getPropertyCategory("Structural");
-		if(structuralCategory == null) {
+
+		// **** Structural Category ****
+		structuralCategory = o.getTable().getModel().getParentModel().getPropertyCategory("Structural");
+		if (structuralCategory == null) {
 			structuralCategory = FACTORY.createModelPropertyCategory();
 			structuralCategory.setName("Structural");
 			structuralCategory.setDescription("Structural properties");
 			o.getTable().getModel().getParentModel().getPropertyCategories().add(structuralCategory);
-		}		
-		
-		//**** Style Category ****
-		styleCategory =  o.getTable().getModel().getParentModel().getPropertyCategory("Style");
-		if(styleCategory == null) {
+		}
+
+		// **** Style Category ****
+		styleCategory = o.getTable().getModel().getParentModel().getPropertyCategory("Style");
+		if (styleCategory == null) {
 			styleCategory = FACTORY.createModelPropertyCategory();
 			styleCategory.setName("Style");
 			styleCategory.setDescription("Style properties");
 			o.getTable().getModel().getParentModel().getPropertyCategories().add(styleCategory);
 		}
-		
-		//**** Other Category ****
-		otherCategory =  o.getTable().getModel().getParentModel().getPropertyCategory("Other");
-		if(otherCategory == null) {
+
+		// **** Other Category ****
+		otherCategory = o.getTable().getModel().getParentModel().getPropertyCategory("Other");
+		if (otherCategory == null) {
 			otherCategory = FACTORY.createModelPropertyCategory();
 			otherCategory.setName("Physical Reference");
 			otherCategory.setDescription("The reference to the original physical object");
 			o.getTable().getModel().getParentModel().getPropertyCategories().add(otherCategory);
 		}
-		
-		
+
 		// AGGREGATION TYPE
 		propertyType = null;
-		
-		if(rootModel != null) propertyType = rootModel.getPropertyType(COLUMN_AGGTYPE);
-		if(propertyType == null) {
+
+		if (rootModel != null)
+			propertyType = rootModel.getPropertyType(COLUMN_AGGTYPE);
+		if (propertyType == null) {
 			propertyType = FACTORY.createModelPropertyType();
-			propertyType.setId( COLUMN_AGGTYPE );
+			propertyType.setId(COLUMN_AGGTYPE);
 			propertyType.setName("Agregation Type");
 			propertyType.setDescription("The preferred agregation type for the give column (COUNT, SUM, AVG, MAX, MIN)");
 			propertyType.setCategory(structuralCategory);
@@ -176,21 +177,23 @@ public class BusinessModelDefaultPropertiesInitializer implements IPropertiesIni
 			propertyType.getAdmissibleValues().add("MAX");
 			propertyType.getAdmissibleValues().add("MIN");
 			propertyType.setDefaultValue("COUNT");
-			
-			if(rootModel != null) rootModel.getPropertyTypes().add(propertyType);
+
+			if (rootModel != null)
+				rootModel.getPropertyTypes().add(propertyType);
 		}
-		
+
 		property = FACTORY.createModelProperty();
 		property.setPropertyType(propertyType);
 		o.getProperties().put(property.getPropertyType().getId(), property);
-		
+
 		// DATA TYPE
 		propertyType = null;
-		
-		if(rootModel != null) propertyType = rootModel.getPropertyType(COLUMN_DATATYPE);
-		if(propertyType == null) {
+
+		if (rootModel != null)
+			propertyType = rootModel.getPropertyType(COLUMN_DATATYPE);
+		if (propertyType == null) {
 			propertyType = FACTORY.createModelPropertyType();
-			propertyType.setId( COLUMN_DATATYPE );
+			propertyType.setId(COLUMN_DATATYPE);
 			propertyType.setName("Data Type");
 			propertyType.setDescription("The data type of the given column (VARCHAR, INTEGER, DOUBLE, ...)");
 			propertyType.setCategory(structuralCategory);
@@ -201,20 +204,22 @@ public class BusinessModelDefaultPropertiesInitializer implements IPropertiesIni
 			propertyType.getAdmissibleValues().add("TIMESTAMP");
 			// ...
 			propertyType.setDefaultValue("VARCHAR");
-			
-			if(rootModel != null) rootModel.getPropertyTypes().add(propertyType);
+			propertyType.setDefaultValue("BOOLEAN");
+
+			if (rootModel != null)
+				rootModel.getPropertyTypes().add(propertyType);
 		}
-		
+
 		property = FACTORY.createModelProperty();
 		property.setPropertyType(propertyType);
 		o.getProperties().put(property.getPropertyType().getId(), property);
-		
-		
+
 		// ALIGNMENT TYPE
-		if(rootModel != null) propertyType = rootModel.getPropertyType(COLUMN_ALIGNMENT);
-		if(propertyType == null) {
+		if (rootModel != null)
+			propertyType = rootModel.getPropertyType(COLUMN_ALIGNMENT);
+		if (propertyType == null) {
 			propertyType = FACTORY.createModelPropertyType();
-			propertyType.setId( COLUMN_ALIGNMENT );
+			propertyType.setId(COLUMN_ALIGNMENT);
 			propertyType.setName("Alignment Type");
 			propertyType.setDescription("The preferred alignment type for the give column (LEFT, CENTER, RIGHT, JUSTIFIED)");
 			propertyType.setCategory(structuralCategory);
@@ -223,41 +228,44 @@ public class BusinessModelDefaultPropertiesInitializer implements IPropertiesIni
 			propertyType.getAdmissibleValues().add("RIGHT");
 			propertyType.getAdmissibleValues().add("JUSTIFIED");
 			propertyType.setDefaultValue("LEFT");
-			
-			if(rootModel != null) rootModel.getPropertyTypes().add(propertyType);
+
+			if (rootModel != null)
+				rootModel.getPropertyTypes().add(propertyType);
 		}
-		
+
 		property = FACTORY.createModelProperty();
 		property.setPropertyType(propertyType);
 		o.getProperties().put(property.getPropertyType().getId(), property);
-		
+
 		// Column Physical Table TYPE
 		propertyType = null;
-		
-		if(rootModel != null) propertyType = rootModel.getPropertyType(COLUMN_PHYSICAL_TABLE);
-		if(propertyType == null) {
+
+		if (rootModel != null)
+			propertyType = rootModel.getPropertyType(COLUMN_PHYSICAL_TABLE);
+		if (propertyType == null) {
 			propertyType = FACTORY.createModelPropertyType();
 			propertyType.setId(COLUMN_PHYSICAL_TABLE);
 			propertyType.setName("Physical Table");
 			propertyType.setDescription("The original physical table of this column");
 			propertyType.setCategory(otherCategory);
 			propertyType.setDefaultValue(o.getPhysicalColumn().getTable().getName());
-			
-			if(rootModel != null) rootModel.getPropertyTypes().add(propertyType);
+
+			if (rootModel != null)
+				rootModel.getPropertyTypes().add(propertyType);
 		}
-		
+
 		property = FACTORY.createModelProperty();
 		property.setPropertyType(propertyType);
 		o.getProperties().put(property.getPropertyType().getId(), property);
-		
+
 	}
-	
+
 	private void initIdentifierProperties(BusinessIdentifier o) {
-		
+
 	}
-	
+
 	private void initRelationshipProperties(BusinessRelationship o) {
-		
+
 	}
 
 }
