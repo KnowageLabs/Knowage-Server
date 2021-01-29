108f56cc38a89c2393d945c32f955b73bc64e860
Marco Balestri <marco.balestri@eng.it>
2021-01-29 11:47:00 +0100

    [KNOWAGE-5743] Removed all dependencies from datamining engine build

--- knowagedataminingengine/src/main/java/it/eng/spagobi/engines/datamining/common/FunctionExecutionUtils.java
+++
@@ -1,9 +0,0 @@
-package it.eng.spagobi.engines.datamining.common;
-
-import org.apache.log4j.Logger;
-
-public class FunctionExecutionUtils {
-
-	static protected Logger logger = Logger.getLogger(FunctionExecutionUtils.class);
-
-}
