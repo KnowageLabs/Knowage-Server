9fe2791c5b4b9d31ece89f9a60e997c0c03851eb
Marco Balestri <marco.balestri@eng.it>
2021-02-22 12:07:28 +0100

    Hide catalog function features from cockpit in version 7.4
    The feature will be available from Knowage 8

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/commons/catalog-function/catalogFunction.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/commons/catalog-function/catalogFunction.js
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 	angular.module('cockpitModule').directive('catalogFunction',function(){
 		return{
-			template:   '<button class="md-button md-knowage-theme" ng-click="addNewCatalogFunction()" ng-disabled="isFunctionInUse() && !selectedItem" ng-class="{\'md-icon-button\':selectedItem && !insideMenu}">'+
+			template:   '<button class="md-button md-knowage-theme" ng-show="false" ng-click="addNewCatalogFunction()" ng-disabled="isFunctionInUse() && !selectedItem" ng-class="{\'md-icon-button\':selectedItem && !insideMenu}">'+
 						'	<md-icon md-font-icon="fas fa-square-root-alt" ng-if="selectedItem"></md-icon>'+
 						'	<span ng-if="!selectedItem">{{::translate.load("sbi.cockpit.widgets.table.catalogFunctions.add")}}</span>'+
 						'	<span ng-if="selectedItem && insideMenu">{{::translate.load("sbi.cockpit.widgets.table.catalogFunctions.edit")}}</span>'+
