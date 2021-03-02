eca9d85f4c572fd5f98c07361e657fd55f72ec8b
Marco Balestri <marco.balestri@eng.it>
2021-03-02 18:10:11 +0100

    Modules refactoring to conform to Knowage naming conventions

--- Knowage-Python/src/app/services/__init__.py
+++ Knowage-Python/src/app/services/__init__.py
@@ -6,10 +6,10 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    from app.services.editmode_service import editMode
-    from app.services.viewmode_service import viewMode
-    from app.services.dataset_service import dataset
-    from app.services.catalog_service import catalog
+    from app.services.widgetedit_resource import editMode
+    from app.services.widgetview_resource import viewMode
+    from app.services.dataset_resource import dataset
+    from app.services.catalog_resource import catalog
     app.register_blueprint(editMode, url_prefix='/widget/edit')
     app.register_blueprint(viewMode, url_prefix='/widget/view')
     app.register_blueprint(dataset, url_prefix='/dataset')
