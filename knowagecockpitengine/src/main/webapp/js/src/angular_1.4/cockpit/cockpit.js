573ae7eb848c44a4dc3ab4c2d86debe82dd5cb91
Davide Vernassa <davide.vernassa@eng.it>
2021-03-05 18:17:16 +0100

    Revert "Revert "[Ticket#2021022687000013] - Wrapped initialization on variables promise""
    This reverts commit 3eccf5dac9debe4b5343736c427039743236e63d.

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/cockpit.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/cockpit.js
@@ -175,18 +175,19 @@ function cockpitMasterControllerFunction($scope,cockpitModule_widgetServices,coc
 	})
 
 	$scope.variablesInit = function(){
-		if(cockpitModule_template.configuration && cockpitModule_template.configuration.variables){
-			if(!cockpitModule_properties.VARIABLES) cockpitModule_properties.VARIABLES = {};
-			cockpitModule_template.configuration.variables.forEach(function(variable){
+		if(!cockpitModule_properties.VARIABLES) cockpitModule_properties.VARIABLES = {};
+		return Promise.all(cockpitModule_template.configuration.variables.map(function(variable){
+			return new Promise (function(resolve, reject){
 				cockpitModule_variableService.getVariableValue(variable).then(
-						function(response){
-							cockpitModule_properties.VARIABLES[variable.name] = response;
-						},function(error){
-							console.error('error during the variables recovery.')
-						}
-					)
+					function(response){
+						cockpitModule_properties.VARIABLES[variable.name] = response;
+						resolve(response)
+					},function(error){
+						reject('error during the variables recovery.')
+					}
+				)
 			})
-		}
+		}));
 	}
 
 	$scope.initializedSheets = [0]; // first sheet is always loaded
@@ -195,24 +196,67 @@ function cockpitMasterControllerFunction($scope,cockpitModule_widgetServices,coc
 	$scope.datasetLoaded=false;
 
 	cockpitModule_datasetServices.loadDatasetsFromTemplate().then(function(){
-		$scope.datasetLoaded=true;
-		if(!cockpitModule_properties.PARAMETERS) cockpitModule_properties.PARAMETERS = cockpitModule_datasetServices.returnParametersArray();
-		var dsNotInCache = cockpitModule_templateServices.getDatasetAssociatedNotUsedByWidget();
-		if(dsNotInCache.length>0){
-			cockpitModule_datasetServices.addDatasetInCache(dsNotInCache)
-			.then(function(){
-				$rootScope.$broadcast("WIDGET_INITIALIZED");
-			});
-			//WIDGET_INITIALIZED at the end
-		}else{
-			$rootScope.$broadcast("WIDGET_INITIALIZED");
-		}
+		
+		var initSheet = $scope.$watch('cockpitModule_properties.CURRENT_SHEET',function(newValue,oldValue){
+			if(!cockpitModule_properties.HASDEFAULTSELECTION) cockpitModule_properties.HASDEFAULTSELECTION = {};
+			//if(cockpitModule_template.getSelections().length == 0) cockpitModule_properties.HASDEFAULTSELECTION[newValue] = checkForDefaultSelections(cockpitModule_template, newValue);
+	        var currentSheet; // get sheet checking proper index
+	        for(var i=0; i < cockpitModule_template.sheets.length; i++){
+	            if(cockpitModule_template.sheets[i].index == newValue){
+	                currentSheet = cockpitModule_template.sheets[i];
+	                break;
+	            }
+	        }
 
-		if(!cockpitModule_properties.EDIT_MODE){
-			cockpitModule_nearRealtimeServices.init();
-			cockpitModule_realtimeServices.init();
+	        if(currentSheet && currentSheet.widgets){
+	            if(newValue!=undefined && $scope.initializedSheets.indexOf(newValue) == -1){
+	                for(var i=0; i < currentSheet.widgets.length; i++){
+	                    var widgetId = currentSheet.widgets[i].id;
+	                    var tempElement = angular.element(document.querySelector('#w' + widgetId));
+	                    $rootScope.$broadcast("WIDGET_EVENT" + widgetId, "INIT", {element:tempElement});
+	                }
+	                $scope.initializedSheets.push(newValue);
+	            }else{
+	                for(var i=0; i < currentSheet.widgets.length; i++){
+	                    var widgetId = currentSheet.widgets[i].id;
+	                    if(cockpitModule_properties.DIRTY_WIDGETS.indexOf(widgetId) > -1){
+	                        var tempElement = angular.element(document.querySelector('#w' + widgetId));
+	                        $rootScope.$broadcast("WIDGET_EVENT" + widgetId, "UPDATE_FROM_SHEET_CHANGE", {element:tempElement});
+	                    }
+	                }
+	            }
+	        }
+	    })
+		
+		var loadDatasetList = function(){
+			$scope.datasetLoaded=true;
+			if(!cockpitModule_properties.PARAMETERS) cockpitModule_properties.PARAMETERS = cockpitModule_datasetServices.returnParametersArray();
+			var dsNotInCache = cockpitModule_templateServices.getDatasetAssociatedNotUsedByWidget();
+			if(dsNotInCache.length>0){
+				cockpitModule_datasetServices.addDatasetInCache(dsNotInCache)
+				.then(function(){
+					$rootScope.$broadcast("WIDGET_INITIALIZED");
+				});
+				//WIDGET_INITIALIZED at the end
+			}else{
+				$rootScope.$broadcast("WIDGET_INITIALIZED");
+			}
+
+			if(!cockpitModule_properties.EDIT_MODE){
+				cockpitModule_nearRealtimeServices.init();
+				cockpitModule_realtimeServices.init();
+			}
 		}
-		$scope.variablesInit();
+		
+		if(cockpitModule_template.configuration && cockpitModule_template.configuration.variables){
+			$scope.variablesInit().then(function(value){
+				loadDatasetList();
+			},function(error){
+				loadDatasetList();
+				console.log(error)
+			})
+		}else loadDatasetList();
+		
 	},function(){
 		console.error("error when load dataset list")
 	});
