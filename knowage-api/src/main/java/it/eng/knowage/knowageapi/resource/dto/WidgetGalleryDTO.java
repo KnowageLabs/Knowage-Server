60e23ac63fc77c041e098a00dc2f27283db6b033
Alberto Nale <alberto.nale@eng.it>
2021-03-18 12:56:00 +0100

    Added image to DTO

--- knowage-api/src/main/java/it/eng/knowage/knowageapi/resource/dto/WidgetGalleryDTO.java
+++ knowage-api/src/main/java/it/eng/knowage/knowageapi/resource/dto/WidgetGalleryDTO.java
@@ -1,5 +1,6 @@
 package it.eng.knowage.knowageapi.resource.dto;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -11,6 +12,9 @@ public class WidgetGalleryDTO {
 	private String label;
 	private String type;
 	private List<String> tags = new ArrayList<String>();
+	// TODO correctly image handling
+	private byte[] imageBase64Content = null;
+	private String image = null;
 
 	private Code code = new Code();
 
@@ -58,7 +62,8 @@ public class WidgetGalleryDTO {
 		this.code = code;
 	}
 
-	public WidgetGalleryDTO(UUID id, String author, String label, String type, List<String> tags, String html, String javaScript, String python, String css) {
+	public WidgetGalleryDTO(UUID id, String author, String label, String type, List<String> tags, String html, String javaScript, String python, String css,
+			String image) {
 		super();
 		this.id = id;
 		this.author = author;
@@ -69,10 +74,20 @@ public class WidgetGalleryDTO {
 		this.code.setHtml(html);
 		this.code.setJavascript(javaScript);
 		this.code.setPython(python);
+		this.imageBase64Content = image != null ? image.getBytes(Charset.forName("utf8")) : null;
+		this.image = image;
 	}
 
 	public WidgetGalleryDTO() {
 		super();
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
 	}
 
 }
