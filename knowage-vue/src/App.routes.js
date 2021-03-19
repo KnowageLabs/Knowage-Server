b39c674b955427bb5b99dacc3ca9f4dadfedb106
Alberto Nale <alberto.nale@eng.it>
2021-03-19 11:27:35 +0100

    Changed whitespaces with tabs

--- knowage-vue/src/App.routes.js
+++ knowage-vue/src/App.routes.js
@@ -3,51 +3,56 @@ import IframeRenderer from '@/modules/shared/IframeRenderer.vue'
 import managersRoutes from '@/modules/managers/managers.routes.js'
 
 const baseRoutes = [
-  {
-    path: '/knowage',
-    name: 'home',
-    component: () => import('@/views/Home.vue')
-  },
-  {
-    path: '/knowage/about',
-    name: 'about',
-    component: () => import('@/views/About.vue')
-  },
-  {
-    path: '/knowage/servlet/:catchAll(.*)',
-    name: 'knowageUrl',
-    component: IframeRenderer,
-    props: route => ({ url: route.fullPath })
-  },
-  {
-    path: '/knowage/restful-services/publish:catchAll(.*)',
-    component: IframeRenderer,
-    props: route => ({ url: route.fullPath })
-  },
-  {
-    path: '/knowage/restful-services/signup:catchAll(.*)',
-    component: IframeRenderer,
-    props: route => ({ url: route.fullPath })
-  },
-  {
-    path: "/:catchAll(.*)",
-    component: () => import('@/modules/shared/404.vue')
-  },
+	{
+		path: '/knowage',
+		name: 'home',
+		component: () => import('@/views/Home.vue')
+	},
+	{
+		path: '/knowage/about',
+		name: 'about',
+		component: () => import('@/views/About.vue')
+	},
+	{
+		path: '/knowage/servlet/:catchAll(.*)',
+		name: 'knowageUrl',
+		component: IframeRenderer,
+		props: (route) => ({ url: route.fullPath })
+	},
+	{
+		path: '/knowage/restful-services/publish:catchAll(.*)',
+		component: IframeRenderer,
+		props: (route) => ({ url: route.fullPath })
+	},
+	{
+		path: '/knowage/restful-services/signup:catchAll(.*)',
+		component: IframeRenderer,
+		props: (route) => ({ url: route.fullPath })
+	},
+	{
+		path: '/knowage/themes:catchAll(.*)',
+		component: IframeRenderer,
+		props: (route) => ({ url: route.fullPath })
+	},
+	{
+		path: '/:catchAll(.*)',
+		component: () => import('@/modules/shared/404.vue')
+	}
 ]
 
 const routes = baseRoutes.concat(managersRoutes)
 
 const router = createRouter({
-  base: '/knowage/',
-  history: createWebHistory(),
-  routes
+	base: '/knowage/',
+	history: createWebHistory(),
+	routes
 })
 
 /*router.beforeEach((to, from, next) => {
-  console.log('to',to)
-  console.log('from',from)
-  console.log('next',next)
-  next()
+	console.log('to',to)
+	console.log('from',from)
+	console.log('next',next)
+	next()
 })*/
 
 export default router
--- knowage-vue/src/App.store.js
+++ knowage-vue/src/App.store.js
@@ -2,24 +2,28 @@ import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
-  state () {
-    return {
-      user: {},
-      error: {},
-      locale: "it_IT"
-    }
-  },
-  mutations: {
-    setUser (state, user){
-      state.user = user
-    },
-    setError(state, msg){
-      state.error = {visible: true, msg: msg}
-    },
-    setLocale(state, locale){
-      state.locale = locale
-    }
-  }
+	state() {
+		return {
+			user: {},
+			error: {},
+			locale: "it_IT",
+			download: false,
+		}
+	},
+	mutations: {
+		setUser(state, user) {
+			state.user = user
+		},
+		setError(state, msg) {
+			state.error = { visible: true, msg: msg }
+		},
+		setLocale(state, locale) {
+			state.locale = locale
+		},
+		setDownload(state, hasDownload) {
+			state.download = hasDownload
+		}
+	}
 })
 
 export default store
