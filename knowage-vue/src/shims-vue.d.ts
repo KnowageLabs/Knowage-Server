3ed959ee463095ee16fd4895ece73d7579620390
Alberto Nale <alberto.nale@eng.it>
2021-03-22 12:04:49 +0100

    Added VueRouter

--- knowage-vue/src/shims-vue.d.ts
+++ knowage-vue/src/shims-vue.d.ts
@@ -4,3 +4,10 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'vue/types/vue' {
+  import VueRouter, { Route } from 'vue-router'
+  interface Vue {
+    $router: VueRouter
+  }
+}
