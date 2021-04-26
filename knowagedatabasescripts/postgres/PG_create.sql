012b55849847237279f7bd34c76ffedac77b2ea3
Alberto Nale <alberto.nale@eng.it>
2021-04-26 16:31:15 +0200

    Changed fields to bytea

--- knowagedatabasescripts/postgres/PG_create.sql
+++ knowagedatabasescripts/postgres/PG_create.sql
@@ -3181,8 +3181,8 @@ CREATE TABLE SBI_WIDGET_GALLERY (
   NAME VARCHAR(200) NOT NULL,
   DESCRIPTION VARCHAR(500) NULL,
   TYPE VARCHAR(45) NULL, -- HTML/CUSTOM CHART/PYTHON/R
-  PREVIEW_IMAGE TEXT NULL, -- binary of preview file
-  TEMPLATE TEXT NULL, -- text with template as a JSON
+  PREVIEW_IMAGE BYTEA NULL, -- binary of preview file
+  TEMPLATE BYTEA NULL, -- text with template as a JSON
   AUTHOR VARCHAR(100) NULL,
   USAGE_COUNTER INT NULL, -- counter to see how many times the widgets was used
   USER_IN    VARCHAR(100) NOT NULL,
