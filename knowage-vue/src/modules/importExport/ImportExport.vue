5302e44afc084855f5275584a865335e5258afb3
Davide Vernassa <davide.vernassa@eng.it>
2021-04-28 11:09:14 +0200

    Added import-export skeleton

---
+++ knowage-vue/src/modules/importExport/ImportExport.vue
@@ -0,0 +1,20 @@
+<template>
+    ciao
+    <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'import-export',
+    components: {},
+    data() {
+        return {}
+    },
+    created() {},
+    methods: {}
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ knowage-vue/src/modules/importExport/gallery/ImportExportGallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'import-export-gallery',
+    components: {},
+    data() {
+        return {}
+    },
+    created() {},
+    methods: {}
+})
+</script>
+
+<style lang="scss" scoped></style>
