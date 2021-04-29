f233eab9c5dca64f976c9d83b11f955c1f3e601f
Alberto Nale <alberto.nale@eng.it>
2021-04-29 18:17:48 +0200

    Moved style to descriptor

--- knowage-vue/src/modules/importExport/ImportExport.vue
+++ knowage-vue/src/modules/importExport/ImportExport.vue
@@ -1,63 +1,49 @@
 <template>
-    <div class="kn-importExport kn-page">
-        <Toolbar class="kn-toolbar kn-toolbar--primary">
-            <template #left>
-                {{ $t('importExport.title') }}
-            </template>
-            <template #right>
-                <Button class="kn-button p-button-text">{{ $t(common.import) }}</Button>
-                <Button class="kn-button p-button-text">{{ $t(common.export) }}</Button>
-            </template>
-        </Toolbar>
-        <div class="kn-page-content p-grid p-m-0">
-            <div class="functionalities-container p-col-3 p-sm-3 p-md-2">
-                <KnTabCard :element="type" v-for="(type, index) in importExportDescriptor.functionalities" v-bind:key="index" @click="selectType(type)"></KnTabCard>
-            </div>
-            <div class="p-col-9 p-sm-9 p-md-10 p-p-0 p-m-0">
-                <router-view />
-            </div>
-        </div>
-    </div>
+	<div class="kn-importExport kn-page">
+		<Toolbar class="kn-toolbar kn-toolbar--primary">
+			<template #left>
+				{{ $t('importExport.title') }}
+			</template>
+			<template #right>
+				<Button class="kn-button p-button-text">{{ $t('common.import') }}</Button>
+				<Button class="kn-button p-button-text">{{ $t('common.export') }}</Button>
+			</template>
+		</Toolbar>
+		<div class="kn-page-content p-grid p-m-0">
+			<div class="functionalities-container p-col-3 p-sm-3 p-md-2">
+				<KnTabCard :element="type" v-for="(type, index) in importExportDescriptor.functionalities" v-bind:key="index" @click="selectType(type)"></KnTabCard>
+			</div>
+			<div class="p-col-9 p-sm-9 p-md-10 p-p-0 p-m-0">
+				<router-view />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import importExportDescriptor from './ImportExportDescriptor.json'
-import KnTabCard from '@/components/UI/KnTabCard.vue'
+	import { defineComponent } from 'vue'
+	import importExportDescriptor from './ImportExportDescriptor.json'
+	import KnTabCard from '@/components/UI/KnTabCard.vue'
 
-export default defineComponent({
-    name: 'import-export',
-    components: { KnTabCard },
-    data() {
-        return {
-            importExportDescriptor: importExportDescriptor
-        }
-    },
-    created() {},
-    methods: {
-        selectType(type) {
-            this.$router.push(type.route)
-        }
-    }
-})
+	export default defineComponent({
+		name: 'import-export',
+		components: { KnTabCard },
+		data() {
+			return {
+				importExportDescriptor: importExportDescriptor
+			}
+		},
+		created() {},
+		methods: {
+			selectType(type) {
+				this.$router.push(type.route)
+			}
+		}
+	})
 </script>
 
 <style lang="scss" scoped>
-.functionalities-container {
-    flex: 1;
-}
-
-.importExportTags {
-    &:deep(.p-inputtext) {
-        border: none !important;
-        border-radius: 0;
-        width: 100%;
-    }
-}
-
-.imageOverlayPanel {
-    position: absolute !important;
-    top: 0px !important;
-    left: 0px !important;
-}
+	.functionalities-container {
+		flex: 1;
+	}
 </style>
