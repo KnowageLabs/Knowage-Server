9974043c1e08dd775e9d280d585d530db9aa08b8
Davide Vernassa <davide.vernassa@eng.it>
2021-05-03 12:43:37 +0200

    Added import Dialog

---
+++ knowage-vue/src/modules/importExport/ImportDialog.vue
@@ -0,0 +1,56 @@
+<template>
+    <Dialog class="kn-dialog--toolbar--primary importExportDialog" v-bind:visible="visibility" footer="footer" :header="$t('common.import')" :closable="false" modal>
+        <FileUpload name="demo[]" :customUpload="true" @uploader="onUpload" auto="true" :maxFileSize="1000000">
+            <template #empty>
+                <p>Drag and drop files to here to upload.</p>
+            </template>
+        </FileUpload>
+        <template #footer>
+            <Button class="p-button-text kn-button" v-t="'common.close'" @click="closeDialog" />
+            <Button class="kn-button kn-button--primary" v-t="'common.import'" @click="saveImport" />
+        </template>
+    </Dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Dialog from 'primevue/dialog'
+import FileUpload from 'primevue/fileupload'
+
+export default defineComponent({
+    name: 'import-dialog',
+    components: { Dialog, FileUpload },
+    props: {
+        visibility: Boolean
+    },
+    data() {
+        return {}
+    },
+    created() {},
+    emits: ['update:visibility'],
+    methods: {
+        closeDialog(): void {
+            this.$emit('update:visibility', false)
+        },
+        onUpload(upload) {
+            console.log(upload.files[0])
+        },
+        saveImport(): void {
+            console.log('ok')
+        }
+    }
+})
+</script>
+<style lang="scss">
+.importExportDialog {
+    min-width: 600px;
+    width: 60%;
+    max-width: 1200px;
+    .p-fileupload-buttonbar {
+        border: none;
+        .p-button:not(.p-fileupload-choose) {
+            display: none;
+        }
+    }
+}
+</style>
--- knowage-vue/src/modules/importExport/ImportExport.vue
+++ knowage-vue/src/modules/importExport/ImportExport.vue
@@ -1,49 +1,55 @@
 <template>
-	<div class="kn-importExport kn-page">
-		<Toolbar class="kn-toolbar kn-toolbar--primary">
-			<template #left>
-				{{ $t('importExport.title') }}
-			</template>
-			<template #right>
-				<Button class="kn-button p-button-text">{{ $t('common.import') }}</Button>
-				<Button class="kn-button p-button-text">{{ $t('common.export') }}</Button>
-			</template>
-		</Toolbar>
-		<div class="kn-page-content p-grid p-m-0">
-			<div class="functionalities-container p-col-3 p-sm-3 p-md-2">
-				<KnTabCard :element="type" v-for="(type, index) in importExportDescriptor.functionalities" v-bind:key="index" @click="selectType(type)"></KnTabCard>
-			</div>
-			<div class="p-col-9 p-sm-9 p-md-10 p-p-0 p-m-0">
-				<router-view />
-			</div>
-		</div>
-	</div>
+    <div class="kn-importExport kn-page">
+        <ImportDialog v-model:visibility="displayImportDialog"></ImportDialog>
+        <Toolbar class="kn-toolbar kn-toolbar--primary">
+            <template #left>
+                {{ $t('importExport.title') }}
+            </template>
+            <template #right>
+                <Button class="kn-button p-button-text" @click="openImportDialog">{{ $t('common.import') }}</Button>
+                <Button class="kn-button p-button-text">{{ $t('common.export') }}</Button>
+            </template>
+        </Toolbar>
+        <div class="kn-page-content p-grid p-m-0">
+            <div class="functionalities-container p-col-3 p-sm-3 p-md-2">
+                <KnTabCard :element="type" v-for="(type, index) in importExportDescriptor.functionalities" v-bind:key="index" @click="selectType(type)"></KnTabCard>
+            </div>
+            <div class="p-col-9 p-sm-9 p-md-10 p-p-0 p-m-0">
+                <router-view />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-	import { defineComponent } from 'vue'
-	import importExportDescriptor from './ImportExportDescriptor.json'
-	import KnTabCard from '@/components/UI/KnTabCard.vue'
+import { defineComponent } from 'vue'
+import importExportDescriptor from './ImportExportDescriptor.json'
+import ImportDialog from './ImportDialog.vue'
+import KnTabCard from '@/components/UI/KnTabCard.vue'
 
-	export default defineComponent({
-		name: 'import-export',
-		components: { KnTabCard },
-		data() {
-			return {
-				importExportDescriptor: importExportDescriptor
-			}
-		},
-		created() {},
-		methods: {
-			selectType(type) {
-				this.$router.push(type.route)
-			}
-		}
-	})
+export default defineComponent({
+    name: 'import-export',
+    components: { KnTabCard, ImportDialog },
+    data() {
+        return {
+            importExportDescriptor: importExportDescriptor,
+            displayImportDialog: false
+        }
+    },
+    created() {},
+    methods: {
+        selectType(type): void {
+            this.$router.push(type.route)
+        },
+        openImportDialog(): void {
+            this.displayImportDialog = !this.displayImportDialog
+        }
+    }
+})
 </script>
 
 <style lang="scss" scoped>
-	.functionalities-container {
-		flex: 1;
-	}
+.functionalities-container {
+    flex: 1;
+}
 </style>
