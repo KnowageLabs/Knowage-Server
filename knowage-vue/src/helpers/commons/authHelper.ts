fb36ba887327f618d96b564cae2b20eab33a1e99
Davide Vernassa <davide.vernassa@eng.it>
2021-05-04 09:35:18 +0200

    Changed logout url to be dynamic

--- knowage-vue/src/helpers/commons/authHelper.ts
+++ knowage-vue/src/helpers/commons/authHelper.ts
@@ -1,6 +1,6 @@
-
 export default {
-    logout(){
-        window.location.href = process.env.VUE_APP_HOST_URL + '/knowage/servlet/AdapterHTTP?ACTION_NAME=LOGOUT_ACTION&LIGHT_NAVIGATOR_DISABLED=TRUE&NEW_SESSION=TRUE'
+    logout() {
+        let url = new URL(window.location.origin)
+        window.location.href = url + '/knowage/servlet/AdapterHTTP?ACTION_NAME=LOGOUT_ACTION&LIGHT_NAVIGATOR_DISABLED=TRUE&NEW_SESSION=TRUE'
     }
 }
