e7a14deed0145eda9f74c8ee67dd4d763722bb03
Marco Libanori <marco.libanori@eng.it>
2021-05-06 14:11:18 +0200

    Knowage-Vue is now a Spring Boot SPA application

---
+++ knowage-vue/src/main/java/it/eng/knowage/knowagevue/KnowageVueApplication.java
@@ -0,0 +1,30 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2021 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.eng.knowage.knowagevue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowageVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowageVueApplication.class, args);
+	}
+
+}
---
+++ knowage-vue/src/main/java/it/eng/knowage/knowagevue/controller/SpaErrorController.java
@@ -0,0 +1,47 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2021 Engineering Ingegneria Informatica S.p.A.
+ *
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.eng.knowage.knowagevue.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+	public Object error(HttpServletRequest request, HttpServletResponse response) {
+		if (request.getMethod().equalsIgnoreCase(HttpMethod.GET.name())) {
+			response.setStatus(HttpStatus.OK.value());
+			return "forward:/index.html";
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+}
