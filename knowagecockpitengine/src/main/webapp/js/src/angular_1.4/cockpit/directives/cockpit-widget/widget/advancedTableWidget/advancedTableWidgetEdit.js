858618d561359107de25baa094659136a803911a
Davide Vernassa <davide.vernassa@eng.it>
2021-05-20 09:21:09 +0200

    [KNOWAGE-5946] - Added row thresholds table widget configuration [KNOWAGE-5946] - Override default cell alignment [KNOWAGE-5946] - Changed row threshold configuration [KNOWAGE-5946] - Added translations
    [KNOWAGE-5946] - Aligned CSS with 7.4 release

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/advancedTableWidget/advancedTableWidgetEdit.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/advancedTableWidget/advancedTableWidgetEdit.js
@@ -16,10 +16,15 @@ angular
 	.module('cockpitModule')
 	.controller('advancedTableWidgetEditControllerFunction',advancedTableWidgetEditControllerFunction)
 
-function advancedTableWidgetEditControllerFunction($scope,$compile,finishEdit,$q,model,sbiModule_translate,$mdDialog,mdPanelRef,$mdToast,cockpitModule_datasetServices,cockpitModule_generalOptions, cockpitModule_analyticalDrivers, sbiModule_restServices,cockpitModule_template){
+function advancedTableWidgetEditControllerFunction($scope,$compile,finishEdit,$q,model,sbiModule_translate,$mdDialog,mdPanelRef,$mdToast,cockpitModule_datasetServices,cockpitModule_generalOptions, cockpitModule_analyticalDrivers, sbiModule_restServices,cockpitModule_template, cockpitModule_properties){
 	$scope.translate=sbiModule_translate;
 	$scope.newModel = angular.copy(model);
 	$scope.cockpitModule_generalOptions = cockpitModule_generalOptions;
+	$scope.cockpitModule_properties = cockpitModule_properties;
+	$scope.rowThresholdsConditions = cockpitModule_generalOptions.conditions;
+	if($scope.rowThresholdsConditions.indexOf('IN') == -1) $scope.rowThresholdsConditions.push('IN');
+	$scope.cockpitModule_analyticalDrivers = cockpitModule_analyticalDrivers;
+	$scope.cockpitModule_template = cockpitModule_template;
 	$scope.availableAggregations = ["NONE","SUM","AVG","MAX","MIN","COUNT","COUNT_DISTINCT"];
 	$scope.availableSummaryAggregations = ["SUM","AVG","COUNT","COUNT_DISTINCT", "MAX", "MIN"];
 	$scope.typesMap = cockpitModule_generalOptions.typesMap;
@@ -30,6 +35,10 @@ function advancedTableWidgetEditControllerFunction($scope,$compile,finishEdit,$q
 			$scope.allHidden = false;
 			break;
 		}
+	}
+	
+	$scope.isObject = function(item){
+		return typeof item == 'object';
 	}
 
 	function uuidv4() {
@@ -316,7 +325,51 @@ function advancedTableWidgetEditControllerFunction($scope,$compile,finishEdit,$q
 			}, function() {
 			});
 	}
+	
+	$scope.setRowThresholdStyle = function(index){
+		$mdDialog.show({
+			templateUrl:  baseScriptPath+ '/directives/cockpit-columns-configurator/templates/rowThresholdStyle.html',
+			parent : angular.element(document.body),
+			clickOutsideToClose:true,
+			escapeToClose :true,
+			preserveScope: false,
+			autoWrap:false,
+			fullscreen: true,
+			locals:{style : $scope.newModel.settings.rowThresholds.list[index].style},
+			controller: function(scope,sbiModule_translate,cockpitModule_generalOptions,$mdDialog,style){
+				scope.translate = sbiModule_translate;
+				scope.tempStyle = angular.copy(style);
+				scope.cockpitModule_generalOptions = cockpitModule_generalOptions;
+				
+				scope.colorPickerOptions = {
+					placeholder:scope.translate.load('sbi.cockpit.color.select'),
+					format:'rgb'
+				}
+				
+				scope.cancel = function(){
+					$mdDialog.cancel();
+				}
 
+				scope.save = function(){
+					$mdDialog.hide(scope.tempStyle);
+				}
+			}
+		}).then(function(tempStyle) {
+			$scope.newModel.settings.rowThresholds.list[index].style = tempStyle;
+		}, function() {
+			console.log("Selected column:", $scope.selectedColumn);
+		});
+	}
+
+	$scope.setRowThresholdFormula = function(index){
+		if(typeof $scope.newModel.settings.rowThresholds.list[index].formula != "undefined") delete $scope.newModel.settings.rowThresholds.list[index].formula;
+		else $scope.newModel.settings.rowThresholds.list[index].formula = "";
+	}
+	
+	$scope.hasFormula = function(threshold){
+		return threshold.formula || threshold.formula == '';
+	}
+	
 	function columnsGroupController(scope,sbiModule_translate,cockpitModule_generalOptions,$mdDialog,model){
 		scope.translate=sbiModule_translate;
 		scope.cockpitModule_generalOptions = cockpitModule_generalOptions;
@@ -515,6 +568,14 @@ function advancedTableWidgetEditControllerFunction($scope,$compile,finishEdit,$q
 	$scope.removeSummaryRow = function(i){
 		$scope.newModel.settings.summary.list.splice(i,1);
 	}
+	
+	$scope.addRowThreshold = function(){
+		$scope.newModel.settings.rowThresholds.list.push({});
+	}
+	
+	$scope.removeRowThreshold = function(i){
+		$scope.newModel.settings.rowThresholds.list.splice(i,1);
+	}
 
 	$scope.checkForPinnedColumns = function(columns){
 		for(var k in columns){
@@ -529,6 +590,12 @@ function advancedTableWidgetEditControllerFunction($scope,$compile,finishEdit,$q
 				$scope.showNoPinnedColumnWarning = $scope.checkForPinnedColumns($scope.newModel.content.columnSelectedOfDataset);
 			}
 			if(!$scope.newModel.settings.summary.list) $scope.newModel.settings.summary.list = [{}];
+		}
+	})
+	
+	$scope.$watch('newModel.settings.rowThresholds.enabled',function(newValue,oldValue){
+		if(newValue){
+			if(!$scope.newModel.settings.rowThresholds.list) $scope.newModel.settings.rowThresholds.list = [];
 		}
 	})
 
