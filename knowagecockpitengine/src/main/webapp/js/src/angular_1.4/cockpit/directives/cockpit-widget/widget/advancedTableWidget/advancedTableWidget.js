b91fb3519fe9ff09382023aa648ed63706590503
Davide Vernassa <davide.vernassa@eng.it>
2021-05-20 09:21:09 +0200

    Revert "[KNOWAGE-5946] - Added row thresholds table widget configuration"
    This reverts commit 858618d561359107de25baa094659136a803911a.

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/advancedTableWidget/advancedTableWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/advancedTableWidget/advancedTableWidget.js
@@ -89,21 +89,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 			}
 		}
 		
-		var replacePlaceholders = function(text, data){
-			function adaptToType(value) {
-				return isNaN(value) ? '"'+value+'"' : value;
-			}
+		var replacePlaceholders = function(text){
 			// variables
 			text = text.replace(/\$V\{([a-zA-Z0-9\_\-\.]+)\}/g, function(match,variable){
-				return adaptToType(cockpitModule_properties.VARIABLES[variable]);
-			});
-			// fields
-			text = text.replace(/\$F\{([a-zA-Z0-9\_\-\.]+)\}/g, function(match,field){
-				return adaptToType(data[field]);
+				return cockpitModule_properties.VARIABLES[variable];
 			});
 			// parameters
 			text = text.replace(/\$P\{([a-zA-Z0-9\_\-\.]+)\}/g, function(match,parameter){
-				return adaptToType(cockpitModule_analyticalDrivers[parameter]);
+				return cockpitModule_analyticalDrivers[parameter];
 			});
 			return text;
 		}
@@ -691,64 +684,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 				obj["type"] = 'table';
 			return obj;
 		}
-		
-		function rowThresholdComparer(data){
-			var validThresholds = [];
-			for(var k in $scope.ngModel.settings.rowThresholds.list){
-				var threshold = angular.copy($scope.ngModel.settings.rowThresholds.list[k]);
-				if(typeof threshold.formula != 'undefined'){
-					threshold.formula = replacePlaceholders(threshold.formula, data);
-					if(eval(threshold.formula)){
-						// changing border color property to override ag-grid default behaviour
-						if(threshold.style['border-bottom-color']) threshold.style['border-bottom'] = "1px solid "+threshold.style['border-bottom-color'];
-						if(threshold.style['border-top-color']) threshold.style['border-top'] = "1px solid "+threshold.style['border-top-color'];
-						validThresholds.push(threshold.style)
-					}
-				}
-				else{
-					// getting the value to compare with
-					var valueToCompare;
-					if(threshold.compareValueType == 'static') valueToCompare = threshold.compareValue;
-					if(threshold.compareValueType == 'variable') {
-						if(threshold.compareValueKey) valueToCompare = cockpitModule_properties.VARIABLES[threshold.compareValue][threshold.compareValueKey];
-						else valueToCompare = cockpitModule_properties.VARIABLES[threshold.compareValue];
-					}
-					if(threshold.compareValueType == 'parameter') valueToCompare = cockpitModule_analyticalDrivers[threshold.compareValue];
-					
-					// getting the condition to compare with and comparing
-					var fullfilledCondition = false;
-					switch(threshold.condition){
-					case '==':
-						fullfilledCondition = data[threshold.column] == valueToCompare;
-						break;
-					case '>=':
-						fullfilledCondition = data[threshold.column] >= valueToCompare;
-						break;
-					case '<=':
-						fullfilledCondition = data[threshold.column] <= valueToCompare;
-						break;
-					case 'IN':
-						fullfilledCondition = valueToCompare.split(',').indexOf(data[threshold.column]) != -1;
-						break;
-					case '>':
-						fullfilledCondition = data[threshold.column] > valueToCompare;
-					case '<':
-						fullfilledCondition = data[threshold.column] < valueToCompare;
-					case '!=':
-						fullfilledCondition = data[threshold.column] != valueToCompare;
-					}
-					
-					if(fullfilledCondition){
-						// changing border color property to override ag-grid default behaviour
-						if(threshold.style['border-bottom-color']) threshold.style['border-bottom'] = "1px solid "+threshold.style['border-bottom-color'];
-						if(threshold.style['border-top-color']) threshold.style['border-top'] = "1px solid "+threshold.style['border-top-color'];
-						if(threshold.condition == '==') return threshold.style;
-						else validThresholds.push(threshold.style)
-					}
-				}
-			}
-			return validThresholds[0];
-		}
 
 		$scope.advancedTableGrid = {
 				angularCompileRows: true,
@@ -769,9 +704,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 				onColumnResized: columnResized,
 				getRowHeight: getRowHeight,
 				getRowStyle: function(params) {
-					if($scope.ngModel.settings.rowThresholds && $scope.ngModel.settings.rowThresholds.enabled){
-						if(rowThresholdComparer(mapRow(params.data))) return rowThresholdComparer(mapRow(params.data));
-					}
 					// TODO : make this a CSS rule with a custom class
 					if($scope.ngModel.settings.alternateRows && $scope.ngModel.settings.alternateRows.enabled){
 					    if($scope.ngModel.settings.alternateRows.oddRowsColor && params.node.rowIndex % 2 === 0) {
