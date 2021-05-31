9759c5e2b67404819948d64d2d824816fc3c36de
dbulatovicx32 <darko.bulatovic@eng.it>
2021-05-31 12:46:23 +0200

    Configuration Management v1.0

---
+++ knowage-vue/src/modules/managers/configurationManagement/ConfigurationManagement.routes.js
@@ -0,0 +1,9 @@
+let routes = [
+    {
+        path: '/configuration-management',
+        name: 'configuration-management',
+        component: () => import('@/modules/managers/configurationManagement/ConfigurationManagement.vue')
+    }
+]
+
+export default routes
--- knowage-vue/src/modules/managers/managers.routes.js
+++ knowage-vue/src/modules/managers/managers.routes.js
@@ -1,7 +1,9 @@
 import galleryManagementRoutes from '@/modules/managers/galleryManagement/GalleryManagement.routes.js'
+import configurationManagementRoutes from '@/modules/managers/configurationManagement/ConfigurationManagement.routes.js'
+
 
 const baseRoutes = [];
 
-const routes = baseRoutes.concat(galleryManagementRoutes)
+const routes = baseRoutes.concat(galleryManagementRoutes).concat(configurationManagementRoutes)
 
 export default routes;
