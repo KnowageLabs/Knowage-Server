07d51157728a7ee9a87c55e6fbc20e1acf2d0ac8
Aleksandar Simonovic <aleksandarsimonovic@live.com>
2021-06-02 16:46:41 +0200

    Profile attributes management V1

---
+++ knowage-vue/src/modules/managers/profileAttributesManagement/AttributesListBox.vue
@@ -0,0 +1,80 @@
+<template>
+  <Listbox
+    v-if="!load"
+    class="kn-list--column"
+    :options="attributes"
+    :filter="true"
+    :filterPlaceholder="$t('common.search')"
+    optionLabel="userId"
+    filterMatchMode="contains"
+    :filterFields="profileAttributesManagementDescriptor.globalFilterFields"
+    :emptyFilterMessage="$t('managers.widgetGallery.noResults')"
+    @change="onAttributeSelect"
+    data-test="attributes-list"
+  >
+    <template #empty>{{ $t("common.info.noDataFound") }}</template>
+    <template #option="slotProps">
+      <div class="kn-list-item" data-test="list-item">
+        <div class="kn-list-item-text">
+          <span>{{ slotProps.option.attributeName }}</span>
+          <span class="kn-list-item-text-secondary">{{
+            slotProps.option.attributeDescription
+          }}</span>
+        </div>
+        <Button
+          icon="pi pi-trash"
+          class="p-button-link p-button-sm"
+          @click="deleteAttribute(slotProps.option.attributeId)"
+          :data-test="'delete-button'"
+        />
+      </div>
+    </template>
+  </Listbox>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Listbox from "primevue/listbox";
+import ProfileAttributesManagementDescriptor from "./ProfileAttributesManagementDescriptor.json";
+import { iAttribute } from "./ProfileAttributesManagement";
+
+export default defineComponent({
+  name: "users-list-box",
+  components: {
+    Listbox,
+  },
+  emits: ["selectedAttribute", "deleteAttribute"],
+  props: {
+    attributes: Object,
+    loading: Boolean,
+  },
+  watch: {
+    attributes: {
+      handler: function (attr) {
+        this.listAttributes = attr;
+      },
+    },
+    loading: {
+      handler: function (l) {
+        this.load = l;
+      },
+    },
+  },
+  data() {
+    return {
+      load: false as Boolean,
+      listAttributes: [] as iAttribute[],
+      selectedAttribute: null as iAttribute | null,
+      profileAttributesManagementDescriptor: ProfileAttributesManagementDescriptor,
+    };
+  },
+  methods: {
+    deleteAttribute(attributeId: number) {
+      this.$emit("deleteAttribute", attributeId);
+    },
+    onAttributeSelect(event: any) {
+      this.$emit("selectedAttribute", event.value);
+    },
+  },
+});
+</script>
---
+++ knowage-vue/src/modules/managers/profileAttributesManagement/ProfileAttributesManagement.spec.js
@@ -0,0 +1,177 @@
+import { mount } from "@vue/test-utils";
+import axios from "axios";
+import Button from "primevue/button";
+import flushPromises from "flush-promises";
+import ProfileAttributesManagement from "./ProfileAttributesManagement.vue";
+import InputText from "primevue/inputtext";
+import ProgressBar from "primevue/progressbar";
+import Card from "primevue/card";
+import Toolbar from "primevue/toolbar";
+
+const mockedAttributes = [
+  {
+    attributeId: 3,
+    attributeName: "address",
+    attributeDescription: "address",
+    allowUser: null,
+    multivalue: null,
+    syntax: null,
+    lovId: null,
+    value: null,
+  },
+  {
+    attributeId: 4,
+    attributeName: "birth_date",
+    attributeDescription: "birth date",
+    allowUser: null,
+    multivalue: null,
+    syntax: null,
+    lovId: null,
+    value: null,
+  },
+  {
+    attributeId: 5,
+    attributeName: "email",
+    attributeDescription: "email",
+    allowUser: null,
+    multivalue: null,
+    syntax: null,
+    lovId: null,
+    value: null,
+  },
+];
+
+jest.mock("axios", () => ({
+  get: jest.fn(() =>
+    Promise.resolve({
+      data: mockedAttributes,
+    })
+  ),
+  delete: jest.fn(() => Promise.resolve()),
+  post: jest.fn(() => Promise.resolve()),
+}));
+
+const $confirm = {
+  require: jest.fn(),
+};
+const $store = {
+  commit: jest.fn(),
+};
+
+const factory = () => {
+  return mount(ProfileAttributesManagement, {
+    attachToDocument: true,
+    global: {
+      plugins: [],
+      stubs: { Button, InputText, ProgressBar, Toolbar, Card },
+      mocks: {
+        $t: (msg) => msg,
+        $store,
+        $confirm,
+      },
+    },
+  });
+};
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("ProfileAttributes Management loading", () => {
+  it("show progress bar when loading", async () => {
+    const wrapper = factory();
+
+    expect(wrapper.vm.loading).toBe(true);
+    expect(wrapper.find('[data-test="progress-bar"]').exists()).toBe(true);
+  });
+
+  it('shows "no data" label when loaded empty', async () => {
+    axios.get.mockReturnValueOnce(
+      Promise.resolve({
+        data: [],
+      })
+    );
+    const wrapper = factory();
+    await flushPromises();
+    expect(wrapper.vm.attributes.length).toBe(0);
+    expect(
+      wrapper.find('[data-test="profile-attributes-listbox"]').html()
+    ).toContain("common.info.noDataFound");
+  });
+});
+
+describe("Profile Attributes Management", () => {
+  it("opens empty form when the '+' button is clicked", async () => {
+    const wrapper = factory();
+    const openButton = wrapper.find('[data-test="open-form-button"]');
+    await openButton.trigger("click");
+    expect(wrapper.vm.hideForm).toBe(false);
+  });
+
+  it("shows form when a row is clicked", async () => {
+    const wrapper = factory();
+    await flushPromises();
+    const dataTable = wrapper.find('[data-test="profile-attributes-listbox"]');
+    await dataTable.find("ul li").trigger("click");
+
+    expect(wrapper.vm.hideForm).toBe(false);
+    expect(wrapper.vm.attribute).toStrictEqual({
+      attributeId: 3,
+      attributeName: "address",
+      attributeDescription: "address",
+      allowUser: null,
+      multivalue: null,
+      syntax: null,
+      lovId: null,
+      value: null,
+    });
+  });
+});
+
+describe("Profille Attributes Management Search", () => {
+  it("filters the list if a label or name is provided", async () => {
+    const wrapper = factory();
+    await flushPromises();
+    const attributesListBox = wrapper.find(
+      '[data-test="profile-attributes-listbox"]'
+    );
+    const searchInput = attributesListBox.find("input");
+
+    expect(attributesListBox.html()).toContain("address");
+    expect(attributesListBox.html()).toContain("birth_date");
+    expect(attributesListBox.html()).toContain("email");
+
+    // attributeName
+    await searchInput.setValue("email");
+    await attributesListBox.trigger("filter");
+    expect(attributesListBox.html()).toContain("email");
+    expect(attributesListBox.html()).not.toContain("address");
+    expect(attributesListBox.html()).not.toContain("birth_date");
+
+    // attributeDescription
+    await searchInput.setValue("birth_date");
+    await attributesListBox.trigger("filter");
+    expect(attributesListBox.html()).not.toContain("address");
+    expect(attributesListBox.html()).not.toContain("email");
+    expect(attributesListBox.html()).toContain("birth_date");
+  });
+  it("returns no data if the label is not present", async () => {
+    const wrapper = factory();
+    await flushPromises();
+    const attributesListBox = wrapper.find(
+      '[data-test="profile-attributes-listbox"]'
+    );
+    const searchInput = attributesListBox.find("input");
+
+    expect(attributesListBox.html()).toContain("address");
+    expect(attributesListBox.html()).toContain("email");
+    expect(attributesListBox.html()).toContain("birth_date");
+
+    await searchInput.setValue("not present value");
+    await attributesListBox.trigger("filter");
+
+    expect(attributesListBox.html()).not.toContain("birth_date");
+    expect(attributesListBox.html()).not.toContain("email");
+    expect(attributesListBox.html()).not.toContain("address");
+  });
+});
