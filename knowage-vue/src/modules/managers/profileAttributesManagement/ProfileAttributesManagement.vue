5b18e1a1f4a3e719c3e64dda71fc936dafc069ca
Davide Vernassa <davide.vernassa@eng.it>
2021-06-07 09:58:48 +0200

    [Profile-attributes-management] Aligned CSS and layout to master
    Changed knHint component to show also html binded code

--- knowage-vue/src/modules/managers/profileAttributesManagement/ProfileAttributesManagement.vue
+++ knowage-vue/src/modules/managers/profileAttributesManagement/ProfileAttributesManagement.vue
@@ -1,87 +1,74 @@
 <template>
-  <div class="kn-page">
-    <div class="kn-page-content p-grid p-m-0">
-      <div class="kn-list--column p-col-4 p-sm-4 p-md-3 p-p-0">
-        <Toolbar class="kn-toolbar kn-toolbar--primary">
-          <template #left>
-            {{ $t("managers.profileAttributesManagement.title") }}
-          </template>
-          <template #right>
-            <KnFabButton icon="fas fa-plus" @click="showForm()" data-test="open-form-button"></KnFabButton>
-          </template>
-        </Toolbar>
-        <ProgressBar
-          mode="indeterminate"
-          class="kn-progress-bar"
-          v-if="loading"
-          data-test="progress-bar"
-        />
-        <AttributesListBox
-          :attributes="attributes"
-          :loading="loading"
-          @deleteAttribute="onAttributeDelete"
-          @selectedAttribute="onAttributeSelect"
-          data-test="profile-attributes-listbox"
-        ></AttributesListBox>
-      </div>
+    <div class="kn-page">
+        <div class="kn-page-content p-grid p-m-0">
+            <div class="kn-list--column p-col-4 p-sm-4 p-md-3 p-p-0">
+                <Toolbar class="kn-toolbar kn-toolbar--primary">
+                    <template #left>
+                        {{ $t('managers.profileAttributesManagement.title') }}
+                    </template>
+                    <template #right>
+                        <KnFabButton icon="fas fa-plus" @click="showForm()" data-test="open-form-button"></KnFabButton>
+                    </template>
+                </Toolbar>
+                <ProgressBar mode="indeterminate" class="kn-progress-bar" v-if="loading" data-test="progress-bar" />
+                <AttributesListBox :attributes="attributes" :loading="loading" @deleteAttribute="onAttributeDelete" @selectedAttribute="onAttributeSelect" data-test="profile-attributes-listbox"></AttributesListBox>
+            </div>
 
-      <div class="p-col-8 p-sm-8 p-md-9 p-p-0 p-m-0" :hidden="hideForm">
-        <ProfileAttributesForm
-          :selectedAttribute="attribute"
-          @refreshRecordSet="loadAllAttributes"
-          @closesForm="closeForm"
-          @dataChanged="dirty=true"
-        ></ProfileAttributesForm>
-      </div>
+            <div class="p-col-8 p-sm-8 p-md-9 p-p-0 p-m-0">
+                <KnHint :title="'managers.profileAttributesManagement.title'" :hint="'managers.profileAttributesManagement.hint'" v-if="hideForm"></KnHint>
+                <ProfileAttributesDetail :selectedAttribute="attribute" @refreshRecordSet="loadAllAttributes" @closesForm="closeForm" @dataChanged="dirty = true" v-if="!hideForm"></ProfileAttributesDetail>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import axios from "axios";
-import KnFabButton from "@/components/UI/KnFabButton.vue";
-import ProfileAttributesManagementDescriptor from "./ProfileAttributesManagementDescriptor.json";
-import ProfileAttributesForm from "./ProfileAttributesForm.vue";
-import AttributesListBox from "./AttributesListBox.vue";
-import { iAttribute } from "./ProfileAttributesManagement";
+import { defineComponent } from 'vue'
+import axios from 'axios'
+import KnFabButton from '@/components/UI/KnFabButton.vue'
+import KnHint from '@/components/UI/KnHint.vue'
+import ProfileAttributesManagementDescriptor from './ProfileAttributesManagementDescriptor.json'
+import ProfileAttributesDetail from './ProfileAttributesDetail.vue'
+import AttributesListBox from './AttributesListBox.vue'
+import { iAttribute } from './ProfileAttributesManagement'
 
 export default defineComponent({
-  name: "profile-attributes",
-  components: {
-    KnFabButton,
-    ProfileAttributesForm,
-    AttributesListBox,
-  },
-  data() {
-    return {
-      apiUrl: process.env.VUE_APP_RESTFUL_SERVICES_PATH + "2.0/",
-      attributes: [] as iAttribute[],
-      attribute: {} as iAttribute,
-      tempAttribute: {} as iAttribute,
-      profileAttributesManagementDescriptor: ProfileAttributesManagementDescriptor,
-      columns: ProfileAttributesManagementDescriptor.columns,
-      loading: false as Boolean,
-      hideForm: false as Boolean,
-      dirty: false as Boolean
-    };
-  },
-  async created() {
-    await this.loadAllAttributes();
-  },
-  methods: {
-    async loadAllAttributes() {
-      this.loading = true;
-      await axios
-        .get(this.apiUrl + "attributes")
-        .then((response) => {
-          this.attributes = response.data;
-        })
-        .finally(() => (this.loading = false));
+    name: 'profile-attributes',
+    components: {
+        AttributesListBox,
+        KnFabButton,
+        KnHint,
+        ProfileAttributesDetail
     },
-    onAttributeSelect(attribute: iAttribute) {
-      if (this.dirty) {
-         this.$confirm.require({
+    data() {
+        return {
+            apiUrl: process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/',
+            attributes: [] as iAttribute[],
+            attribute: {} as iAttribute,
+            tempAttribute: {} as iAttribute,
+            profileAttributesManagementDescriptor: ProfileAttributesManagementDescriptor,
+            columns: ProfileAttributesManagementDescriptor.columns,
+            loading: false as Boolean,
+            hideForm: true as Boolean,
+            dirty: false as Boolean
+        }
+    },
+    async created() {
+        await this.loadAllAttributes()
+    },
+    methods: {
+        async loadAllAttributes() {
+            this.loading = true
+            await axios
+                .get(this.apiUrl + 'attributes')
+                .then((response) => {
+                    this.attributes = response.data
+                })
+                .finally(() => (this.loading = false))
+        },
+        onAttributeSelect(attribute: iAttribute) {
+            if (this.dirty) {
+                this.$confirm.require({
                     message: this.$t('common.toast.unsavedChangesMessage'),
                     header: this.$t('common.toast.unsavedChangesHeader'),
                     icon: 'pi pi-exclamation-triangle',
@@ -90,58 +77,57 @@ export default defineComponent({
                         this.prepareFormData(attribute)
                     }
                 })
-      } else {
-        this.prepareFormData(attribute)
-      }
-    },
-    prepareFormData(attribute: iAttribute) {
-      this.attribute = { ...attribute };
-      if (this.hideForm) {
-        this.hideForm = false;
-      }
-
-    },
-    onAttributeDelete(id: number) {
-      this.deleteAttribute(id);
-    },
-    showForm() {
-      this.hideForm = false;
-      this.attribute = {
-        attributeId: null,
-        attributeName: "",
-        attributeDescription: "",
-        allowUser: null,
-        multivalue: null,
-        syntax: null,
-        lovId: null,
-        value: {},
-      };
-    },
-    closeForm() {
-      this.hideForm = true;
-    },
-    async deleteAttribute(id: number) {
-      this.$confirm.require({
-        message: this.$t('common.toast.deleteMessage'),
-        header: this.$t('common.toast.deleteConfirmTitle'),
-        icon: "pi pi-exclamation-triangle",
-        accept: async () => {
-          this.loading = true;
-          this.axios
-            .delete(this.apiUrl + "attributes/" + id)
-            .then(() => {
-              this.$store.commit("setInfo", {
-                title: this.$t("managers.profileAttributesManagement.info.deleteTitle"),
-                msg: this.$t("managers.profileAttributesManagement.info.deleteMessage"),
-              });
-              this.loadAllAttributes();
-            })
-            .finally(() => {
-              this.loading = false;
-            });
+            } else {
+                this.prepareFormData(attribute)
+            }
         },
-      });
-    },
-  },
-});
+        prepareFormData(attribute: iAttribute) {
+            this.attribute = { ...attribute }
+            if (this.hideForm) {
+                this.hideForm = false
+            }
+        },
+        onAttributeDelete(id: number) {
+            this.deleteAttribute(id)
+        },
+        showForm() {
+            this.hideForm = false
+            this.attribute = {
+                attributeId: null,
+                attributeName: '',
+                attributeDescription: '',
+                allowUser: null,
+                multivalue: null,
+                syntax: null,
+                lovId: null,
+                value: {}
+            }
+        },
+        closeForm() {
+            this.hideForm = true
+        },
+        async deleteAttribute(id: number) {
+            this.$confirm.require({
+                message: this.$t('common.toast.deleteMessage'),
+                header: this.$t('common.toast.deleteConfirmTitle'),
+                icon: 'pi pi-exclamation-triangle',
+                accept: async () => {
+                    this.loading = true
+                    this.axios
+                        .delete(this.apiUrl + 'attributes/' + id)
+                        .then(() => {
+                            this.$store.commit('setInfo', {
+                                title: this.$t('managers.profileAttributesManagement.info.deleteTitle'),
+                                msg: this.$t('managers.profileAttributesManagement.info.deleteMessage')
+                            })
+                            this.loadAllAttributes()
+                        })
+                        .finally(() => {
+                            this.loading = false
+                        })
+                }
+            })
+        }
+    }
+})
 </script>
