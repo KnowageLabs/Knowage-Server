a20a3836b5baafe83d6f11153acf62b9872dfe38
dbulatovicx32 <darko.bulatovic@eng.it>
2021-06-16 10:21:51 +0200

    Download merge. Changed icons.

--- knowage-vue/src/modules/mainMenu/dialogs/LicenseDialog/LicenseDialog.vue
+++ knowage-vue/src/modules/mainMenu/dialogs/LicenseDialog/LicenseDialog.vue
@@ -1,11 +1,30 @@
 <template>
-    <Dialog class="kn-dialog--toolbar--primary" v-bind:visible="visibility" footer="footer" :closable="false" modal>
-        <template #header>
-            {{ $t('licenceDialog.title') + ' - MyHostName:' + ' TO DO' }}
-        </template>
-        <TabView class="knTab kn-tab">
-            <TabPanel>
-                <LicenceTab :licenses="licenses['DESKTOP-MG3LIEH']"></LicenceTab>
+    <Dialog class="kn-dialog--toolbar--primary" :style="licenseDialogDescriptor.card.style" v-bind:visible="visibility" footer="footer" :closable="false" modal>
+        <template #header> {{ $t('licenseDialog.title') + ' - MyHostName: ' }}{{ hosts[0].hostName }} </template>
+        <ProgressBar mode="indeterminate" class="kn-progress-bar" v-if="loading" data-test="progress-bar" />
+        <div v-if="hosts.length === 0">
+            <p>{{ $t('licenseDialog.noLicenses') }}</p>
+        </div>
+        <TabView class="kn-tab">
+            <TabPanel v-for="host in hosts" :key="host.hardwareId">
+                <template #header>
+                    <span>TO DO ???</span>
+                </template>
+
+                <div id="host-info">
+                    <div id="host-labels">
+                        <p>{{ $t('licenseDialog.hostName') }}</p>
+                        <p>{{ $t('licenseDialog.hardwareId') }}</p>
+                        <p>{{ $t('licenseDialog.numberOfCpu') }}</p>
+                    </div>
+                    <div>
+                        <p>{{ host.hostName }}</p>
+                        <p>{{ host.hardwareId }}</p>
+                        <p>{{ 'TODO NUMBER OF CPU' }}</p>
+                    </div>
+                </div>
+
+                <LicenceTab :licenses="licenses[host.hostName]" :host="hosts"></LicenceTab>
             </TabPanel>
         </TabView>
         <template #footer>
@@ -17,9 +36,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { iHost, iLicense } from './License'
-import Dialog from 'primevue/dialog'
 import axios from 'axios'
-import LicenceTab from './LicenceTab.vue'
+import Dialog from 'primevue/dialog'
+import licenseDialogDescriptor from './LicenseDialogDescriptor.json'
+import LicenceTab from './LicenseTab.vue'
 import TabView from 'primevue/tabview'
 import TabPanel from 'primevue/tabpanel'
 
@@ -28,8 +48,10 @@ export default defineComponent({
     components: { Dialog, LicenceTab, TabView, TabPanel },
     data() {
         return {
+            licenseDialogDescriptor,
             hosts: [] as iHost[],
-            licenses: {} as { [key: string]: iLicense[] }
+            licenses: {} as { [key: string]: iLicense[] },
+            loading: false
         }
     },
     async created() {
@@ -46,19 +68,31 @@ export default defineComponent({
             this.$emit('update:visibility', false)
         },
         async loadLicenses() {
-            await axios.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/license').then((response) => {
-                this.hosts = response.data.hosts
-                this.licenses = response.data.licenses
-            })
+            this.loading = true
+            await axios
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/license')
+                .then((response) => {
+                    this.hosts = response.data.hosts
+                    this.licenses = response.data.licenses
+                })
+                .finally(() => (this.loading = false))
         }
     }
 })
 </script>
 
-<style scoped lang="scss">
-.newsDialog {
-    min-width: 800px;
-    max-width: 1200px;
-    width: 800px;
+<style scoped>
+#host-info {
+    font-size: 0.7rem;
+    border: 1px solid rgba(59, 103, 140, 0.1);
+    background-color: #eaf0f6;
+    margin: 0;
+    width: 80%;
+    display: flex;
+    flex-direction: row;
+}
+
+#host-labels {
+    flex: 0.7;
 }
 </style>
---
+++ knowage-vue/src/modules/mainMenu/dialogs/LicenseDialog/LicenseTab.vue
@@ -0,0 +1,127 @@
+<template>
+    <Toolbar class="kn-toolbar p-mb-2">
+        <template #right>
+            <FabButton icon="fas fa-plus" :style="licenseDialogDescriptor.fabButton.style" v-tooltip.top="$t('licenseDialog.dataRequired')" />
+        </template>
+    </Toolbar>
+    <Listbox class="kn-list--column" :style="licenseDialogDescriptor.list.style" :options="licensesList">
+        <template #option="slotProps">
+            <div class="kn-list-item" data-test="list-item">
+                <Avatar :image="require(`@/assets/images/licenseImages/${slotProps.option.product}.png`)" size="medium" />
+                <div class="kn-list-item-text">
+                    <span>{{ slotProps.option.product }}</span>
+                    <span class="kn-list-item-text-secondary" :class="setLicenseClass(slotProps.option.status)">{{ licenseText(slotProps.option.status) }}</span>
+                </div>
+                <div class="kn-list-item-text">
+                    <span class="kn-list-item-text-secondary">{{ $t('licenseDialog.licenseId') }}</span>
+                    <span>{{ slotProps.option.licenseId }}</span>
+                </div>
+                <Button icon="pi pi-download" class="p-button-link" v-tooltip.top="$t('licenseDialog.downloadLicense')" @click="downloadLicence(slotProps.option.product)" />
+                <Button icon="pi pi-pencil" class="p-button-link" v-tooltip.top="$t('licenseDialog.changeLicense')" />
+                <Button icon="pi pi-trash" class="p-button-link" v-tooltip.top="$t('licenseDialog.deleteLicense')" />
+            </div>
+        </template>
+    </Listbox>
+    hosts: {{ hostsList[0] }}
+    <br />
+    licenses: {{ licensesList }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { iLicense, iHost } from './License'
+import { downloadDirect } from '@/helpers/commons/fileHelper'
+import licenseDialogDescriptor from './LicenseDialogDescriptor.json'
+import axios from 'axios'
+import FabButton from '@/components/UI/KnFabButton.vue'
+import Avatar from 'primevue/avatar'
+import Listbox from 'primevue/listbox'
+import Tooltip from 'primevue/tooltip'
+
+export default defineComponent({
+    name: 'license-tab',
+    components: {
+        Avatar,
+        FabButton,
+        Listbox
+    },
+    directives: {
+        tooltip: Tooltip
+    },
+    props: {
+        licenses: {
+            type: Array,
+            required: true
+        },
+        host: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {
+            licenseDialogDescriptor,
+            licensesList: [] as iLicense[],
+            hostsList: [] as iHost[]
+        }
+    },
+    watch: {
+        licenses() {
+            this.loadLicenses()
+        },
+        host() {
+            this.loadHost()
+        }
+    },
+    created() {
+        this.loadLicenses()
+        this.loadHost()
+    },
+    methods: {
+        loadLicenses() {
+            this.licensesList = this.licenses as iLicense[]
+        },
+        loadHost() {
+            this.hostsList = this.host as any[]
+        },
+        setLicenseClass(status: string) {
+            return status === 'LICENSE_VALID' ? 'valid' : 'invalid'
+        },
+        licenseText(status: string) {
+            return status === 'LICENSE_VALID' ? this.$t('licenseDialog.validLicense') : this.$t('licenseDialog.invalidLicense')
+        },
+        async downloadLicence(productName) {
+            console.log(productName)
+            await axios
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `1.0/license/download` + `/${this.hostsList[0].hostName}/` + `${productName}`, {
+                    headers: {
+                        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+                    }
+                })
+                .then(
+                    (response) => {
+                        if (response.data.errors) {
+                            this.$store.commit('setError', { title: this.$t('common.error.downloading'), msg: this.$t('common.error.errorCreatingPackage') })
+                        } else {
+                            this.$store.commit('setInfo', { title: this.$t('common.toast.success') })
+                            var contentDisposition = response.headers['content-disposition']
+                            var fileAndExtension = contentDisposition.match(/filename[^;\n=]*=((['"]).*?\2|[^;\n]*)/i)[1]
+                            var completeFileName = fileAndExtension.replaceAll('"', '')
+                            downloadDirect(response.data, completeFileName, 'application/zip; charset=utf-8')
+                        }
+                    },
+                    (error) => this.$store.commit('setError', { title: this.$t('common.error.downloading'), msg: this.$t(error) })
+                )
+        }
+    }
+})
+</script>
+
+<style scoped>
+.valid {
+    color: #4caf50 !important;
+}
+.invalid {
+    color: red !important;
+}
+</style>
