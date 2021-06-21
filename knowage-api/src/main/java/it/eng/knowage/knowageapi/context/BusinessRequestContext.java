bfc5abba454c8613d4fdba752fec7301b405814c
Marco Libanori <marco.libanori@eng.it>
2021-06-21 22:30:15 +0200

    JPA-based DAO for function catalog with UUIT

---
+++ knowage-api/src/main/java/it/eng/knowage/knowageapi/context/BusinessRequestContext.java
@@ -0,0 +1,59 @@
+/*
+ * Knowage, Open Source Business Intelligence suite
+ * Copyright (C) 2021 Engineering Ingegneria Informatica S.p.A.
+
+ * Knowage is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Knowage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.eng.knowage.knowageapi.context;
+
+import java.util.UUID;
+
+/**
+ * Holds business data from the request.
+ *
+ * @author Marco Libanori
+ */
+public class BusinessRequestContext {
+
+	private UUID uuid = UUID.randomUUID();
+
+	private String username;
+
+	private String organization;
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+}
