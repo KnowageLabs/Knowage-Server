ae551fd297658cc599caa8a13de977badd87b796
dbulatovicx32 <darko.bulatovic@eng.it>
2021-06-28 13:08:21 +0200

    tabview dynamic

--- knowage-vue/src/modules/managers/internationalizationManagement/InternationalizationManagement.vue
+++ knowage-vue/src/modules/managers/internationalizationManagement/InternationalizationManagement.vue
@@ -1,7 +1,11 @@
 <template>
-    <TabView>
-        <TabPanel v-for="language in availableLanguages" :key="language" :header="language">
-            <p>{{ language }}</p>
+    <TabView @tab-click="logEvent">
+        <TabPanel v-for="language in languages" :key="language">
+            <template #header>
+                {{ $t(`language.${language.locale}`) }}
+                <span v-if="language.isDefault">(DEFAULT)</span>
+            </template>
+            {{ language }}
         </TabPanel>
     </TabView>
 </template>
@@ -11,18 +15,29 @@ import { defineComponent } from 'vue'
 import axios from 'axios'
 import TabView from 'primevue/tabview'
 import TabPanel from 'primevue/tabpanel'
+// import Column from 'primevue/column'
+// import DataTable from 'primevue/datatable'
+
+interface Language {
+    locale: string
+    isDefault: boolean | false
+}
 
 export default defineComponent({
     name: 'internationalization-management',
     components: {
         TabView,
         TabPanel
+        // Column,
+        // DataTable
     },
     data() {
         return {
             loading: false,
-            availableLanguages: [] as any,
-            currentUser: {} as any
+            languages: Array<Language>(),
+            currentUser: {} as any,
+            messages: [] as any,
+            defaultLangMessages: [] as any
         }
     },
     async created() {
@@ -34,10 +49,17 @@ export default defineComponent({
             return axios
                 .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `2.0/languages`)
                 .then((response) => {
-                    this.availableLanguages = response.data
-                    console.log(this.availableLanguages)
+                    console.log('response.data: ', response.data)
+                    let languagesArray = response.data.sort()
+                    for (var idx in languagesArray) {
+                        var isDefault = false
+                        if (languagesArray[idx] === this.$i18n.locale) {
+                            isDefault = true
+                        }
+                        this.languages.push({ locale: languagesArray[idx], isDefault: isDefault })
+                    }
                 })
-                .finally(() => (this.loading = false))
+                .finally(() => ((this.loading = false), console.log('after sorting', this.languages)))
         },
         async getCurrentUser() {
             return axios
@@ -47,6 +69,9 @@ export default defineComponent({
                     console.log(this.currentUser)
                 })
                 .finally(() => (this.loading = false))
+        },
+        logEvent(event) {
+            console.log('IM CLICKED', event)
         }
     }
 })
