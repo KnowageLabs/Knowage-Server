2cc1516c1de884c12607cb905298e01254cf855e
dbulatovicx32 <darko.bulatovic@eng.it>
2021-07-21 12:29:28 +0200

    Functionalities Management v1.0  (#201)
    * Added alphanumericNoSpaces regex.
    
    * Change vue.config.js for proxy
    
    * Initial commit
    
    * Layout, API, started creating tree
    
    * Tree showing func, delete, select
    
    * Move up, move down
    
    * Added Style for tree node
    
    * added form
    
    * Unit tests
    
    * Move up, delete tests
    
    * Move down test
    
    * Minor fixes
    
    * Hint, touched, select changes, tests
    
    * figure out checkboxes in datatble
    
    * datatable checkbox progress
    
    * Unit tests
    
    * Roles checkbox
    
    * Tests, refactored check function
    
    * Fixed bug, added test
    
    * check/uncheck All buttons
    
    * Fixed bug, finished tests
    
    * Fixed bug with clicking New button
    
    * Newbutton added v-if
    
    * Fixed functionality staying selected after delete
    
    * Fixed test
    
    * FIx
    
    * Checkbox disabled
    
    * Checkbox changes
    
    * create/update functionality
    
    * Changes when creating new functionality
    
    * create new functionality
    
    * Fixed tests, minor changes
    
    * Added translation
    
    * Fixed reload issue
    
    * fixed touched and create new description
    
    * Fixed reload
    
    * Fixed submit test
    
    * check/uncheck all functionality, fixing tests
    
    * form/hint fix
    
    * Minor change in move up/down
    
    * Fixed button padding on hover in tree
    
    * Added disabled propery on buttons for future styling
    
    Co-authored-by: BojanSovticEngIT <Bojan.Sovtic@eng.it>
    Co-authored-by: Vojislav Stanojevic <vojislav.stanojevic@eng.it>
    Co-authored-by: Davide Vernassa <davide.vernassa@eng.it>

---
+++ knowage-vue/src/modules/managers/functionalitiesManagement/FunctionalitiesManagement.vue
@@ -0,0 +1,265 @@
+<template>
+    <div class="kn-page">
+        <div class="kn-page-content p-grid p-m-0">
+            <div class="p-col-4 p-sm-4 p-md-3 p-p-0">
+                <Toolbar class="kn-toolbar kn-toolbar--primary">
+                    <template #left>
+                        {{ $t('managers.functionalitiesManagement.title') }}
+                    </template>
+                    <template #right>
+                        <FabButton v-if="selectedFunctionality" icon="fas fa-plus" @click="showForm(null, selectedFunctionality.id)" data-test="new-button" />
+                    </template>
+                </Toolbar>
+                <ProgressBar mode="indeterminate" class="kn-progress-bar" v-if="loading" data-test="progress-bar" />
+                <div>
+                    <Tree id="document-tree" :value="nodes" selectionMode="single" :expandedKeys="expandedKeys" @node-select="showForm($event.data, $event.data.parentId)" data-test="functionality-tree">
+                        <template #default="slotProps">
+                            <div class="p-d-flex p-flex-row p-ai-center" @mouseover="buttonsVisible[slotProps.node.id] = true" @mouseleave="buttonsVisible[slotProps.node.id] = false" :data-test="'tree-item-' + slotProps.node.id">
+                                <span>{{ slotProps.node.label }}</span>
+                                <div v-show="buttonsVisible[slotProps.node.id]" class="p-ml-2">
+                                    <Button
+                                        v-if="canBeMovedUp(slotProps.node.data)"
+                                        icon="fa fa-arrow-up"
+                                        v-tooltip.top="$t('managers.functionalitiesManagement.moveUp')"
+                                        class="p-button-link p-button-sm p-p-0"
+                                        @click.stop="moveUp(slotProps.node.id)"
+                                        :data-test="'move-up-button-' + slotProps.node.id"
+                                    />
+                                    <Button
+                                        v-if="canBeMovedDown(slotProps.node.data)"
+                                        icon="fa fa-arrow-down"
+                                        v-tooltip.top="$t('managers.functionalitiesManagement.moveDown ')"
+                                        class="p-button-link p-button-sm p-p-0"
+                                        @click.stop="moveDown(slotProps.node.id)"
+                                        :data-test="'move-down-button-' + slotProps.node.id"
+                                    />
+                                    <Button v-if="canBeDeleted(slotProps.node)" icon="far fa-trash-alt" v-tooltip.top="$t('common.delete')" class="p-button-link p-button-sm p-p-0" @click.stop="deleteFunctionalityConfirm(slotProps.node.id)" :data-test="'delete-button-' + slotProps.node.id" />
+                                </div>
+                            </div>
+                        </template>
+                    </Tree>
+                </div>
+            </div>
+
+            <div class="p-col-8 p-sm-8 p-md-9 p-p-0 p-m-0">
+                <KnHint :title="'managers.functionalitiesManagement.title'" :hint="'managers.functionalitiesManagement.hint'" v-if="showHint" data-test="functionality-hint"></KnHint>
+                <FunctionalitiesManagementDetail v-if="formVisible" :functionality="selectedFunctionality" :parentId="functionalityParentId" :rolesShort="rolesShort" @touched="touched = true" @close="onClose" @inserted="loadPage($event)" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { iFunctionality, iNode } from './FunctionalitiesManagement'
+import FunctionalitiesManagementDetail from './detailTabView/FunctionalitiesManagementDetail.vue'
+import axios from 'axios'
+import FabButton from '@/components/UI/KnFabButton.vue'
+import functionalitiesManagementDescriptor from './FunctionalitiesManagementDescriptor.json'
+import KnHint from '@/components/UI/KnHint.vue'
+import Tree from 'primevue/tree'
+
+export default defineComponent({
+    name: 'functionalities-management',
+    components: {
+        FunctionalitiesManagementDetail,
+        FabButton,
+        KnHint,
+        Tree
+    },
+    data() {
+        return {
+            functionalitiesManagementDescriptor,
+            functionalities: [] as iFunctionality[],
+            rolesShort: [] as { id: number; name: 'string' }[],
+            nodes: [] as iNode[],
+            selectedFunctionality: null as iFunctionality | null,
+            functionalityParentId: null as number | null,
+            expandedKeys: {},
+            showHint: true,
+            touched: false,
+            loading: false,
+            buttonsVisible: [],
+            formVisible: false
+        }
+    },
+    async created() {
+        await this.loadPage(null)
+    },
+    methods: {
+        async loadFunctionalities() {
+            await axios.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/functionalities/').then((response) => (this.functionalities = response.data))
+        },
+        async loadRolesShort() {
+            this.rolesShort = []
+            await axios.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/roles/short/').then((response) => (this.rolesShort = response.data))
+        },
+        createNodeTree() {
+            this.nodes = []
+            const foldersWithMissingParent = [] as iNode[]
+            this.functionalities.forEach((functionality: iFunctionality) => {
+                const node = {
+                    key: functionality.id,
+                    id: functionality.id,
+                    parentId: functionality.parentId,
+                    label: functionality.name,
+                    children: [] as iNode[],
+                    data: functionality,
+                    style: this.functionalitiesManagementDescriptor.node.style
+                }
+                node.children = foldersWithMissingParent.filter((folder: iNode) => node.id === folder.parentId)
+
+                this.attachFolderToTree(node, foldersWithMissingParent)
+            })
+        },
+        attachFolderToTree(folder: iNode, foldersWithMissingParent: iNode[]) {
+            if (folder.parentId) {
+                let parentFolder = null as iNode | null
+
+                for (let i = 0; i < foldersWithMissingParent.length; i++) {
+                    if (folder.parentId === foldersWithMissingParent[i].id) {
+                        foldersWithMissingParent[i].children.push(folder)
+                        break
+                    }
+                }
+
+                for (let i = 0; i < this.nodes.length; i++) {
+                    parentFolder = this.findParentFolder(folder, this.nodes[i])
+                    if (parentFolder) {
+                        parentFolder.children?.push(folder)
+                        break
+                    }
+                }
+
+                if (!parentFolder) {
+                    foldersWithMissingParent.push(folder)
+                }
+            } else {
+                this.nodes.push(folder)
+            }
+        },
+        findParentFolder(folderToAdd: iNode, folderToSearch: iNode) {
+            if (folderToAdd.parentId === folderToSearch.id) {
+                return folderToSearch
+            } else {
+                let tempFolder = null as iNode | null
+                if (folderToSearch.children) {
+                    for (let i = 0; i < folderToSearch.children.length; i++) {
+                        tempFolder = this.findParentFolder(folderToAdd, folderToSearch.children[i])
+                        if (tempFolder) {
+                            break
+                        }
+                    }
+                }
+                return tempFolder
+            }
+        },
+        expandAll() {
+            for (let node of this.nodes) {
+                this.expandNode(node)
+            }
+
+            this.expandedKeys = { ...this.expandedKeys }
+        },
+
+        expandNode(node: iNode) {
+            if (node.children && node.children.length) {
+                this.expandedKeys[node.key] = true
+
+                for (let child of node.children) {
+                    this.expandNode(child)
+                }
+            }
+        },
+        showForm(functionality: iFunctionality, parentId: number) {
+            this.showHint = false
+            this.functionalityParentId = parentId
+            if (!this.touched) {
+                this.setSelected(functionality)
+            } else {
+                this.$confirm.require({
+                    message: this.$t('common.toast.unsavedChangesMessage'),
+                    header: this.$t('common.toast.unsavedChangesHeader'),
+                    icon: 'pi pi-exclamation-triangle',
+                    accept: () => {
+                        this.touched = false
+                        this.setSelected(functionality)
+                    }
+                })
+            }
+        },
+        onClose() {
+            this.touched = false
+            this.formVisible = false
+            this.showHint = true
+        },
+        setSelected(functionality: iFunctionality) {
+            this.selectedFunctionality = functionality
+            this.formVisible = true
+        },
+        canBeMovedUp(functionality: iFunctionality) {
+            return functionality.prog !== 1
+        },
+        moveUp(functionalityId: number) {
+            axios.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `2.0/functionalities/moveUp/${functionalityId}`).then(() => this.loadPage(null))
+        },
+        canBeMovedDown(functionality: iFunctionality) {
+            let canBeMoved = false
+            this.functionalities.forEach((currentFunctionality) => {
+                if (functionality.parentId === currentFunctionality.parentId && functionality.prog < currentFunctionality.prog) {
+                    canBeMoved = true
+                }
+            })
+
+            return canBeMoved
+        },
+        moveDown(functionalityId: number) {
+            axios.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `2.0/functionalities/moveDown/${functionalityId}`).then(() => this.loadPage(null))
+        },
+        canBeDeleted(functionality: iFunctionality) {
+            return functionality.parentId && functionality.codType !== 'LOW_FUNCT'
+        },
+        deleteFunctionalityConfirm(functionalityId: number) {
+            this.$confirm.require({
+                message: this.$t('common.toast.deleteMessage'),
+                header: this.$t('common.toast.deleteTitle'),
+                icon: 'pi pi-exclamation-triangle',
+                accept: () => {
+                    this.touched = false
+                    this.deleteFunctionality(functionalityId)
+                }
+            })
+        },
+        async deleteFunctionality(functionalityId: number) {
+            await axios.delete(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `2.0/functionalities/${functionalityId}`).then((response) => {
+                if (response.data.errors) {
+                    this.$store.commit('setError', {
+                        title: this.$t('common.toast.deleteTitle'),
+                        msg: response.data.errors[0].message
+                    })
+                } else {
+                    this.$store.commit('setInfo', {
+                        title: this.$t('common.toast.deleteTitle'),
+                        msg: this.$t('common.toast.deleteSuccess')
+                    })
+                }
+                this.selectedFunctionality = null
+                this.formVisible = false
+                this.showHint = true
+                this.loadPage(null)
+            })
+        },
+        async loadPage(functionalityId: any) {
+            this.loading = true
+            await this.loadFunctionalities()
+            await this.loadRolesShort()
+            this.createNodeTree()
+            this.expandAll()
+            const id = functionalityId ? functionalityId : this.selectedFunctionality?.id
+            this.selectedFunctionality = this.functionalities.find((functionality) => functionality.id === id) as any
+            this.touched = false
+            this.loading = false
+        }
+    }
+})
+</script>
---
+++ knowage-vue/src/modules/managers/functionalitiesManagement/detailTabView/FunctionalitiesManagementDetail.vue
@@ -0,0 +1,298 @@
+<template>
+    <Toolbar class="kn-toolbar kn-toolbar--secondary p-m-0">
+        <template #left> {{ selectedFolder.name }} </template>
+        <template #right>
+            <Button icon="pi pi-save" class="p-button-text p-button-rounded p-button-plain" :disabled="buttonDisabled" @click="handleSubmit" data-test="submit-button" />
+            <Button icon="pi pi-times" class="p-button-text p-button-rounded p-button-plain" @click="closeTemplate" />
+        </template>
+    </Toolbar>
+    <div v-if="!selectedFolder.id || selectedFolder.parentId">
+        <Card :style="detailDescriptor.card.style">
+            <template #content>
+                <form class="p-fluid p-m-3">
+                    <div class="p-field" :style="detailDescriptor.pField.style">
+                        <span class="p-float-label">
+                            <InputText
+                                id="label"
+                                class="kn-material-input"
+                                type="text"
+                                v-model.trim="v$.selectedFolder.code.$model"
+                                :class="{
+                                    'p-invalid': v$.selectedFolder.code.$invalid && v$.selectedFolder.code.$dirty
+                                }"
+                                maxLength="100"
+                                @blur="v$.selectedFolder.code.$touch()"
+                                @input="$emit('touched')"
+                                data-test="code-input"
+                            />
+                            <label for="label" class="kn-material-input-label"> {{ $t('common.label') }} * </label>
+                        </span>
+                        <KnValidationMessages
+                            :vComp="v$.selectedFolder.code"
+                            :additionalTranslateParams="{
+                                fieldName: $t('common.label')
+                            }"
+                        />
+                    </div>
+                    <div class="p-field" :style="detailDescriptor.pField.style">
+                        <span class="p-float-label">
+                            <InputText
+                                id="name"
+                                class="kn-material-input"
+                                type="text"
+                                v-model.trim="v$.selectedFolder.name.$model"
+                                :class="{
+                                    'p-invalid': v$.selectedFolder.name.$invalid && v$.selectedFolder.name.$dirty
+                                }"
+                                maxLength="255"
+                                @blur="v$.selectedFolder.name.$touch()"
+                                @input="$emit('touched')"
+                                data-test="name-input"
+                            />
+                            <label for="name" class="kn-material-input-label"> {{ $t('common.name') }} * </label>
+                        </span>
+                        <KnValidationMessages
+                            :vComp="v$.selectedFolder.name"
+                            :additionalTranslateParams="{
+                                fieldName: $t('common.name')
+                            }"
+                        />
+                    </div>
+                    <div class="p-field" :style="detailDescriptor.pField.style">
+                        <span class="p-float-label">
+                            <InputText id="description" class="kn-material-input" type="text" v-model.trim="selectedFolder.description" maxLength="255" @input="$emit('touched')" data-test="description-input" />
+                            <label for="description" class="kn-material-input-label">{{ $t('common.description') }}</label>
+                        </span>
+                    </div>
+                </form>
+            </template>
+        </Card>
+        <Card :style="detailDescriptor.card.style">
+            <template #content>
+                <DataTable v-if="!loading" :value="roles" dataKey="id" class="p-datatable-sm kn-table" responsiveLayout="scroll" data-test="roles-table">
+                    <Column field="name" :header="$t('managers.functionalitiesManagement.roles')" :sortable="true" />
+                    <Column :header="$t('managers.functionalitiesManagement.development')">
+                        <template #body="slotProps">
+                            <Checkbox v-model="slotProps.data.development" :binary="true" :disabled="!slotProps.data['devRoles'].checkable" />
+                        </template>
+                    </Column>
+                    <Column :header="$t('managers.functionalitiesManagement.test')">
+                        <template #body="slotProps">
+                            <Checkbox v-model="slotProps.data.test" :binary="true" :disabled="!slotProps.data['testRoles'].checkable" />
+                        </template>
+                    </Column>
+                    <Column :header="$t('managers.functionalitiesManagement.execution')">
+                        <template #body="slotProps">
+                            <Checkbox v-model="slotProps.data.execution" :binary="true" :disabled="!slotProps.data['execRoles'].checkable" />
+                        </template>
+                    </Column>
+                    <Column :header="$t('managers.functionalitiesManagement.creation')">
+                        <template #body="slotProps">
+                            <Checkbox v-model="slotProps.data.creation" :binary="true" :disabled="!slotProps.data['createRoles'].checkable" />
+                        </template>
+                    </Column>
+                    <Column @rowClick="false">
+                        <template #body="slotProps">
+                            <Button icon="pi pi-check" class="p-button-link" @click="checkAll(slotProps.data)" :disabled="slotProps.data.isButtonDisabled" :data-test="'check-all-' + slotProps.data.id" />
+                            <Button icon="pi pi-times" class="p-button-link" @click="uncheckAll(slotProps.data)" :disabled="slotProps.data.isButtonDisabled" :data-test="'uncheck-all-' + slotProps.data.id" />
+                        </template>
+                    </Column>
+                </DataTable>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { createValidations } from '@/helpers/commons/validationHelper'
+import axios from 'axios'
+import useValidate from '@vuelidate/core'
+import KnValidationMessages from '@/components/UI/KnValidatonMessages.vue'
+import detailDescriptor from './FunctionalitiesManagementDetailDescriptor.json'
+import validationDescriptor from './FunctionalitiesManagementValidation.json'
+import Card from 'primevue/card'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Checkbox from 'primevue/checkbox'
+
+export default defineComponent({
+    emits: ['touched', 'close', 'inserted'],
+    props: {
+        functionality: Object,
+        rolesShort: Array as any,
+        parentId: Number
+    },
+    components: {
+        Card,
+        DataTable,
+        Column,
+        Checkbox,
+        KnValidationMessages
+    },
+    data() {
+        return {
+            v$: useValidate() as any,
+            detailDescriptor,
+            validationDescriptor,
+            formVisible: false,
+            selectedFolder: {} as any,
+            parentFolder: null as any,
+            roles: [] as any,
+            checked: [] as any,
+            loading: false,
+            dirty: false
+        }
+    },
+    computed: {
+        buttonDisabled(): Boolean {
+            return this.v$.$invalid
+        }
+    },
+    validations() {
+        return {
+            selectedFolder: createValidations('selectedFolder', validationDescriptor.validations.selectedFolder)
+        }
+    },
+    async created() {
+        this.loading = true
+        this.selectedFolder = { ...this.functionality }
+        await this.loadParentFolder()
+        this.loadRoles()
+        this.loading = false
+    },
+    watch: {
+        async functionality() {
+            this.loading = true
+            this.v$.$reset()
+            this.selectedFolder = { ...this.functionality }
+            await this.loadParentFolder()
+            this.loadRoles()
+            this.loading = false
+        },
+        rolesShort() {
+            this.loadRoles()
+        }
+    },
+    methods: {
+        closeTemplate() {
+            this.$emit('close')
+        },
+        loadRoles() {
+            this.roles = []
+            const tempFolder = this.selectedFolder.id ? this.selectedFolder : this.parentFolder
+            this.rolesShort.forEach((role: any) => {
+                const tempRole = {
+                    id: role.id,
+                    name: role.name,
+                    development: false,
+                    test: false,
+                    execution: false,
+                    creation: false,
+                    isButtonDisabled: false
+                }
+                this.roleIsChecked(tempRole, tempFolder.devRoles, 'development')
+                this.roleIsChecked(tempRole, tempFolder.testRoles, 'test')
+                this.roleIsChecked(tempRole, tempFolder.execRoles, 'execution')
+                this.roleIsChecked(tempRole, tempFolder.createRoles, 'creation')
+
+                for (let field of ['devRoles', 'testRoles', 'execRoles', 'createRoles']) {
+                    this.isCheckable(tempRole, field)
+                }
+
+                if (tempRole['devRoles'].checkable == false && tempRole['testRoles'].checkable == false && tempRole['execRoles'].checkable == false && tempRole['createRoles'].checkable == false) tempRole.isButtonDisabled = true
+
+                this.roles.push(tempRole)
+            })
+        },
+        async loadParentFolder() {
+            await axios.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `2.0/functionalities/getParent/${this.parentId}`).then((response) => (this.parentFolder = response.data))
+        },
+        roleIsChecked(role: any, roles: [], roleField: string) {
+            if (roles) {
+                const index = roles.findIndex((currentRole: any) => role.id === currentRole.id)
+
+                if (index > -1) {
+                    role[roleField] = true
+                }
+            }
+        },
+        isCheckable(role: any, roleField: string) {
+            role[roleField] = { checkable: false }
+            if (this.parentFolder.path === '/Functionalities') {
+                role[roleField].checkable = true
+            } else if (this.parentFolder[roleField] && this.parentFolder[roleField].length > 0) {
+                this.parentFolder[roleField].forEach((currentRole) => {
+                    if (role.name === currentRole.name) {
+                        role[roleField].checkable = true
+                    }
+                })
+            }
+        },
+        prepareFunctionalityToSend(functionalityToSend) {
+            var roles = [...this.roles]
+            functionalityToSend.codeType = functionalityToSend.codType
+            delete functionalityToSend.codType
+            delete functionalityToSend.biObjects
+            this.emptyFunctionalityRoles(functionalityToSend)
+            roles.forEach((role) => {
+                if (role.development) functionalityToSend.devRoles.push(role)
+                if (role.test) functionalityToSend.testRoles.push(role)
+                if (role.execution) functionalityToSend.execRoles.push(role)
+                if (role.creation) functionalityToSend.createRoles.push(role)
+            })
+            if (!functionalityToSend.id) {
+                this.prepareNewFunctionality(functionalityToSend)
+            }
+        },
+        prepareNewFunctionality(functionalityToSend) {
+            functionalityToSend.codeType = this.parentFolder.codType
+            functionalityToSend.parentId = this.parentFolder.id
+            functionalityToSend.path = this.parentFolder.path + '/' + functionalityToSend.name
+            if (!functionalityToSend.description) functionalityToSend.description = ''
+        },
+        emptyFunctionalityRoles(functionality) {
+            functionality.devRoles = []
+            functionality.testRoles = []
+            functionality.execRoles = []
+            functionality.createRoles = []
+        },
+        async createOrUpdate(functionalityToSend) {
+            return this.selectedFolder.id ? axios.put(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `2.0/functionalities/${functionalityToSend.id}`, functionalityToSend) : axios.post(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/functionalities/', functionalityToSend)
+        },
+        async handleSubmit() {
+            if (this.v$.$invalid) {
+                return
+            }
+            let functionalityToSend = { ...this.selectedFolder }
+            this.prepareFunctionalityToSend(functionalityToSend)
+            await this.createOrUpdate(functionalityToSend).then((response) => {
+                if (response.data.errors) {
+                    this.$store.commit('setError', { title: 'Error', msg: response.data.error })
+                } else {
+                    this.$store.commit('setInfo', { title: this.$t('common.toast.success') })
+                    this.$emit('inserted', response.data.id)
+                }
+            })
+            this.dirty = false
+        },
+        checkSingleRole(role, roleField, checkboxField, value) {
+            if (role[roleField].checkable) {
+                role[checkboxField] = value
+            }
+        },
+        checkAll(role) {
+            this.checkSingleRole(role, 'createRoles', 'creation', true)
+            this.checkSingleRole(role, 'devRoles', 'development', true)
+            this.checkSingleRole(role, 'execRoles', 'execution', true)
+            this.checkSingleRole(role, 'testRoles', 'test', true)
+        },
+        uncheckAll(role) {
+            this.checkSingleRole(role, 'createRoles', 'creation', false)
+            this.checkSingleRole(role, 'devRoles', 'development', false)
+            this.checkSingleRole(role, 'execRoles', 'execution', false)
+            this.checkSingleRole(role, 'testRoles', 'test', false)
+        }
+    }
+})
+</script>
