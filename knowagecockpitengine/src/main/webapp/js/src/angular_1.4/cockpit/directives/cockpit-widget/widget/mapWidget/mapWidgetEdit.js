21a12a4725753b4ad0255dfeb5b4a1e1a7ae6566
Marco Balestri <marco.balestri@eng.it>
2021-07-21 17:36:18 +0200

    [KNOWAGE-6062] Merge "map-widget-legend" branch
    [KNOWAGE-6062] Hide/show legend
    
    
    Added support for legend range mode
    
    
    Added support for legend position
    
    
    [KNOWAGE-6062] Changed map legend layout

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/mapWidget/mapWidgetEdit.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/mapWidget/mapWidgetEdit.js
@@ -45,6 +45,9 @@ function mapWidgetEditControllerFunction(
 	$scope.visualizationTypes = [{"name":"markers","enabled":true,"class":"markers"},{"name":"clusters","enabled":true,"class":"clusters"},{"name":"heatmap","enabled":true,"class":"heatmap"},{"name":"choropleth","enabled":true,"class":"choropleth"}];
 	$scope.uploadImg = {};
 	$scope.widgetSpinner = false;
+	$scope.availableLegendAlignments = [{'label':$scope.translate.load('sbi.cockpit.style.textAlign.left'),'value':'left'},{'label':$scope.translate.load('sbi.cockpit.style.textAlign.center'),'value':'center'},{'label':$scope.translate.load('sbi.cockpit.style.textAlign.right'),'value':'right'}];
+	$scope.availableVisualizationTypes = ['Linear Gradient', 'Range'];
+
 	$scope.getTemplateUrl = function(template){
 		return cockpitModule_generalServices.getTemplateUrl('mapWidget',template);
 	}
