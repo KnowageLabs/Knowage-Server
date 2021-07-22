eb802785471c078855f4a38755ee77b207e07dc9
zpesic <87060611+zpesic@users.noreply.github.com>
2021-07-22 14:22:27 +0200

    Create AlertDetail.vue

---
+++ knowage-vue/src/modules/kpi/alert/AlertDetail.vue
@@ -0,0 +1,14 @@
+<template>
+    <Toolbar class="kn-toolbar kn-toolbar--secondary p-p-0 p-m-0">
+        <template #right>
+            <Button icon="pi pi-save" class="kn-button p-button-text p-button-rounded" />
+            <Button class="kn-button p-button-text p-button-rounded" icon="pi pi-times" />
+        </template>
+    </Toolbar>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'alert-details'
+})
+</script>
