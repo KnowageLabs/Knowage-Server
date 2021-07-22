54dec931a1b3f95e66c09bad0711db2213d5fd75
zpesic <87060611+zpesic@users.noreply.github.com>
2021-07-22 18:18:26 +0200

    Added card for name and listener

--- knowage-vue/src/modules/kpi/alert/AlertDetail.vue
+++ knowage-vue/src/modules/kpi/alert/AlertDetail.vue
@@ -5,14 +5,39 @@
             <Button icon="pi pi-times" class="kn-button p-button-text p-button-rounded" @click="closeTemplate" />
         </template>
     </Toolbar>
+    <div class="p-grid p-m-0 p-fluid p-jc-center">
+        <div class="p-col-9">
+            <Card>
+                <template #content>
+                    <form class="p-fluid p-m-5">
+                        <div class="p-d-flex p-jc-between">
+                            <div class="p-field">
+                                <span class="p-float-label">
+                                    <InputText id="name" class="kn-material-input" type="text" v-model="selectedAlert.name" />
+                                    <label for="name" class="kn-material-input-label">{{ $t('kpi.alert.name') }} * </label>
+                                </span>
+                            </div>
+                            <div class="p-field">
+                                <span class="p-float-label">
+                                    <Dropdown id="listener" class="kn-material-input" v-model="selectedAlert.alertListener" :options="listeners" optionLabel="name" />
+                                    <label for="category" class="kn-material-input-label"> {{ $t('kpi.alert.kpiListener') }} * </label>
+                                </span>
+                            </div>
+                        </div>
+                    </form>
+                </template>
+            </Card>
+        </div>
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { iAlert } from './Alert'
+import { iAlert, iListener } from './Alert'
+import Dropdown from 'primevue/dropdown'
 import axios from 'axios'
 export default defineComponent({
     name: 'alert-details',
-    components: {},
+    components: { Dropdown },
     props: {
         id: {
             type: String,
@@ -28,10 +53,13 @@ export default defineComponent({
         if (this.id) {
             this.loadAlert()
         }
+        this.loadListener()
     },
     data() {
         return {
-            selectedAlert: {} as iAlert
+            selectedAlert: {} as iAlert,
+            listeners: [] as iListener[],
+            selectedListener: {} as iListener
         }
     },
     methods: {
@@ -40,6 +68,14 @@ export default defineComponent({
                 .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/alert/' + this.id + '/load')
                 .then((response) => {
                     this.selectedAlert = response.data
+                })
+                .finally(() => console.log('selected', this.selectedAlert))
+        },
+        async loadListener() {
+            await axios
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/alert/listListener')
+                .then((response) => {
+                    this.listeners = response.data
                 })
                 .finally(() => console.log('selected', this.selectedAlert))
         },
