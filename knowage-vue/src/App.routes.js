a24346cb5df2fbb20f8fc71ea5bd98008c430f25
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2021-07-29 14:03:07 +0200

    Template pruning bugfix (#208)
    * Added alphanumericNoSpaces regex.
    
    * Change vue.config.js for proxy
    
    * Added kpi folder and routes file
    
    * Added author common translation
    
    * Added cloneConfirmTitle common translation
    
    * Added copyOf common translation
    
    * Added run common translation
    
    * Added cron en_US translations.
    
    * en_US fix
    
    * Added common translation
    
    * Translation fix
    
    * Apply translation added to en_US
    
    * Added cron component
    
    * Added 'clone' to common en_US
    
    * Updatet Cron for reactivity, added watcher
    
    * Removed logs
    
    * Bug fix
    
    Co-authored-by: Vojislav Stanojevic <vojislav.stanojevic@eng.it>

--- knowage-vue/src/App.routes.js
+++ knowage-vue/src/App.routes.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import IframeRenderer from '@/modules/commons/IframeRenderer.vue'
 import managersRoutes from '@/modules/managers/managers.routes.js'
 import importExportRoutes from '@/modules/importExport/ImportExport.routes.js'
+import kpiRoutes from '@/modules/kpi/kpi.routes.js'
 
 const baseRoutes = [
     {
@@ -51,7 +52,10 @@ const baseRoutes = [
     }
 ]
 
-const routes = baseRoutes.concat(managersRoutes).concat(importExportRoutes)
+const routes = baseRoutes
+    .concat(managersRoutes)
+    .concat(importExportRoutes)
+    .concat(kpiRoutes)
 
 const router = createRouter({
     base: process.env.VUE_APP_PUBLIC_PATH,
