0a1b2f30ce8493e11f78b62c99b91b7a8cdebd87
zpesic <87060611+zpesic@users.noreply.github.com>
2021-07-29 11:41:28 +0200

    added test

--- knowage-vue/src/modules/kpi/alert/AlertDetail.spec.js
+++ knowage-vue/src/modules/kpi/alert/AlertDetail.spec.js
@@ -1,11 +1,23 @@
 import { mount } from '@vue/test-utils'
 import AlertDetail from './AlertDetail.vue'
-
+import Toolbar from 'primevue/toolbar'
+import Button from 'primevue/button'
+import Card from 'primevue/card'
+import Dropdown from 'primevue/dropdown'
+import Menu from 'primevue/menu'
+import InputText from 'primevue/inputtext'
 const factory = () => {
     return mount(AlertDetail, {
         global: {
             plugins: [],
-            stubs: {},
+            stubs: {
+                Button,
+                Card,
+                Toolbar,
+                Dropdown,
+                Menu,
+                InputText
+            },
             mocks: {
                 $t: (msg) => msg
             }
@@ -20,4 +32,17 @@ describe('Alert Definition Detail', () => {
         expect(formWrapper.vm.buttonDisabled).toBe(true)
     })
     // it('disables the save button if no kpi is selected', () => {})
+})
+describe('Alert Definition kpi action', () => {
+    it('shows a dialog when kpi action is clicked', async () => {
+        // const wrapper = factory()
+        // expect(wrapper.vm.isListenerSelected).toBe(true)
+        // expect(wrapper.vm.actionList.length).toBe(1)
+        // console.log(wrapper.html())
+        // await wrapper.find('[data-test="add-action-button"]').trigger('click')
+        // expect(wrapper.vm.dialogVisiable).toBe(true)
+    })
+    it('shows a wysiwyg editor if send mail is selected', () => {})
+    it('shows a form if context broker is selected', () => {})
+    it('shows a selectable table if etl document is selected', () => {})
 })
