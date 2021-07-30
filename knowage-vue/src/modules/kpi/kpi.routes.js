05b48292b7fb93565d1be0735cc6346dfea47c95
zpesic <87060611+zpesic@users.noreply.github.com>
2021-07-30 14:35:04 +0200

    Kpi target dev (#206)
    * Added alphanumericNoSpaces regex.
    
    * Change vue.config.js for proxy
    
    * Added kpi folder and routes file
    
    * Added author common translation
    
    * Added cloneConfirmTitle common translation
    
    * Added copyOf common translation
    
    * initial commit
    
    * Implemented ListBox
    
    * Added run common translation
    
    * Target definition detail  added
    
    * TargetDefinitionDetailDescriptor added
    
    * Added dialog for adding kpi
    
    * changed way to access form
    
    * Added submit
    
    * Added .spec.js file
    
    * Added new tests
    
    * Added missing translation
    
    * Added cron en_US translations.
    
    * en_US fix
    
    * added TargedtDefinitionDetail.spec.js
    
    * remove tooltip
    
    * removed console.log
    
    * Dialog added
    
    * Added common translation
    
    * Translation fix
    
    * Target form
    
    * Implemented ApplyTargetCard
    
    * Fixing style issues, deleting KPI assosiation
    
    * Fixed remove kpi
    
    * removed console.log
    
    * Fix table column width
    
    * Added hint as route
    
    * Apply translation added to en_US
    
    * added missing translation
    
    * Removed unnecessary code
    
    * Fixed test for hint
    
    Co-authored-by: BojanSovticEngIT <Bojan.Sovtic@eng.it>
    Co-authored-by: Vojislav Stanojevic <vojislav.stanojevic@eng.it>
    Co-authored-by: Davide Vernassa <davide.vernassa@eng.it>

--- knowage-vue/src/modules/kpi/kpi.routes.js
+++ knowage-vue/src/modules/kpi/kpi.routes.js
@@ -1,7 +1,10 @@
 import measureDefinitionRoutes from '@/modules/kpi/measureDefinition/MeasureDefinition.routes.js'
+import targetDefinitionRoutes from '@/modules/kpi/targetDefinition/TargetDefinition.routes.js'
 
 const baseRoutes = []
 
-const routes = baseRoutes.concat(measureDefinitionRoutes)
+const routes = baseRoutes
+  .concat(measureDefinitionRoutes)
+  .concat(targetDefinitionRoutes)
 
 export default routes
