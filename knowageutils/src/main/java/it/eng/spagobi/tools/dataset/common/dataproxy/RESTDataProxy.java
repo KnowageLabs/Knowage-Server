ada32c4b8dd6201d5d68b2d3700752d13a9f42ef
Marco Libanori <marco.libanori@eng.it>
2021-08-10 09:47:58 +0200

    [KNOWAGE-6183] Fix management of nullable parameters in REST dataset

--- knowageutils/src/main/java/it/eng/spagobi/tools/dataset/common/dataproxy/RESTDataProxy.java
+++ knowageutils/src/main/java/it/eng/spagobi/tools/dataset/common/dataproxy/RESTDataProxy.java
@@ -23,6 +23,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.httpclient.HttpStatus;
@@ -266,7 +267,10 @@ public class RESTDataProxy extends AbstractDataProxy {
 			for (Entry<String, String> entry : entrySet) {
 				String key = entry.getKey();
 				String value = entry.getValue();
-				value = value.replaceAll("^'", "").replaceAll("'$", "");
+				value = Optional.ofNullable(value)
+						.orElse("" /* TODO "" or "null" ??? */)
+						.replaceAll("^'", "")
+						.replaceAll("'$", "");
 				String keyPlaceholderRegex = "\\$P\\{" + key  + "\\}";
 
 				unparametrizedAddress = unparametrizedAddress.replaceAll(keyPlaceholderRegex, /* TODO : needs some escapes? */ value);
