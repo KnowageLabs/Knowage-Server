64d59d0a24d7ef5d5a58b2c020a181c34f802ad1
zpesic <87060611+zpesic@users.noreply.github.com>
2021-08-31 10:19:41 +0200

    Added dialog descriptor

---
+++ knowage-vue/src/modules/managers/glossaryDefinition/dialogs/GlossaryDefinitionWordEdit.vue
@@ -0,0 +1,3 @@
+<template>
+    <Dialog :header="$t('managers.glossaryDefinition.word')" :breakpoints="targetDefinitionDetailDescriptor.dialog.breakpoints" :style="targetDefinitionDetailDescriptor.dialog.style" :visible="dialogVisible" :modal="true" :closable="false" class="p-fluid kn-dialog--toolbar--primary"> </Dialog>
+</template>
