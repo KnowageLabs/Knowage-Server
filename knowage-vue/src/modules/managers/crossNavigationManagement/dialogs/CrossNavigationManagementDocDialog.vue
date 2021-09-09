73063d8039422a333a40d8fccaa1bdd15c465222
zpesic <87060611+zpesic@users.noreply.github.com>
2021-09-09 10:03:10 +0200

    added filter and sort to doc table

--- knowage-vue/src/modules/managers/crossNavigationManagement/dialogs/CrossNavigationManagementDocDialog.vue
+++ knowage-vue/src/modules/managers/crossNavigationManagement/dialogs/CrossNavigationManagementDocDialog.vue
@@ -10,7 +10,30 @@
                 </template>
             </Toolbar>
         </template>
-        <DataTable :paginator="true" :rows="15" :rowsPerPageOptions="[10, 15, 20]" v-model:selection="selected" :value="documents.item" class="p-datatable-sm kn-table" dataKey="DOCUMENT_ID" responsiveLayout="stack" selectionMode="single" :loading="loading" @row-select="hadleSelect">
+        <DataTable
+            :paginator="true"
+            :rows="15"
+            :rowsPerPageOptions="[10, 15, 20]"
+            v-model:selection="selected"
+            :value="documents.item"
+            class="p-datatable-sm kn-table"
+            dataKey="DOCUMENT_ID"
+            responsiveLayout="stack"
+            selectionMode="single"
+            :loading="loading"
+            @row-select="hadleSelect"
+            v-model:filters="filters"
+            filterDisplay="menu"
+            :globalFilterFields="dialogDescriptor.globalFilterFields"
+        >
+            <template #header>
+                <div class="table-header">
+                    <span class="p-input-icon-left">
+                        <i class="pi pi-search" />
+                        <InputText class="kn-material-input" type="text" v-model="filters['global'].value" :placeholder="$t('common.search')" badge="0" data-test="search-input" />
+                    </span>
+                </div>
+            </template>
             <template #empty>
                 {{ $t('common.info.noDataFound') }}
             </template>
@@ -18,7 +41,7 @@
                 {{ $t('common.info.dataLoading') }}
             </template>
 
-            <Column v-for="col of dialogDescriptor.columnsDoc" :field="col.field" :header="$t(col.header)" :key="col.field" class="kn-truncated">
+            <Column v-for="col of dialogDescriptor.columnsDoc" :field="col.field" :header="$t(col.header)" :key="col.field" :sortable="true" class="kn-truncated">
                 <template #body="slotProps">
                     <span>{{ slotProps.data[slotProps.column.props.field] }}</span>
                 </template>
@@ -32,6 +55,8 @@ import axios from 'axios'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
 import Dialog from 'primevue/dialog'
+import { filterDefault } from '@/helpers/commons/filterHelper'
+import { FilterOperator } from 'primevue/api'
 import dialogDescriptor from './CrossNavigationManagementDialogDescriptor.json'
 export default defineComponent({
     name: 'doc-dialog',
@@ -56,13 +81,25 @@ export default defineComponent({
             dialogDescriptor,
             loading: false,
             selected: {} as any,
-            documents: [] as any
+            documents: [] as any,
+            filters: {
+                global: [filterDefault],
+                DOCUMENT_LABEL: {
+                    operator: FilterOperator.AND,
+                    constraints: [filterDefault]
+                },
+                DOCUMENT_NAME: {
+                    operator: FilterOperator.AND,
+                    constraints: [filterDefault]
+                }
+            } as Object
         }
     },
     watch: {
         async selectedDoc() {
             await this.loadAllDoc()
             this.selected = this.documents.item.find((doc) => doc.DOCUMENT_ID === this.selectedDoc)
+            console.log('watch')
         }
     },
     methods: {
