3cfc2419acbfafff0befd11d8bd5346baea46848
Alberto Nale <alberto.nale@eng.it>
2021-09-14 10:55:22 +0200

    [KNOWAGE-6066] - Fiexd behaviour considering session and default roles

--- knowage-vue/src/modules/mainMenu/dialogs/RoleDialog.vue
+++ knowage-vue/src/modules/mainMenu/dialogs/RoleDialog.vue
@@ -1,6 +1,6 @@
 <template>
 	<Dialog class="kn-dialog--toolbar--primary RoleDialog" v-bind:visible="visibility" footer="footer" :header="$t('role.roleSelection')" :closable="false" modal>
-		<Dropdown id="role" v-model="sessionRole" class="kn-material-input" @change="setDirty" :options="user.roles" :placeholder="$t('role.defaultRolePlaceholder')" />
+		<Dropdown v-model="user.sessionRole" class="kn-material-input" @change="setDirty" :options="[$t('role.defaultRolePlaceholder'), ...user.roles]" :placeholder="$t('role.defaultRolePlaceholder')" />
 		<template #footer>
 			<Button class="p-button-text kn-button" v-t="'common.close'" @click="closeDialog" />
 			<Button class="kn-button kn-button--primary" v-t="'common.save'" @click="changeRole" />
@@ -21,12 +21,6 @@
 			Dialog,
 			Dropdown
 		},
-		data() {
-			return {
-				sessionRole: ''
-			}
-		},
-		created() {},
 		props: {
 			visibility: Boolean
 		},
@@ -37,19 +31,18 @@
 					.reduce((p, c) => p + `&${c}=${encodeURIComponent(x[c])}`, '')
 					.substring(1)
 			},
-
 			changeRole() {
+				let role = this.user.sessionRole === this.$t('role.defaultRolePlaceholder') ? '' : this.user.sessionRole
 				let headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
-				let data = this.formUrlEncoded({ ACTION_NAME: 'SET_SESSION_ROLE_ACTION', SELECTED_ROLE: this.sessionRole })
+				let data = this.formUrlEncoded({ ACTION_NAME: 'SET_SESSION_ROLE_ACTION', SELECTED_ROLE: role })
 				let postUrl = '/knowage/servlet/AdapterHTTP'
 
-				axios
-					.post(postUrl, data, { headers: headers })
-					.then(() => {
-						this.closeDialog()
-						this.$router.go(0)
-					})
-					.catch((error) => console.error(error))
+				axios.post(postUrl, data, { headers: headers }).then(() => {
+					this.$store.commit('setUser', this.user)
+					localStorage.setItem('sessionRole', this.user.sessionRole)
+					this.closeDialog()
+					this.$router.go(0)
+				})
 			},
 			closeDialog() {
 				this.$emit('update:visibility', false)
@@ -67,6 +60,10 @@
 	.p-dialog {
 		.p-dropdown {
 			margin: 10px 0 0 0;
+
+			&.p-component.p-inputwrapper.p-inputwrapper-filled.kn-material-input {
+				width: 100%;
+			}
 		}
 	}
 	.RoleDialog #role {
