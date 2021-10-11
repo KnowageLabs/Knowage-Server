dd02bfd62debc400e2e1e03f9c72266be4f4577c
Davide Vernassa <davide.vernassa@eng.it>
2021-10-11 12:22:42 +0200

    Fixed axios interceptor instantiation

--- knowage-vue/src/main.ts
+++ knowage-vue/src/main.ts
@@ -4,10 +4,8 @@ import PrimeVue from 'primevue/config'
 import router from './App.routes.js'
 import store from './App.store.js'
 
-import axios from 'axios'
 import VueAxios from 'vue-axios'
 import interceptor from './axios.js'
-interceptor()
 
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/monokai.css'
@@ -48,7 +46,7 @@ import internationalizationPlugin from './plugins/internationalization.js'
 import i18n from '@/App.i18n'
 
 createApp(App)
-    .use(VueAxios, axios)
+    .use(VueAxios, interceptor)
     .use(store)
     .use(router)
     .use(i18n)
