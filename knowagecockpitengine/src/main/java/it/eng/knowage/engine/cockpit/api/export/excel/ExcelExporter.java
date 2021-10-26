c6b6617ca9fa749346aa5df123f2f815f6201eb1
Marco Balestri <marco.balestri@eng.it>
2021-10-26 18:53:12 +0200

    Fixed XLSX cockpit export
    This reverts commit c424370e6d7ac52caae6d83d95c1764cdc1b4757.
    
    Revert "Fixed precision with hidden columns in excel export"
    
    This reverts commit ca5ddddcaf5ae9bf975a2d0ea23f1c03f1f09cd5.
    
    Fixed XLSX cockpit export

--- knowagecockpitengine/src/main/java/it/eng/knowage/engine/cockpit/api/export/excel/ExcelExporter.java
+++ knowagecockpitengine/src/main/java/it/eng/knowage/engine/cockpit/api/export/excel/ExcelExporter.java
@@ -23,11 +23,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.UriBuilder;
 
@@ -45,7 +49,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import it.eng.knowage.engine.cockpit.api.export.AbstractExporter;
 import it.eng.qbe.serializer.SerializationException;
 import it.eng.spago.error.EMFAbstractError;
 import it.eng.spago.error.EMFUserError;
@@ -54,6 +57,8 @@ import it.eng.spagobi.commons.SingletonConfig;
 import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.commons.dao.DAOFactory;
 import it.eng.spagobi.tools.dataset.bo.IDataSet;
+import it.eng.spagobi.tools.dataset.bo.SolrDataSet;
+import it.eng.spagobi.tools.dataset.bo.VersionedDataSet;
 import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
 
 /**
@@ -61,15 +66,19 @@ import it.eng.spagobi.utilities.exceptions.SpagoBIRuntimeException;
  * @author Marco Balestri (marco.balestri@eng.it)
  */
 
-public class ExcelExporter extends AbstractExporter {
+public class ExcelExporter {
 
 	static private Logger logger = Logger.getLogger(ExcelExporter.class);
 
 	public static final String UNIQUE_ALIAS_PLACEHOLDER = "_$_";
 
+	private final String userUniqueIdentifier;
 	private final boolean isSingleWidgetExport;
+	private final JSONObject body;
+	private Locale locale;
 	private int uniqueId = 0;
 	private String requestURL = "";
+	private List<Integer> hiddenColumns;
 
 	private static final String[] WIDGETS_TO_IGNORE = { "image", "text", "selector", "selection", "html" };
 	private static final String SCRIPT_NAME = "cockpit-export-xls.js";
@@ -80,15 +89,17 @@ public class ExcelExporter extends AbstractExporter {
 
 	// used only for scheduled export
 	public ExcelExporter(String outputType, String userUniqueIdentifier, Map<String, String[]> parameterMap, String requestURL) {
-		super(userUniqueIdentifier, new JSONObject());
+		this.userUniqueIdentifier = userUniqueIdentifier;
 		this.isSingleWidgetExport = false;
 		this.requestURL = requestURL;
+		this.body = new JSONObject();
 	}
 
 	public ExcelExporter(String outputType, String userUniqueIdentifier, JSONObject body) {
-		super(userUniqueIdentifier, body);
+		this.userUniqueIdentifier = userUniqueIdentifier;
 		this.isSingleWidgetExport = body.optBoolean("exportWidget");
-		locale = getLocale(body);
+		this.body = body;
+		this.locale = getLocale(body);
 	}
 
 	private Locale getLocale(JSONObject body) {
@@ -284,6 +295,18 @@ public class ExcelExporter extends AbstractExporter {
 		}
 	}
 
+	private JSONObject getCockpitVariables() {
+		try {
+			if (body.get("COCKPIT_VARIABLES") instanceof JSONObject)
+				return body.getJSONObject("COCKPIT_VARIABLES");
+			else
+				return body.getJSONArray("COCKPIT_VARIABLES").getJSONObject(0);
+		} catch (JSONException e) {
+			logger.error("Cannot retrieve cockpit variables", e);
+			return new JSONObject();
+		}
+	}
+
 	private String getDatasetLabel(JSONObject template, int dsId) {
 		try {
 			JSONArray cockpitDatasets = template.getJSONObject("configuration").getJSONArray("datasets");
@@ -392,7 +415,6 @@ public class ExcelExporter extends AbstractExporter {
 		return getDataStoreForWidget(template, widget, 0, -1);
 	}
 
-	@Override
 	protected JSONObject getDataStoreForWidget(JSONObject template, JSONObject widget, int offset, int fetchSize) {
 		Map<String, Object> map = new java.util.HashMap<String, Object>();
 		JSONObject datastore = null;
@@ -426,6 +448,51 @@ public class ExcelExporter extends AbstractExporter {
 					e);
 		}
 		return datastore;
+	}
+
+	private JSONObject getCockpitSelectionsFromBody(JSONObject widget) {
+		JSONObject cockpitSelections = new JSONObject();
+		if (body == null || body.length() == 0)
+			return cockpitSelections;
+		try {
+			if (isSingleWidgetExport) { // export single widget
+				cockpitSelections = body.getJSONObject("COCKPIT_SELECTIONS");
+			} else { // export whole cockpit
+				JSONArray allWidgets = body.getJSONArray("widget");
+				int i;
+				for (i = 0; i < allWidgets.length(); i++) {
+					JSONObject curWidget = allWidgets.getJSONObject(i);
+					if (curWidget.getLong("id") == widget.getLong("id"))
+						break;
+				}
+				cockpitSelections = body.getJSONArray("COCKPIT_SELECTIONS").getJSONObject(i);
+			}
+			forceUniqueHeaders(cockpitSelections);
+		} catch (Exception e) {
+			logger.error("Cannot get cockpit selections", e);
+			return new JSONObject();
+		}
+		return cockpitSelections;
+	}
+
+	private void forceUniqueHeaders(JSONObject cockpitSelections) throws JSONException {
+		JSONObject aggregations = cockpitSelections.getJSONObject("aggregations");
+		JSONArray measures = aggregations.getJSONArray("measures");
+		manipulateDimensions(measures);
+		JSONArray categories = aggregations.getJSONArray("categories");
+		manipulateDimensions(categories);
+	}
+
+	private void manipulateDimensions(JSONArray dimensions) throws JSONException {
+		Set<String> dimensionsAliases = new HashSet<String>();
+		for (int i = 0; i < dimensions.length(); i++) {
+			JSONObject d = dimensions.getJSONObject(i);
+			String alias = d.getString("alias");
+			if (dimensionsAliases.contains(alias)) {
+				d.put("alias", alias + UNIQUE_ALIAS_PLACEHOLDER + i);
+			}
+			dimensionsAliases.add(alias);
+		}
 	}
 
 	private JSONObject getMultiCockpitSelectionsFromBody(JSONObject widget, int datasetId) {
@@ -670,6 +737,96 @@ public class ExcelExporter extends AbstractExporter {
 		}
 	}
 
+	private JSONObject[] getColumnsStyles(JSONArray columnsOrdered, JSONObject widgetContent) {
+		try {
+			JSONObject[] toReturn = new JSONObject[columnsOrdered.length() + 10];
+			JSONArray columns = widgetContent.getJSONArray("columnSelectedOfDataset");
+			for (int i = 0; i < columnsOrdered.length(); i++) {
+				JSONObject orderedCol = columnsOrdered.getJSONObject(i);
+				for (int j = 0; j < columns.length(); j++) {
+					JSONObject col = columns.getJSONObject(j);
+					if (orderedCol.getString("header").equals(col.getString("alias"))) {
+						if (col.has("style")) {
+							toReturn[i] = col.getJSONObject("style");
+						}
+					}
+				}
+			}
+			return toReturn;
+		} catch (Exception e) {
+			logger.error("Error while retrieving table columns styles.", e);
+			return new JSONObject[columnsOrdered.length() + 10];
+		}
+	}
+
+	private List<Integer> getHiddenColumnsList(JSONArray columns) {
+		List<Integer> hiddenColumns = new ArrayList<Integer>();
+		try {
+			for (int i = 0; i < columns.length(); i++) {
+				JSONObject column = columns.getJSONObject(i);
+				if (column.has("style")) {
+					JSONObject style = column.optJSONObject("style");
+					if (style.has("hiddenColumn")) {
+						if (style.getString("hiddenColumn").equals("true")) {
+							hiddenColumns.add(i);
+						}
+					}
+				}
+			}
+			return hiddenColumns;
+		} catch (Exception e) {
+			logger.error("Error while getting hidden columns list");
+			return new ArrayList<Integer>();
+		}
+	}
+
+	private JSONArray getTableOrderedColumns(JSONArray columnsNew, JSONArray columnsOld) {
+		JSONArray columnsOrdered = new JSONArray();
+		// new columns are in the correct order
+		// for each of them we have to find the correspondent old column and push it into columnsOrdered
+		try {
+			for (int i = 0; i < columnsNew.length(); i++) {
+
+				if (hiddenColumns.contains(i))
+					continue;
+
+				JSONObject columnNew = columnsNew.getJSONObject(i);
+				String newHeader = getTableColumnHeaderValue(columnNew);
+
+				for (int j = 0; j < columnsOld.length(); j++) {
+					JSONObject columnOld = columnsOld.getJSONObject(j);
+					if (columnOld.getString("header").equals(newHeader)) {
+						columnsOrdered.put(columnOld);
+						break;
+					}
+				}
+			}
+			return columnsOrdered;
+		} catch (Exception e) {
+			logger.error("Error retrieving ordered columns");
+			return new JSONArray();
+		}
+	}
+
+	private String getTableColumnHeaderValue(JSONObject column) {
+		String header = null;
+		try {
+			if (column.has("variables")) {
+				JSONArray variables = column.getJSONArray("variables");
+				for (int i = 0; i < variables.length(); i++) {
+					JSONObject variable = variables.getJSONObject(i);
+					if (variable.getString("action").equalsIgnoreCase("header"))
+						header = getCockpitVariables().getString(variable.getString("variable"));
+				}
+			} else
+				header = column.getString("aliasToShow");
+			return header;
+		} catch (Exception e) {
+			logger.error("Error retrieving table column header values.", e);
+			return "";
+		}
+	}
+
 	private Row createHeaderColumnNames(Sheet sheet, Map<String, String> mapGroupsAndColumns, JSONArray columnsOrdered, int startRowOffset) {
 		try {
 			Row header = null;
@@ -713,6 +870,49 @@ public class ExcelExporter extends AbstractExporter {
 		}
 	}
 
+	private HashMap<String, String> getMapFromGroupsArray(JSONArray groupsArray, JSONArray aggr) {
+		HashMap<String, String> returnMap = new HashMap<String, String>();
+		try {
+			if (aggr != null && groupsArray != null) {
+
+				for (int i = 0; i < groupsArray.length(); i++) {
+
+					String id = groupsArray.getJSONObject(i).getString("id");
+					String groupName = groupsArray.getJSONObject(i).getString("name");
+
+					for (int ii = 0; ii < aggr.length(); ii++) {
+						JSONObject column = aggr.getJSONObject(ii);
+
+						if (column.has("group") && column.getString("group").equals(id)) {
+							String nameToInsert = getTableColumnHeaderValue(column);
+							returnMap.put(nameToInsert, groupName);
+						}
+
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new SpagoBIRuntimeException("Couldn't create map from groups array", e);
+		}
+		return returnMap;
+
+	}
+
+	private JSONArray filterDataStoreColumns(JSONArray columns) {
+		try {
+			for (int i = 0; i < columns.length(); i++) {
+				String element = columns.getString(i);
+				if (element != null && element.equals("recNo")) {
+					columns.remove(i);
+					break;
+				}
+			}
+		} catch (JSONException e) {
+			logger.error("Can not filter Columns Array");
+		}
+		return columns;
+	}
+
 	public static String[] toStringArray(JSONArray array) {
 		if (array == null)
 			return null;
@@ -728,4 +928,249 @@ public class ExcelExporter extends AbstractExporter {
 		// if pagination is disabled offset = 0, fetchSize = -1
 		return getDatastore(datasetLabel, map, selections, 0, -1);
 	}
+
+	private JSONObject getDatastore(String datasetLabel, Map<String, Object> map, String selections, int offset, int fetchSize) {
+		ExcelExporterClient client = new ExcelExporterClient();
+		try {
+			JSONObject datastore = client.getDataStore(map, datasetLabel, userUniqueIdentifier, selections, offset, fetchSize);
+			return datastore;
+		} catch (Exception e) {
+			String message = "Unable to get data";
+			logger.error(message, e);
+			throw new SpagoBIRuntimeException(message);
+		}
+	}
+
+	private boolean isSolrDataset(IDataSet dataSet) {
+		if (dataSet instanceof VersionedDataSet) {
+			dataSet = ((VersionedDataSet) dataSet).getWrappedDataset();
+		}
+		return dataSet instanceof SolrDataSet;
+	}
+
+	private JSONArray getSummaryRowFromWidget(JSONObject widget) {
+		try {
+			JSONObject settings = widget.optJSONObject("settings");
+			JSONArray jsonArrayForSummary = new JSONArray();
+			if (settings != null) {
+				JSONObject summary = settings.optJSONObject("summary");
+				if (settings.has("summary") && summary.has("enabled") && summary.optBoolean("enabled")) {
+
+					if (summary.has("list")) {
+						JSONArray listArray = summary.getJSONArray("list");
+
+						if (listArray.length() > 1) {
+							for (int jj = 0; jj < listArray.length(); jj++) {
+
+								JSONObject aggrObj = listArray.getJSONObject(jj);
+
+								if (!aggrObj.has("aggregation")) {
+
+									JSONArray measures = new JSONArray();
+									JSONObject content = widget.optJSONObject("content");
+									if (content != null) {
+										JSONArray columns = content.optJSONArray("columnSelectedOfDataset");
+										if (columns != null) {
+											for (int i = 0; i < columns.length(); i++) {
+												JSONObject column = columns.getJSONObject(i);
+												if ("MEASURE".equalsIgnoreCase(column.getString("fieldType"))) {
+													JSONObject measure = new JSONObject();
+													measure.put("id", column.getString("alias"));
+													measure.put("alias", column.getString("aliasToShow"));
+
+													String formula = column.optString("formula");
+													String name = formula.isEmpty() ? column.optString("name") : formula;
+													if (column.has("formula")) {
+														measure.put("formula", name);
+													} else
+														measure.put("columnName", name);
+
+													measure.put("funct", column.getString("funcSummary"));
+
+													boolean hidden = false;
+
+													if (column.has("style")) {
+
+														JSONObject style = column.optJSONObject("style");
+														if (style != null) {
+
+															String hideSummary = style.optString("hideSummary");
+
+															if (hideSummary != null && !hideSummary.isEmpty() && hideSummary.equalsIgnoreCase("true")) {
+																hidden = true;
+															}
+
+														}
+
+													}
+													if (!hidden)
+														measures.put(measure);
+												}
+											}
+										}
+									}
+									JSONObject summaryRow = new JSONObject();
+									summaryRow.put("measures", measures);
+
+									JSONObject dataset = widget.optJSONObject("dataset");
+									if (dataset != null) {
+										int dsId = dataset.getInt("dsId");
+										summaryRow.put("dataset", dsId);
+									}
+
+									jsonArrayForSummary.put(summaryRow);
+
+								} else {
+
+									JSONArray measures = new JSONArray();
+									JSONObject content = widget.optJSONObject("content");
+									if (content != null) {
+										JSONArray columns = content.optJSONArray("columnSelectedOfDataset");
+										if (columns != null) {
+											for (int i = 0; i < columns.length(); i++) {
+												JSONObject column = columns.getJSONObject(i);
+												if ("MEASURE".equalsIgnoreCase(column.getString("fieldType"))) {
+													JSONObject measure = new JSONObject();
+													measure.put("id", column.getString("alias"));
+													measure.put("alias", column.getString("aliasToShow"));
+
+													String formula = column.optString("formula");
+													String name = formula.isEmpty() ? column.optString("name") : formula;
+													if (column.has("formula")) {
+														measure.put("formula", name);
+													} else
+														measure.put("columnName", name);
+
+													measure.put("funct", aggrObj.get("aggregation"));
+
+													boolean hidden = false;
+
+													if (column.has("style")) {
+
+														JSONObject style = column.optJSONObject("style");
+														if (style != null) {
+
+															String hideSummary = style.optString("hideSummary");
+
+															if (hideSummary != null && !hideSummary.isEmpty() && hideSummary.equalsIgnoreCase("true")) {
+																hidden = true;
+															}
+
+														}
+
+													}
+													if (!hidden)
+														measures.put(measure);
+												}
+											}
+										}
+									}
+									JSONObject summaryRow = new JSONObject();
+									summaryRow.put("measures", measures);
+
+									JSONObject dataset = widget.optJSONObject("dataset");
+									if (dataset != null) {
+										int dsId = dataset.getInt("dsId");
+										summaryRow.put("dataset", dsId);
+									}
+
+									jsonArrayForSummary.put(summaryRow);
+
+								}
+
+							}
+						} else {
+							JSONArray measures = new JSONArray();
+							JSONObject content = widget.optJSONObject("content");
+							if (content != null) {
+								JSONArray columns = content.optJSONArray("columnSelectedOfDataset");
+								if (columns != null) {
+									for (int i = 0; i < columns.length(); i++) {
+										JSONObject column = columns.getJSONObject(i);
+										if ("MEASURE".equalsIgnoreCase(column.getString("fieldType"))) {
+											JSONObject measure = new JSONObject();
+											measure.put("id", column.getString("alias"));
+											measure.put("alias", column.getString("aliasToShow"));
+
+											String formula = column.optString("formula");
+											String name = formula.isEmpty() ? column.optString("name") : formula;
+											if (column.has("formula")) {
+												measure.put("formula", name);
+											} else
+												measure.put("columnName", name);
+
+											measure.put("funct", column.getString("funcSummary"));
+
+											boolean hidden = false;
+
+											if (column.has("style")) {
+
+												JSONObject style = column.optJSONObject("style");
+												if (style != null) {
+
+													String hideSummary = style.optString("hideSummary");
+
+													if (hideSummary != null && !hideSummary.isEmpty() && hideSummary.equalsIgnoreCase("true")) {
+														hidden = true;
+													}
+
+												}
+
+											}
+											if (!hidden)
+												measures.put(measure);
+										}
+									}
+								}
+							}
+							JSONObject summaryRow = new JSONObject();
+							summaryRow.put("measures", measures);
+
+							JSONObject dataset = widget.optJSONObject("dataset");
+							if (dataset != null) {
+								int dsId = dataset.getInt("dsId");
+								summaryRow.put("dataset", dsId);
+							}
+
+							jsonArrayForSummary.put(summaryRow);
+						}
+					}
+					return jsonArrayForSummary;
+				}
+			}
+			return null;
+		} catch (Exception e) {
+			throw new SpagoBIRuntimeException(e);
+		}
+	}
+
+	private boolean getRealtimeFromWidget(int dsId, JSONObject configuration) {
+		try {
+			JSONObject dataset = getDataset(dsId, configuration);
+			return !dataset.optBoolean("useCache");
+		} catch (Exception e) {
+			throw new SpagoBIRuntimeException(e);
+		}
+	}
+
+	private JSONObject getDataset(int dsId, JSONObject configuration) {
+		try {
+			JSONArray datasets = configuration.getJSONArray("datasets");
+			for (int i = 0; i < datasets.length(); i++) {
+				JSONObject dataset = (JSONObject) datasets.get(i);
+				int id = dataset.getInt("dsId");
+				if (id == dsId) {
+					return dataset;
+				}
+			}
+			return null;
+		} catch (Exception e) {
+			throw new SpagoBIRuntimeException(e);
+		}
+	}
+
+	protected Locale getLocale() {
+		return locale;
+	}
+
 }
