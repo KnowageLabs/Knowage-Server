98beff6c6d9ab6e001de38821a05790a992db555
Alberto Nale <alberto.nale@eng.it>
2021-10-27 15:05:43 +0200

    Added setup and basic structure for overlays

--- knowage-vue/src/App.routes.js
+++ knowage-vue/src/App.routes.js
@@ -2,61 +2,65 @@ import { createRouter, createWebHistory } from 'vue-router'
 import IframeRenderer from '@/modules/commons/IframeRenderer.vue'
 import managersRoutes from '@/modules/managers/managers.routes.js'
 import importExportRoutes from '@/modules/importExport/ImportExport.routes.js'
+import overlayRoutes from '@/overlay/Overlay.routes.js'
 
 const baseRoutes = [
-	{
-		path: '/',
-		name: 'home',
-		component: () => import('@/views/Home.vue')
-	},
-	{
-		path: '/about',
-		name: 'about',
-		component: () => import('@/views/About.vue')
-	},
-	{
-		path: '/knowage/servlet/:catchAll(.*)',
-		name: 'knowageUrl',
-		component: IframeRenderer,
-		props: (route) => ({ url: route.fullPath })
-	},
-	{
-		path: '/knowage/restful-services/publish:catchAll(.*)',
-		component: IframeRenderer,
-		props: (route) => ({ url: route.fullPath })
-	},
-	{
-		path: '/knowage/restful-services/signup:catchAll(.*)',
-		component: IframeRenderer,
-		props: (route) => ({ url: route.fullPath })
-	},
-	{
-		path: '/knowage/restful-services/2.0/installconfig',
-		component: IframeRenderer,
-		props: (route) => ({ url: route.fullPath })
-	},
-	{
-		path: '/knowage/themes:catchAll(.*)',
-		component: IframeRenderer,
-		props: (route) => ({ url: route.fullPath })
-	},
-	{
-		path: '/login',
-		name: 'login',
-		redirect: process.env.VUE_APP_HOST_URL + '/knowage/servlet/AdapterHTTP?ACTION_NAME=LOGOUT_ACTION&LIGHT_NAVIGATOR_DISABLED=TRUE&NEW_SESSION=TRUE'
-	},
-	{
-		path: '/:catchAll(.*)',
-		component: () => import('@/modules/commons/404.vue')
-	}
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('@/views/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('@/views/About.vue')
+    },
+    {
+        path: '/knowage/servlet/:catchAll(.*)',
+        name: 'knowageUrl',
+        component: IframeRenderer,
+        props: (route) => ({ url: route.fullPath })
+    },
+    {
+        path: '/knowage/restful-services/publish:catchAll(.*)',
+        component: IframeRenderer,
+        props: (route) => ({ url: route.fullPath })
+    },
+    {
+        path: '/knowage/restful-services/signup:catchAll(.*)',
+        component: IframeRenderer,
+        props: (route) => ({ url: route.fullPath })
+    },
+    {
+        path: '/knowage/restful-services/2.0/installconfig',
+        component: IframeRenderer,
+        props: (route) => ({ url: route.fullPath })
+    },
+    {
+        path: '/knowage/themes:catchAll(.*)',
+        component: IframeRenderer,
+        props: (route) => ({ url: route.fullPath })
+    },
+    {
+        path: '/login',
+        name: 'login',
+        redirect: process.env.VUE_APP_HOST_URL + '/knowage/servlet/AdapterHTTP?ACTION_NAME=LOGOUT_ACTION&LIGHT_NAVIGATOR_DISABLED=TRUE&NEW_SESSION=TRUE'
+    },
+    {
+        path: '/:catchAll(.*)',
+        component: () => import('@/modules/commons/404.vue')
+    }
 ]
 
-const routes = baseRoutes.concat(managersRoutes).concat(importExportRoutes)
+const routes = baseRoutes
+    .concat(managersRoutes)
+    .concat(importExportRoutes)
+    .concat(overlayRoutes)
 
 const router = createRouter({
-	base: process.env.VUE_APP_PUBLIC_PATH,
-	history: createWebHistory(process.env.VUE_APP_PUBLIC_PATH),
-	routes
+    base: process.env.VUE_APP_PUBLIC_PATH,
+    history: createWebHistory(process.env.VUE_APP_PUBLIC_PATH),
+    routes
 })
 
 /* router.beforeEach((to, from, next) => {
---
+++ knowage-vue/src/overlay/Overlay.routes.js
@@ -0,0 +1,7 @@
+import genericFunctionalityRoutes from '@/overlay/GenericFunctionality/GenericFunctionality.routes.js'
+
+const baseRoutes = []
+
+baseRoutes.concat(genericFunctionalityRoutes)
+
+export default baseRoutes
