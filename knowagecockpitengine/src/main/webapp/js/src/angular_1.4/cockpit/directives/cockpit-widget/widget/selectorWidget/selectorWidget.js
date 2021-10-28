e8fd06a6a3130f081962dd917ec87484b9cfc158
Davide Vernassa <davide.vernassa@gmail.com>
2021-10-28 15:50:02 +0200

    [KNOWAGE-6365] - Removed other widget styles from gridwidth getter.

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/selectorWidget/selectorWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/selectorWidget/selectorWidget.js
@@ -170,7 +170,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		}
 
 		$scope.gridWidth = function() {
-			var tempStyle = $scope.ngModel.style ? $scope.ngModel.style : {};
+			var tempStyle = {};
 			if($scope.ngModel.settings.modalityView == 'grid' && $scope.ngModel.settings.gridColumnsWidth){
 				tempStyle.width = $scope.ngModel.settings.gridColumnsWidth;
 			}else{
