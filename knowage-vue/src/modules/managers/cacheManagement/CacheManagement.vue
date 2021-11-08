f9a7497259e42505b1133a6965720d607ac6633a
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2021-11-08 21:44:15 +0100

    Cache management bugfix (#282)
    * Added alphanumericNoSpaces regex.
    
    * Change vue.config.js for proxy
    
    * Added kpi folder and routes file
    
    * Added author common translation
    
    * Added cloneConfirmTitle common translation
    
    * Added copyOf common translation
    
    * Added run common translation
    
    * Added cron en_US translations.
    
    * en_US fix
    
    * Added common translation
    
    * Translation fix
    
    * Apply translation added to en_US
    
    * Added cron component
    
    * Added 'clone' to common en_US
    
    * Updatet Cron for reactivity, added watcher
    
    * Removed logs
    
    * added helper for codemirror mathematica modified
    
    * Renamed Cron.vue to KpiCron.vue
    
    * Added js-base64 dependency.
    
    * Added maxLength message to common.validations
    
    * Fixed kpiCron bug
    
    * Glossary common translation en_US
    
    * Added active/inactive common translation en_US
    
    * Added Document Execution folder
    
    * added catch to error, displays toast now
    
    * Revert "added catch to error, displays toast now"
    
    This reverts commit caa91373c8453e0f33958f3cc03956071d52fd46.
    
    * KNOWAGE-6400 Bugfix
    
    Co-authored-by: Vojislav Stanojevic <vojislav.stanojevic@eng.it>
    Co-authored-by: dbulatovicx32 <darko.bulatovic@eng.it>

--- knowage-vue/src/modules/managers/cacheManagement/CacheManagement.vue
+++ knowage-vue/src/modules/managers/cacheManagement/CacheManagement.vue
@@ -8,7 +8,7 @@
         <ProgressBar mode="indeterminate" class="kn-progress-bar" v-if="showProgressBar" data-test="progress-bar" />
         <div class="p-d-flex p-flex-wrap kn-page-content">
             <div class="p-col-4 p-sm-12 p-md-4 p-p-0">
-                <RuntimeInformationCard v-if="selectedDatasource" :item="cache" :chartData="chartData"></RuntimeInformationCard>
+                <RuntimeInformationCard v-if="selectedDatasource" :item="cache" :chartData="chartData" @refresh="loadCache"></RuntimeInformationCard>
             </div>
             <div class="p-col-8 p-sm-12 p-md-8 p-p-0">
                 <GeneralSettingsCard v-if="settingsPendingCount == 0" :item="settings" :datasources="datasources" :selectedDatasource="selectedDatasource" @inserted="pageReload"></GeneralSettingsCard>
@@ -21,132 +21,132 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue'
-    import { iCache, iMeta, iSettings } from './CacheManagement'
-    import { AxiosResponse } from 'axios'
-    import DatasetTableCard from './cards/DatasetTableCard/DatasetTableCard.vue'
-    import GeneralSettingsCard from './cards/GeneralSettingsCard/GeneralSettingsCard.vue'
-    import RuntimeInformationCard from './cards/RuntimeInformationCard/RuntimeInformationCard.vue'
+import { defineComponent } from 'vue'
+import { iCache, iMeta, iSettings } from './CacheManagement'
+import { AxiosResponse } from 'axios'
+import DatasetTableCard from './cards/DatasetTableCard/DatasetTableCard.vue'
+import GeneralSettingsCard from './cards/GeneralSettingsCard/GeneralSettingsCard.vue'
+import RuntimeInformationCard from './cards/RuntimeInformationCard/RuntimeInformationCard.vue'
 
-    export default defineComponent({
-        name: 'cache-management',
-        components: {
-            DatasetTableCard,
-            GeneralSettingsCard,
-            RuntimeInformationCard
-        },
-        data() {
-            return {
-                cache: {} as iCache,
-                loading: false,
-                datasetMetadataLoading: true,
-                settingsPendingCount: 10,
-                chartData: [] as any,
-                datasetMetadataList: [] as iMeta[],
-                settings: {} as iSettings,
-                selectedDatasource: null as any,
-                datasources: [] as any
-            }
-        },
-        computed: {
-            showProgressBar(): boolean {
-                return this.loading || this.settingsPendingCount != 0
-            }
-        },
-        async created() {
-            this.loadPage()
-        },
-        methods: {
-            loadCache() {
-                this.$http.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/cacheee').then((response: AxiosResponse<any>) => {
-                    this.cache = response.data
-                    this.chartData = [this.cache.availableMemoryPercentage, 100 - this.cache.availableMemoryPercentage]
-                })
-            },
-            loadSettings() {
-                this.settings = {} as iSettings
-                this.settingsPendingCount = 10
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.NAMEPREFIX')
-                    .then((response: AxiosResponse<any>) => (this.settings.prefixForCacheTablesName = response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.LIMIT_FOR_CLEAN')
-                    .then((response: AxiosResponse<any>) => (this.settings.limitForClean = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.SCHEDULING_FULL_CLEAN')
-                    .then((response: AxiosResponse<any>) => (this.settings.schedulingFullClean = { label: response.data.valueCheck, value: response.data.valueCheck }))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.DS_LAST_ACCESS_TTL')
-                    .then((response: AxiosResponse<any>) => (this.settings.lastAccessTtl = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.CREATE_AND_PERSIST_TABLE.TIMEOUT')
-                    .then((response: AxiosResponse<any>) => (this.settings.createAndPersistTimeout = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.SPACE_AVAILABLE')
-                    .then((response: AxiosResponse<any>) => (this.settings.spaceAvailable = +response.data.valueCheck / 1048576))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.LIMIT_FOR_STORE')
-                    .then((response: AxiosResponse<any>) => (this.settings.cacheLimitForStore = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.WORKMANAGER.SQLDBCACHE.TIMEOUT')
-                    .then((response: AxiosResponse<any>) => (this.settings.sqldbCacheTimeout = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.HAZELCAST.TIMEOUT')
-                    .then((response: AxiosResponse<any>) => (this.settings.hazelcastTimeout = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.HAZELCAST.LEASETIME')
-                    .then((response: AxiosResponse<any>) => (this.settings.hazelcastLeaseTime = +response.data.valueCheck))
-                    .finally(() => this.settingsPendingCount--)
-            },
-            async loadDataSources() {
-                this.loading = true
-                await this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/datasources/?type=cache')
-                    .then((response: AxiosResponse<any>) => {
-                        this.datasources = []
-                        response.data.map((datasource: any) => {
-                            if (datasource.readOnly === false) {
-                                this.datasources.push(datasource)
-                            }
-                            if (datasource.writeDefault === true) {
-                                this.selectedDatasource = datasource
-                            }
-                        })
-                    })
-                    .finally(() => (this.loading = false))
-
-                if (this.selectedDatasource === null) {
-                    this.$store.commit('setError', {
-                        title: this.$t('managers.cacheManagement.noDefaultDatasetTitle'),
-                        msg: this.$t('managers.cacheManagement.noDefaultDataset')
-                    })
-                }
-            },
-            loadDatasetsMetadata() {
-                this.datasetMetadataLoading = true
-                this.$http
-                    .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/cacheee/meta')
-                    .then((response: AxiosResponse<any>) => (this.datasetMetadataList = response.data))
-                    .finally(() => (this.datasetMetadataLoading = false))
-            },
-            async loadPage() {
-                await this.loadDataSources()
-                this.loadCache()
-                this.loadSettings()
-                this.loadDatasetsMetadata()
-            },
-            pageReload() {
-                this.loadPage()
-            }
+export default defineComponent({
+    name: 'cache-management',
+    components: {
+        DatasetTableCard,
+        GeneralSettingsCard,
+        RuntimeInformationCard
+    },
+    data() {
+        return {
+            cache: {} as iCache,
+            loading: false,
+            datasetMetadataLoading: true,
+            settingsPendingCount: 10,
+            chartData: [] as any,
+            datasetMetadataList: [] as iMeta[],
+            settings: {} as iSettings,
+            selectedDatasource: null as any,
+            datasources: [] as any
         }
-    })
+    },
+    computed: {
+        showProgressBar(): boolean {
+            return this.loading || this.settingsPendingCount != 0
+        }
+    },
+    async created() {
+        this.loadPage()
+    },
+    methods: {
+        loadCache() {
+            this.$http.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/cacheee').then((response: AxiosResponse<any>) => {
+                this.cache = response.data
+                this.chartData = [this.cache.availableMemoryPercentage, 100 - this.cache.availableMemoryPercentage]
+            })
+        },
+        loadSettings() {
+            this.settings = {} as iSettings
+            this.settingsPendingCount = 10
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.NAMEPREFIX')
+                .then((response: AxiosResponse<any>) => (this.settings.prefixForCacheTablesName = response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.LIMIT_FOR_CLEAN')
+                .then((response: AxiosResponse<any>) => (this.settings.limitForClean = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.SCHEDULING_FULL_CLEAN')
+                .then((response: AxiosResponse<any>) => (this.settings.schedulingFullClean = { label: response.data.valueCheck, value: response.data.valueCheck }))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.DS_LAST_ACCESS_TTL')
+                .then((response: AxiosResponse<any>) => (this.settings.lastAccessTtl = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.CREATE_AND_PERSIST_TABLE.TIMEOUT')
+                .then((response: AxiosResponse<any>) => (this.settings.createAndPersistTimeout = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.SPACE_AVAILABLE')
+                .then((response: AxiosResponse<any>) => (this.settings.spaceAvailable = +response.data.valueCheck / 1048576))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.LIMIT_FOR_STORE')
+                .then((response: AxiosResponse<any>) => (this.settings.cacheLimitForStore = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.WORKMANAGER.SQLDBCACHE.TIMEOUT')
+                .then((response: AxiosResponse<any>) => (this.settings.sqldbCacheTimeout = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.HAZELCAST.TIMEOUT')
+                .then((response: AxiosResponse<any>) => (this.settings.hazelcastTimeout = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/configs/label/SPAGOBI.CACHE.HAZELCAST.LEASETIME')
+                .then((response: AxiosResponse<any>) => (this.settings.hazelcastLeaseTime = +response.data.valueCheck))
+                .finally(() => this.settingsPendingCount--)
+        },
+        async loadDataSources() {
+            this.loading = true
+            await this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '2.0/datasources/?type=cache')
+                .then((response: AxiosResponse<any>) => {
+                    this.datasources = []
+                    response.data.map((datasource: any) => {
+                        if (datasource.readOnly === false) {
+                            this.datasources.push(datasource)
+                        }
+                        if (datasource.writeDefault === true) {
+                            this.selectedDatasource = datasource
+                        }
+                    })
+                })
+                .finally(() => (this.loading = false))
+
+            if (this.selectedDatasource === null) {
+                this.$store.commit('setError', {
+                    title: this.$t('managers.cacheManagement.noDefaultDatasetTitle'),
+                    msg: this.$t('managers.cacheManagement.noDefaultDataset')
+                })
+            }
+        },
+        loadDatasetsMetadata() {
+            this.datasetMetadataLoading = true
+            this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + '1.0/cacheee/meta')
+                .then((response: AxiosResponse<any>) => (this.datasetMetadataList = response.data))
+                .finally(() => (this.datasetMetadataLoading = false))
+        },
+        async loadPage() {
+            await this.loadDataSources()
+            this.loadCache()
+            this.loadSettings()
+            this.loadDatasetsMetadata()
+        },
+        pageReload() {
+            this.loadPage()
+        }
+    }
+})
 </script>
