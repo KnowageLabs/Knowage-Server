b1ab5586e2d0e276fa9617e3ee67d9db341fc727
Matteo Massarotto <matteo.massarotto@eng.it>
2021-11-22 17:43:14 +0100

    Added cockpitModule_userPalette in order to customize chart conf palette

--- knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/chartWidget/chartWidget.js
+++ knowagecockpitengine/src/main/webapp/js/src/angular_1.4/cockpit/directives/cockpit-widget/widget/chartWidget/chartWidget.js
@@ -107,7 +107,8 @@ function cockpitChartWidgetControllerFunction(
 		cockpitModule_properties,
 		cockpitModule_template,
 		sbiModule_util,
-		$mdDialog){
+		$mdDialog,
+		$mdColorPalette){
 	$scope.property={style:{}};
 	$scope.selectedTab = {'tab' : 0};
 	$scope.cockpitModule_widgetSelection = cockpitModule_widgetSelection;
@@ -1505,7 +1506,7 @@ function setAggregationsOnChartEngine(wconf,sbiModule_util){
 				obj['orderType'] = chartCategory.drillOrder && chartCategory.drillOrder[chartCategory.column] ? chartCategory.drillOrder[chartCategory.column].orderType : chartCategory.orderType ;
 				obj['orderColumn'] =  chartCategory.drillOrder && chartCategory.drillOrder[chartCategory.column] ? chartCategory.drillOrder[chartCategory.column].orderColumn : chartCategory.orderColumn ;
 				obj['fieldType'] = "ATTRIBUTE";
-				if(wconf.columnSelectedOfDatasetAggregations[index].boundFunction){
+				if(wconf.columnSelectedOfDatasetAggregations && wconf.columnSelectedOfDatasetAggregations[index] && wconf.columnSelectedOfDatasetAggregations[index].boundFunction){
 					obj.boundFunction = wconf.columnSelectedOfDatasetAggregations[index].boundFunction;
 				}
 				aggregations.push(obj);
