f4ff9efc9f987b405b393c74458080140efd1dc2
dbulatovicx32 <darko.bulatovic@eng.it>
2021-12-08 11:25:18 +0100

    added start layout,translation,interface file

--- knowage-vue/src/modules/managers/businessModelCatalogue/metaweb/Metaweb.vue
+++ knowage-vue/src/modules/managers/businessModelCatalogue/metaweb/Metaweb.vue
@@ -1,28 +1,26 @@
 <template>
-    <Dialog class="document-details-dialog remove-padding p-fluid kn-dialog--toolbar--primary" :contentStyle="mainDescriptor.style.flex" :visible="visible" :modal="false" :closable="false" position="right" :baseZIndex="1" :autoZIndex="true">
+    <Dialog class="metaweb-dialog remove-padding p-fluid kn-dialog--toolbar--primary" :contentStyle="mainDescriptor.style.flex" :visible="visible" :modal="false" :closable="false" position="right" :baseZIndex="1" :autoZIndex="true">
         <template #header>
             <Toolbar class="kn-toolbar kn-toolbar--primary p-p-0 p-m-0 p-col-12">
-                <template #left>
-                    {{ $t('documentExecution.documentDetails.title') }}
-                </template>
+                <template #left> {{ $t('metaweb.title') }} : NAME HERE </template>
                 <template #right>
                     <Button icon="pi pi-save" class="p-button-text p-button-rounded p-button-plain" v-tooltip.bottom="$t('common.save')" />
                     <Button icon="pi pi-times" class="p-button-text p-button-rounded p-button-plain" v-tooltip.bottom="$t('common.close')" @click="$emit('closeMetaweb')" />
                 </template>
             </Toolbar>
         </template>
-        <div class="document-details-tab-container p-d-flex p-flex-column" :style="mainDescriptor.style.flexOne">
+        <div class="metaweb-tab-container p-d-flex p-flex-column" :style="mainDescriptor.style.flexOne">
             <ProgressBar v-if="loading" class="kn-progress-bar" mode="indeterminate" data-test="progress-bar" />
-            <TabView v-if="!loading" class="document-details-tabview" :style="mainDescriptor.style.flex">
+            <TabView v-if="!loading" class="metaweb-tabview" :style="mainDescriptor.style.flex">
                 <TabPanel>
                     <template #header>
-                        <span>{{ $t('documentExecution.documentDetails.info.infoTitle') }}</span>
+                        <span>{{ $t('metaweb.businessModel.title') }}</span>
                     </template>
                     test1
                 </TabPanel>
                 <TabPanel>
                     <template #header>
-                        <span>{{ $t('documentExecution.documentDetails.drivers.title') }}</span>
+                        <span>{{ $t('metaweb.physicalModel.title') }}</span>
                     </template>
                 </TabPanel>
             </TabView>
@@ -37,7 +35,7 @@ import Dialog from 'primevue/dialog'
 import TabView from 'primevue/tabview'
 import TabPanel from 'primevue/tabpanel'
 export default defineComponent({
-    name: 'document-details',
+    name: 'metaweb',
     components: { TabView, TabPanel, Dialog },
     props: { visible: { type: Boolean } },
     emits: ['closeMetaweb'],
@@ -56,10 +54,10 @@ export default defineComponent({
 .right-border {
     border-right: 1px solid #ccc;
 }
-.document-details-tabview .p-tabview-panels {
+.metaweb-tabview .p-tabview-panels {
     padding: 0 !important;
 }
-.document-details-dialog.p-dialog {
+.metaweb-dialog.p-dialog {
     max-height: 100%;
     height: 100vh;
     width: calc(100vw - #{$mainmenu-width});
@@ -70,13 +68,10 @@ export default defineComponent({
     padding: 0;
     margin: 0;
 }
-.document-details-tab-container .p-tabview .p-tabview-panel,
-.document-details-tab-container .p-tabview .p-tabview-panels {
+.metaweb-tab-container .p-tabview .p-tabview-panel,
+.metaweb-tab-container .p-tabview .p-tabview-panels {
     display: flex;
     flex-direction: column;
     flex: 1;
-}
-.details-warning-color {
-    color: red;
 }
 </style>
