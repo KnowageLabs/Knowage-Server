e029f331fb1a220137327e8b461ef8c1ba9ce30f
BojanSovticEngIT <Bojan.Sovtic@eng.it>
2021-12-08 16:57:45 +0100

    Physical Model

---
+++ knowage-vue/src/modules/managers/businessModelCatalogue/metaweb/physicalModel/MetawebPhysicalModel.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="p-grid p-m-0 kn-flex">
+        <div class="p-col-4 p-sm-4 p-md-3 p-p-0 p-d-flex p-flex-column kn-flex">
+            <Toolbar class="kn-toolbar kn-toolbar--primary">
+                <template #left> {{ $t('metaweb.physicalModel.tables') }}</template>
+                <template #right>
+                    <Button class="p-button-text p-button-rounded p-button-plain p-button-sm">{{ $t('metaweb.physicalModel.updatePhysicalModel') }}</Button>
+                </template>
+            </Toolbar>
+        </div>
+
+        <div class="p-col-8 p-sm-8 p-md-9 p-p-0 p-m-0 p-d-flex p-flex-column">
+            <Toolbar class="kn-toolbar kn-toolbar--secondary">
+                <template #left>
+                    test
+                </template>
+            </Toolbar>
+            <div class="metaweb-tab-container p-d-flex p-flex-column kn-flex">
+                <TabView class="metaweb-tabview p-d-flex p-flex-column kn-flex" scrollable>
+                    <TabPanel>
+                        <template #header>
+                            <span>{{ $t('metaweb.physicalModel.propertyList') }}</span>
+                        </template>
+                    </TabPanel>
+                    <TabPanel>
+                        <template #header>
+                            <span>{{ $t('metaweb.physicalModel.foreignKey') }}</span>
+                        </template>
+                    </TabPanel>
+                </TabView>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TabView from 'primevue/tabview'
+import TabPanel from 'primevue/tabpanel'
+
+export default defineComponent({
+    name: 'metaweb-physical-model',
+    components: { TabView, TabPanel },
+    data() {
+        return {}
+    },
+    async created() {},
+    methods: {}
+})
+</script>
