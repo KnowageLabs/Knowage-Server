51b8e7a36626fc4d7b07b9487b980f1fdae5bda9
dbulatovicx32 <darko.bulatovic@eng.it>
2021-12-08 12:32:45 +0100

    fixed layout

--- knowage-vue/src/modules/managers/businessModelCatalogue/metaweb/businessModel/MetawebBusinessModel.vue
+++ knowage-vue/src/modules/managers/businessModelCatalogue/metaweb/businessModel/MetawebBusinessModel.vue
@@ -22,10 +22,25 @@
                     <Button label="Delete" class="p-button-text p-button-rounded p-button-plain" :style="mainDescriptor.style.white" />
                 </template>
             </Toolbar>
-            <div class="kn-flex kn-relative">
-                <div :style="mainDescriptor.style.absoluteScroll">
-                    {{ mainDescriptor.test }}
-                </div>
+            <div class="metaweb-tab-container p-d-flex p-flex-column kn-flex">
+                <TabView class="metaweb-tabview p-d-flex p-flex-column kn-flex">
+                    <TabPanel>
+                        <template #header>
+                            <span>{{ $t('metaweb.businessModel.title') }}</span>
+                        </template>
+                        <div :style="mainDescriptor.style.absoluteScroll">
+                            {{ mainDescriptor.test }}
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <template #header>
+                            <span>{{ $t('metaweb.physicalModel.title') }}</span>
+                        </template>
+                        <div :style="mainDescriptor.style.absoluteScroll">
+                            {{ mainDescriptor.test }}
+                        </div>
+                    </TabPanel>
+                </TabView>
             </div>
         </div>
     </div>
@@ -35,10 +50,12 @@
 import { defineComponent } from 'vue'
 import mainDescriptor from '../MetawebDescriptor.json'
 import KnFabButton from '@/components/UI/KnFabButton.vue'
+import TabView from 'primevue/tabview'
+import TabPanel from 'primevue/tabpanel'
 
 export default defineComponent({
     name: 'metaweb-business-model',
-    components: { KnFabButton },
+    components: { KnFabButton, TabView, TabPanel },
     props: {},
     computed: {},
     data() {
