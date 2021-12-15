d6bfe4af84c512548fb89751d9dd1a5f042bfa66
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2021-12-15 11:39:14 +0100

    Initial commit

---
+++ knowage-vue/src/modules/documentExecution/olap/Olap.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>IT WORKS</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'olap',
+    components: {},
+    data() {
+        return {}
+    },
+    async created() {},
+    methods: {}
+})
+</script>
