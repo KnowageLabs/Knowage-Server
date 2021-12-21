e8ba4f9570715fdc97ab00b8d5b8369f91e93205
dbulatovicx32 <darko.bulatovic@eng.it>
2021-12-21 09:58:18 +0100

    moving branches

---
+++ knowage-vue/src/modules/documentExecution/olap/filterPanel/OlapFilterPanel.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="filterpanel" class="p-d-flex "></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import panelDescriptor from './OlapFilterPanelDescriptor.json'
+
+export default defineComponent({
+    components: {},
+    props: {},
+    emits: [],
+    data() {
+        return {
+            panelDescriptor
+        }
+    },
+    created() {},
+    methods: {}
+})
+</script>
