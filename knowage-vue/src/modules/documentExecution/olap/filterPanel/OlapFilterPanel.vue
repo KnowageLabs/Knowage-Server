4141f4189059243231c325d1eda4fab3ae80c457
dbulatovicx32 <darko.bulatovic@eng.it>
2021-12-21 12:52:41 +0100

    olap filter panel initial

--- knowage-vue/src/modules/documentExecution/olap/filterPanel/OlapFilterPanel.vue
+++ knowage-vue/src/modules/documentExecution/olap/filterPanel/OlapFilterPanel.vue
@@ -1,21 +1,41 @@
 <template>
-    <div id="filterpanel" class="p-d-flex "></div>
+    <div id="filterPanel" class="p-d-flex filterPanel">
+        <div v-if="filterCardList.length == 0" class="p-d-flex p-flex-row kn-flex p-jc-center">
+            <InlineMessage class="kn-flex p-m-1" :style="panelDescriptor.style.noFilters" severity="info" closable="false">{{ $t('documentExecution.olap.filterPanel.filterPanelEmpty') }}</InlineMessage>
+        </div>
+        <FilterCard :filterCardList="filterCardList" />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { iOlapFilter } from '@/modules/documentExecution/olap/Olap'
 import panelDescriptor from './OlapFilterPanelDescriptor.json'
+import InlineMessage from 'primevue/inlinemessage'
+import FilterCard from './OlapFilterCard.vue'
 
 export default defineComponent({
-    components: {},
-    props: {},
+    components: { InlineMessage, FilterCard },
+    props: { olapProp: { type: Object, required: true } },
     emits: [],
     data() {
         return {
-            panelDescriptor
+            panelDescriptor,
+            filterCardList: [] as iOlapFilter[]
         }
     },
-    created() {},
-    methods: {}
+    created() {
+        this.loadData()
+    },
+    methods: {
+        loadData() {
+            this.filterCardList = this.olapProp.filters as iOlapFilter[]
+        }
+    }
 })
 </script>
+<style lang="scss" scoped>
+#filterPanel {
+    min-height: 45px;
+}
+</style>
