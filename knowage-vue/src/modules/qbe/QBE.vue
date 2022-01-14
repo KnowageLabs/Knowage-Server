efefb3bf2935f14674448f66d42064f8c958da9c
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-01-14 16:38:49 +0100

    Initial commit

---
+++ knowage-vue/src/modules/qbe/QBE.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>QBE WORKS</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'qbe',
+    components: {},
+    data() {
+        return {}
+    },
+    created() {},
+    methods: {}
+})
+</script>
