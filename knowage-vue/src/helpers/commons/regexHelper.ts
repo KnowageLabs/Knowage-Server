6d2186914fa57d2b63287d8e1162e8fc5427044a
vstanojevic <84330422+vstanojevic@users.noreply.github.com>
2022-01-25 15:47:54 +0100

    [KNOWAGE-6562]
    * Added alphanumericNoSpaces regex.
    
    * Change vue.config.js for proxy
    
    * Added kpi folder and routes file
    
    * Added author common translation
    
    * Added cloneConfirmTitle common translation
    
    * Added copyOf common translation
    
    * Added run common translation
    
    * Added cron en_US translations.
    
    * en_US fix
    
    * Added common translation
    
    * Translation fix
    
    * Apply translation added to en_US
    
    * Added cron component
    
    * Added 'clone' to common en_US
    
    * Updatet Cron for reactivity, added watcher
    
    * Removed logs
    
    * added helper for codemirror mathematica modified
    
    * Renamed Cron.vue to KpiCron.vue
    
    * Added js-base64 dependency.
    
    * Added maxLength message to common.validations
    
    * Fixed bugs - 6138, 6139
    
    * Fixed kpiCron bug
    
    * Glossary common translation en_US
    
    * Added active/inactive common translation en_US
    
    * Added Document Execution folder
    
    * added catch to error, displays toast now
    
    * Revert "added catch to error, displays toast now"
    
    This reverts commit caa91373c8453e0f33958f3cc03956071d52fd46.
    
    * KNOWAGE-6304 Bugfix
    
    * Bugfixes: 6412, 6421, 6424, 6426, 6428
    
    * Revert "Bugfixes: 6412, 6421, 6424, 6426, 6428"
    
    This reverts commit ee08e2847b6ede71002af78daa2a74424e9c604a.
    
    * scheduling date fix for dataset
    
    * Revert "scheduling date fix for dataset"
    
    This reverts commit 64a7c7c82529e1bdd4faf0212539c96293a97f4c.
    
    * Fix Regex issue
    
    Co-authored-by: BojanSovticEngIT <Bojan.Sovtic@eng.it>
    Co-authored-by: dbulatovicx32 <darko.bulatovic@eng.it>

--- knowage-vue/src/helpers/commons/regexHelper.ts
+++ knowage-vue/src/helpers/commons/regexHelper.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-useless-escape
-export const extendedAlphanumeric = /^([\pL\w\s\-\_\(\)\[\]\;\:\!\?\,\.\'\"\x2F\x5F%])*$/;
+export const extendedAlphanumeric = /^([\p{L}\w\s\-\\_\\;\\(\\)\\[\]\\:\\!\\?\\,\\.\\'\\"\\x2F\\x5F%])*$/u
 
 // eslint-disable-next-line no-useless-escape
-export const alphanumericNoSpaces = /^([a-zA-Z0-9\-\_])*$/;
+export const alphanumericNoSpaces = /^([a-zA-Z0-9\-\_])*$/
