28ae7e84925094920a68431b5a5ce34f0c99a27a
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-01-25 11:20:04 +0100

    Changes

---
+++ knowage-vue/src/modules/qbe/qbeDialogs/qbeAdvancedFilterDialog/QBENode.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>IT WORKS</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'qbe-node',
+    components: {},
+    data() {
+        return {}
+    },
+    async created() {},
+    methods: {}
+})
+</script>
