5239949c4c3a51faf0d1f269f8753013a6368f7f
vstanojevic <84330422+vstanojevic@users.noreply.github.com>
2022-01-25 17:58:58 +0100

    [KNOWAGE-6552]
    * Added alphanumericNoSpaces regex.
    
    * Change vue.config.js for proxy
    
    * Added kpi folder and routes file
    
    * Added author common translation
    
    * Added cloneConfirmTitle common translation
    
    * Added copyOf common translation
    
    * Added run common translation
    
    * Added cron en_US translations.
    
    * en_US fix
    
    * Added common translation
    
    * Translation fix
    
    * Apply translation added to en_US
    
    * Added cron component
    
    * Added 'clone' to common en_US
    
    * Updatet Cron for reactivity, added watcher
    
    * Removed logs
    
    * added helper for codemirror mathematica modified
    
    * Renamed Cron.vue to KpiCron.vue
    
    * Added js-base64 dependency.
    
    * FIXING BUGS 6130, 6143, 6141, 6058
    
    * Add Translation for error missingDefaultRole
    
    * Added maxLength message to common.validations
    
    * BugFix, added regex validator, roles selection
    
    * FIx multiple value format - simple, complex
    
    * Fixed kpiCron bug
    
    * Glossary common translation en_US
    
    * Added active/inactive common translation en_US
    
    * Resolvin bugfixes   6194, 6195, 6198
    
    * BugFix for issues 6122
    
    * Added Document Execution folder
    
    * Bug fix
    
    * Default role dropdown adjusted
    
    * Fix issues 6122, 6218
    
    * added catch to error, displays toast now
    
    * Revert "added catch to error, displays toast now"
    
    This reverts commit caa91373c8453e0f33958f3cc03956071d52fd46.
    
    * Bugfixes: 6412, 6421, 6424, 6426, 6428
    
    * Revert "Bugfixes: 6412, 6421, 6424, 6426, 6428"
    
    This reverts commit ee08e2847b6ede71002af78daa2a74424e9c604a.
    
    * Fix KNOWAGE-6307 and KNOWAGE-6176
    
    * scheduling date fix for dataset
    
    * Revert "scheduling date fix for dataset"
    
    This reverts commit 64a7c7c82529e1bdd4faf0212539c96293a97f4c.
    
    * Fix KNOWAGE-6552
    
    Co-authored-by: BojanSovticEngIT <Bojan.Sovtic@eng.it>
    Co-authored-by: dbulatovicx32 <darko.bulatovic@eng.it>

--- knowage-vue/src/helpers/commons/regexHelper.ts
+++ knowage-vue/src/helpers/commons/regexHelper.ts
@@ -2,4 +2,7 @@
 export const extendedAlphanumeric = /^([\p{L}\w\s\-\\_\\;\\(\\)\\[\]\\:\\!\\?\\,\\.\\'\\"\\x2F\\x5F%])*$/u
 
 // eslint-disable-next-line no-useless-escape
+export const fullnameRegex = /^[\p{L}\w]+([\p{L}\w\s\-\\,\\.\\'\\x2F])*$/u
+
+// eslint-disable-next-line no-useless-escape
 export const alphanumericNoSpaces = /^([a-zA-Z0-9\-\_])*$/
