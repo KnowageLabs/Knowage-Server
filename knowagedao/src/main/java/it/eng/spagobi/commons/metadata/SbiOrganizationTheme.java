a2ce56dfc5ae4342741d2cd79b9caea0d205f7ed
Alberto Nale <alberto.nale@eng.it>
2022-02-08 15:54:09 +0100

    [KNOWAGE-6646] - Added commonInfo fields

--- knowagedao/src/main/java/it/eng/spagobi/commons/metadata/SbiOrganizationTheme.java
+++ knowagedao/src/main/java/it/eng/spagobi/commons/metadata/SbiOrganizationTheme.java
@@ -21,6 +21,8 @@
  */
 package it.eng.spagobi.commons.metadata;
 
+import java.util.Date;
+
 /**
  * @author albnale
  *
@@ -35,6 +37,16 @@ public class SbiOrganizationTheme extends SbiHibernateModel {
 	private SbiOrganizationThemeId id;
 	private String config;
 	private boolean active;
+	private String userIn;
+	private String userUp;
+	private String userDe;
+	private Date timeIn;
+	private Date timeUp;
+	private Date timeDe;
+	private String sbiVersionIn;
+	private String sbiVersionUp;
+	private String sbiVersionDe;
+	private String metaVersion;
 
 	public SbiOrganizationTheme() {
 
@@ -42,15 +54,35 @@ public class SbiOrganizationTheme extends SbiHibernateModel {
 
 	/**
 	 * @param id
-	 * @param themeName
 	 * @param config
 	 * @param active
+	 * @param userIn
+	 * @param userUp
+	 * @param userDe
+	 * @param timeIn
+	 * @param timeUp
+	 * @param timeDe
+	 * @param sbiVersionIn
+	 * @param sbiVersionUp
+	 * @param sbiVersionDe
+	 * @param metaVersion
 	 */
-	public SbiOrganizationTheme(SbiOrganizationThemeId id, String config, boolean active) {
+	public SbiOrganizationTheme(SbiOrganizationThemeId id, String config, boolean active, String userIn, String userUp, String userDe, Date timeIn, Date timeUp,
+			Date timeDe, String sbiVersionIn, String sbiVersionUp, String sbiVersionDe, String metaVersion) {
 		super();
 		this.id = id;
 		this.config = config;
 		this.active = active;
+		this.userIn = userIn;
+		this.userUp = userUp;
+		this.userDe = userDe;
+		this.timeIn = timeIn;
+		this.timeUp = timeUp;
+		this.timeDe = timeDe;
+		this.sbiVersionIn = sbiVersionIn;
+		this.sbiVersionUp = sbiVersionUp;
+		this.sbiVersionDe = sbiVersionDe;
+		this.metaVersion = metaVersion;
 	}
 
 	/**
@@ -93,6 +125,86 @@ public class SbiOrganizationTheme extends SbiHibernateModel {
 	 */
 	public void setActive(boolean active) {
 		this.active = active;
+	}
+
+	public String getUserIn() {
+		return userIn;
+	}
+
+	public void setUserIn(String userIn) {
+		this.userIn = userIn;
+	}
+
+	public String getUserUp() {
+		return userUp;
+	}
+
+	public void setUserUp(String userUp) {
+		this.userUp = userUp;
+	}
+
+	public String getUserDe() {
+		return userDe;
+	}
+
+	public void setUserDe(String userDe) {
+		this.userDe = userDe;
+	}
+
+	public Date getTimeIn() {
+		return timeIn;
+	}
+
+	public void setTimeIn(Date timeIn) {
+		this.timeIn = timeIn;
+	}
+
+	public Date getTimeUp() {
+		return timeUp;
+	}
+
+	public void setTimeUp(Date timeUp) {
+		this.timeUp = timeUp;
+	}
+
+	public Date getTimeDe() {
+		return timeDe;
+	}
+
+	public void setTimeDe(Date timeDe) {
+		this.timeDe = timeDe;
+	}
+
+	public String getSbiVersionIn() {
+		return sbiVersionIn;
+	}
+
+	public void setSbiVersionIn(String sbiVersionIn) {
+		this.sbiVersionIn = sbiVersionIn;
+	}
+
+	public String getSbiVersionUp() {
+		return sbiVersionUp;
+	}
+
+	public void setSbiVersionUp(String sbiVersionUp) {
+		this.sbiVersionUp = sbiVersionUp;
+	}
+
+	public String getSbiVersionDe() {
+		return sbiVersionDe;
+	}
+
+	public void setSbiVersionDe(String sbiVersionDe) {
+		this.sbiVersionDe = sbiVersionDe;
+	}
+
+	public String getMetaVersion() {
+		return metaVersion;
+	}
+
+	public void setMetaVersion(String metaVersion) {
+		this.metaVersion = metaVersion;
 	}
 
 }
