a4dd7baa149029c9c724b3c8664066d9fd6b73d4
Alberto Nale <alberto.nale@eng.it>
2022-02-09 10:40:58 +0100

    Revert "[KNOWAGE-6622] - Prevented reset when coming from cross navigation"
    This reverts commit 1e36e9811bbb6cf2e1aea2f9c0c4e08fe9f4d616.

--- knowage/src/main/webapp/js/src/angular_1.4/tools/documentexecution/utils/documentExecutionServices.js
+++ knowage/src/main/webapp/js/src/angular_1.4/tools/documentexecution/utils/documentExecutionServices.js
@@ -254,7 +254,7 @@
 			// memorize parameters in session
 			docExecute_sessionParameterService.saveParameters(dataPost.parameters, parametersDetail);
 			console.log('params', parametersDetail);	
-			if(!execProperties.executionInstance.isFromCross) dataPost.parameters = {};
+			dataPost.parameters = {};
 			sbiModule_restServices.alterContextPath( sbiModule_config.contextName);
 			console.log('dataPost', dataPost);
 			var postObject = {
