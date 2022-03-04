6df794542e5fb597787c7ab84b8afdf6965063f5
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-03-04 16:39:43 +0100

    Change

--- knowage-vue/src/modules/documentBrowser/DocumentBrowserCockpitContainer.vue
+++ knowage-vue/src/modules/documentBrowser/DocumentBrowserCockpitContainer.vue
@@ -24,9 +24,7 @@ export default defineComponent({
             const uniqueID = user.userUniqueIdentifier
             const country = user.locale.split('_')[1]
 
-            this.url =
-                process.env.VUE_APP_HOST_URL +
-                `/knowagecockpitengine/api/1.0/pages/edit?NEW_SESSION=TRUE&SBI_LANGUAGE=${language}&user_id=${uniqueID}&SBI_COUNTRY=${country}&SBI_EXECUTION_ID=095e7ac39bc211ec90095bb127e68384&SBI_ENVIRONMENT=DOCBROWSER&IS_TECHNICAL_USER=true&documentMode=EDIT&FUNCTIONALITY_ID=${this.functionalityId}`
+            this.url = process.env.VUE_APP_HOST_URL + `/knowagecockpitengine/api/1.0/pages/edit?NEW_SESSION=TRUE&SBI_LANGUAGE=${language}&user_id=${uniqueID}&SBI_COUNTRY=${country}&SBI_ENVIRONMENT=DOCBROWSER&IS_TECHNICAL_USER=true&documentMode=EDIT&FUNCTIONALITY_ID=${this.functionalityId}`
 
             console.log('functionalityId: ', this.functionalityId)
             console.log('NEW URL: ', this.url)
