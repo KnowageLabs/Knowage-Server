93f7887b17a6c8e621414ba8e5ade394e7a1768b
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-03-11 10:43:38 +0100

    Bugfix - CR KNOWAGE-6704

--- knowage-vue/src/modules/documentExecution/main/dialogs/documentExecutionNotesDialog/DocumentExecutionNotesDialog.vue
+++ knowage-vue/src/modules/documentExecution/main/dialogs/documentExecutionNotesDialog/DocumentExecutionNotesDialog.vue
@@ -5,6 +5,9 @@
                 <template #start>
                     {{ $t('common.notes') }}
                 </template>
+                <template #end>
+                    <FabButton icon="fas fa-plus" @click="createNewNote" />
+                </template>
             </Toolbar>
         </template>
         <ProgressBar mode="indeterminate" class="kn-progress-bar" v-if="loading" />
@@ -17,20 +20,20 @@
 
                 <DocumentExecutionNotesForm :selectedNote="selectedNote"></DocumentExecutionNotesForm>
             </TabPanel>
-            <!-- TODO: Notes list will be implemented when backend services are fixed. -->
-            <!-- <TabPanel>
+
+            <TabPanel>
                 <template #header>
                     <span>{{ $t('common.notesList') }}</span>
                 </template>
 
                 <DocumentExecutionNotesList :propNotes="notes" :document="document" @editNote="onEditNote" @deleteNote="onDeleteNote"></DocumentExecutionNotesList>
-            </TabPanel> -->
+            </TabPanel>
         </TabView>
 
         <template #footer>
             <div class="p-d-flex p-flex-row">
-                <Button v-if="activeTab === 1" class="kn-button kn-button--primary" @click="exportNotes('pdf')"> {{ $t('documentExecution.main.exportInPDF') }}</Button>
-                <Button v-if="activeTab === 1" class="kn-button kn-button--primary" @click="exportNotes('rtf')"> {{ $t('documentExecution.main.exportInRTF') }}</Button>
+                <Button v-if="activeTab === 1" class="kn-button kn-button--primary" @click="exportNotes('pdf')" :disabled="exportButtonDisabled"> {{ $t('documentExecution.main.exportInPDF') }}</Button>
+                <Button v-if="activeTab === 1" class="kn-button kn-button--primary" @click="exportNotes('rtf')" :disabled="exportButtonDisabled"> {{ $t('documentExecution.main.exportInRTF') }}</Button>
 
                 <Button class="kn-button kn-button--primary p-ml-auto" @click="closeDialog"> {{ $t('common.close') }}</Button>
                 <Button v-if="activeTab === 0" class="kn-button kn-button--primary" @click="saveNote" :disabled="saveButtonDisabled"> {{ $t('common.save') }}</Button>
@@ -40,146 +43,147 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue'
-    import { AxiosResponse } from 'axios'
-    import { downloadDirect } from '@/helpers/commons/fileHelper'
-    import { iNote } from '../../DocumentExecution'
-    import Dialog from 'primevue/dialog'
-    import documentExecutionNotesDialogDescriptor from './DocumentExecutionNotesDialogDescriptor.json'
-    import DocumentExecutionNotesForm from './DocumentExecutionNotesForm.vue'
-    // TODO: Notes list will be implemented when backend services are fixed.
-    // import DocumentExecutionNotesList from './DocumentExecutionNotesList.vue'
-    import TabView from 'primevue/tabview'
-    import TabPanel from 'primevue/tabpanel'
+import { defineComponent } from 'vue'
+import { AxiosResponse } from 'axios'
+import { downloadDirect } from '@/helpers/commons/fileHelper'
+import { iNote } from '../../DocumentExecution'
+import Dialog from 'primevue/dialog'
+import documentExecutionNotesDialogDescriptor from './DocumentExecutionNotesDialogDescriptor.json'
+import DocumentExecutionNotesForm from './DocumentExecutionNotesForm.vue'
+import DocumentExecutionNotesList from './DocumentExecutionNotesList.vue'
+import FabButton from '@/components/UI/KnFabButton.vue'
+import TabView from 'primevue/tabview'
+import TabPanel from 'primevue/tabpanel'
 
-    export default defineComponent({
-        name: 'document-execution-notes-dialog',
-        components: { Dialog, DocumentExecutionNotesForm, TabView, TabPanel },
-        props: { visible: { type: Boolean }, propDocument: { type: Object } },
-        emits: ['close'],
-        data() {
-            return {
-                documentExecutionNotesDialogDescriptor,
-                document: null as any,
-                notes: [] as iNote[],
-                selectedNote: {} as iNote,
-                activeTab: 0,
-                loading: false
-            }
-        },
-        watch: {
-            async visible() {
-                if (this.visible) {
-                    await this.loadDocument()
-                }
-            },
-            async propDocument() {
+export default defineComponent({
+    name: 'document-execution-notes-dialog',
+    components: { Dialog, DocumentExecutionNotesForm, DocumentExecutionNotesList, FabButton, TabView, TabPanel },
+    props: { visible: { type: Boolean }, propDocument: { type: Object } },
+    emits: ['close'],
+    data() {
+        return {
+            documentExecutionNotesDialogDescriptor,
+            document: null as any,
+            notes: [] as iNote[],
+            selectedNote: {} as iNote,
+            activeTab: 0,
+            loading: false
+        }
+    },
+    watch: {
+        async visible() {
+            if (this.visible) {
                 await this.loadDocument()
             }
         },
-        computed: {
-            saveButtonDisabled(): boolean {
-                return !this.selectedNote.type
-            }
-        },
-        async created() {
+        async propDocument() {
             await this.loadDocument()
-        },
-        methods: {
-            async loadDocument() {
-                this.document = this.propDocument
-
-                if (this.document && this.document.id) {
-                    await this.loadNotes()
-                }
-            },
-            closeDialog() {
-                this.selectedNote = {} as iNote
-                this.$emit('close')
-            },
-            async loadNotes() {
-                this.loading = true
-                await this.$http
-                    .post(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `documentnotes/getListNotes`, { idObj: this.document.id })
-                    .then((response: AxiosResponse<any>) => (this.notes = response.data))
-                    .catch((error: any) =>
-                        this.$store.commit('setError', {
-                            title: this.$t('common.error.generic'),
-                            msg: error
-                        })
-                    )
-                this.loading = false
-
-                if (this.notes.length > 0) {
-                    this.selectedNote = this.notes[0]
-                }
-            },
-            async saveNote() {
-                this.loading = true
-                await this.$http
-                    .post(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `documentnotes/saveNote`, { type: this.selectedNote.type, content: this.selectedNote.content, idObj: this.document.id })
-                    .then(async () => {
-                        this.$store.commit('setInfo', {
-                            title: this.$t('common.toast.createTitle'),
-                            msg: this.$t('common.toast.success')
-                        })
-                        await this.loadNotes()
-                    })
-                    .catch((error: any) =>
-                        this.$store.commit('setError', {
-                            title: this.$t('common.error.generic'),
-                            msg: error
-                        })
-                    )
-                this.loading = false
-            },
-            onEditNote(note: iNote) {
-                this.selectedNote = { ...note }
-                this.activeTab = 0
-            },
-            async onDeleteNote(note: iNote) {
-                this.loading = true
-                await this.$http
-                    .post(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `documentnotes/deleteNote`, { id: this.document.id, execReq: note.execReq, owner: note.owner })
-                    .then(async () => {
-                        this.$store.commit('setInfo', {
-                            title: this.$t('common.toast.deleteTitle'),
-                            msg: this.$t('common.toast.deleteSuccess')
-                        })
-                        await this.loadNotes()
-                    })
-                    .catch((error: any) =>
-                        this.$store.commit('setError', {
-                            title: this.$t('common.error.generic'),
-                            msg: error
-                        })
-                    )
-                this.loading = false
-            },
-            async exportNotes(type: string) {
-                this.loading = true
-                await this.$http
-                    .post(
-                        process.env.VUE_APP_RESTFUL_SERVICES_PATH + `documentnotes/getDownalNote`,
-                        { idObj: this.document.id, type: type },
-                        {
-                            headers: {
-                                Accept: 'application/json, text/plain, */*'
-                            }
-                        }
-                    )
-                    .then((response: AxiosResponse<any>) => {
-                        const byteArray = new Uint8Array(response.data.file)
-                        downloadDirect(byteArray, this.document.label, type === 'pdf' ? 'application/pdf' : 'application/rtf')
-                    })
-                    .catch((error: any) =>
-                        this.$store.commit('setError', {
-                            title: this.$t('common.error.generic'),
-                            msg: error
-                        })
-                    )
-                this.loading = false
-            }
         }
-    })
+    },
+    computed: {
+        saveButtonDisabled(): boolean {
+            return !this.selectedNote.type
+        },
+        exportButtonDisabled(): boolean {
+            return this.notes.length === 0
+        }
+    },
+    async created() {
+        await this.loadDocument()
+    },
+    methods: {
+        async loadDocument() {
+            this.document = this.propDocument
+
+            if (this.document && this.document.id) {
+                await this.loadNotes()
+            }
+        },
+        closeDialog() {
+            this.selectedNote = {} as iNote
+            this.$emit('close')
+        },
+        async loadNotes() {
+            this.loading = true
+            this.notes = []
+            await this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `document-notes/${this.document.id}`)
+                .then((response: AxiosResponse<any>) => {
+                    this.notes = response.data?.map((note: iNote) => {
+                        return { ...note, type: note.public ? 'Public' : 'Private' }
+                    })
+                })
+                .catch((error: any) =>
+                    this.$store.commit('setError', {
+                        title: this.$t('common.error.generic'),
+                        msg: error
+                    })
+                )
+            this.loading = false
+        },
+        async saveNote() {
+            this.loading = true
+            await this.$http
+                .post(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `document-notes/${this.document.id}`, { public: this.selectedNote.type === 'Public', content: this.selectedNote.content, id: this.selectedNote.id })
+                .then(async (response: AxiosResponse<any>) => {
+                    this.$store.commit('setInfo', {
+                        title: this.$t('common.toast.createTitle'),
+                        msg: this.$t('common.toast.success')
+                    })
+                    this.selectedNote = { ...response.data, type: response.data.public ? 'Public' : 'Private' }
+                    await this.loadNotes()
+                })
+                .catch((error: any) =>
+                    this.$store.commit('setError', {
+                        title: this.$t('common.error.generic'),
+                        msg: error
+                    })
+                )
+            this.loading = false
+        },
+        onEditNote(note: iNote) {
+            this.selectedNote = { ...note }
+            this.activeTab = 0
+        },
+        async onDeleteNote(note: iNote) {
+            this.loading = true
+            await this.$http
+                .delete(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `document-notes/${this.document.id}/${note.id}`)
+                .then(async () => {
+                    this.$store.commit('setInfo', {
+                        title: this.$t('common.toast.deleteTitle'),
+                        msg: this.$t('common.toast.deleteSuccess')
+                    })
+
+                    if (this.selectedNote.id === note.id) this.selectedNote = {} as iNote
+                    await this.loadNotes()
+                })
+                .catch((error: any) =>
+                    this.$store.commit('setError', {
+                        title: this.$t('common.error.generic'),
+                        msg: error
+                    })
+                )
+            this.loading = false
+        },
+        async exportNotes(type: string) {
+            this.loading = true
+            await this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `document-notes/${this.document.id}/${this.notes[0].id}/${type}`, {
+                    headers: {
+                        Accept: 'application/json, text/plain, */*'
+                    }
+                })
+                .then((response: AxiosResponse<any>) => {
+                    const byteArray = new Uint8Array(response.data.file)
+                    downloadDirect(byteArray, this.document.label, type === 'pdf' ? 'application/pdf' : 'application/rtf')
+                })
+                .catch(() => {})
+            this.loading = false
+        },
+        createNewNote() {
+            this.selectedNote = {} as iNote
+        }
+    }
+})
 </script>
