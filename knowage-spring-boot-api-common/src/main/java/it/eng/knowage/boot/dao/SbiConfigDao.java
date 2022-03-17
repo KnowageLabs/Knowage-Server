7228981076696ed41e25dc522779087c031c763e
Marco Libanori <marco.libanori@eng.it>
2022-03-17 10:08:03 +0100

    Add little note on the javadoc of SbiConfigDao

--- knowage-spring-boot-api-common/src/main/java/it/eng/knowage/boot/dao/SbiConfigDao.java
+++ knowage-spring-boot-api-common/src/main/java/it/eng/knowage/boot/dao/SbiConfigDao.java
@@ -2,9 +2,15 @@ package it.eng.knowage.boot.dao;
 
 import java.util.List;
 
+import it.eng.knowage.boot.context.BusinessRequestContext;
 import it.eng.knowage.boot.dao.dto.SbiConfig;
 
 /**
+ * Dao for the <code>SBI_CONFIG</code> table.
+ *
+ * All the implementations of this class must be independent from {@link BusinessRequestContext} and every other
+ * references to a user or a request.
+ *
  * @author Matteo Massarotto
  */
 public interface SbiConfigDao {
