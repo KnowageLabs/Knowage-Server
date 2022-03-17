b11bbce87193c8c5ec7f8e74ad0951f3acbff484
dbulatovicx32 <darko.bulatovic@eng.it>
2022-03-17 09:56:17 +0100

    fix for metadata and tooltip

--- knowage-vue/src/modules/managers/datasetManagement/detailView/advancedCard/DatasetManagementAdvancedCard.vue
+++ knowage-vue/src/modules/managers/datasetManagement/detailView/advancedCard/DatasetManagementAdvancedCard.vue
@@ -79,7 +79,7 @@
         <Toolbar class="kn-toolbar kn-toolbar--secondary p-mt-3">
             <template #start>
                 <InputSwitch v-model="dataset.isPersisted" :disabled="disablePersist" class="p-mr-2" @change="$emit('touched')" />
-                <span>{{ $t('managers.datasetManagement.isPersisted') }}</span>
+                <span v-tooltip.top="{ value: $t('managers.datasetManagement.peristenceWarning'), disabled: !disablePersist }">{{ $t('managers.datasetManagement.isPersisted') }}</span>
             </template>
         </Toolbar>
         <Card v-if="dataset.isPersisted">
@@ -116,87 +116,87 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue'
-    import { createValidations, ICustomValidatorMap } from '@/helpers/commons/validationHelper'
-    import useValidate from '@vuelidate/core'
-    import advancedCardDescriptor from './DatasetManagementAdvancedCardDescriptor.json'
-    import DatasetScheduler from './DatasetManagementScheduler.vue'
-    import KnValidationMessages from '@/components/UI/KnValidatonMessages.vue'
-    import Card from 'primevue/card'
-    import Checkbox from 'primevue/checkbox'
-    import InputSwitch from 'primevue/inputswitch'
+import { defineComponent } from 'vue'
+import { createValidations, ICustomValidatorMap } from '@/helpers/commons/validationHelper'
+import useValidate from '@vuelidate/core'
+import advancedCardDescriptor from './DatasetManagementAdvancedCardDescriptor.json'
+import DatasetScheduler from './DatasetManagementScheduler.vue'
+import KnValidationMessages from '@/components/UI/KnValidatonMessages.vue'
+import Card from 'primevue/card'
+import Checkbox from 'primevue/checkbox'
+import InputSwitch from 'primevue/inputswitch'
 
-    export default defineComponent({
-        components: { Card, InputSwitch, Checkbox, KnValidationMessages, DatasetScheduler },
-        props: {
-            selectedDataset: { type: Object as any },
-            transformationDataset: { type: Object as any },
-            schedulingData: { type: Object as any }
-        },
-        computed: {
-            disablePersist() {
-                if (this.dataset['pars'] && this.dataset['pars'].length > 0) {
-                    return true
-                }
-                return false
+export default defineComponent({
+    components: { Card, InputSwitch, Checkbox, KnValidationMessages, DatasetScheduler },
+    props: {
+        selectedDataset: { type: Object as any },
+        transformationDataset: { type: Object as any },
+        schedulingData: { type: Object as any }
+    },
+    computed: {
+        disablePersist() {
+            if (this.dataset['pars'] && this.dataset['pars'].length > 0) {
+                return true
             }
-        },
-        emits: ['touched'],
-        data() {
-            return {
-                v$: useValidate() as any,
-                advancedCardDescriptor,
-                dataset: {} as any,
-                testInput: 'testinput',
-                testCheckbox: true,
-                isTransformable: false
-            }
-        },
-        created() {
+            return false
+        }
+    },
+    emits: ['touched'],
+    data() {
+        return {
+            v$: useValidate() as any,
+            advancedCardDescriptor,
+            dataset: {} as any,
+            testInput: 'testinput',
+            testCheckbox: true,
+            isTransformable: false
+        }
+    },
+    created() {
+        this.dataset = this.selectedDataset
+        this.isDatasetTransformable()
+    },
+    watch: {
+        selectedDataset() {
             this.dataset = this.selectedDataset
             this.isDatasetTransformable()
-        },
-        watch: {
-            selectedDataset() {
-                this.dataset = this.selectedDataset
-                this.isDatasetTransformable()
+        }
+    },
+    validations() {
+        const transformationFieldsRequired = (value) => {
+            return !this.isTransformable || value
+        }
+        const persistFieldsRequired = (value) => {
+            return !this.dataset.isPersisted || value
+        }
+        const customValidators: ICustomValidatorMap = {
+            'transformable-field-required': transformationFieldsRequired,
+            'persist-field-required': persistFieldsRequired
+        }
+        const validationObject = {
+            dataset: createValidations('dataset', advancedCardDescriptor.validations.advancedTab, customValidators)
+        }
+        return validationObject
+    },
+    methods: {
+        isDatasetTransformable() {
+            if (this.dataset.trasfTypeCd && this.dataset.trasfTypeCd == this.transformationDataset.VALUE_CD) {
+                this.isTransformable = true
+            } else {
+                this.isTransformable = false
             }
         },
-        validations() {
-            const transformationFieldsRequired = (value) => {
-                return !this.isTransformable || value
-            }
-            const persistFieldsRequired = (value) => {
-                return !this.dataset.isPersisted || value
-            }
-            const customValidators: ICustomValidatorMap = {
-                'transformable-field-required': transformationFieldsRequired,
-                'persist-field-required': persistFieldsRequired
-            }
-            const validationObject = {
-                dataset: createValidations('dataset', advancedCardDescriptor.validations.advancedTab, customValidators)
-            }
-            return validationObject
-        },
-        methods: {
-            isDatasetTransformable() {
-                if (this.dataset.trasfTypeCd && this.dataset.trasfTypeCd == this.transformationDataset.VALUE_CD) {
-                    this.isTransformable = true
-                } else {
-                    this.isTransformable = false
-                }
-            },
-            setTransformationType() {
-                if (this.isTransformable) {
-                    this.dataset.trasfTypeCd = this.transformationDataset.VALUE_CD
-                } else {
-                    this.dataset.trasfTypeCd ? (this.dataset.trasfTypeCd = '') : null
-                    this.dataset.pivotColName ? (this.dataset.pivotColName = '') : null
-                    this.dataset.pivotColValue ? (this.dataset.pivotColValue = '') : null
-                    this.dataset.pivotIsNumRows ? (this.dataset.pivotIsNumRows = '') : null
-                    this.dataset.pivotRowName ? (this.dataset.pivotRowName = '') : null
-                }
+        setTransformationType() {
+            if (this.isTransformable) {
+                this.dataset.trasfTypeCd = this.transformationDataset.VALUE_CD
+            } else {
+                this.dataset.trasfTypeCd ? (this.dataset.trasfTypeCd = '') : null
+                this.dataset.pivotColName ? (this.dataset.pivotColName = '') : null
+                this.dataset.pivotColValue ? (this.dataset.pivotColValue = '') : null
+                this.dataset.pivotIsNumRows ? (this.dataset.pivotIsNumRows = '') : null
+                this.dataset.pivotRowName ? (this.dataset.pivotRowName = '') : null
             }
         }
-    })
+    }
+})
 </script>
