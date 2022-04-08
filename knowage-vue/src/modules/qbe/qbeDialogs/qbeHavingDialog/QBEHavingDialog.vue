fc85a9ed3c2be6ac812ce74f397f1bc7ec27bcb3
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-04-08 10:16:44 +0200

    Bugfix KNOWAGE-6757

--- knowage-vue/src/modules/qbe/qbeDialogs/qbeHavingDialog/QBEHavingDialog.vue
+++ knowage-vue/src/modules/qbe/qbeDialogs/qbeHavingDialog/QBEHavingDialog.vue
@@ -84,7 +84,7 @@ export default defineComponent({
                     leftOperandDefaultValue: null,
                     leftOperandLastValue: null,
                     operator: 'EQUALS TO',
-                    rightOperandAggregator: '',
+                    rightOperandAggregator: field.funct,
                     rightOperandValue: [],
                     rightOperandDescription: '',
                     rightOperandLongDescription: '',
