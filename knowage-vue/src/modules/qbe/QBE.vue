3bc1441d6f76ef3f6076f068aa1fab08886268cc
dbulatovicx32 <darko.bulatovic@eng.it>
2022-04-12 10:12:30 +0200

    KNOWAGE-6740 added blob to fix download

--- knowage-vue/src/modules/qbe/QBE.vue
+++ knowage-vue/src/modules/qbe/QBE.vue
@@ -477,15 +477,14 @@ export default defineComponent({
             if (mimeType == 'csv') {
                 fileName = 'report.csv'
                 fileType = 'text/csv'
-            } else if (mimeType == 'xlsx') {
+            } else {
                 fileName = 'report.xlsx'
                 fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-            } else {
-                console.log('Unsupported mime type: ', mimeType, fileName, fileType)
             }
+
             const postData = { catalogue: this.qbe?.qbeJSONQuery.catalogue.queries, meta: this.formatQbeMeta(), pars: this.qbe?.pars, qbeJSONQuery: {}, schedulingCronLine: '0 * * * * ?' }
             await this.$http
-                .post(process.env.VUE_APP_QBE_PATH + `qbequery/export/?SBI_EXECUTION_ID=${this.uniqueID}&currentQueryId=${this.selectedQuery.id}&outputType=${mimeType}`, postData, { headers: { Accept: 'application/json, text/plain, */*' } })
+                .post(process.env.VUE_APP_QBE_PATH + `qbequery/export/?SBI_EXECUTION_ID=${this.uniqueID}&currentQueryId=${this.selectedQuery.id}&outputType=${mimeType}`, postData, { headers: { Accept: 'application/json, text/plain, */*' }, responseType: 'blob' })
                 .then((response: AxiosResponse<any>) => {
                     downloadDirect(response.data, fileName, fileType)
                 })
