b1a524ea48a68bcf656a9ca1eafdafc8fe2f2391
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-05-05 17:23:11 +0200

    Scorecards List

---
+++ knowage-vue/src/modules/managers/scorecards/Scorecards.d.ts
@@ -0,0 +1,86 @@
+export interface iScorecard {
+    id: number,
+    name: string,
+    creationDate: number | Date,
+    author: string,
+    perspectives: iPerspective[]
+}
+
+export interface iPerspective {
+    id: number,
+    name: string,
+    criterion: iScorecardCriterion,
+    options: {
+        criterionPriority: []
+    },
+    status: string,
+    targets: iScorecardTarget[],
+    groupedKpis: { status: null, count: number }[]
+}
+
+export interface iScorecardCriterion {
+    valueId: number,
+    valueCd: string,
+    valueName: string,
+    valueDescription: string,
+    domainCode: string,
+    domainName: string,
+    translatedValueName: string,
+    translatedValueDescription: string
+}
+
+export interface iKpi {
+    id: number,
+    version: number,
+    name: string,
+    author: string,
+    dateCreation: number | Date,
+    active: boolean,
+    enableVersioning: boolean,
+    definition: string,
+    cardinality: string,
+    placeholder: string,
+    category: {
+        valueId: number,
+        valueCd: string,
+        valueName: string,
+        valueDescription: string,
+        domainCode: string,
+        domainName: string,
+        translatedValueName: string,
+        translatedValueDescription: string
+    },
+    threshold: {
+        id: number,
+        description: string,
+        name: string,
+        typeId: number,
+        type: string,
+        thresholdValues: {
+            id: number,
+            position: number,
+            label: string,
+            color: string,
+            severityId: number,
+            severityCd: string,
+            minValue: number,
+            includeMin: boolean,
+            maxValue: number,
+            includeMax: boolean
+        }[],
+        usedByKpi: boolean
+    },
+    status: null
+}
+
+export interface iScorecardTarget {
+    id: number,
+    name: string,
+    criterion: iScorecardCriterion,
+    options: {
+        criterionPriority: []
+    },
+    status: string,
+    kpis: iKpi[],
+    groupedKpis: { status: null, count: number }[]
+}
---
+++ knowage-vue/src/modules/managers/scorecards/ScorecardsDesigner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="kn-page">
+        <div class="kn-page-content p-grid p-m-0">
+            <div class="kn-list--column p-col-4 p-sm-8 p-md-6 p-p-0">
+                <Toolbar class="kn-toolbar kn-toolbar--primary">
+                    <template #start>
+                        {{ $t('managers.scorecards.scorecardDesigner') }}
+                    </template>
+                </Toolbar>
+            </div>
+
+            <div class="p-col-4 p-sm-4 p-md-6 p-p-0 p-m-0">
+                PERSPECTIVES LIST
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'scorecards-designer',
+    components: {},
+    data() {
+        return {}
+    },
+    async created() {},
+    methods: {}
+})
+</script>
