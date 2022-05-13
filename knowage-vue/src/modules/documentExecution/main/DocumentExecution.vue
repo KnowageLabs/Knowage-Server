1b33ccf383a96c5d46c0eeeb2a83949e168aaaa8
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-05-13 14:32:07 +0200

    Hotfix

--- knowage-vue/src/modules/documentExecution/main/DocumentExecution.vue
+++ knowage-vue/src/modules/documentExecution/main/DocumentExecution.vue
@@ -900,7 +900,7 @@ export default defineComponent({
             await this.$http.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `1.0/crossNavigation/${this.document.label}/loadCrossNavigationByDocument`).then((response: AxiosResponse<any>) => (temp = response.data))
             this.loading = false
 
-            if (temp.length > 0) {
+            if (temp.length > 1) {
                 this.crossNavigationDocuments = temp
                 this.destinationSelectDialogVisible = true
             } else {
