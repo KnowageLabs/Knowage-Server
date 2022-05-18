8f0900c0028228c87cfca181872cb013965d42c7
dbulatovicx32 <darko.bulatovic@eng.it>
2022-05-18 11:22:42 +0200

    implemented open data prep method

--- knowage-vue/src/modules/managers/datasetManagement/detailView/typeCard/preparedDataset/DatasetManagementPreparedDataset.vue
+++ knowage-vue/src/modules/managers/datasetManagement/detailView/typeCard/preparedDataset/DatasetManagementPreparedDataset.vue
@@ -6,7 +6,7 @@
                     <Button :label="$t('managers.datasetManagement.monitoring')" class="kn-button kn-button--primary" @click="showMonitoringDialog = true" />
                 </div>
                 <div class="p-field p-col-6">
-                    <Button :label="$t('managers.datasetManagement.openDP')" class="kn-button kn-button--primary" @click="routeToDataPreparation" />
+                    <Button :label="$t('managers.datasetManagement.openDP')" class="kn-button kn-button--primary" @click="prepareForDataPreparation" />
                 </div>
             </form>
         </template>
@@ -17,6 +17,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { AxiosResponse } from 'axios'
 import descriptor from './DatasetManagementPreparedDataset.json'
 import Card from 'primevue/card'
 import MonitoringDialog from '@/modules/workspace/dataPreparation/DataPreparationMonitoring/DataPreparationMonitoringDialog.vue'
@@ -30,6 +31,7 @@ export default defineComponent({
             descriptor,
             dataset: {} as any,
             availableDatasets: [] as any,
+            avroDatasets: [] as any,
             showMonitoringDialog: false
         }
     },
@@ -52,6 +54,68 @@ export default defineComponent({
                     this.$router.push(path)
                 }
             })
+        },
+
+        async getAllAvroDataSets() {
+            await this.$http
+                .get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `3.0/datasets/avro`)
+                .then((response: AxiosResponse<any>) => {
+                    this.avroDatasets = response.data
+                })
+                .catch(() => {})
+        },
+
+        async prepareForDataPreparation() {
+            await this.getAllAvroDataSets()
+            await this.openDataPreparation(this.selectedDataset)
+        },
+
+        isAvroReady(dsLabel: String) {
+            if (this.avroDatasets.indexOf(dsLabel) >= 0) return true
+            else return false
+        },
+
+        openDataPreparation(dataset: any) {
+            if (dataset.dsTypeCd == 'Prepared') {
+                //edit existing data prep
+                this.$http.get(process.env.VUE_APP_RESTFUL_SERVICES_PATH + `3.0/datasets/advanced/${dataset.label}`).then(
+                    (response: AxiosResponse<any>) => {
+                        let instanceId = response.data.configuration.dataPrepInstanceId
+                        this.$http.get(process.env.VUE_APP_DATA_PREPARATION_PATH + `1.0/process/by-instance-id/${instanceId}`).then(
+                            (response: AxiosResponse<any>) => {
+                                let transformations = response.data.definition
+                                let processId = response.data.id
+                                let datasetLabel = response.data.instance.dataSetLabel
+                                if (this.isAvroReady(datasetLabel))
+                                    // check if Avro file has been deleted or not
+                                    this.$router.push({ name: 'data-preparation', params: { id: datasetLabel, transformations: JSON.stringify(transformations), processId: processId, instanceId: instanceId, dataset: JSON.stringify(dataset) } })
+                                else {
+                                    this.$store.commit('setInfo', {
+                                        title: 'Avro file is missing',
+                                        msg: 'Generate it again and then retry'
+                                    })
+                                }
+                            },
+                            () => {
+                                this.$store.commit('setError', { title: 'Save error', msg: 'Cannot create process' })
+                            }
+                        )
+                    },
+                    () => {
+                        this.$store.commit('setError', {
+                            title: 'Cannot open data preparation'
+                        })
+                    }
+                )
+            } else if (this.isAvroReady(dataset.label)) {
+                // original dataset already exported in Avro
+                this.$router.push({ name: 'data-preparation', params: { id: dataset.label } })
+            } else {
+                this.$store.commit('setInfo', {
+                    title: 'Avro file is missing',
+                    msg: 'Generate it again and then retry'
+                })
+            }
         }
     }
 })
