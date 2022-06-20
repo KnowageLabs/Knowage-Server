1cb73ae0b80a446b21e229128d80f9942a998da4
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-06-20 23:02:24 +0200

    Update

--- knowage-vue/vite.config.js
+++ knowage-vue/vite.config.js
@@ -5,6 +5,7 @@ const path = require("path");
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
