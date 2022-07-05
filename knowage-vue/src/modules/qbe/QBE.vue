606f9ec2d77680c6eab0d63da332353a879e15d3
dbulatovicx32 <darko.bulatovic@eng.it>
2022-07-05 10:06:39 +0200

    KNOWAGE-7013, improved smart view updating

--- knowage-vue/src/modules/qbe/QBE.vue
+++ knowage-vue/src/modules/qbe/QBE.vue
@@ -76,6 +76,9 @@
                 </Toolbar>
                 <div class="kn-relative kn-flex p-mt-2">
                     <div class="kn-height-full kn-width-full kn-absolute">
+                        <!-- {{ pagination }}
+                        {{ queryPreviewData }} -->
+
                         <QBESimpleTable
                             v-if="!smartView"
                             :query="selectedQuery"
@@ -564,7 +567,7 @@ export default defineComponent({
             this.selectedQuery.fields = []
             this.selectedQuery.havings = []
             this.qbeMetadata = []
-            if (this.smartView) this.executeQBEQuery(false)
+            this.updateSmartView()
         },
         deleteFieldFromQuery(fieldID) {
             let indexOfFieldToDelete = this.selectedQuery.fields.findIndex((field) => {
@@ -925,7 +928,12 @@ export default defineComponent({
             await this.executeQBEQuery(true)
         },
         updateSmartView() {
-            this.smartView ? this.executeQBEQuery(false) : ''
+            this.smartView && this.selectedQuery.fields.length > 0 ? this.executeQBEQuery(false) : this.resetQueryPreviewAndPagination()
+        },
+        resetQueryPreviewAndPagination() {
+            console.log('called')
+            this.pagination = { start: 0, limit: 25, size: 0 }
+            this.queryPreviewData = {} as any
         },
         smartViewFieldHidden() {
             this.checkIfHiddenColumnsExist()
