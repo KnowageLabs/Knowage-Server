c7ac3fb1410278529cf7e387cd290b311badf772
Davide Vernassa <davide.vernassa@eng.it>
2022-07-08 16:13:11 +0200

    Changed publicPath to base to fit vite config

--- knowage-vue/vite.config.js
+++ knowage-vue/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig((command, mode) => {
                 }
             }
         },
-        publicPath: env.VITE_PUBLIC_PATH,
+        base: env.VITE_PUBLIC_PATH,
         build: {
             outDir: './src/main/webapp'
         },
