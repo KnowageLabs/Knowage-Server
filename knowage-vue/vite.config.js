b91c9c93fa4fa2a78406169e3487cdbe7ea880ed
Davide Vernassa <davide.vernassa@gmail.com>
2022-07-11 18:26:30 +0200

    Removed assetFileNames from vite.config to prevent error

--- knowage-vue/vite.config.js
+++ knowage-vue/vite.config.js
@@ -29,13 +29,6 @@ export default defineConfig((command, mode) => {
             outDir: './src/main/webapp',
             rollupOptions: {
                 output: {
-                    assetFileNames: (assetInfo) => {
-                        let extType = assetInfo.name.split('.').at(1)
-                        if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                            extType = 'img'
-                        }
-                        return `assets/${extType}/[name]-[hash][extname]`
-                    },
                     chunkFileNames: 'assets/js/[name]-[hash].js',
                     entryFileNames: 'assets/js/[name]-[hash].js'
                 }
