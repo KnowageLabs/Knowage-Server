cb66ed3d0b438b673f318e024eba2c3dd0693617
dbulatovicx32 <darko.bulatovic@eng.it>
2022-07-26 11:45:50 +0200

    removed redundant index code

--- knowage-vue/src/modules/documentExecution/dashboard/dataset/DatasetEditorDataTab/DatasetEditorDataDetail/DatasetEditorDataDetailIndexes.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/dataset/DatasetEditorDataTab/DatasetEditorDataDetail/DatasetEditorDataDetailIndexes.vue
@@ -1,34 +1,15 @@
 <template>
     <Accordion>
         <AccordionTab header="Indexes">
-            <form class="p-fluid p-formgrid p-grid">
-                <div class="p-field p-col-12 p-d-flex">
-                    <div class="kn-flex">
-                        <span class="p-float-label">
-                            <Dropdown id="field" class="kn-material-input" v-model="selectedField" :options="selectedDatasetProp.metadata.fieldsMeta" optionLabel="alias" optionValue="alias" />
-                            <label for="field" class="kn-material-input-label"> {{ $t('common.field') }}</label>
-                        </span>
-                    </div>
-                    <Button icon="pi pi-plus-circle" class="p-button-text p-button-rounded p-button-plain p-ml-2 p-as-end" @click="checkFieldValidity" />
-                </div>
-                <div v-for="(field, index) of datasetIndexes" :key="index" class="p-field p-col-12 p-d-flex p-ai-baseline">
-                    <div class="kn-flex">
-                        {{ field }}
-                    </div>
-                    <Button icon="pi pi-trash" class="p-button-text p-button-rounded p-button-plain" @click="removeFieldFromIndexes(field)" />
-                </div>
-            </form>
-
-            <!-- MULTIVALUE SOLUTION -----------------------------------
             <div class="p-col-12 p-d-flex">
                 <div class="kn-flex">
                     <span class="p-float-label">
-                        <MultiSelect class="kn-material-input" :style="descriptor.style.indexMultiselect" v-model="datasetIndexes" :options="selectedDatasetProp.metadata.fieldsMeta" optionLabel="alias" optionValue="alias" @change="logMulti" />
+                        <MultiSelect class="kn-material-input" :style="descriptor.style.indexMultiselect" v-model="datasetIndexes" :options="selectedDatasetProp.metadata.fieldsMeta" optionLabel="alias" optionValue="alias" @change="updateDatasetIndexes" />
                         <label for="field" class="kn-material-input-label"> {{ $t('common.field') }}</label>
                     </span>
                 </div>
                 <i class="fas fa-info-circle p-button-text p-button-plain p-as-center p-ml-2" :style="descriptor.style.indexInfoIcon" v-tooltip.top="$t('dashboard.datasetEditor.indexHint')" />
-            </div> -->
+            </div>
         </AccordionTab>
     </Accordion>
 </template>
@@ -38,13 +19,13 @@ import { defineComponent } from 'vue'
 import Card from 'primevue/card'
 import Accordion from 'primevue/accordion'
 import AccordionTab from 'primevue/accordiontab'
-import Dropdown from 'primevue/dropdown'
+import MultiSelect from 'primevue/multiselect'
 import descriptor from './DatasetEditorDataDetailDescriptor.json'
 import mainStore from '../../../../../../App.store'
 
 export default defineComponent({
     name: 'dataset-editor-data-detail-info',
-    components: { Card, Accordion, AccordionTab, Dropdown },
+    components: { Card, Accordion, AccordionTab, MultiSelect },
     props: { selectedDatasetProp: { required: true, type: Object }, dashboardDatasetsProp: { required: true, type: Array as any } },
     emits: [],
     data() {
@@ -66,32 +47,10 @@ export default defineComponent({
             this.datasetIndexes = this.dashboardDatasetsProp?.find((dataset) => dataset.id === this.selectedDatasetProp.id.dsId).indexes
             console.log(this.datasetIndexes)
         },
-        checkFieldValidity() {
-            if (this.selectedField != '') {
-                if (this.datasetIndexes.indexOf(this.selectedField) > -1) {
-                    this.store.setInfo({
-                        title: this.$t('common.toast.warning'),
-                        msg: this.$t('dashboard.datasetEditor.indexPresentMsg')
-                    })
-                } else {
-                    this.addFieldToIndexes()
-                    this.selectedField = ''
-                    this.getIndexesFromModel()
-                }
-            } else return
-        },
-        addFieldToIndexes() {
-            this.dashboardDatasetsProp.find((dataset) => dataset.id === this.selectedDatasetProp.id.dsId).indexes.push(this.selectedField)
-        },
-        removeFieldFromIndexes(fieldToRemove) {
-            this.datasetIndexes = this.datasetIndexes.filter((field) => field !== fieldToRemove)
-            this.dashboardDatasetsProp.find((dataset) => dataset.id === this.selectedDatasetProp.id.dsId).indexes = this.datasetIndexes
+        updateDatasetIndexes(event) {
+            console.log(event.value)
+            this.dashboardDatasetsProp.find((dataset) => dataset.id === this.selectedDatasetProp.id.dsId).indexes = event.value
         }
-        // MULTIVALUE SOLUTION ------------------------------------
-        // saveMultivalueChanges(event) {
-        //     console.log(event.value)
-        //     this.dashboardDatasetsProp.find((dataset) => dataset.id === this.selectedDatasetProp.id.dsId).indexes = event.value
-        // }
     }
 })
 </script>
