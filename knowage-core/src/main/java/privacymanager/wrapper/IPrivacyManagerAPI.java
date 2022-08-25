ee3f41596e61bd04346b74f391946e739640e729
Marco Libanori <marco.libanori@eng.it>
2022-08-25 16:11:07 +0200

    [KNOWAGE-6938] Manage encrypted data in QBE

--- knowage-core/src/main/java/privacymanager/wrapper/IPrivacyManagerAPI.java
+++ knowage-core/src/main/java/privacymanager/wrapper/IPrivacyManagerAPI.java
@@ -1,48 +1,30 @@
 /*
  * Knowage, Open Source Business Intelligence suite
- * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- *
+ * Copyright (C) 2022 Engineering Ingegneria Informatica S.p.A.
+
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+
  * Knowage is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
+
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package privacymanager.wrapper;
 
-public class PMkeyDTO {
+import privacymanager.wrapper.exception.PrivacyManagerFailureException;
 
-	private PMServiceProviderDTO serviceProvider;
+public interface IPrivacyManagerAPI {
 
-	private int keyLength;
+	String getToken(String pmUser, String pmPwd) throws PrivacyManagerFailureException;
 
-	public PMServiceProviderDTO getServiceProvider() {
-		return serviceProvider;
-	}
+	String retrieveKey() throws PrivacyManagerFailureException;
 
-	public void setServiceProvider(PMServiceProviderDTO serviceProvider) {
-		this.serviceProvider = serviceProvider;
-	}
-
-	public int getKeyLength() {
-		return keyLength;
-	}
-
-	public void setKeyLength(int keyLength) {
-		this.keyLength = keyLength;
-	}
-
-	public PMkeyDTO(PMServiceProviderDTO serviceProvider, int keyLength) {
-		super();
-		this.serviceProvider = serviceProvider;
-		this.keyLength = keyLength;
-	}
-
+	String saveApplication() throws PrivacyManagerFailureException;
 }
