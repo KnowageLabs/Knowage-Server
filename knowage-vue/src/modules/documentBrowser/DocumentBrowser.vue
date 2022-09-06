425efc65d462ae231b2061b1689dc6267f0dc1e6
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-09-06 10:12:29 +0200

    Bugfixes KNOWAGE-7324, KNOWAGE-7326

--- knowage-vue/src/modules/documentBrowser/DocumentBrowser.vue
+++ knowage-vue/src/modules/documentBrowser/DocumentBrowser.vue
@@ -229,7 +229,7 @@ export default defineComponent({
         },
         loadSavedCockpit(cockpit: any) {
             this.closeIframe()
-            this.selectedItem = { item: { ...cockpit, routerId: cryptoRandomString({ length: 16, type: 'base64' }), name: cockpit.DOCUMENT_NAME, label: cockpit.DOCUMENT_LABEL, showMode: 'execute' } }
+            this.selectedItem = { item: { ...cockpit, routerId: cryptoRandomString({ length: 16, type: 'base64' }), name: cockpit.DOCUMENT_NAME, label: cockpit.DOCUMENT_LABEL, showMode: 'execute' }, mode: 'execute' }
             this.tabs[this.activeIndex - 1] = this.selectedItem
             this.$router.push(`/document-browser/document-composite/${cockpit.DOCUMENT_LABEL}`)
         },
