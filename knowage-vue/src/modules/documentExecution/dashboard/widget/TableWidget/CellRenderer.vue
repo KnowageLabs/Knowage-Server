24797fb4c095c1eff36f82576b4a1c51fb02a28a
dbulatovicx32 <darko.bulatovic@eng.it>
2022-09-19 15:07:50 +0200

    added custom cell renderer and column styling

---
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/TableWidget/CellRenderer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="custom-cell-container p-d-flex" :style="params.styleString">
+        <div class="custom-cell-label">{{ params.value }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        params: {
+            required: true,
+            type: Object
+        }
+    },
+    data() {
+        return {}
+    },
+    mounted() {
+        // console.log('\n \n cell RENDERER PARAMS \n', this.params)
+    },
+    methods: {}
+})
+</script>
