f908f50319492949383748d4c96173249c717b71
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-09-29 09:38:19 +0200

    Added descriptor for default values

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/helpers/tableWidget/TableWidgetDefaultValues.ts
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/helpers/tableWidget/TableWidgetDefaultValues.ts
@@ -1,283 +1,101 @@
-import { ITableWidgetBordersStyle, ITableWidgetColumnGroups, ITableWidgetColumnStyles, ITableWidgetConditionalStyle, ITableWidgetConditionalStyles, ITableWidgetCrossNavigation, ITableWidgetCustomMessages, ITableWidgetExports, ITableWidgetHeaders, ITableWidgetLinks, ITableWidgetPaddingStyle, ITableWidgetPagination, ITableWidgetPreview, ITableWidgetResponsive, ITableWidgetRows, ITableWidgetRowsStyle, ITableWidgetSelection, ITableWidgetShadowsStyle, ITableWidgetSummaryRows, ITableWidgetSummaryStyle, ITableWidgetTooltipStyle, ITableWidgetVisualization, ITawbleWidgetHeadersStyle } from "../../../../Dashboard"
-
+import { ITableWidgetBordersStyle, ITableWidgetColumnGroups, ITableWidgetColumnStyles, ITableWidgetConditionalStyle, ITableWidgetConditionalStyles, ITableWidgetCrossNavigation, ITableWidgetCustomMessages, ITableWidgetExports, ITableWidgetHeaders, ITableWidgetLinks, ITableWidgetPaddingStyle, ITableWidgetPagination, ITableWidgetPreview, ITableWidgetResponsive, ITableWidgetRows, ITableWidgetRowsStyle, ITableWidgetSelection, ITableWidgetShadowsStyle, ITableWidgetSummaryRows, ITableWidgetSummaryStyle, ITableWidgetTooltipStyle, ITableWidgetVisibilityCondition, ITableWidgetVisualization, ITawbleWidgetHeadersStyle } from "../../../../Dashboard"
+import descriptor from './TableWidgetHelpersDescriptor.json'
 
 export const getDefaultConditionalStyles = () => {
-    return {
-        enabled: false,
-        conditions: []
-    } as ITableWidgetConditionalStyles
+    return descriptor.defaultConditionalStyles as ITableWidgetConditionalStyles
 }
 
 export const getDefaultConditionalStyle = () => {
-    return {
-        target: '',
-        applyToWholeRow: false,
-        condition: {
-            type: '',
-
-            operator: '',
-            value: ''
-        },
-        properties: {
-            "justify-content": '',
-            "font-family": '',
-            "font-size": '',
-            "font-style": '',
-            "font-weight": '',
-            color: '',
-            "background-color": '',
-            icon: ''
-        }
-    } as ITableWidgetConditionalStyle
+    return descriptor.defaultConditionalStyle as ITableWidgetConditionalStyle
 }
 
 export const getDefaultColumnGroups = () => {
-    return {
-        enabled: false,
-        groups: []
-    } as ITableWidgetColumnGroups
+    return descriptor.defaultColumnGroups as ITableWidgetColumnGroups
 }
 
 export const getDefaultExportsConfiguration = () => {
-    return {
-        pdf: {
-            enabled: false,
-            custom: {
-                height: 0,
-                width: 0,
-                enabled: false
-            },
-            a4landscape: false,
-            a4portrait: false
-        },
-        showExcelExport: false,
-        showScreenshot: false
-    } as ITableWidgetExports
+    return descriptor.defaultExportsConfiguration as ITableWidgetExports
 }
 
 export const getDefaultHeadersConfiguration = () => {
-    return {
-        enabled: true, enabledMultiline: false, custom: { enabled: false, rules: [] }
-    } as ITableWidgetHeaders
+    return descriptor.defaultHeadersConfiguration as ITableWidgetHeaders
 }
 
 export const getDefaultRowsConfiguration = () => {
-    return {
-        indexColumn: false,
-        rowSpan: {
-            enabled: false,
-            column: ''
-        }
-    } as ITableWidgetRows
+    return descriptor.defaultRowsConfiguration as ITableWidgetRows
 }
 
 export const getDefaultSummaryRowsConfiguration = () => {
-    return {
-        enabled: false,
-        list: [],
-        style: { pinnedOnly: false }
-    } as ITableWidgetSummaryRows
+    return descriptor.defaultSummaryRowsConfiguration as ITableWidgetSummaryRows
 }
 
 export const getDefaultCustomMessages = () => {
-    return {
-        hideNoRowsMessage: true,
-        noRowsMessage: ''
-    } as ITableWidgetCustomMessages
+    return descriptor.defaultCustomMessages as ITableWidgetCustomMessages
 }
 
 export const getDefaultCrossNavigation = () => {
-    return {
-        enabled: false,
-        type: '',
-        column: '',
-        name: '',
-        parameters: []
-    } as ITableWidgetCrossNavigation
+    return descriptor.defaultCrossNavigation as ITableWidgetCrossNavigation
 }
 
 export const getDefaultLinks = () => {
-    return {
-        enabled: false,
-        links: []
-    } as ITableWidgetLinks
+    return descriptor.defaultLinks as ITableWidgetLinks
 }
 
 export const getDefaultPreview = () => {
-    return {
-        enabled: false,
-        type: '',
-        dataset: -1,
-        parameters: [],
-        directDownload: false
-    } as ITableWidgetPreview
+    return descriptor.defaultPreview as ITableWidgetPreview
 }
 
 
 export const getDefaultSelection = () => {
-    return {
-        enabled: false,
-        modalColumn: '',
-        multiselection: {
-            enabled: false,
-            properties: {
-                "background-color": '',
-                color: ''
-            }
-        }
-    } as ITableWidgetSelection
+    return descriptor.defaultSelection as ITableWidgetSelection
 }
 
 export const getDefaultPagination = () => {
-    return { enabled: false, itemsNumber: 0 } as ITableWidgetPagination
+    return descriptor.defaultPagination as ITableWidgetPagination
 }
 
 
 export const getDefaultBordersStyle = () => {
-    return {
-        enabled: false,
-        properties: {
-            "border-bottom-left-radius": "",
-            "border-bottom-right-radius": "",
-            "border-style": "",
-            "border-top-left-radius": "",
-            "border-top-right-radius": "",
-            "border-width": "",
-            "border-color": "rgb(212, 212, 212)"
-        }
-    } as ITableWidgetBordersStyle
+    return descriptor.defaultBordersStyle as ITableWidgetBordersStyle
 }
 
 export const getDefaultColumnStyles = () => {
-
-    return {
-        enabled: true,
-        styles: [{
-            target: 'all',
-            properties: {
-                "background-color": '',
-                color: '',
-                "justify-content": '',
-                "font-size": '',
-                "font-family": '',
-                "font-style": '',
-                "font-weight": ''
-            }
-        }]
-    } as ITableWidgetColumnStyles
+    return descriptor.defaultColumnStyles as ITableWidgetColumnStyles
 }
 
 export const getDefaultHeadersStyle = () => {
-    return {
-        height: 25,
-        properties: {
-            "background-color": "rgb(137, 158, 175)",
-            color: 'rgb(255, 255, 255)',
-            "justify-content": 'center',
-            "font-size": "14px",
-            "font-family": "",
-            "font-style": "normal",
-            "font-weight": "",
-        }
-    } as ITawbleWidgetHeadersStyle
+    return descriptor.defaultHeadersStyle as ITawbleWidgetHeadersStyle
 }
 
 export const getDefaultPaddingStyle = () => {
-    return {
-        enabled: false,
-        properties: {
-            "padding-top": '',
-            "padding-left": '',
-            "padding-bottom": '',
-            "padding-right": '',
-            unlinked: false
-        }
-    } as ITableWidgetPaddingStyle
+    return descriptor.defaultPaddingStyle as ITableWidgetPaddingStyle
 }
 
 export const getDefaultRowsStyle = () => {
-    return {
-        height: 0,
-        multiselectable: false,
-        selectionColor: '',
-        alternatedRows: {
-            enabled: false,
-            evenBackgroundColor: 'rgb(228, 232, 236)',
-            oddBackgroundColor: ''
-
-        }
-    } as ITableWidgetRowsStyle
+    return descriptor.defaultRowsStyle as ITableWidgetRowsStyle
 }
 
 export const getDefaultShadowsStyle = () => {
-    return {
-        enabled: false,
-        properties: {
-            "box-shadow": '',
-            "backgroundColor": ''
-        }
-    } as ITableWidgetShadowsStyle
+    return descriptor.defaultShadowsStyle as ITableWidgetShadowsStyle
 }
 
 
 export const getDefualtSummryStyle = () => {
-    return {
-        "background-color": "",
-        "color": "",
-        "font-family": "",
-        "font-size": "",
-        "font-style": "",
-        "font-weight": "",
-        "justify-content": ""
-    } as ITableWidgetSummaryStyle
+    return descriptor.defaultSummaryStyle as ITableWidgetSummaryStyle
 }
 
 export const getDefaultVisualizations = () => {
-    return {
-        visualizationTypes: {
-            enabled: false,
-            types: [{
-                target: 'all',
-                type: 'Text',
-                prefix: '',
-                suffix: '',
-                pinned: '',
-            }]
-        },
-        visibilityConditions: {
-            enabled: false,
-            conditions: []
-        }
-    } as ITableWidgetVisualization
+    return descriptor.defaultVisualizations as ITableWidgetVisualization
 }
 
 export const getDefaultVisibilityCondition = () => {
-    return {
-        target: [],
-        hide: false,
-        hidePdf: false,
-        condition: {
-            type: '',
-            value: ''
-        }
-    }
+    return descriptor.defaultVisibilityCondition as ITableWidgetVisibilityCondition
 }
 
 export const getDefaultTooltips = () => {
-    const allTooltip = {
-        target: 'all',
-        enabled: false,
-        prefix: '',
-        suffix: '',
-        precision: 0,
-        header: {
-            enabled: false,
-            text: ''
-        }
-    }
-    return [allTooltip] as ITableWidgetTooltipStyle[]
+    return descriptor.defaultTooltips as ITableWidgetTooltipStyle[]
 }
 
 export const getDefaultResponsivnes = () => {
-    return { xs: true, sm: true, md: true, lg: true, xl: true } as ITableWidgetResponsive
+    return descriptor.defaultResponsivnes as ITableWidgetResponsive
 }
