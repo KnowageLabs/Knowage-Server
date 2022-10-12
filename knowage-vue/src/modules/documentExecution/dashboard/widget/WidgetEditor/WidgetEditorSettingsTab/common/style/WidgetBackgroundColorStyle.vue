d9f271e6ed8a7815655ded3301be537965628f99
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-10-12 12:35:08 +0200

    BackgroundColor

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/common/style/WidgetBackgroundColorStyle.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/common/style/WidgetBackgroundColorStyle.vue
@@ -1,31 +1,44 @@
 <template>
-    <div class="p-grid p-jc-center p-ai-center kn-flex p-p-4">
+    <div v-if="backgroundStyleModel" class="p-grid p-jc-center p-ai-center kn-flex p-p-4">
+        <div class="p-col-12 p-d-flex p-flex-row p-ai-center p-mb-4">
+            <div class="kn-flex p-m-2">
+                <InputSwitch v-model="backgroundStyleModel.enabled" @change="backgroundColorStyleChanged"></InputSwitch>
+                <label class="kn-material-input-label p-ml-2">{{ $t('common.enable') }}</label>
+            </div>
+        </div>
+
         <div class="p-col-12">
-            <WidgetEditorColorPicker :initialValue="backgroundColor" :label="$t('dashboard.widgetEditor.iconTooltips.backgroundColor')" @change="onBackroundColorChanged"></WidgetEditorColorPicker>
+            <WidgetEditorColorPicker :initialValue="backgroundStyleModel.property['background-color']" :label="$t('dashboard.widgetEditor.iconTooltips.backgroundColor')" :disabled="backgroundStyleDisabled" @change="onBackroundColorChanged"></WidgetEditorColorPicker>
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import { IWidget } from '@/modules/documentExecution/Dashboard/Dashboard'
+import { IWidget, IWidgetBackgroundStyle } from '@/modules/documentExecution/Dashboard/Dashboard'
 import { emitter } from '../../../../../DashboardHelpers'
 import { getTranslatedLabel } from '@/helpers/commons/dropdownHelper'
 import descriptor from '../../WidgetEditorSettingsTabDescriptor.json'
+import InputSwitch from 'primevue/inputswitch'
 import WidgetEditorColorPicker from '../WidgetEditorColorPicker.vue'
 
 export default defineComponent({
     name: 'widget-background-color-style',
-    components: { WidgetEditorColorPicker },
+    components: { InputSwitch, WidgetEditorColorPicker },
     props: {
         widgetModel: { type: Object as PropType<IWidget>, required: true }
     },
     data() {
         return {
             descriptor,
-            backgroundColor: '' as string,
+            backgroundStyleModel: null as IWidgetBackgroundStyle | null,
             widgetType: '' as string,
             getTranslatedLabel
+        }
+    },
+    computed: {
+        backgroundStyleDisabled() {
+            return !this.backgroundStyleModel || !this.backgroundStyleModel.enabled
         }
     },
     created() {
@@ -35,19 +48,19 @@ export default defineComponent({
         loadBackgroundColor() {
             if (!this.widgetModel) return
             this.widgetType = this.widgetModel.type
-            if (this.widgetModel.settings?.style) this.backgroundColor = this.widgetModel.settings.style['background-color'] ?? ''
+            if (this.widgetModel.settings?.style?.background) this.backgroundStyleModel = this.widgetModel.settings.style.background
         },
         backgroundColorStyleChanged() {
-            emitter.emit('backgroundColorStyleChanged', this.backgroundColor)
+            emitter.emit('backgroundColorStyleChanged', this.backgroundStyleModel)
             switch (this.widgetType) {
                 case 'selector':
                     emitter.emit('refreshSelector', this.widgetModel.id)
             }
         },
         onBackroundColorChanged(event: string | null) {
-            if (event && this.widgetModel?.settings?.style) {
-                this.widgetModel.settings.style['background-color'] = event
-            }
+            if (!event || !this.backgroundStyleModel) return
+            this.backgroundStyleModel.property['background-color'] = event
+            this.backgroundColorStyleChanged()
         }
     }
 })
