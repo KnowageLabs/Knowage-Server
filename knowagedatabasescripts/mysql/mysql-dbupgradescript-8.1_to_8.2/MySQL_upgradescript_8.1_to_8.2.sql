19526af0d61342abcc7f7032691c2b423ea6febc
Alberto Nale <alberto.nale@eng.it>
2022-10-28 17:45:39 +0200

    [KNOWAGE-7496] - Removed configuration to enable/disable password encryption

--- knowagedatabasescripts/mysql/mysql-dbupgradescript-8.1_to_8.2/MySQL_upgradescript_8.1_to_8.2.sql
+++ knowagedatabasescripts/mysql/mysql-dbupgradescript-8.1_to_8.2/MySQL_upgradescript_8.1_to_8.2.sql
@@ -1,1 +1,3 @@
 UPDATE SBI_CONFIG SET CATEGORY = 'PASSWORD' WHERE LABEL IN ('MAIL.PROFILES.keyStore.password','MAIL.PROFILES.kpi_alarm.password','MAIL.PROFILES.scheduler.password','MAIL.PROFILES.user.password','MAIL.PROFILES.trustedStore.password');
+
+DELETE FROM SBI_CONFIG WHERE LABEL = 'internal.security.encript.password';
