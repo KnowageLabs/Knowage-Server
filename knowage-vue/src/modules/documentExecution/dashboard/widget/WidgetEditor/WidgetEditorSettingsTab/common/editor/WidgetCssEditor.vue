e44653701bc4330485828751b3b19612a91d4700
dbulatovicx32 <darko.bulatovic@eng.it>
2022-11-07 15:12:55 +0100

    added editors for html&css in widget

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/common/editor/WidgetCssEditor.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/common/editor/WidgetCssEditor.vue
@@ -1,20 +1,53 @@
 <template>
-    <div>CSS EDITOR</div>
+    <!-- <Button icon="fas fa-terminal" class="p-button-text p-button-rounded p-button-plain" @click="logModel" /> -->
+    <div class="cssMirrorContainer" style="height: 500px; width: 100%">
+        <VCodeMirror ref="codeMirrorCssEditor" v-model:value="widgetModel.settings.configuration.cssToRender" :options="scriptOptions" />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { IWidget } from '@/modules/documentExecution/Dashboard/Dashboard'
-import InputSwitch from 'primevue/inputswitch'
+import VCodeMirror from 'codemirror-editor-vue3'
 
 export default defineComponent({
     name: 'widget-responsive',
-    components: { InputSwitch },
+    components: { VCodeMirror },
     props: { widgetModel: { type: Object as PropType<IWidget>, required: true } },
     data() {
-        return {}
+        return {
+            codeMirrorCssEditor: null as any,
+            scriptOptions: {
+                cursor: true,
+                line: false,
+                lineNumbers: true,
+                indentWithTabs: true,
+                smartIndent: true,
+                lineWrapping: true,
+                matchBrackets: true,
+                mode: 'css',
+                tabSize: 4,
+                theme: 'eclipse'
+            }
+        }
     },
-    created() {},
-    methods: {}
+    created() {
+        this.setupCodeMirror()
+    },
+    methods: {
+        setupCodeMirror() {
+            const interval = setInterval(() => {
+                if (!this.$refs.codeMirrorCssEditor) return
+                this.codeMirrorCssEditor = (this.$refs.codeMirrorCssEditor as any).cminstance as any
+                setTimeout(() => {
+                    this.codeMirrorCssEditor.refresh()
+                }, 0)
+                clearInterval(interval)
+            }, 200)
+        },
+        logModel() {
+            console.log(this.widgetModel)
+        }
+    }
 })
 </script>
