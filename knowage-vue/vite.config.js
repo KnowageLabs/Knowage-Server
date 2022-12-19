7f6399fea6d196d4b1284001a250da5799af213e
Davide Vernassa <davide.vernassa@eng.it>
2022-12-19 16:35:20 +0100

    Added PWA capabilities to master

--- knowage-vue/vite.config.js
+++ knowage-vue/vite.config.js
@@ -3,6 +3,7 @@ const { resolve } = require('path')
 import vue from '@vitejs/plugin-vue'
 import builtins from 'rollup-plugin-node-builtins'
 import forwardToTrailingSlashPlugin from './forward-to-trailing-slash-plugin.js'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const path = require('path')
 
@@ -19,7 +20,60 @@ const build = {
 export default defineConfig((command, mode) => {
     const env = loadEnv(mode, process.cwd())
     return {
-        plugins: [vue(), builtinsPlugin, forwardToTrailingSlashPlugin(Object.keys(build.rollupOptions.input))],
+        plugins: [
+            vue(),
+            builtinsPlugin,
+            forwardToTrailingSlashPlugin(Object.keys(build.rollupOptions.input)),
+            VitePWA({
+                registerType: 'autoUpdate',
+                devOptions: {
+                    enabled: true
+                },
+                manifest: {
+                    name: 'Knowage',
+                    short_name: 'Knowage',
+                    start_url: '.',
+                    display: 'standalone',
+                    scope: '/knowage-vue/',
+                    orientation: 'landscape',
+                    background_color: '#3b678c',
+                    theme_color: '#3b678c',
+                    description: 'The business intelligence open-source solution',
+                    icons: [
+                        {
+                            src: 'icons/48.png',
+                            sizes: '48x48',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icons/72.png',
+                            sizes: '72x72',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icons/96.png',
+                            sizes: '96x96',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icons/144.png',
+                            sizes: '144x144',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icons/168.png',
+                            sizes: '168x168',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icons/192.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        }
+                    ]
+                }
+            })
+        ],
         define: {
             _KNOWAGE_VERSION: JSON.stringify(process.env.npm_package_version)
         },
@@ -48,6 +102,8 @@ export default defineConfig((command, mode) => {
             }
         },
         server: {
+            port: 3000,
+            host: 'localhost',
             https: env.VITE_HOST_HTTPS === 'true',
             proxy: {
                 '^/knowagedossierengine/api': {
