0fa3e3bf5d129b817f84ef1c013ce45b5a5c7dc5
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2022-12-28 14:59:47 +0100

    ActivityGauge

--- knowage-vue/src/modules/documentExecution/dashboard/widget/ChartWidget/classes/highcharts/updater/KnowageHighchartsActivityGaugeChartUpdater.ts
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/ChartWidget/classes/highcharts/updater/KnowageHighchartsActivityGaugeChartUpdater.ts
@@ -1,5 +1,5 @@
 import { IHighchartsChartModel } from '@/modules/documentExecution/dashboard/interfaces/highcharts/DashboardHighchartsWidget'
-import { getFormattedBandsSettings, getFormattedPaneSettings, getFormattedScaleSettings, getFormattedTickSettings } from './KnowageHighchartsCommonGaugeUpdater'
+import { getFormattedTickSettings } from './KnowageHighchartsCommonGaugeUpdater'
 import { getForamttedLabels, getFormattedLegend, getFormattedNoDataConfiguration, getFormattedSeries, getFormattedTooltipSettings } from './KnowageHighchartsCommonUpdater'
 
 export const updateActivityGaugeChartModel = (oldModel: any, newModel: IHighchartsChartModel) => {
@@ -10,7 +10,6 @@ export const updateActivityGaugeChartModel = (oldModel: any, newModel: IHighchar
     getForamttedLabels(oldModel, newModel)
     getFormattedSeries(oldModel, newModel)
     getFormattedTooltipSettings(oldModel, newModel)
-    getFormattedPaneSettings(oldModel, newModel)
     if (oldModel.CHART.AXES_LIST && oldModel.CHART.AXES_LIST.AXIS && oldModel.CHART.AXES_LIST.AXIS[0]) {
         getFormattedTickSettings(oldModel, newModel)
     }
