dfc2716735cc8052e739a771d794fba7d9ef2d9d
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-01-04 15:11:45 +0100

    TextWidget

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/TextWidget/TextWidgetSettingsAccordionHeader.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/TextWidget/TextWidgetSettingsAccordionHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-d-flex p-flex-row p-ai-center">
-        <InputSwitch v-if="model" class="p-mr-3" v-model="model.enabled" @click.stop="onModelChange"></InputSwitch>
+        <InputSwitch v-if="model" class="p-mr-3" v-model="model.enabled" @click.stop="() => {}"></InputSwitch>
         <label class="kn-material-input-label">{{ title ? $t(title) : '' }}</label>
     </div>
 </template>
@@ -8,13 +8,12 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { IWidget } from '@/modules/documentExecution/dashboard/Dashboard'
-import { emitter } from '@/modules/documentExecution/dashboard/DashboardHelpers'
 import Checkbox from 'primevue/checkbox'
 import Dropdown from 'primevue/dropdown'
 import InputSwitch from 'primevue/inputswitch'
 
 export default defineComponent({
-    name: 'selections-widget-settings-accordion-header',
+    name: 'text-widget-settings-accordion-header',
     components: { Checkbox, Dropdown, InputSwitch },
     props: { widgetModel: { type: Object as PropType<IWidget>, required: true }, title: { type: String }, type: { type: String, required: true } },
     data() {
@@ -35,8 +34,6 @@ export default defineComponent({
         loadModel() {
             if (!this.widgetModel || !this.widgetModel.settings) return null
             switch (this.type) {
-                case 'NoSelections':
-                    return this.widgetModel.settings.configuration.noSelections
                 case 'Title':
                     return this.widgetModel.settings.style.title
                 case 'LabelStyle':
@@ -49,15 +46,13 @@ export default defineComponent({
                     return this.widgetModel.settings.style.padding
                 case 'ShadowsStyle':
                     return this.widgetModel.settings.style.shadows
+                case 'CrossNavigation':
+                    return this.widgetModel.settings.interactions.crossNavigation
+                case 'Preview':
+                    return this.widgetModel.settings.interactions.preview
                 default:
                     return null
             }
-        },
-        onModelChange() {
-            if (this.type) this.refreshSelections()
-        },
-        refreshSelections() {
-            setTimeout(() => emitter.emit('refreshSelections', this.widgetModel.id), 250)
         }
     }
 })
