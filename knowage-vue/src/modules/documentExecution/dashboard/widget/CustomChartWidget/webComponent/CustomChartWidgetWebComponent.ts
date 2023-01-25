5df160114088a2e17e06d571402e0477a3dc8467
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-01-25 12:59:01 +0100

    Highcharts

--- knowage-vue/src/modules/documentExecution/dashboard/widget/CustomChartWidget/webComponent/CustomChartWidgetWebComponent.ts
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/CustomChartWidget/webComponent/CustomChartWidgetWebComponent.ts
@@ -20,33 +20,6 @@ class CustomChartWidgetWebComponent extends HTMLElement {
         wrapper.textContent = ''
         shadow.appendChild(style)
         shadow.appendChild(wrapper)
-
-        console.log('>>>>>>>>>>>>>>>>>>> TEST:', document)
-        console.log('>>>>>>>>>>>>>>>>>>> TEST 2:', shadow)
-        console.log('>>>>>>>>>>>>>>>>>>> TEST 3:', this)
-        const scr = document.createElement('script')
-        scr.type = 'text/javascript'
-        scr.text = 'alert("ALERT 1")'
-        scr.addEventListener('onload', () => {
-            console.log("IT'S LOADED")
-        })
-        var webCompEl = document.getElementById('webComponent')
-        console.log('>>>>>>>>>>>>>>>>>>> webCompEl:', webCompEl)
-        var JS = document.createElement('script')
-        window.bojanTest = 'bojan test web component'
-        window.bojanFunction = function () {
-            alert('THIS IS ALSO WORKING')
-        }
-
-        console.log('>>>>>>>> TYPE OF: ', typeof window.bojanFunction)
-        // JS.text = `alert('test')`
-        // JS.text = `alert(bojanTest)`
-        JS.text = `alert(bojanFunction())`
-
-        JS.text = "function test() {alert('stil working')} test()"
-        document.body.appendChild(JS)
-        // webCompEl?.body.appendChild(JS)
-        //  eval(`alert(dataToShow)`)
     }
 
     get htmlContent() {
@@ -56,7 +29,7 @@ class CustomChartWidgetWebComponent extends HTMLElement {
     set htmlContent(value: string) {
         if (this.shadowRoot) {
             const temp = this.shadowRoot.querySelector('.component-wrapper')
-            if (temp) temp.innerHTML = value
+            if (temp) temp.innerHTML = '<script src="https://code.highcharts.com/highcharts.js"></script>' + value
         }
     }
 
@@ -68,6 +41,42 @@ class CustomChartWidgetWebComponent extends HTMLElement {
         if (this.shadowRoot) {
             const temp = this.shadowRoot.querySelector('.style-wrapper')
             if (temp) temp.innerHTML = value
+        }
+    }
+
+    get webComponentJs() {
+        return this.htmlContent
+    }
+
+    set webComponentJs(value: string) {
+        if (this.shadowRoot) {
+            console.log("-------- js value: ", value)
+            console.log('>>>>>>>>>>>>>>>>>>> TEST:', document)
+            console.log('>>>>>>>>>>>>>>>>>>> TEST 2:', this)
+            const scr = document.createElement('script')
+            scr.type = 'text/javascript'
+            scr.text = 'alert("ALERT 1")'
+            scr.addEventListener('onload', () => {
+                console.log("IT'S LOADED")
+            })
+            var webCompEl = document.getElementById('webComponent')
+            console.log('>>>>>>>>>>>>>>>>>>> webCompEl:', webCompEl)
+            var JS = document.createElement('script')
+            // window.bojanTest = 'bojan test web component'
+            // window.bojanFunction = function () {
+            //     console.log('THIS IS ALSO WORKING')
+            // }
+
+            // console.log('>>>>>>>> TYPE OF: ', typeof window.bojanFunction)
+            // JS.text = `alert('test')`
+            // JS.text = `alert(bojanTest)`
+            //   JS.text = `console.log(bojanFunction())`
+
+            //JS.text = "function test() {console.log('stil working')} test()"
+            JS.text = value
+            document.body.appendChild(JS)
+            // webCompEl?.body.appendChild(JS)
+            //  eval(`alert(dataToShow)`)
         }
     }
 }
