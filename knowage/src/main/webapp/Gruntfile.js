209af1c8525b633baa36f564d74ca0551adfbbf8
Marco Libanori <marco.libanori@eng.it>
2023-01-25 19:10:17 +0100

    Fix execution of npm and grunt

--- knowage/src/main/webapp/Gruntfile.js
+++ knowage/src/main/webapp/Gruntfile.js
@@ -10,78 +10,78 @@ module.exports = function(grunt) {
           nonull: true,
         },
         basic_and_extras: {
-        	files: {
-	        	'dist/angular-bundle.js': [
-	        	  'js/lib/angular/angular_1.4/angular.min.js',
-	        	  'js/lib/angular/angular_1.4/angular-animate.min.js',
-	        	  'js/lib/angular/angular_1.4/angular-aria.min.js',
-	        	  'js/lib/angular/angular_1.4/angular-sanitize.min.js',
-	        	  'js/lib/angular/angular_1.4/angular-messages.min.js',
-	        	  'js/lib/angular/angular_1.4/angular-cookies.js'
-	        	  ],
-	        	 'dist/polyfills-bundle.js': [
-	        		 'polyfills/*/*polyfill.js',
-	        		 'polyfills/canvas-toBlob/canvas-toBlob.js'
-	              ],
-	        	 'dist/knowage-lib-bundle.js' : [
-	        		 'js/lib/angular/angular-material_1.1.0/angular-material.min.js',
-	        		 'js/lib/angular/angular-tree/angular-ui-tree.js',
-	        		 'js/lib/angular/contextmenu/ng-context-menu.js',
-	        		 'js/lib/angular/pagination/dirPagination.js',
-	        		 'js/lib/angular/expander-box/expanderBox.js',
-	        		 'js/lib/angular/color-picker/tinycolor-min.js',
-	        		 'js/lib/angular/color-picker/tinygradient.min.js',
-	        		 'js/lib/angular/color-picker/angularjs-color-picker.js',
-	        		 'js/lib/xml2js/xml2json.js',
-	        		 'js/lib/angular/angular-base64/angular-base64.min.js'
-	        	 ],
-	        	 'dist/knowage-modules-bundle.js': [
-	        		 'node_modules/moment/min/moment-with-locales.min.js',
-	        		 'node_modules/toastify-js/src/toastify.js',
-	        		 'node_modules/jsonformatter/dist/json-formatter.min.js',
-	        		 'node_modules/xregexp/xregexp-all.js',
-	        		 'node_modules/angular-xregexp/angular-xregexp.js',
-	        		 'node_modules/angular-tree-control/angular-tree-control.js',
-	        		 'node_modules/angular-tree-control/context-menu.js',
-	        		 'node_modules/ng-wysiwyg/dist/wysiwyg.min.js',
-	        		 'node_modules/ag-grid-community/dist/ag-grid-community.min.js'
-	        	 ],
-	        	 'dist/knowage-modules-styles-bundle.css' : [
-	        		 'themes/commons/css/reset_2018.css',
-	        		 'node_modules/@fortawesome/fontawesome-free/css/v4-shims.min.css',
-	        		 'js/lib/angular/angular-material_1.1.0/angular-material.min.css',
-	        		 'js/lib/angular/angular-tree/angular-ui-tree.min.css',
-	        		 'node_modules/toastify-js/src/toastify.css',
-	        		 'themes/sbi_default/css/angular-toastr.css',
-	        		 'js/lib/angular/color-picker/angularjs-color-picker.min.css',
-	        		 'js/lib/angular/color-picker/mdColorPickerPersonalStyle.css',
-	        		 'node_modules/angular-tree-control/css/tree-control.css'
-	        	 ],
-	        	 'dist/knowage-sources-bundle_<%= pkg.version %>.js' : [
-	        		 'js/src/angular_1.4/tools/commons/sbiModule_services/sbiModule_dateServices.js',
-	        		 'js/src/angular_1.4/tools/commons/AngularList.js',
-	        		 'js/src/angular_1.4/tools/commons/angular-table/AngularTable.js',
-	        		 'js/src/angular_1.4/tools/commons/angular-table/utils/daff.js',
-	        		 'js/src/angular_1.4/tools/commons/kn-table/knTable.js',
-	        		 'js/src/angular_1.4/tools/commons/document-tree/DocumentTree.js',
-	        		 'js/src/angular_1.4/tools/commons/component-tree/componentTree.js',
-	        		 'js/src/angular_1.4/tools/commons/upload-file/FileUpload.js',
-	        		 'js/src/angular_1.4/tools/commons/upload-file/FileUploadBase64.js',
-	        		 'js/src/angular_1.4/tools/commons/angular-time-picker/angularTimePicker.js',
-	        		 'js/src/angular_1.4/tools/commons/angular-list-detail/angularListDetail.js',
-	        		 'js/src/angular_1.4/tools/commons/angular-list-detail/angular2Col.js',
-	        		 'js/src/angular_1.4/tools/commons/angular-toastr.tpls.js',
-	        		 'js/src/angular_1.4/tools/workspace/scripts/services/qbeViewer.js',
-	        		 'js/src/angular_1.4/tools/workspace/scripts/services/qbeViewerCommunicationService.js',
-	        		 'js/src/angular_1.4/tools/workspace/scripts/directive/dataset-save/datasetSaveModule.js',
-	        		 'js/src/angular_1.4/tools/workspace/scripts/directive/dataset-save/datasetSave.js',
-	        		 'js/src/angular_1.4/tools/workspace/scripts/services/datasetSave_service.js',
-	        		 'js/src/angular_1.4/tools/scheduler/dataset-scheduler/datasetSchedulerModule.js',
-	        		 'js/src/angular_1.4/tools/scheduler/dataset-scheduler/datasetScheduler_service.js',
-	        		 'js/src/angular_1.4/tools/scheduler/dataset-scheduler/schedulerTimeUnit.js',
-	        		 'js/src/angular_1.4/tools/commons/services/knModule_aggridLabels.js'
-	        	 ]
-        	}
+            files: {
+                'dist/angular-bundle.js': [
+                  'js/lib/angular/angular_1.4/angular.min.js',
+                  'js/lib/angular/angular_1.4/angular-animate.min.js',
+                  'js/lib/angular/angular_1.4/angular-aria.min.js',
+                  'js/lib/angular/angular_1.4/angular-sanitize.min.js',
+                  'js/lib/angular/angular_1.4/angular-messages.min.js',
+                  'js/lib/angular/angular_1.4/angular-cookies.js'
+                  ],
+                 'dist/polyfills-bundle.js': [
+                     'polyfills/*/*polyfill.js',
+                     'polyfills/canvas-toBlob/canvas-toBlob.js'
+                  ],
+                 'dist/knowage-lib-bundle.js' : [
+                     'js/lib/angular/angular-material_1.1.0/angular-material.min.js',
+                     'js/lib/angular/angular-tree/angular-ui-tree.js',
+                     'js/lib/angular/contextmenu/ng-context-menu.js',
+                     'js/lib/angular/pagination/dirPagination.js',
+                     'js/lib/angular/expander-box/expanderBox.js',
+                     'js/lib/angular/color-picker/tinycolor-min.js',
+                     'js/lib/angular/color-picker/tinygradient.min.js',
+                     'js/lib/angular/color-picker/angularjs-color-picker.js',
+                     'js/lib/xml2js/xml2json.js',
+                     'js/lib/angular/angular-base64/angular-base64.min.js'
+                 ],
+                 'dist/knowage-modules-bundle.js': [
+                     'node_modules/moment/min/moment-with-locales.min.js',
+                     'node_modules/toastify-js/src/toastify.js',
+                     'node_modules/jsonformatter/dist/json-formatter.min.js',
+                     'node_modules/xregexp/xregexp-all.js',
+                     'node_modules/angular-xregexp/angular-xregexp.js',
+                     'node_modules/angular-tree-control/angular-tree-control.js',
+                     'node_modules/angular-tree-control/context-menu.js',
+                     'node_modules/ng-wysiwyg/dist/wysiwyg.min.js',
+                     'node_modules/ag-grid-community/dist/ag-grid-community.min.js'
+                 ],
+                 'dist/knowage-modules-styles-bundle.css' : [
+                     'themes/commons/css/reset_2018.css',
+                     'node_modules/@fortawesome/fontawesome-free/css/v4-shims.min.css',
+                     'js/lib/angular/angular-material_1.1.0/angular-material.min.css',
+                     'js/lib/angular/angular-tree/angular-ui-tree.min.css',
+                     'node_modules/toastify-js/src/toastify.css',
+                     'themes/sbi_default/css/angular-toastr.css',
+                     'js/lib/angular/color-picker/angularjs-color-picker.min.css',
+                     'js/lib/angular/color-picker/mdColorPickerPersonalStyle.css',
+                     'node_modules/angular-tree-control/css/tree-control.css'
+                 ],
+                 'dist/knowage-sources-bundle_<%= pkg.version %>.js' : [
+                     'js/src/angular_1.4/tools/commons/sbiModule_services/sbiModule_dateServices.js',
+                     'js/src/angular_1.4/tools/commons/AngularList.js',
+                     'js/src/angular_1.4/tools/commons/angular-table/AngularTable.js',
+                     'js/src/angular_1.4/tools/commons/angular-table/utils/daff.js',
+                     'js/src/angular_1.4/tools/commons/kn-table/knTable.js',
+                     'js/src/angular_1.4/tools/commons/document-tree/DocumentTree.js',
+                     'js/src/angular_1.4/tools/commons/component-tree/componentTree.js',
+                     'js/src/angular_1.4/tools/commons/upload-file/FileUpload.js',
+                     'js/src/angular_1.4/tools/commons/upload-file/FileUploadBase64.js',
+                     'js/src/angular_1.4/tools/commons/angular-time-picker/angularTimePicker.js',
+                     'js/src/angular_1.4/tools/commons/angular-list-detail/angularListDetail.js',
+                     'js/src/angular_1.4/tools/commons/angular-list-detail/angular2Col.js',
+                     'js/src/angular_1.4/tools/commons/angular-toastr.tpls.js',
+                     'js/src/angular_1.4/tools/workspace/scripts/services/qbeViewer.js',
+                     'js/src/angular_1.4/tools/workspace/scripts/services/qbeViewerCommunicationService.js',
+                     'js/src/angular_1.4/tools/workspace/scripts/directive/dataset-save/datasetSaveModule.js',
+                     'js/src/angular_1.4/tools/workspace/scripts/directive/dataset-save/datasetSave.js',
+                     'js/src/angular_1.4/tools/workspace/scripts/services/datasetSave_service.js',
+                     'js/src/angular_1.4/tools/scheduler/dataset-scheduler/datasetSchedulerModule.js',
+                     'js/src/angular_1.4/tools/scheduler/dataset-scheduler/datasetScheduler_service.js',
+                     'js/src/angular_1.4/tools/scheduler/dataset-scheduler/schedulerTimeUnit.js',
+                     'js/src/angular_1.4/tools/commons/services/knModule_aggridLabels.js'
+                 ]
+            }
         }
     },
     uglify: {
@@ -99,12 +99,12 @@ module.exports = function(grunt) {
         }
       },
       cssmin: {
-    	  target: {
-			  files: {
-				  'dist/knowage-modules-styles-bundle.min.css': ['dist/knowage-modules-styles-bundle.css']
-		    }
-    	  }
-    	}
+          target: {
+              files: {
+                  'dist/knowage-modules-styles-bundle.min.css': ['dist/knowage-modules-styles-bundle.css']
+            }
+          }
+        }
   });
 
   // Load the plugin that provides the "concat" task.
