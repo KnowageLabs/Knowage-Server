f15b468293a1ac5de68afd56d15f25fd0f7e4e9b
Alberto Nale <alberto.nale@eng.it>
2023-02-15 14:48:19 +0100

    First commit with ESLint

--- knowage-vue/.eslintrc.js
+++ knowage-vue/.eslintrc.js
@@ -1,13 +1,3 @@
-module.exports = {
-    env: {
-        es2021: true,
-        jest: true
-    },
-    globals: {
-        process: true
-    },
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript'],
-    rules: {
-        '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }]
-    }
-}
+module.exports = { env: { node: true, es2021: true, jest: true }, globals: { process: true, defineProps: 'readonly', defineEmits: 'readonly', withDefaults: 'readonly' } , 'ignorePatterns': ['**/*.spec.js'], extends: ['@vue/typescript/recommended', 'eslint:recommended', 'plugin:vue/vue3-recommended',
+'prettier'], rules: {'no-unused-vars': ['off'], '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }], 'vue/component-definition-name-casing': ['error', 'kebab-case'], '@typescript-eslint/ban-ts-comment':['off'],'@typescript-eslint/no-explicit-any':['off'],
+'@typescript-eslint/no-empty-function':['off'] , '@typescript-eslint/ban-types' :[ 'error', { 'types': {'Function': false, 'Object':false }, 'extendDefaults': true } ], 'vue/multi-word-component-names':['off'] } }
