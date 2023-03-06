688a60ddbc6bc96bf133b14cbcf13f4286ba51c7
Matteo Massarotto <matteo.massarotto@eng.it>
2023-03-06 16:07:18 +0100

    [KNOWAGE-7575] - Fixed persisted dataset datasource retrieving

--- knowageqbeengine/src/main/java/it/eng/spagobi/engines/qbe/services/initializers/QbeEngineStartResource.java
+++ knowageqbeengine/src/main/java/it/eng/spagobi/engines/qbe/services/initializers/QbeEngineStartResource.java
@@ -198,7 +198,8 @@ public class QbeEngineStartResource extends AbstractQbeEngineResource {
 	protected void addDatasetToEnvEngine(String sourceDatasetLabel, Map env) {
 
 		IDataSet dataset = this.getDataSetServiceProxy().getDataSetByLabelAndUserCategories(sourceDatasetLabel);
-		dataset.setDataSourceForReading(dataset.getDataSource());
+		dataset.setDataSourceForReading(dataset.getDataSource() != null ? dataset.getDataSource() : dataset.getDataSourceForReading());
+
 		if (!dataset.isPersisted() && !dataset.isFlatDataset() && !dataset.toSpagoBiDataSet().getType().equals("SbiFileDataSet") && dataset.getDsType() != null
 				&& !dataset.getDsType().equals("SbiQueryDataSet")) {
 			logger.error("Dataset [" + sourceDatasetLabel + "] is not persisted. Cannot start qbe.");
