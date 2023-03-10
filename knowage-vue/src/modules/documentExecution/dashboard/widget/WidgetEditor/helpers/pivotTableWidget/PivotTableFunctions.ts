9e93ac32bf87f66f118704043aab993d40fcc534
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-03-10 14:32:51 +0100

    removing from pivot column/field style

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/helpers/pivotTableWidget/PivotTableFunctions.ts
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/helpers/pivotTableWidget/PivotTableFunctions.ts
@@ -23,7 +23,8 @@ export const createNewPivotTableWidgetSettings = () => {
         },
         style: {
             title: widgetCommonDefaultValues.getDefaultTitleStyle(),
-            fields: { enabled: false, styles: [] },
+            fields: pivotTableDefaultValues.getDefaultColumnStyles(),
+            fieldHeaders: pivotTableDefaultValues.getDefaultColumnStyles(),
             background: widgetCommonDefaultValues.getDefaultBackgroundStyle(),
             borders: widgetCommonDefaultValues.getDefaultBordersStyle(),
             padding: widgetCommonDefaultValues.getDefaultPaddingStyle(),
@@ -40,6 +41,8 @@ export const createNewPivotTableWidgetSettings = () => {
 
 export const removeColumnFromPivotTableWidgetModel = (widgetModel: IWidget, column: IWidgetColumn) => {
     removeColumnFromConditionalStyles(widgetModel, column)
+    removeColumnFromSubmodel(column, widgetModel.settings.style.fields.styles, 'target', 'columnRemovedFromColumnStyle', true)
+    removeColumnFromSubmodel(column, widgetModel.settings.style.fieldHeaders.styles, 'target', 'columnRemovedFromColumnStyle', true)
     removeColumnFromSubmodel(column, widgetModel.settings.visualization.visualizationTypes.types, 'target', 'columnRemovedFromVisualizationTypes', true)
     removeColumnFromSubmodel(column, widgetModel.settings.tooltips, 'target', 'columnRemovedFromTooltips', true)
 }
@@ -47,7 +50,6 @@ export const removeColumnFromPivotTableWidgetModel = (widgetModel: IWidget, colu
 const removeColumnFromConditionalStyles = (widgetModel: IWidget, column: IWidgetColumn) => {
     const conditionalStyles = widgetModel.settings.conditionalStyles.conditions
     for (let i = conditionalStyles.length - 1; i >= 0; i--) {
-        console.log(conditionalStyles[i].target + ' ===  ' + column.id)
         if (conditionalStyles[i].target === column.id) conditionalStyles.splice(i, 1)
     }
 }
