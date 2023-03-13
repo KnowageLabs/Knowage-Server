ce6d8a4f17c848659afd82c6c5d6ba7acb24a7ad
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-03-13 14:42:12 +0100

    Color Picker bugfix

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/helpers/WidgetEditorHelpers.ts
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/helpers/WidgetEditorHelpers.ts
@@ -107,8 +107,9 @@ export function getRGBColorFromString(color: string) {
         ?.substring(5, color.length - 1)
         ?.split(',')
 
+    const alpha = isNaN(+temp[3]) ? 0 : +temp[3]
     if (temp) {
-        return { r: +temp[0], g: +temp[1], b: +temp[2], a: +temp[3] }
+        return { r: +temp[0], g: +temp[1], b: +temp[2], a: alpha }
     } else return { r: 0, g: 0, b: 0, a: 0 }
 }
 
