63f694b61f18891a3480326ce70dff5800e0c4c6
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-04-12 12:44:23 +0200

    LayersList Drag N Drop

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/MapWidget/MapWidgetLayersTabList.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/MapWidget/MapWidgetLayersTabList.vue
@@ -5,10 +5,20 @@
             <Button :label="$t('workspace.gis.dnl.addLayer')" icon="pi pi-plus-circle" class="p-button-outlined p-ml-auto p-mr-1" @click="openLayersDialog" />
         </div>
 
-        <Listbox class="kn-list kn-list-no-border-right dashboard-editor-list" :options="widgetModel.layers" :filter="true" :filter-placeholder="$t('common.search')" :filter-fields="dataListDescriptor.filterFields" :empty-filter-message="$t('common.info.noDataFound')">
+        <Listbox class="kn-list kn-list-no-border-right dashboard-editor-list" :options="layers" :filter="true" :filter-placeholder="$t('common.search')" :filter-fields="dataListDescriptor.filterFields" :empty-filter-message="$t('common.info.noDataFound')">
             <template #empty>{{ $t('common.info.noDataFound') }}</template>
             <template #option="slotProps">
-                <div class="kn-list-item kn-draggable" draggable="true" :style="dataListDescriptor.style.list.listItem" @dragstart="onDragStart($event, slotProps.option)" @click="$emit('layerSelected', slotProps.option)">
+                <div
+                    class="kn-list-item kn-draggable"
+                    draggable="true"
+                    :style="dataListDescriptor.style.list.listItem"
+                    @dragstart="onDragStart($event, slotProps.index)"
+                    @drop.stop="onDropComplete($event, slotProps.index)"
+                    @dragover.prevent
+                    @dragenter.prevent
+                    @dragleave.prevent
+                    @click="$emit('layerSelected', slotProps.option)"
+                >
                     <i class="pi pi-bars" :style="dataListDescriptor.style.list.listIcon"></i>
                     <i :style="dataListDescriptor.style.list.listIcon" :class="slotProps.option.fieldType === 'ATTRIBUTE' ? 'fas fa-font' : 'fas fa-hashtag'" class="p-ml-2"></i>
                     <div class="kn-list-item-text">
@@ -25,7 +35,7 @@
 <script lang="ts">
 import { PropType, defineComponent } from 'vue'
 import { IDataset, IWidget } from '../../../Dashboard'
-
+import { IMapWidgetLayer } from '../../../interfaces/mapWidget/DashboardMapWidget'
 import LayersDialog from './MapWidgetLayersTabDialog.vue'
 import Listbox from 'primevue/listbox'
 import dataListDescriptor from './MapWidgetLayersTabListDescriptor.json'
@@ -53,21 +63,33 @@ export default defineComponent({
     data() {
         return {
             dataListDescriptor,
+            layers: [] as IMapWidgetLayer[],
             layersDialogVisible: false
         }
     },
-    watch: {},
-    created() {},
-    unmounted() {},
+    created() {
+        this.loadLayers()
+    },
     methods: {
+        loadLayers() {
+            this.layers = this.widgetModel.layers
+        },
         openLayersDialog() {
             this.layersDialogVisible = true
         },
         closeLayersDialog() {
             this.layersDialogVisible = false
         },
-        onDragStart(event, datasetColumn) {
-            console.log(event, datasetColumn)
+        onDragStart(event: any, startIndex: number) {
+            event.dataTransfer.setData('text/plain', JSON.stringify(startIndex))
+            event.dataTransfer.dropEffect = 'move'
+            event.dataTransfer.effectAllowed = 'move'
+        },
+        onDropComplete(event: any, dropIndex: number) {
+            const eventData = JSON.parse(event.dataTransfer.getData('text/plain'))
+            const temp = this.widgetModel.layers[eventData]
+            this.layers.splice(eventData, 1)
+            this.layers.splice(dropIndex, 0, temp)
         }
     }
 })
