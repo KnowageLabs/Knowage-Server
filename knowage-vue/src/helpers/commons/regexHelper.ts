4370549b7bec5eb21cdca3d5eac24d9fe3790e24
Davide Vernassa <davide.vernassa@eng.it>
2023-04-13 17:40:18 +0200

    [KNOWAGE-7841] Added @ to user name regex

--- knowage-vue/src/helpers/commons/regexHelper.ts
+++ knowage-vue/src/helpers/commons/regexHelper.ts
@@ -1,11 +1,9 @@
 // eslint-disable-next-line no-useless-escape
 export const extendedAlphanumeric = /^([\p{L}\w\s\-\\_\\;\\(\\)\\[\]\\:\\!\\\/?\\,\\.\\'\\"\\x2F\\x5F%])*$/u
 
-// eslint-disable-next-line no-useless-escape
-export const fullnameRegex = /^[\p{L}\w]+([\p{L}\w\s\-\\,\\.\\'\\x2F])*$/u
+export const fullnameRegex = /^[\p{L}\w]+([\p{L}\w\s\-,.'\x2F\\@])*$/u
 
-// eslint-disable-next-line no-useless-escape
-export const alphanumericNoSpaces = /^([a-zA-Z0-9\-\_])*$/
+export const alphanumericNoSpaces = /^([a-zA-Z0-9\-_])*$/
 
 // eslint-disable-next-line no-useless-escape
 export const alphanumeric = /^([a-zA-Z0-9\\s\\-\\_])*$/
