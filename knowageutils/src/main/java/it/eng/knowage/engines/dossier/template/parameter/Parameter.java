fa3e6e199bcf7cfff348006aec73dacec0ffe30a
Matteo Massarotto <matteo.massarotto@eng.it>
2023-04-17 11:29:59 +0200

    [KNOWAGE_TM-515] - Restored old Parameter class

--- knowageutils/src/main/java/it/eng/knowage/engines/dossier/template/parameter/Parameter.java
+++ knowageutils/src/main/java/it/eng/knowage/engines/dossier/template/parameter/Parameter.java
@@ -1,30 +1,7 @@
-/*
- * Knowage, Open Source Business Intelligence suite
- * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- *
- * Knowage is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Knowage is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package it.eng.knowage.engines.dossier.template.parameter;
 
-import java.util.Objects;
-
-import javax.xml.bind.annotation.XmlAttribute;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Parameter {
 
 	private String type;
@@ -33,7 +10,6 @@ public class Parameter {
 	private String value;
 	private String urlNameDescription;
 
-	@XmlAttribute
 	public String getType() {
 		return type;
 	}
@@ -42,7 +18,6 @@ public class Parameter {
 		this.type = type;
 	}
 
-	@XmlAttribute
 	public String getUrlName() {
 		return urlName;
 	}
@@ -52,7 +27,6 @@ public class Parameter {
 		this.urlName = urlName;
 	}
 
-	@XmlAttribute
 	public String getDossierUrlName() {
 		return dossierUrlName;
 	}
@@ -62,7 +36,6 @@ public class Parameter {
 		this.dossierUrlName = dossierUrlName;
 	}
 
-	@XmlAttribute
 	public String getValue() {
 		return value;
 	}
@@ -84,38 +57,6 @@ public class Parameter {
 	@JsonSetter("url_name_description")
 	public void setUrlNameDescription(String urlNameDescription) {
 		this.urlNameDescription = urlNameDescription;
-	}
-
-	public Parameter(String type, String urlName, String dossierUrlName, String value, String urlNameDescription) {
-		super();
-		if (type != null)
-			this.type = type;
-		if (urlName != null)
-			this.urlName = urlName;
-		if (dossierUrlName != null)
-			this.dossierUrlName = dossierUrlName;
-		if (value != null)
-			this.value = value;
-		if (urlNameDescription != null)
-			this.urlNameDescription = urlNameDescription;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(dossierUrlName, type, urlName, urlNameDescription, value);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Parameter other = (Parameter) obj;
-		return Objects.equals(dossierUrlName, other.dossierUrlName) && Objects.equals(type, other.type) && Objects.equals(urlName, other.urlName)
-				&& Objects.equals(urlNameDescription, other.urlNameDescription) && Objects.equals(value, other.value);
 	}
 
 }
