4d76116395934d924e66934df155a4925effeda9
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-04-19 10:23:12 +0200

    Visualization Type

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/MapWidget/visualization/MapVisualizationType.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/MapWidget/visualization/MapVisualizationType.vue
@@ -2,10 +2,11 @@
     <div v-for="(visType, visTypeIndex) in visualizationTypeModel" :key="visTypeIndex" class="p-d-flex p-flex-column p-m-2">
         <div class="p-d-flex kn-flex p-ai-center p-mb-2">
             <span class="p-float-label kn-flex">
-                <Dropdown id="attributes" v-model="dropdownSelectedLayer" class="kn-material-input kn-width-full" :options="dropdownAvailableLayers" option-label="name" option-value="name" />
-                <label for="attributes" class="kn-material-input-label"> {{ $t('documentExecution.documentDetails.info.attribute') }} </label>
+                <MultiSelect id="attributes" v-model="visType.target" class="kn-material-input kn-width-full" :options="widgetModel.layers" option-label="name" option-value="name" />
+                <label for="attributes" class="kn-material-input-label"> {{ $t('common.layers') }} </label>
             </span>
-            <Button v-if="visTypeIndex == 0" icon="fas fa-plus-circle fa-1x" class="p-button-text p-button-plain p-js-center p-ml-2" />
+            <Button v-if="visTypeIndex == 0" icon="fas fa-plus-circle fa-1x" class="p-button-text p-button-plain p-js-center p-ml-2" @click="addVisualizationType" />
+            <Button icon="pi pi-trash kn-cursor-pointer" class="p-button-text p-button-plain p-js-center p-ml-2" @click="removeVisualizationType(visTypeIndex)" />
         </div>
 
         <div class="p-grid gap-1 p-m-0" style="column-gap: 0.5em; row-gap: 0.5em">
@@ -16,11 +17,7 @@
 
         <hr class="kn-width-full p-my-2" />
 
-        <VisTypeConfig :vis-type-prop="visType" />
-
-        <br />
-        <br />
-        <br />
+        <VisTypeConfig :vis-type-prop="visType" class="p-mb-2" />
     </div>
 </template>
 
@@ -28,24 +25,20 @@
 import { IWidget } from '@/modules/documentExecution/dashboard/Dashboard'
 import { IMapWidgetVisualizationType } from '@/modules/documentExecution/dashboard/interfaces/mapWidget/DashboardMapWidget'
 import { defineComponent, PropType } from 'vue'
-
 import descriptor from './MapVisualizationTypeDescriptor.json'
-
-import Dropdown from 'primevue/dropdown'
+import MultiSelect from 'primevue/multiselect'
 import VisTypeConfig from './MapVisualizationTypeConfigurations.vue'
+import * as mapWidgetDefaultValues from '../../../../WidgetEditor/helpers/mapWidget/MapWidgetDefaultValues'
 
 export default defineComponent({
     name: 'map-visualization-type',
-    components: { Dropdown, VisTypeConfig },
+    components: { MultiSelect, VisTypeConfig },
     props: { widgetModel: { type: Object as PropType<IWidget>, required: true } },
     emits: [],
     data() {
         return {
             descriptor,
-            visualizationTypeModel: [] as IMapWidgetVisualizationType[],
-            dropdownSelectedLayer: null as any,
-            //TODO: hardcoded, implement real layers from widget
-            dropdownAvailableLayers: [{ name: 'Layer 1' }, { name: 'Layer 2' }]
+            visualizationTypeModel: [] as IMapWidgetVisualizationType[]
         }
     },
     watch: {
@@ -59,13 +52,31 @@ export default defineComponent({
     methods: {
         loadVisTypeModel() {
             if (this.widgetModel.settings?.visualization?.types) this.visualizationTypeModel = this.widgetModel.settings?.visualization?.types as IMapWidgetVisualizationType[]
-            console.log('visualizationTypeModel', this.visualizationTypeModel)
         },
         getImageSource(visType: string) {
             return `${import.meta.env.VITE_PUBLIC_PATH}images/dashboard/mapVisTypes/${visType}.svg`
         },
         selectVisTypeConfig(visTypeIndex, visTypeConfigName) {
             this.visualizationTypeModel[visTypeIndex].type = visTypeConfigName
+        },
+        addVisualizationType() {
+            this.visualizationTypeModel.push(this.createDefaultVisualizationType())
+        },
+        removeVisualizationType(index: number) {
+            if (index === 0) this.visualizationTypeModel[0] = this.createDefaultVisualizationType()
+            else this.visualizationTypeModel.splice(index, 1)
+        },
+        createDefaultVisualizationType() {
+            return {
+                target: [],
+                type: 'markers',
+                markerConf: mapWidgetDefaultValues.getDefaultVisualizationMarkerConfiguration(),
+                balloonConf: mapWidgetDefaultValues.getDefaultVisualizationBalloonsConfiguration(),
+                pieConf: mapWidgetDefaultValues.getDefaultVisualizationPieConfiguration(),
+                clusterConf: mapWidgetDefaultValues.getDefaultVisualizationClusterConfiguration(),
+                heatmapConf: mapWidgetDefaultValues.getDefaultVisualizationHeatmapConfiguration(),
+                analysisConf: mapWidgetDefaultValues.getDefaultVisualizationChoroplethConfiguration()
+            }
         }
     }
 })
