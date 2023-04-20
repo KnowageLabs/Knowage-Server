06e0fb3320be9e18a83e867f51a9c6ddde725785
Davide Vernassa <davide.vernassa@eng.it>
2023-04-20 10:05:48 +0200

    Added dashboard "add widget" Images and translations

--- knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetPicker/WidgetPickerCard.vue
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/WidgetPicker/WidgetPickerCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div id="widget-card" class="p-m-2" :style="descriptor.style.widgetCard">
-        <div id="widget-card-icon-container" class="p-d-flex p-ai-center p-jc-center" :style="descriptor.style.cardIconContainer as any">
-            <i :class="widget.cssClass" :style="descriptor.style.mainIcon" />
+    <div id="widget-card" class="p-m-2" :style="descriptor.style.widgetCard" :title="$t(`dashboard.widgets.${widget.type}.description`)">
+        <div id="widget-card-icon-container" class="p-d-flex p-ai-center p-jc-center">
+            <div class="innerIcon" :style="documentImageSource()"></div>
         </div>
         <div class="p-d-flex p-flex-column p-ai-start p-jc-center">
-            <span class="p-ml-2">{{ widget.name }}</span>
+            <span class="p-ml-2 widgetTitle">{{ $t(`dashboard.widgets.${widget.type}.title`) }}</span>
         </div>
     </div>
 </template>
@@ -27,17 +27,54 @@ export default defineComponent({
             descriptor
         }
     },
-    computed: {},
-    created() {},
-    methods: {}
+    methods: {
+        documentImageSource(): any {
+            return {
+                'mask-image': `url(${descriptor.imagePath}${this.widget.type}${descriptor.imageExtension})`,
+                '-webkit-mask-image': `url(${descriptor.imagePath}${this.widget.type}${descriptor.imageExtension})`
+            }
+        }
+    }
 })
 </script>
 <style lang="scss" scoped>
-#widget-card:hover {
-    border-color: #43749e !important;
+#widget-card {
+    cursor: pointer;
+    .widgetTitle {
+        text-transform: capitalize;
+        font-size: 1.1rem;
+    }
     #widget-card-icon-container {
-        border-color: #43749e !important;
-        transition: 0.5s ease;
+        background-color: var(--kn-color-secondary);
+        border-right: 1px solid var(--kn-color-borders);
+        transition: 0.2s ease-in;
+        .innerIcon {
+            width: 100px;
+            height: 100%;
+            mask-size: 80%;
+            mask-repeat: no-repeat;
+            mask-position: 180% 180%;
+            -webkit-mask-size: 80%;
+            -webkit-mask-repeat: no-repeat;
+            -webkit-mask-position: 180% 180%;
+            background-repeat: no-repeat;
+            background-color: var(--kn-color-primary);
+            position: relative;
+            overflow: hidden;
+            cursor: pointer;
+            transition: 0.2s ease-in;
+        }
+    }
+    &:hover {
+        border-color: var(--kn-color-primary) !important;
+        #widget-card-icon-container {
+            background-color: var(--kn-color-primary);
+            .innerIcon {
+                background-color: white;
+                mask-position: center;
+                -webkit-mask-position: center;
+            }
+        }
     }
 }
 </style>
