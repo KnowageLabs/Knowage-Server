b5d175ce9bb91d90a067235b8ae7905cb4b3d44f
dbulatovicx32 <darko.bulatovic@eng.it>
2023-09-01 12:12:48 +0200

    Wrapped with Try block
    Wrapped requested code with try block.

--- knowage-core/src/main/java/it/eng/spagobi/analiticalmodel/document/service/ManagePreviewFileAction.java
+++ knowage-core/src/main/java/it/eng/spagobi/analiticalmodel/document/service/ManagePreviewFileAction.java
@@ -82,13 +82,14 @@ public class ManagePreviewFileAction extends AbstractSpagoBIAction {
 				if (!file.exists()) {
 					writeBackToClient(404, "File not found.", false, null, "text/plain");
 				} else {
-					FileInputStream fis = new FileInputStream(file);
-					HttpServletResponse response = getHttpResponse();
-					response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\";");
-					byte[] content = SpagoBIUtilities.getByteArrayFromInputStream(fis);
-					response.setContentLength(content.length);
-					response.getOutputStream().write(content);
-					response.getOutputStream().flush();
+					try (FileInputStream fis = new FileInputStream(file)) {
+						HttpServletResponse response = getHttpResponse();
+						response.setHeader("Content-Disposition", "attachment; filename=" + file.getName() + ";");
+						byte[] content = SpagoBIUtilities.getByteArrayFromInputStream(fis);
+						response.setContentLength(content.length);
+						response.getOutputStream().write(content);
+						response.getOutputStream().flush();
+					}
 				}
 
 			} else {
