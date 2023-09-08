28c4a25436257ec64bc61dfd488657c5e9bc26c9
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-09-08 12:46:11 +0200

    Bugfix https://production.eng.it/jira/browse/KNOWAGE-8044
    Bugfix https://production.eng.it/jira/browse/KNOWAGE-8044

--- knowagewhatifengine/src/main/webapp/js/src/angular_1.x/scripts/directives/components/TableToolbarRight.js
+++ knowagewhatifengine/src/main/webapp/js/src/angular_1.x/scripts/directives/components/TableToolbarRight.js
@@ -251,7 +251,7 @@ function tableToolobarController($scope, $timeout, $window, $mdDialog, $http, $s
 
 	//Getting clicked buttons trough XML (clicked="true")
 	filterClickedButtons = function(data){
-		var regEx = /[A-Z]+_*/g;
+		var regEx = /(?:[A-Z]+_*){1,4}/g;
 		var i;
 		while (i = regEx.exec(data)){
 			$scope.clickedButtons.push(i[0]);
