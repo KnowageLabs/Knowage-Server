ae8d78e13fa6a96309440429c93773a694dd299b
Davide Vernassa <davide.vernassa@gmail.com>
2023-10-27 12:48:18 +0200

    Added home button url config constant

--- knowage-core/src/main/java/it/eng/spagobi/api/v2/FinalUserConfigResource.java
+++ knowage-core/src/main/java/it/eng/spagobi/api/v2/FinalUserConfigResource.java
@@ -49,7 +49,7 @@ public class FinalUserConfigResource extends AbstractSpagoBIResource {
 	 */
 	private static final List<String> USER_CONFIG_LABELS = Arrays.asList("SPAGOBI.SESSION_PARAMETERS_MANAGER.enabled",
 			"SPAGOBI.DATE-FORMAT-SERVER.format", "SPAGOBI.TIMESTAMP-FORMAT.format",
-			"KNOWAGE.EMBEDDING_APPLICATION_VALUE", "KNOWAGE.RESOURCE.UPLOAD.MAX_SIZE");
+			"KNOWAGE.EMBEDDING_APPLICATION_VALUE", "KNOWAGE.RESOURCE.UPLOAD.MAX_SIZE", "home.button.url");
 
 	@GET
 	@Path("/")
