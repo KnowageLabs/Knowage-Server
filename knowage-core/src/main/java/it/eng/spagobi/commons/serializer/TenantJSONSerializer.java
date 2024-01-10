031d9e89c9aebec8b31c9c3919edc4c6f0e93cec
Davide Zerbetto <davide.zerbetto@eng.it>
2024-01-10 09:34:46 +0100

    Fixing tenant JSON serializer

--- knowage-core/src/main/java/it/eng/spagobi/commons/serializer/TenantJSONSerializer.java
+++ knowage-core/src/main/java/it/eng/spagobi/commons/serializer/TenantJSONSerializer.java
@@ -1,7 +1,7 @@
 /*
  * Knowage, Open Source Business Intelligence suite
  * Copyright (C) 2016 Engineering Ingegneria Informatica S.p.A.
- * 
+ *
  * Knowage is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,45 +11,42 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package it.eng.spagobi.commons.serializer;
 
-import it.eng.spagobi.commons.metadata.SbiTenant;
-
 import java.util.Locale;
 
 import org.json.JSONObject;
 
+import it.eng.spagobi.commons.metadata.SbiTenant;
+
 public class TenantJSONSerializer implements Serializer {
 
-	public static final String TENANT_ID = "MULTITENANT_ID";
-	private static final String TENANT_NAME = "MULTITENANT_NAME";
-	private static final String TENANT_THEME = "MULTITENANT_THEME";
-	
+	public static final String TENANT_ID = "TENANT_ID";
+	private static final String TENANT_NAME = "TENANT_NAME";
+	private static final String TENANT_THEME = "TENANT_THEME";
+
+	@Override
 	public Object serialize(Object o, Locale locale) throws SerializationException {
-		JSONObject  result = null;
-		
-		if( !(o instanceof SbiTenant) ) {
+		JSONObject result = null;
+
+		if (!(o instanceof SbiTenant)) {
 			throw new SerializationException("TenantJSONSerializer is unable to serialize object of type: " + o.getClass().getName());
 		}
-		
+
 		try {
-			SbiTenant ds = (SbiTenant)o;
+			SbiTenant ds = (SbiTenant) o;
 			result = new JSONObject();
-			
 			result.put(TENANT_ID, ds.getId());
-			result.put(TENANT_NAME, ds.getName() );
-			result.put(TENANT_THEME, ds.getTheme() );	
-		
-		} catch (Throwable t) {
-			throw new SerializationException("An error occurred while serializing object: " + o, t);
-		} finally {
-			
+			result.put(TENANT_NAME, ds.getName());
+			result.put(TENANT_THEME, ds.getTheme());
+		} catch (Exception e) {
+			throw new SerializationException("An error occurred while serializing object: " + o, e);
 		}
-		
+
 		return result;
 	}
 
