12868c797609c11154bac2ffd2abb2019e53bd25
BojanSovticEngIT <bojan.sovtic@eng.it>
2024-01-15 11:02:14 +0100

    Bugfix KNOWAGE-8377

--- knowage-vue/src/modules/managers/lovsManagement/cards/LovsManagementWizardCard/LovsManagementTestDialog/LovsManagementTestDialogHelpers.ts
+++ knowage-vue/src/modules/managers/lovsManagement/cards/LovsManagementWizardCard/LovsManagementTestDialog/LovsManagementTestDialogHelpers.ts
@@ -1,5 +1,5 @@
 export const removeUnusedVisibleColumnsFromModel = (treeListTypeModel) => {
-    if (!treeListTypeModel || !treeListTypeModel['VISIBLE-COLUMNS']) return
+    if (!treeListTypeModel || !treeListTypeModel['VISIBLE-COLUMNS'] || !treeListTypeModel.STMT) return
     const visibleColumns = treeListTypeModel['VISIBLE-COLUMNS'].split(',')
     const filteredColumns = visibleColumns.filter((column: string) => treeListTypeModel.STMT.includes(column.trim()))
     treeListTypeModel['VISIBLE-COLUMNS'] = filteredColumns.join(',')
