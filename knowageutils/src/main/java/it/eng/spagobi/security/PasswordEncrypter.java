706da39deed5923f000abcc669c2d59b4312a0a2
Giorgia Leone <giorgia.leone@eng.it>
2024-04-15 15:37:53 +0200

    Porting JDK 17

--- knowageutils/src/main/java/it/eng/spagobi/security/PasswordEncrypter.java
+++ knowageutils/src/main/java/it/eng/spagobi/security/PasswordEncrypter.java
@@ -21,7 +21,9 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
+import javax.crypto.Cipher;
 import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -30,6 +32,28 @@ public class PasswordEncrypter {
 	private static final String PROVIDER = "HmacSHA1";
 
 	public PasswordEncrypter(byte[] keyBytes) {
+		// All com.sun.crypto.** classes are JDK internal APIs that are not supported and should not be used. 
+		// Provider sunJce = new com.sun.crypto.provider.SunJCE();
+		
+		// Old code can't access a com.sun class by name. 
+		// Access algorithms from the provider by calls like javax.crypto.Cipher
+		Cipher cipher = null;
+		
+		try {
+			cipher = Cipher.getInstance("AES/CTR/NoPadding");			
+			// Cipher.getInstance("AES/CTR/NoPadding", "SunJCE") is not recommended,
+			// otherwise, applications are tied to specific providers that may not be available on other Java implementations.
+			
+		} catch (NoSuchAlgorithmException e) {
+			throw new Error("Unable to find algorithm for security initialization", e);
+		} catch (NoSuchPaddingException e) {
+			throw new Error("Unable to find algorithm for security initialization", e);
+		}
+		
+		Provider sunJce = cipher.getProvider();
+		
+		Security.addProvider(sunJce);
+
 		SecretKey key = new SecretKeySpec(keyBytes, PROVIDER);
 
 		try {
