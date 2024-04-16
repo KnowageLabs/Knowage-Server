f214940fefbbb6ce4647564c9c88c305f79f01d3
Davide Vernassa <davide.vernassa@gmail.com>
2024-04-16 16:11:51 +0200

    [KNOWAGE-8458] Public user logout parameter is now optional

--- knowage-vue/src/helpers/commons/authHelper.ts
+++ knowage-vue/src/helpers/commons/authHelper.ts
@@ -1,7 +1,7 @@
 import store from '@/App.store'
 
 export default {
-    logout(isPublicUser:string):void {
+    logout(isPublicUser?:string):void {
         localStorage.clear()
         store.commit('setUser', {})
         let url = window.location.origin
