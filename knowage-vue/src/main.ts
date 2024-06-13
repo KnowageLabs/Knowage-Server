2e36eb4b3a85b7bb741b2b69e87a0542dd2cbbd5
Davide Vernassa <davide.vernassa@eng.it>
2024-06-13 11:54:29 +0200

    TEMP Removed Service Worker from FE

--- knowage-vue/src/main.ts
+++ knowage-vue/src/main.ts
@@ -7,8 +7,6 @@ import store from "./App.store.js";
 import VueAxios from "vue-axios";
 import interceptor from "./axios.js";
 
-import "./registerServiceWorker";
-
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/monokai.css";
 import "codemirror/theme/eclipse.css";
