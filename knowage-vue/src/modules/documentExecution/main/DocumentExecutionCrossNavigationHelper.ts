0010109b3a45c4265a0843f1320f6e15d775f892
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2024-07-19 11:56:00 +0200

    [KNOWAGE-8571]

--- knowage-vue/src/modules/documentExecution/main/DocumentExecutionCrossNavigationHelper.ts
+++ knowage-vue/src/modules/documentExecution/main/DocumentExecutionCrossNavigationHelper.ts
@@ -42,13 +42,22 @@ function checkIfMultivalueDriverContainsCrossNavigationValue(tempParam: any, cro
 }
 
 export function getValidDate(value: string, serverDateFormat: string) {
-    let momentDate = moment(deepcopy(value))
-    const validFormats = [serverDateFormat, 'DD/MM/YYYY', 'DD/MM/YYYY HH:mm:ss.SSS']
+    const extractedDateValue = extractDatePart(value)
+    let momentDate = moment(deepcopy(extractedDateValue))
+    const tempServerDateFormat = serverDateFormat === 'yyyy-MM-dd' ? 'YYYY-MM-DD' : serverDateFormat
+    const validFormats = [tempServerDateFormat, 'DD/MM/YYYY', 'DD/MM/YYYY HH:mm:ss.SSS']
     for (let i = 0; i < validFormats.length; i++) {
-        momentDate = moment(deepcopy(value), validFormats[i])
+        momentDate = moment(deepcopy(extractedDateValue), validFormats[i])
         if (momentDate.isValid()) return momentDate.toDate()
     }
     return ''
+}
+
+function extractDatePart(dateString: string) {
+    if (dateString.includes('#')) {
+        return dateString.split('#')[0]
+    }
+    return dateString
 }
 
 function formatCrossNavigationComboParameterDescription(tempParam: any) {
