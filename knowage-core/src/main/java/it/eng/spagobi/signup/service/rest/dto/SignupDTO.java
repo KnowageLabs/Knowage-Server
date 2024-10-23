7822c7e9e3e369b5331b789bcf144cb3a4ad5870
Giorgia Leone <giorgia.leone@eng.it>
2024-10-23 16:27:56 +0200

    [KNOWAGE-8565] Added passwordValidation
    (cherry picked from commit 8ee3632be9d78d910dba24546a0d39c3dd14393b)

--- knowage-core/src/main/java/it/eng/spagobi/signup/service/rest/dto/SignupDTO.java
+++ knowage-core/src/main/java/it/eng/spagobi/signup/service/rest/dto/SignupDTO.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import it.eng.spagobi.services.validation.EmailValidation;
 import it.eng.spagobi.services.validation.ExtendedAlphanumeric;
 import it.eng.spagobi.services.validation.NoPunctation;
+import it.eng.spagobi.services.validation.PasswordValidation;
 import it.eng.spagobi.services.validation.Xss;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -32,20 +33,23 @@ public class SignupDTO {
 	@NoPunctation
 	@Xss
 	private String name;
+	
 	@NoPunctation
 	@Xss
 	private String surname;
-	@ExtendedAlphanumeric
+	
+	@PasswordValidation
 	@NotNull
 	@Xss
 	private String password;
+	
 	@EmailValidation
 	@Xss
 	private String email;
 
 	// properties for creation only
 
-	@ExtendedAlphanumeric
+	@PasswordValidation
 	@NotNull
 	private String confirmPassword;
 
