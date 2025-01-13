e2afc713949f3643add9484bd022a31df34686cc
Davide Vernassa <davide.vernassa@gmail.com>
2025-01-13 13:54:39 +0100

    Restored missing declaration

--- knowage-vue/src/components/UI/KnParameterSidebar/KnParameterSidebar.d.ts
+++ knowage-vue/src/components/UI/KnParameterSidebar/KnParameterSidebar.d.ts
@@ -27,6 +27,7 @@ export interface iParameter {
     id: number
     parameterDescription: string[]
     dependentParameters?: iParameter[]
+    dependsOnParameters?: iParameter[]
     dataDependentParameters?: iParameter[]
     dataDependsOnParameters?: iParameter[]
     lovDependsOnParameters?: iParameter[]
