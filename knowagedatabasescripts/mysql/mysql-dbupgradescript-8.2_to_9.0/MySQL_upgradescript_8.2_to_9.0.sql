18471158f99ebf4cb0073ada6104547f95e254e4
Sebastiani <massimo.sebastiani@eng.it>
2025-03-14 10:46:32 +0100

    [KNOWAGE-8806] CONSOLIDAMENTO - ENGINE SVG/COMMONJ/GEOREPORT

--- knowagedatabasescripts/mysql/mysql-dbupgradescript-8.2_to_9.0/MySQL_upgradescript_8.2_to_9.0.sql
+++ knowagedatabasescripts/mysql/mysql-dbupgradescript-8.2_to_9.0/MySQL_upgradescript_8.2_to_9.0.sql
@@ -3,3 +3,18 @@ DROP TABLE SBI_EVENTS_ROLES ;
 
 ALTER TABLE SBI_CACHE_ITEM
 ADD COLUMN PARAMETERS TEXT NULL DEFAULT NULL;
+
+DELETE
+FROM
+	SBI_PRODUCT_TYPE_ENGINE
+where
+	ENGINE_ID in (
+	select
+		ENGINE_ID
+	from
+		SBI_ENGINES
+	WHERE
+		label in('knowagegisengine', 'knowageprocessengine', 'knowagesvgviewerengine')
+
+);
+DELETE FROM SBI_ENGINES WHERE label in('knowagegisengine', 'knowageprocessengine', 'knowagesvgviewerengine');
