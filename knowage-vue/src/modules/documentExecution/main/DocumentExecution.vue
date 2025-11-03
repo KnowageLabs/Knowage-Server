9fca4788bb56cda9d08b789b22ea146a9c813dd6
Davide Vernassa <davide.vernassa@gmail.com>
2025-11-03 16:16:44 +0100

    Change in urlData outputType for Jasper Reports

--- knowage-vue/src/modules/documentExecution/main/DocumentExecution.vue
+++ knowage-vue/src/modules/documentExecution/main/DocumentExecution.vue
@@ -579,6 +579,7 @@ export default defineComponent({
             if (this.document.typeCode === 'OLAP') {
                 this.exportOlap(type)
             } else if (this.document.typeCode === 'REPORT') {
+                let tempUrlData = this.urlData.url.replace(/([?&]outputType=)[^&]+/, '$1'+type).replace(/([?&]outputType_description=)[^&]+/, '$1'+type)
                 window.open(this.urlData?.url + '&outputType=' + type, 'name', 'resizable=1,height=750,width=1000')
             } else {
                 const filteredFrames = Array.prototype.filter.call(window.frames, (frame) => frame.name)
